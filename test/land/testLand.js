3b845921d1bfdd743e6cbb67a1ce4ee2b82493ef
Wighawag <wighawag@gmail.com>
2019-09-20 11:15:21 +0100

    update fixes

--- test/land/testLand.js
+++ test/land/testLand.js
@@ -6,14 +6,23 @@ const {
     gas
 } = require('../utils');
 
-async function deployLand() {
+function ERC721Contract() {
+    this.counter = 0;
+    this.contract = null;
+}
+ERC721Contract.prototype.resetContract = async function () {
     await rocketh.runStages();
-    return getDeployedContract('Land');
-}
-
-async function mint(contract, creator) {
-    const receipt = await tx(contract, 'mint', {from: creator, gas}, creator);
+    this.contract = getDeployedContract('Land');
+    return this.contract;
+};
+ERC721Contract.prototype.mintERC721 = async function (creator) {
+    this.counter++;
+    const receipt = await tx(this.contract, 'mintBlock', {from: creator, gas}, creator, 1, this.counter % 512, Math.floor(this.counter / 512));
     return receipt.events.Transfer.returnValues._tokenId;
-}
+};
+// ERC721Contract.prototype.burnERC721 = function (from, tokenId) {
+//     this.counter++;
+//     return this.contract.methods.burnFrom(from, tokenId, 1).send({from, gas: 3000000});
+// };
 
-// runERC721tests('Land', deployLand, mint);
+runERC721tests('Land', new ERC721Contract());
