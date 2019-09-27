bb93d1c420d99d541b681cae2bbff672917a2065
Clement Lakhal <clementlakhal@gmail.com>
2019-09-27 10:37:21 +0200

    Format code to follow the new Solhint rules

--- src/Land/erc721/ERC721BaseToken.sol
+++ src/Land/erc721/ERC721BaseToken.sol
@@ -1,36 +1,35 @@
+/* solhint-disable func-order */
+
 pragma solidity 0.5.9;
 
-import {
-    ERC721Events
-} from "../../../contracts_common/src/Interfaces/ERC721Events.sol";
+import "../../../contracts_common/src/Interfaces/ERC721Events.sol";
 
 import "../../Sand.sol";
 
-contract ERC721BaseToken is
-    ERC721Events /*ERC721*/
-{
-    uint256 constant SIZE = 408;
 
-    uint256 constant LAYER        = 0xFF00000000000000000000000000000000000000000000000000000000000000;
-    uint256 constant LAYER_1x1    = 0x0000000000000000000000000000000000000000000000000000000000000000;
-    uint256 constant LAYER_3x3    = 0x0100000000000000000000000000000000000000000000000000000000000000;
-    uint256 constant LAYER_6x6    = 0x0200000000000000000000000000000000000000000000000000000000000000;
-    uint256 constant LAYER_12x12  = 0x0300000000000000000000000000000000000000000000000000000000000000;
-    uint256 constant LAYER_24x24  = 0x0400000000000000000000000000000000000000000000000000000000000000;
+contract ERC721BaseToken is ERC721Events {
+    uint256 private constant SIZE = 408;
 
-    mapping(address => uint256) numNFTPerAddress;
-    mapping(uint256 => address) owners;
-    mapping(address => mapping(address => bool)) operatorsForAll;
-    mapping(uint256 => address) operators;
-    mapping(uint256 => string) public metadataURIs;
-    Sand sandContract;
+    uint256 private constant LAYER = 0xFF00000000000000000000000000000000000000000000000000000000000000;
+    uint256 private constant LAYER_1x1 = 0x0000000000000000000000000000000000000000000000000000000000000000;
+    uint256 private constant LAYER_3x3 = 0x0100000000000000000000000000000000000000000000000000000000000000;
+    uint256 private constant LAYER_6x6 = 0x0200000000000000000000000000000000000000000000000000000000000000;
+    uint256 private constant LAYER_12x12 = 0x0300000000000000000000000000000000000000000000000000000000000000;
+    uint256 private constant LAYER_24x24 = 0x0400000000000000000000000000000000000000000000000000000000000000;
 
-    constructor(Sand sand) public {
-        initERC721BaseToken(sand);
+    mapping (address => uint256) public numNFTPerAddress;
+    mapping (uint256 => address) public owners;
+    mapping (address => mapping(address => bool)) public operatorsForAll;
+    mapping (uint256 => address) public operators;
+    mapping (uint256 => string) public metadataURIs;
+    Sand public sandContract;
+
+    constructor(address sandAddress) public {
+        initERC721BaseToken(sandAddress);
     }
 
-    function initERC721BaseToken(Sand sand) public {
-        sandContract = sand;
+    function initERC721BaseToken(address sandAddress) public {
+        sandContract = Sand(sandAddress);
     }
 
     function _transferFrom(address _from, address _to, uint256 _id) internal {
@@ -50,11 +49,9 @@ contract ERC721BaseToken is
         emit Transfer(_from, _to, _id);
     }
 
-    function balanceOf(address _owner)
-        external
-        view
-        returns (uint256 _balance)
-    {
+    function balanceOf(address _owner) external view returns (
+        uint256 _balance
+    ) {
         require(_owner != address(0), "zero owner");
         return numNFTPerAddress[_owner];
     }
@@ -62,24 +59,26 @@ contract ERC721BaseToken is
     function mintBlock(address to, uint8 size, uint16 x, uint16 y) external {
         require(x % size == 0 && y % size == 0, "invalid coordinates");
         require(x < SIZE && y < SIZE, "out of bounds");
+
         uint256 blockId;
         uint256 id = x + y * SIZE;
-        if(size == 1) {
+
+        if (size == 1) {
             blockId = id;
-        } else if(size == 3) {
+        } else if (size == 3) {
             blockId = LAYER_3x3 + id;
-        } else if(size == 6) {
+        } else if (size == 6) {
             blockId = LAYER_6x6 + id;
-        } else if(size == 12) {
+        } else if (size == 12) {
             blockId = LAYER_12x12 + id;
-        } else if(size == 24) {
+        } else if (size == 24) {
             blockId = LAYER_24x24 + id;
         } else {
             require(false, "invalid size");
         }
 
-        for(uint16 xi = x; xi < x+size; xi++) {
-            for(uint16 yi = y; yi < y+size; yi++) {
+        for (uint16 xi = x; xi < x+size; xi++) {
+            for (uint16 yi = y; yi < y+size; yi++) {
                 uint256 id1x1 = xi + yi * SIZE;
                 require(_ownerOf(id1x1) == address(0), "already exists");
                 emit Transfer(address(0), to, id1x1);
@@ -94,19 +93,20 @@ contract ERC721BaseToken is
         uint256 x = _id % SIZE;
         uint256 y = _id / SIZE;
         address owner1x1 = owners[_id];
-        if(owner1x1 != address(0)) {
+
+        if (owner1x1 != address(0)) {
             return owner1x1;
-        }else {
+        } else {
             address owner3x3 = owners[LAYER_3x3 + x + y * SIZE];
-            if(owner3x3 != address(0)) {
+            if (owner3x3 != address(0)) {
                 return owner3x3;
             } else {
                 address owner6x6 = owners[LAYER_6x6 + x + y * SIZE];
-                if(owner6x6 != address(0)) {
+                if (owner6x6 != address(0)) {
                     return owner6x6;
                 } else {
                     address owner12x12 = owners[LAYER_12x12 + x + y * SIZE];
-                    if(owner12x12 != address(0)) {
+                    if (owner12x12 != address(0)) {
                         return owner12x12;
                     } else {
                         return owners[LAYER_24x24 + x + y * SIZE];
@@ -115,39 +115,43 @@ contract ERC721BaseToken is
             }
         }
     }
+
     function ownerOf(uint256 _id) external view returns (address _owner) {
         require(_id & LAYER == 0, "invalid token id");
         _owner = _ownerOf(_id);
         require(_owner != address(0), "does not exist");
     }
 
-    function approveFor(address _sender, address _operator, uint256 _id)
-        external
-    {
+    function approveFor(
+        address _sender,
+        address _operator,
+        uint256 _id
+    ) external {
         require(_id & LAYER == 0, "invalid token id");
         require(
             msg.sender == _sender || msg.sender == address(sandContract),
             "only msg.sender or sandContract can act on behalf of sender"
         );
         require(_ownerOf(_id) == _sender, "only owner can change operator");
+
         operators[_id] = _operator;
         emit Approval(_sender, _operator, _id);
     }
+
     function approve(address _operator, uint256 _id) external {
         require(_id & LAYER == 0, "invalid token id");
         require(_ownerOf(_id) == msg.sender, "only owner can change operator");
+
         operators[_id] = _operator;
         emit Approval(msg.sender, _operator, _id);
     }
-    function getApproved(uint256 _id)
-        external
-        view
-        returns (address _operator)
-    {
+
+    function getApproved(uint256 _id) external view returns (address _operator) {
         require(_id & LAYER == 0, "invalid token id");
         require(_ownerOf(_id) != address(0), "does not exist");
         return operators[_id];
     }
+
     function transferFrom(address _from, address _to, uint256 _id) external {
         require(_id & LAYER == 0, "invalid token id");
         address owner = _ownerOf(_id);
@@ -155,6 +159,7 @@ contract ERC721BaseToken is
         require(owner == _from, "only owner can change operator");
         _transferFrom(_from, _to, _id);
     }
+
     function transferFrom(address _from, address _to, uint256 _id, bytes calldata _data) external {
         require(_id & LAYER == 0, "invalid token id");
         address owner = _ownerOf(_id);
@@ -162,6 +167,7 @@ contract ERC721BaseToken is
         require(owner == _from, "only owner can change operator");
         _transferFrom(_from, _to, _id); // TODO _data
     }
+
     function safeTransferFrom(address _from, address _to, uint256 _id, bytes calldata _data) external {
         require(_id & LAYER == 0, "invalid token id");
         address owner = _ownerOf(_id);
@@ -169,6 +175,7 @@ contract ERC721BaseToken is
         require(owner == _from, "only owner can change operator");
         _transferFrom(_from, _to, _id); // TODO _data + safe
     }
+
     function safeTransferFrom(address _from, address _to, uint256 _id) external {
         require(_id & LAYER == 0, "invalid token id");
         address owner = _ownerOf(_id);
@@ -176,13 +183,16 @@ contract ERC721BaseToken is
         require(owner == _from, "only owner can change operator");
         _transferFrom(_from, _to, _id); // TODO safe
     }
+
     /////////////////////////////////////////////////////////////////////////////////////////////////////
     function name() external pure returns (string memory _name) {
         return "SANDBOX LAND";
     }
+
     function symbol() external pure returns (string memory _symbol) {
         return "SLD"; // TODO define symbol
     }
+
     function tokenURI(uint256 _id) public view returns (string memory) {
         require(_id & LAYER == 0, "invalid token id");
         require(_ownerOf(_id) != address(0));
@@ -196,7 +206,6 @@ contract ERC721BaseToken is
     }
 
     // Operators /////////////////////////////////////////////////////////////////////////////////////
-
     function setApprovalForAllFor(
         address _sender,
         address _operator,
@@ -208,9 +217,11 @@ contract ERC721BaseToken is
         );
         _setApprovalForAll(_sender, _operator, _approved);
     }
+
     function setApprovalForAll(address _operator, bool _approved) external {
         _setApprovalForAll(msg.sender, _operator, _approved);
     }
+
     function _setApprovalForAll(
         address _sender,
         address _operator,
@@ -219,6 +230,7 @@ contract ERC721BaseToken is
         operatorsForAll[_sender][_operator] = _approved;
         emit ApprovalForAll(_sender, _operator, _approved);
     }
+
     function isApprovedForAll(address _owner, address _operator)
         external
         view
