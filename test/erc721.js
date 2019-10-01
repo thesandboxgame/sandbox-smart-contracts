682d540bfc862a30de1d3138b1b75625545eadea
Clement Lakhal <clementlakhal@gmail.com>
2019-10-01 20:42:48 +0200

    Add new utils functions

--- test/erc721.js
+++ test/erc721.js
@@ -10,8 +10,48 @@ function transferFrom(contract, from, to, tokenId, options) {
     return contract.methods.transferFrom(from, to, tokenId).send(options);
 }
 
+function approve(contract, operator, id, options) {
+    return contract.methods.approve(operator, id).send(options);
+}
+
+function approveFor(contract, sender, operator, id, options) {
+    return contract.methods.approveFor(sender, operator, id).send(options);
+}
+
+function setApprovalForAll(contract, operator, approved, options) {
+    return contract.methods.setApprovalForAll(operator, approved).send(options);
+}
+
+function setApprovalForAllFor(contract, sender, operator, approved, options) {
+    return contract.methods.setApprovalForAllFor(sender, operator, approved).send(options);
+}
+
+function isApprovedForAll(contract, owner, operator) {
+    return contract.methods.isApprovedForAll(owner, operator).call();
+}
+
+function getApproved(contract, id) {
+    return contract.methods.getApproved(id).call();
+}
+
+function tokenURI(contract, id) {
+    return contract.methods.tokenURI(id).call();
+}
+
+function name(contract) {
+    return contract.methods.name().call();
+}
+
+function symbol(contract) {
+    return contract.methods.name().symbol();
+}
+
 function balanceOf(contract, account) {
     return contract.methods.balanceOf(account).call();
+}
+
+function ownerOf(contract, tokenId) {
+    return contract.methods.ownerOf(tokenId).call();
 }
 
 module.exports = {
@@ -20,4 +60,14 @@ module.exports = {
     ApprovalForAllEvent,
     transferFrom,
     balanceOf,
+    ownerOf,
+    approve,
+    approveFor,
+    setApprovalForAll,
+    setApprovalForAllFor,
+    getApproved,
+    isApprovedForAll,
+    tokenURI,
+    name,
+    symbol,
 };
