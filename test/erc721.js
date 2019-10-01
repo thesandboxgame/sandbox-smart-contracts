d561c75cefe2df013ce573db1b069b2402543a64
Clement Lakhal <clementlakhal@gmail.com>
2019-10-01 17:12:36 +0200

    Add utils functions

--- test/erc721.js
+++ test/erc721.js
@@ -6,8 +6,18 @@ const TransferEvent = encodeEventSignature('Transfer(address,address,uint256)');
 const ApprovalEvent = encodeEventSignature('Approval(address,address,uint256)');
 const ApprovalForAllEvent = encodeEventSignature('ApprovalForAll(address,address,bool)');
 
+function transfer(contract, to, tokenId, options) {
+    return contract.methods.transfer(to, tokenId).send(options);
+}
+
+function getBalance(contract, account) {
+    return contract.methods.balanceOf(account).call();
+}
+
 module.exports = {
     TransferEvent,
     ApprovalEvent,
     ApprovalForAllEvent,
-}
+    transfer,
+    getBalance,
+};
