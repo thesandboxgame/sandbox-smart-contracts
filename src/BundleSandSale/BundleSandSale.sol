a329f08f8511c6fa0a18cff800d8ea715cd16051
Wighawag <wighawag@gmail.com>
2019-10-29 13:21:04 +0000

    WIP : BundleSale

--- src/BundleSandSale/BundleSandSale.sol
+++ src/BundleSandSale/BundleSandSale.sol
@@ -3,20 +3,22 @@ pragma solidity 0.5.9;
 import "../../contracts_common/src/Libraries/SafeMathWithRequire.sol";
 import "../../contracts_common/src/Interfaces/ERC20.sol";
 import "../../contracts_common/src/Interfaces/Medianizer.sol";
+import "../Asset/ERC1155ERC721.sol";
 
-    
 contract BundleSandSale {
     using SafeMathWithRequire for uint256;
     Medianizer public medianizer;
     ERC20 public sand;
     ERC20 public dai;
+    ERC1155ERC721 _asset;
 
     address public admin;
     address payable public wallet;
     bool public isPaused;
-
-    /* We set the USD price here, 1 SAND = 0.0072 USD */
-    uint256 constant private sandPriceInUsd = 7200000000000000;
+    uint256[] _packIds;
+    uint256[] _packAmounts;
+    uint256 _packSand;
+    uint256 _sandPriceInUsd;
 
     /**
      * @notice Initializes the contract
@@ -27,52 +29,58 @@ contract BundleSandSale {
      * @param initialWallet the address of the wallet that will receive the funds
      */
     constructor(
-        address medianizerContractAddress,
         address sandTokenContractAddress,
+        address assetTokenContractAddress,
+        address medianizerContractAddress,
         address daiTokenContractAddress,
         address initialAdmin,
-        address payable initialWallet
+        address payable initialWallet,
+        uint256[] memory packIds,
+        uint256[] memory packAmounts,
+        uint256 packSand,
+        uint256 sandPriceInUsd
     ) public {
         require(initialWallet != address(0), "need a wallet to receive funds");
         medianizer = Medianizer(medianizerContractAddress);
         sand = ERC20(sandTokenContractAddress);
+        _asset = ERC1155ERC721(assetTokenContractAddress);
         dai = ERC20(daiTokenContractAddress);
         admin = initialAdmin;
         wallet = initialWallet;
+        _packIds = packIds;
+        _packIds = packAmounts;
+        _packSand = packSand;
+        _sandPriceInUsd = sandPriceInUsd;
     }
 
     /**
-     * @notice Buys SAND tokens with Ether
+     * @notice Buys Sand Bundle with Ether
      * @param to The address that will receive the SAND
      */
-    function buySandWithEther(address to) external payable whenNotPaused() {
-        uint256 sandAmount = getSandAmountWithEther(msg.value);
+    function buyBundleWithEther(address to) external payable whenNotPaused() {
+        // require(
+        //     sand.transferFrom(address(this), to, sandAmount),
+        //     "Transfer failed"
+        // );
 
-        require(
-            sand.transferFrom(address(this), to, sandAmount),
-            "Transfer failed"
-        );
-
-        address(wallet).transfer(msg.value);
+        // address(wallet).transfer(msg.value);
     }
 
     /**
-     * @notice Buys SAND with DAI
+     * @notice Buys Sand Bundle with DAI
      * @param daiAmount The amount of DAI
      * @param to The address that will receive the SAND
      */
-    function buySandWithDai(uint256 daiAmount, address to) external whenNotPaused() {
-        require(
-            dai.transferFrom(msg.sender, wallet, daiAmount),
-            "Transfer failed"
-        );
+    function buyBundleWithDai(uint256 daiAmount, address to) external whenNotPaused() {
+        // require(
+        //     dai.transferFrom(msg.sender, wallet, daiAmount),
+        //     "Transfer failed"
+        // );
 
-        uint256 sandAmount = getSandAmountWithDai(daiAmount);
-
-        require(
-            sand.transferFrom(address(this), to, sandAmount),
-            "Transfer failed"
-        );
+        // require(
+        //     sand.transferFrom(address(this), to, sandAmount),
+        //     "Transfer failed"
+        // );
     }
 
     /**
@@ -88,27 +96,23 @@ contract BundleSandSale {
     }
 
     /**
-     * @notice Returns the amount of SAND that can be bought using a specific amount of ETH
-     * @param ethAmount An amount of ETH
-     * @return The amount of SAND
+     * @notice Transfers Assets from this contract to another address
+     * @param to The address that will receive the funds
+     * @param ids ids of the Asset to transfer
+     * @param amounts The amounts to transfer
      */
-    function getSandAmountWithEther(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethUsdPair = getEthUsdPair();
-
-        uint256 usdAmount = ethAmount.mul(ethUsdPair);
-        uint256 sandAmount = usdAmount.div(sandPriceInUsd);
-
-        return sandAmount;
+    function withdrawAssets(address to, uint256[] calldata ids, uint256[] calldata amounts) external onlyAdmin() {
+        _asset.safeBatchTransferFrom(address(this), to, ids, amounts, "");
     }
 
     /**
-     * @notice Returns the amount of SAND that can be bought using a specific amount of DAI
-     * @param daiAmount An amount of DAI
-     * @return The amount of SAND
+     * @notice Returns the amount of USD for a specific amount of ETH
+     * @param ethAmount An amount of ETH
+     * @return The amount of USD
      */
-    function getSandAmountWithDai(uint256 daiAmount) public view returns (uint256) {
-        uint256 sandAmount = daiAmount.div(sandPriceInUsd);
-        return sandAmount;
+    function getUSDAmountWithEther(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethUsdPair = getEthUsdPair();
+        return ethAmount.mul(ethUsdPair);
     }
 
     /**
