17c793d14c30bf934870a4b3e957d87cc962cb12
Wighawag <wighawag@gmail.com>
2019-11-08 17:16:09 +0000

    reorganise

--- src/Land/erc721/LandBaseToken.sol
+++ src/Land/erc721/LandBaseToken.sol
@@ -68,6 +68,128 @@ contract LandBaseToken is ERC721BaseToken {
         return id / GRID_SIZE;
     }
 
+    /**
+     * @notice Mint a new quad (aligned to a quad tree with size 3, 6, 12 or 24 only)
+     * @param to The recipient of the new quad
+     * @param size The size of the new quad
+     * @param x The x coordinate of the new quad
+     * @param y The y coordinate of the new quad
+     */
+    function mintQuad(address to, uint16 size, uint16 x, uint16 y) external {
+        require(
+            isMinter(msg.sender),
+            "Only a minter can mint"
+        );
+        require(x % size == 0 && y % size == 0, "Invalid coordinates");
+        require(x <= GRID_SIZE - size && y <= GRID_SIZE - size, "Out of bounds");
+
+        uint256 quadId;
+        uint256 id = x + y * GRID_SIZE;
+
+        if (size == 1) {
+            quadId = id;
+        } else if (size == 3) {
+            quadId = LAYER_3x3 + id;
+        } else if (size == 6) {
+            quadId = LAYER_6x6 + id;
+        } else if (size == 12) {
+            quadId = LAYER_12x12 + id;
+        } else if (size == 24) {
+            quadId = LAYER_24x24 + id;
+        } else {
+            require(false, "Invalid size");
+        }
+
+        require(_owners[LAYER_24x24 + (x/24) * 24 + ((y/24) * 24) * GRID_SIZE] == 0, "Already minted as 24x24");
+
+        uint256 toX = x+size;
+        uint256 toY = y+size;
+        if (size <= 12) {
+            require(
+                _owners[LAYER_12x12 + (x/12) * 12 + ((y/12) * 12) * GRID_SIZE] == 0,
+                "Already minted as 12x12"
+            );
+        } else {
+            for (uint16 x12i = x; x12i < toX; x12i += 12) {
+                for (uint16 y12i = y; y12i < toY; y12i += 12) {
+                    uint256 id12x12 = LAYER_12x12 + x12i + y12i * GRID_SIZE;
+                    require(_owners[id12x12] == 0, "Already minted as 12x12");
+                }
+            }
+        }
+
+        if (size <= 6) {
+            require(_owners[LAYER_6x6 + (x/6) * 6 + ((y/6) * 6) * GRID_SIZE] == 0, "Already minted as 6x6");
+        } else {
+            for (uint16 x6i = x; x6i < toX; x6i += 6) {
+                for (uint16 y6i = y; y6i < toY; y6i += 6) {
+                    uint256 id6x6 = LAYER_6x6 + x6i + y6i * GRID_SIZE;
+                    require(_owners[id6x6] == 0, "Already minted as 6x6");
+                }
+            }
+        }
+
+        if (size <= 3) {
+            require(_owners[LAYER_3x3 + (x/3) * 3 + ((y/3) * 3) * GRID_SIZE] == 0, "Already minted as 3x3");
+        } else {
+            for (uint16 x3i = x; x3i < toX; x3i += 3) {
+                for (uint16 y3i = y; y3i < toY; y3i += 3) {
+                    uint256 id3x3 = LAYER_3x3 + x3i + y3i * GRID_SIZE;
+                    require(_owners[id3x3] == 0, "Already minted as 3x3");
+                }
+            }
+        }
+
+        for (uint16 xi = x; xi < toX; xi++) {
+            for (uint16 yi = y; yi < toY; yi++) {
+                uint256 id1x1 = xi + yi * GRID_SIZE;
+                require(_owners[id1x1] == 0, "Already minted");
+                emit Transfer(address(0), to, id1x1);
+            }
+        }
+
+        _owners[quadId] = uint256(to);
+        _numNFTPerAddress[to] += size * size;
+    }
+
+    /// @notice transfer one quad (aligned to a quad tree with size 3, 6, 12 or 24 only)
+    /// @param from current owner of the quad
+    /// @param to destination
+    /// @param size size of the quad
+    /// @param x x top coordinates of the quad
+    /// @param y y top coordinates of the quad
+    /// @param data additional data
+    function transferQuad(address from, address to, uint16 size, uint16 x, uint16 y, bytes calldata data) external {
+        require(from != address(0), "from is zero address");
+        require(to != address(0), "can't send to zero address");
+        bool metaTx = msg.sender != from && _metaTransactionContracts[msg.sender];
+        if (msg.sender != from && !metaTx) {
+            require(
+                _superOperators[msg.sender] ||
+                _operatorsForAll[from][msg.sender],
+                "not authorized to transferQuad"
+            );
+        }
+        _transferQuad(from, to, size, x, y);
+        _numNFTPerAddress[from] -= size * size;
+        _numNFTPerAddress[to] += size * size;
+
+        if (to.isContract() && _checkInterfaceWith10000Gas(to, ERC721_MANDATORY_RECEIVER)) {
+            uint256[] memory ids = new uint256[](size*size);
+            for (uint256 i = 0; i < size*size; i++) {
+                if(i % 2 == 0) { // alow ids to follow a path
+                    ids[i] = (x + (i%size)) + ((y + (i/size)) * GRID_SIZE);
+                } else {
+                    ids[i] = ((x + size) - (1 + i%size)) + ((y + (i/size)) * GRID_SIZE);
+                }
+            }
+            require(
+                _checkOnERC721BatchReceived(metaTx ? from : msg.sender, from, to, ids, data),
+                "erc721 batch transfer rejected by to"
+            );
+        }
+    }
+
     /// @notice transfer multiple quad (aligned to a quad tree with size 3, 6, 12 or 24 only)
     /// @param from current owner of the quad
     /// @param to destination
@@ -117,44 +239,6 @@ contract LandBaseToken is ERC721BaseToken {
                         ids[counter] = ((x + size) - (1 + i%size)) + ((y + (i/size)) * GRID_SIZE);
                     }
                     counter++;
-                }
-            }
-            require(
-                _checkOnERC721BatchReceived(metaTx ? from : msg.sender, from, to, ids, data),
-                "erc721 batch transfer rejected by to"
-            );
-        }
-    }
-
-    /// @notice transfer one quad (aligned to a quad tree with size 3, 6, 12 or 24 only)
-    /// @param from current owner of the quad
-    /// @param to destination
-    /// @param size size of the quad
-    /// @param x x top coordinates of the quad
-    /// @param y y top coordinates of the quad
-    /// @param data additional data
-    function transferQuad(address from, address to, uint16 size, uint16 x, uint16 y, bytes calldata data) external {
-        require(from != address(0), "from is zero address");
-        require(to != address(0), "can't send to zero address");
-        bool metaTx = msg.sender != from && _metaTransactionContracts[msg.sender];
-        if (msg.sender != from && !metaTx) {
-            require(
-                _superOperators[msg.sender] ||
-                _operatorsForAll[from][msg.sender],
-                "not authorized to transferQuad"
-            );
-        }
-        _transferQuad(from, to, size, x, y);
-        _numNFTPerAddress[from] -= size * size;
-        _numNFTPerAddress[to] += size * size;
-
-        if (to.isContract() && _checkInterfaceWith10000Gas(to, ERC721_MANDATORY_RECEIVER)) {
-            uint256[] memory ids = new uint256[](size*size);
-            for (uint256 i = 0; i < size*size; i++) {
-                if(i % 2 == 0) { // alow ids to follow a path
-                    ids[i] = (x + (i%size)) + ((y + (i/size)) * GRID_SIZE);
-                } else {
-                    ids[i] = ((x + size) - (1 + i%size)) + ((y + (i/size)) * GRID_SIZE);
                 }
             }
             require(
@@ -326,90 +410,6 @@ contract LandBaseToken is ERC721BaseToken {
         return ownerOfAll || _owners[quadId] == uint256(from);
     }
 
-    /**
-     * @notice Mint a new quad (aligned to a quad tree with size 3, 6, 12 or 24 only)
-     * @param to The recipient of the new quad
-     * @param size The size of the new quad
-     * @param x The x coordinate of the new quad
-     * @param y The y coordinate of the new quad
-     */
-    function mintQuad(address to, uint16 size, uint16 x, uint16 y) external {
-        require(
-            isMinter(msg.sender),
-            "Only a minter can mint"
-        );
-        require(x % size == 0 && y % size == 0, "Invalid coordinates");
-        require(x <= GRID_SIZE - size && y <= GRID_SIZE - size, "Out of bounds");
-
-        uint256 quadId;
-        uint256 id = x + y * GRID_SIZE;
-
-        if (size == 1) {
-            quadId = id;
-        } else if (size == 3) {
-            quadId = LAYER_3x3 + id;
-        } else if (size == 6) {
-            quadId = LAYER_6x6 + id;
-        } else if (size == 12) {
-            quadId = LAYER_12x12 + id;
-        } else if (size == 24) {
-            quadId = LAYER_24x24 + id;
-        } else {
-            require(false, "Invalid size");
-        }
-
-        require(_owners[LAYER_24x24 + (x/24) * 24 + ((y/24) * 24) * GRID_SIZE] == 0, "Already minted as 24x24");
-
-        uint256 toX = x+size;
-        uint256 toY = y+size;
-        if (size <= 12) {
-            require(
-                _owners[LAYER_12x12 + (x/12) * 12 + ((y/12) * 12) * GRID_SIZE] == 0,
-                "Already minted as 12x12"
-            );
-        } else {
-            for (uint16 x12i = x; x12i < toX; x12i += 12) {
-                for (uint16 y12i = y; y12i < toY; y12i += 12) {
-                    uint256 id12x12 = LAYER_12x12 + x12i + y12i * GRID_SIZE;
-                    require(_owners[id12x12] == 0, "Already minted as 12x12");
-                }
-            }
-        }
-
-        if (size <= 6) {
-            require(_owners[LAYER_6x6 + (x/6) * 6 + ((y/6) * 6) * GRID_SIZE] == 0, "Already minted as 6x6");
-        } else {
-            for (uint16 x6i = x; x6i < toX; x6i += 6) {
-                for (uint16 y6i = y; y6i < toY; y6i += 6) {
-                    uint256 id6x6 = LAYER_6x6 + x6i + y6i * GRID_SIZE;
-                    require(_owners[id6x6] == 0, "Already minted as 6x6");
-                }
-            }
-        }
-
-        if (size <= 3) {
-            require(_owners[LAYER_3x3 + (x/3) * 3 + ((y/3) * 3) * GRID_SIZE] == 0, "Already minted as 3x3");
-        } else {
-            for (uint16 x3i = x; x3i < toX; x3i += 3) {
-                for (uint16 y3i = y; y3i < toY; y3i += 3) {
-                    uint256 id3x3 = LAYER_3x3 + x3i + y3i * GRID_SIZE;
-                    require(_owners[id3x3] == 0, "Already minted as 3x3");
-                }
-            }
-        }
-
-        for (uint16 xi = x; xi < toX; xi++) {
-            for (uint16 yi = y; yi < toY; yi++) {
-                uint256 id1x1 = xi + yi * GRID_SIZE;
-                require(_owners[id1x1] == 0, "Already minted");
-                emit Transfer(address(0), to, id1x1);
-            }
-        }
-
-        _owners[quadId] = uint256(to);
-        _numNFTPerAddress[to] += size * size;
-    }
-
     function _ownerOf(uint256 id) internal view returns (address) {
         require(id & LAYER == 0, "Invalid token id");
         uint256 x = id % GRID_SIZE;
@@ -436,7 +436,6 @@ contract LandBaseToken is ERC721BaseToken {
                 }
             }
         }
-        return address(0); // explicit return
     }
 
     function _ownerAndOperatorEnabledOf(uint256 id) internal view returns (address owner, bool operatorEnabled) {
