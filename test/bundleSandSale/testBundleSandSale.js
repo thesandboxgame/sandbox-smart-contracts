9d533590999d77a64ca5b127a917da1dc47e26e2
Clement Lakhal <clementlakhal@gmail.com>
2019-11-19 11:23:24 +0100

    Update BundleSandSale tests

--- test/bundleSandSale/testBundleSandSale.js
+++ test/bundleSandSale/testBundleSandSale.js
@@ -2,9 +2,26 @@ const rocketh = require('rocketh');
 const {getDeployedContract} = require('../../lib');
 const {runBundleSandSaleTests} = require('./bundleSandSale_tests');
 
+function ContractStore() {
+}
+
+ContractStore.prototype.resetContracts = async function () {
+    await rocketh.runStages();
+
+    return {
+        Sand: getDeployedContract('Sand'),
+        Asset: getDeployedContract('Asset'),
+        AssetBouncer: getDeployedContract('ORBBouncer'),
+        BundleSandSale: getDeployedContract('BundleSandSale'),
+        CommonMinter: getDeployedContract('CommonMinter'),
+        GenesisBouncer: getDeployedContract('GenesisBouncer'),
+        FakeDai: getDeployedContract('DAI'),
+    };
+};
+
 async function deployBundleSandSale() {
     await rocketh.runStages();
     return getDeployedContract('BundleSandSale');
 }
 
-runBundleSandSaleTests('BundleSandSale', deployBundleSandSale);
+runBundleSandSaleTests('BundleSandSale', new ContractStore());
