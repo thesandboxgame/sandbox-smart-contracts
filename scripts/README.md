29300727e19f1bdb16abfd5cad6acb06fc44e83e
Wighawag <wighawag@gmail.com>
2019-11-26 10:22:13 +0000

    setup bundle script

--- scripts/README.md
+++ scripts/README.md
@@ -1,5 +1,10 @@
 to mint asset on rinkeby from user and assets id :
-`yarn attach-rinkeby "node scripts\mintGenesisAssets.js mintIds -u https://api-develop.sandbox.game d0e7d6c8-3e83-4a51-8f5c-43a46e5de0cf 0x61c461EcC993aaDEB7e4b47E96d1B8cC37314B20 85e17623-28e7-4b6b-aa3c-7dbc923aad6e 7515cd54-9247-4f4a-bdc8-0a2b8f697e66"`
+
+```
+yarn attach-rinkeby "node scripts\mintGenesisAssets.js mintIds -u dev c8b1335f-f1e8-4612-9ac4-385fc4850336 0x61c461EcC993aaDEB7e4b47E96d1B8cC37314B20 57282f37-9439-4b1a-bfdc-173b24f2176f c620d1d6-903d-4a34-a1c2-294b5bdf9f02"
+```
 
 to setup bundle :
-`yarn attach-rinkeby "node scripts\setupBundleSandSale.js setup"`
+```
+yarn attach-rinkeby "node scripts\setupBundleSandSale.js setup"
+```
--- scripts/setupBundleSandSale.js
+++ scripts/setupBundleSandSale.js
@@ -1,34 +1,99 @@
 const rocketh = require('rocketh');
 const program = require('commander');
+const request = require('request');
+const Web3 = require('web3');
+const web3 = new Web3(rocketh.ethereum);
 
 const {
     sendTxAndWait,
+    call,
+    getDeployedContract,
 } = rocketh;
 
 const {
     bundleSandSaleManager,
 } = rocketh.namedAccounts;
 
-// function reportErrorAndExit(e) {
-//     console.error(e);
-//     process.exit(1);
-// }
+function reportErrorAndExit(e) {
+    console.error(e);
+    process.exit(1);
+}
+
+function waitRequest(options) {
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) {
+                reject(error);
+            } else if (body && body.error) {
+                reject(body.error);
+            } else {
+                resolve({response, body});
+            }
+        });
+    });
+}
+
+async function getJSON(url) {
+    const options = {
+        method: 'GET',
+        url,
+        headers: {'Content-Type': 'application/json'},
+        json: true
+    };
+    const res = await waitRequest(options);
+    if (res && res.body) {
+        return res.body;
+    }
+    throw new Error('nothing');
+}
 
 program
-    .command('setup')
+    .command('setup <numPacks> <sandAmountPerPack> <priceUSDPerPack> <assetIds...>')
     // .description('mint assets from ids')
-    // .option('-u, --url <url>', 'api url')
+    .option('-u, --url <url>', 'api url')
     // .option('-g, --gas <gas>', 'gas limit')
     // .option('-p, --packId <packId>', 'packId')
     // .option('-n, --nonce <nonce>', 'nonce')
     // .option('-t, --test', 'testMode')
-    .action(async () => {
+    .action(async (numPacks, sandAmountPerPack, priceUSDPerPack, assetIds, cmdObj) => {
+        let url = (cmdObj.url || 'http://localhost:8081');
+        if (url === 'production') {
+            url = 'https://api.sandbox.game';
+        } else if (url === 'dev' || url === 'development') {
+            url = 'https://api-develop.sandbox.game';
+        } else if (url === 'staging' || url === 'development') {
+            url = 'https://api-stage.sandbox.game';
+        }
+        const BundleSandSale = getDeployedContract('BundleSandSale');
         const from = bundleSandSaleManager;
-        const to = '0x0000000000000000000000000000000000000000'; // TODO
-        const ids = [0]; // TODO
-        const amounts = [0]; // TODO
-        const emptyBytes = '0x'; // TODO
-        await sendTxAndWait({from}, 'Asset', 'safeBatchTransferFrom', from, to, ids, amounts, emptyBytes);
+        const to = BundleSandSale.address;
+        const tokenIds = [];
+        const amounts = [];
+        for (const assetId of assetIds) {
+            const assetData = await getJSON(url + '/assets/' + assetId);
+            if (!(assetData && assetData.asset && assetData.asset.blockchainId)) {
+                reportErrorAndExit('asset not yet minted ' + assetId);
+            }
+            const tokenId = assetData.asset.blockchainId;
+            const balance = await call('Asset', 'balanceOf', from, tokenId);
+            if (balance === 0) {
+                reportErrorAndExit('zero balance for asset ' + assetId);
+            }
+            tokenIds.push(tokenId);
+            amounts.push(balance);
+        }
+        const data = web3.eth.abi.encodeParameters([
+            'uint256',
+            'uint256',
+            'uint256',
+        ], [
+            numPacks,
+            sandAmountPerPack,
+            priceUSDPerPack,
+        ]);
+        console.log({from, to, tokenIds, amounts, data});
+        // TODO
+        // await sendTxAndWait({from}, 'Asset', 'safeBatchTransferFrom', from, to, tokenIds, amounts, data);
     });
 
 program.parse(process.argv);
