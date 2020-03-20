b6d25a4e9013f1f2d319b226b8534957c547f781
Wighawag <wighawag@gmail.com>
2020-03-20 15:48:46 +0000

    add scripts for presale 3

---
+++ scripts/landPreSale_3/generateLandPreSaleJSON.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const {nonExposedLands, partnersLands} = require('../../data/landPreSale_3/getLands');
+
+const output = process.argv[2];
+fs.writeFileSync(output, JSON.stringify(nonExposedLands, null, '    '));
+
+let totalPartnerLands = 0;
+for (const partnersLand of partnersLands) {
+    totalPartnerLands += partnersLand.size * partnersLand.size;
+}
+
+const partnerLandsOutput = process.argv[3];
+fs.writeFileSync(partnerLandsOutput, JSON.stringify({partnersLands, totalPartnerLands}, null, '    '));
