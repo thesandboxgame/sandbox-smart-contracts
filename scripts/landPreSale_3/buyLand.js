0c57f06978e2decae73080820815411cf6bd0228
Wighawag <wighawag@gmail.com>
2020-04-17 08:57:20 +0100

    new scripts

--- scripts/landPreSale_3/buyLand.js
+++ scripts/landPreSale_3/buyLand.js
@@ -1,73 +1,70 @@
-const Web3 = require('web3');
-const fs = require('fs');
-const rocketh = require('rocketh');
-const program = require('commander');
+const {ethers} = require("@nomiclabs/buidler");
 
-const {
-    emptyBytes,
-    zeroAddress,
-} = require('../../test/utils')
+const program = require("commander");
 
-const sender = rocketh.accounts[0];
+const zeroAddress = "0x0000000000000000000000000000000000000000";
 
 program
-    .command('buy <x> <y> <size>')
-    .description('buy land from presale 3')
-    .option('--gasPrice <gasPrice>', 'gasPrice to user')
-    .option('-t, --test', 'test mode')
-    .action(async (x, y, size, cmdObj) => {
-        x = parseInt(x, 10);
-        y = parseInt(y, 10);
-        size = parseInt(size, 10);
-        const landWithProofsData = fs.readFileSync('./.presale_3_proofs_1.json');
-        const landWithProofs = JSON.parse(landWithProofsData);
-        let landToBuy;
-        for (const land of landWithProofs) {
-            if (land.x === x && land.y === y && land.size === size) {
-                landToBuy = land;
-                break;
-            }
-        }
-        if (!landToBuy) {
-            console.error(`cannot find land ${x}, ${y}, ${size}`);
-            process.exit(1);
-        }
-        if (!landToBuy.reserved) {
-            landToBuy.reserved = zeroAddress;
-        }
+  .command("buy <x> <y> <size>")
+  .description("buy land from presale 3")
+  .option("--gasPrice <gasPrice>", "gasPrice to user")
+  .option("-t, --test", "test mode")
+  .action(async (x, y, size, cmdObj) => {
+    const {deployer} = await getNamedAccounts();
+    const sender = deployer;
 
-        const LandPreSale = rocketh.getDeployedContract('LandPreSale_3');
-        // const gasPrice = cmdObj.gasPrice;
-        // console.log({
-        //     preSale: LandPreSale.options.address,
-        //     sender,
-        //     gasPrice,
-        //     land: landToBuy,
-        // });
-        console.log('PreSale Contract Address:');
-        console.log(LandPreSale.address);
-        console.log('------------------------------------------------');
-        console.log('reserved:');
-        console.log(landToBuy.reserved);
-        console.log('x:');
-        console.log(landToBuy.x);
-        console.log('y:');
-        console.log(landToBuy.y);
-        console.log('size:');
-        console.log(landToBuy.size);
-        console.log('price:');
-        console.log(landToBuy.price);
-        console.log('salt:');
-        console.log(landToBuy.salt);
-        console.log('proof:');
-        console.log(JSON.stringify(landToBuy.proof));
+    x = parseInt(x, 10);
+    y = parseInt(y, 10);
+    size = parseInt(size, 10);
+    const landWithProofsData = fs.readFileSync("./.presale_3_proofs_1.json");
+    const landWithProofs = JSON.parse(landWithProofsData);
+    let landToBuy;
+    for (const land of landWithProofs) {
+      if (land.x === x && land.y === y && land.size === size) {
+        landToBuy = land;
+        break;
+      }
+    }
+    if (!landToBuy) {
+      console.error(`cannot find land ${x}, ${y}, ${size}`);
+      process.exit(1);
+    }
+    if (!landToBuy.reserved) {
+      landToBuy.reserved = zeroAddress;
+    }
 
-        // if (!cmdObj.test) {
-        //     const receipt = await tx({from: sender, gas: 1000000, gasPrice}, LandPreSale, 'buyLandWithETH', sender, destination);
-        //     console.log(receipt);
-        // } else {
-        //     console.log('was for test only');
-        // }
+    const LandPreSale = await ethers.getContract("LandPreSale_3");
+    const gasPrice = cmdObj.gasPrice;
+    console.log({
+      preSale: LandPreSale.address,
+      sender,
+      gasPrice,
+      land: landToBuy,
     });
+    console.log("PreSale Contract Address:");
+    console.log(LandPreSale.address);
+    console.log("------------------------------------------------");
+    console.log("reserved:");
+    console.log(landToBuy.reserved);
+    console.log("x:");
+    console.log(landToBuy.x);
+    console.log("y:");
+    console.log(landToBuy.y);
+    console.log("size:");
+    console.log(landToBuy.size);
+    console.log("price:");
+    console.log(landToBuy.price);
+    console.log("salt:");
+    console.log(landToBuy.salt);
+    console.log("proof:");
+    console.log(JSON.stringify(landToBuy.proof));
+
+    // if (!cmdObj.test) {
+    //     const receipt = await tx({from: sender, gas: 1000000, gasPrice}, LandPreSale, 'buyLandWithETH', sender, destination);
+    //     console.log(receipt);
+    // } else {
+    //     console.log('was for test only');
+    // }
+  });
 
 program.parse(process.argv);
--- scripts/landPreSale_3/mintLands.js
+++ scripts/landPreSale_3/mintLands.js
@@ -1,67 +1,67 @@
-const fs = require('fs');
-const parseAirDropSheet = require('./parseAirDropSheet');
+const fs = require("fs");
+const parseAirDropSheet = require("./parseAirDropSheet");
 
-const landWithProofsData = fs.readFileSync('./.presale_3_proofs_1.json');
+const landWithProofsData = fs.readFileSync("./.presale_3_proofs_1.json");
 const landWithProofs = JSON.parse(landWithProofsData);
 
 async function mint(x, y, size, address) {
-    let landToBuy;
-    for (const land of landWithProofs) {
-        if (land.x === x && land.y === y && land.size === size) {
-            landToBuy = land;
-            break;
-        }
+  let landToBuy;
+  for (const land of landWithProofs) {
+    if (land.x === x && land.y === y && land.size === size) {
+      landToBuy = land;
+      break;
     }
-    if (!landToBuy) {
-        console.error(`cannot find land ${x}, ${y}, ${size}`);
-        process.exit(1);
+  }
+  if (!landToBuy) {
+    console.error(`cannot find land ${x}, ${y}, ${size}`);
+    process.exit(1);
+  }
+  if (landToBuy.reserved) {
+    if (landToBuy.reserved !== address) {
+      if (landToBuy.reserved === "0x7a9fe22691c811ea339d9b73150e6911a5343dca") {
+        console.log("-------------------- from SANDBOX -------------------");
+      } else {
+        throw new Error("reserved address do not match: " + landToBuy.reserved + " != " + address);
+      }
     }
-    if (landToBuy.reserved) {
-        if (landToBuy.reserved !== address) {
-            if (landToBuy.reserved === '0x7a9fe22691c811ea339d9b73150e6911a5343dca') {
-                console.log('-------------------- from SANDBOX -------------------');
-            } else {
-                throw new Error('reserved address do not match: ' + landToBuy.reserved + ' != ' + address);
-            }
-        }
-    } else {
-        throw new Error('cannot mint non reserved address, they are for sale',);
-    }
-    console.log('mintQuad(address to, uint256 size, uint256 x, uint256 y, bytes calldata data');
-    console.log('to:');
-    console.log(address);
-    console.log('size:');
-    console.log(landToBuy.size);
-    console.log('x:');
-    console.log(landToBuy.x);
-    console.log('y:');
-    console.log(landToBuy.y);
-    console.log('data:');
-    console.log('0x');
+  } else {
+    throw new Error("cannot mint non reserved address, they are for sale");
+  }
+  console.log("mintQuad(address to, uint256 size, uint256 x, uint256 y, bytes calldata data");
+  console.log("to:");
+  console.log(address);
+  console.log("size:");
+  console.log(landToBuy.size);
+  console.log("x:");
+  console.log(landToBuy.x);
+  console.log("y:");
+  console.log(landToBuy.y);
+  console.log("data:");
+  console.log("0x");
 }
 
 (async () => {
-    const landGroups = [];
-    const giveaway = await parseAirDropSheet();
-    // console.log(JSON.stringify(giveaway, null, '  '));
-    for (const row of giveaway) {
-        // if (row.sent) {
-        //     continue;
-        // }
-        landGroups.push({
-            partner: row.partner,
-            to: row.to,
-            minter: row.minter,
-            x: row.coordinates.x,
-            y: row.coordinates.y,
-            size: row.size,
-        });
-    }
+  const landGroups = [];
+  const giveaway = await parseAirDropSheet();
+  // console.log(JSON.stringify(giveaway, null, '  '));
+  for (const row of giveaway) {
+    // if (row.sent) {
+    //     continue;
+    // }
+    landGroups.push({
+      partner: row.partner,
+      to: row.to,
+      minter: row.minter,
+      x: row.coordinates.x,
+      y: row.coordinates.y,
+      size: row.size,
+    });
+  }
 
-    for (const landGroup of landGroups) {
-        console.log('------------------- ' + landGroup.partner + ' -----------------------------');
-        mint(landGroup.x + 204, landGroup.y + 204, landGroup.size, landGroup.to);
-        console.log('');
-        console.log('');
-    }
+  for (const landGroup of landGroups) {
+    console.log("------------------- " + landGroup.partner + " -----------------------------");
+    mint(landGroup.x + 204, landGroup.y + 204, landGroup.size, landGroup.to);
+    console.log("");
+    console.log("");
+  }
 })();
