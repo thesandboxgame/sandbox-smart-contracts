e3c2333d5a68f91fdeb50489129614da355f32b6
Wighawag <wighawag@gmail.com>
2020-06-12 14:02:35 +0100

    fix

--- src/Gem.sol
+++ src/Gem.sol
@@ -5,16 +5,22 @@ import "./BaseWithStorage/MintableERC1155Token.sol";
 
 
 contract Gem is MintableERC1155Token {
-    function addGems(string[] calldata names) external {
+    function addGems(string[] memory names) public {
         require(msg.sender == _admin, "only admin");
+        _addGems(names);
+    }
+
+    // TODO metadata + EIP-165
+
+    // ///////////////////
+    function _addGems(string[] memory names) internal {
         uint256 count = _count;
         for (uint256 i = 0; i < names.length; i++) {
             _names[count + i] = names[i];
         }
         _count = count + names.length;
+        // TODO event ?
     }
-
-    // TODO metadata + EIP-165
 
     // /////////////////////
     uint256 _count;
@@ -24,6 +30,9 @@ contract Gem is MintableERC1155Token {
     constructor(
         address metaTransactionContract,
         address admin,
-        address initialMinter
-    ) public MintableERC1155Token(metaTransactionContract, admin, initialMinter) {}
+        address initialMinter,
+        string[] memory initialGems
+    ) public MintableERC1155Token(metaTransactionContract, admin, initialMinter) {
+        _addGems(initialGems);
+    }
 }
