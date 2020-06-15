2f8f4d555cbbd9b9c4b392791f6a52c99325f903
Wighawag <wighawag@gmail.com>
2020-06-15 08:10:09 +0100

    fix buidler.config.js

--- buidler.config.js
+++ buidler.config.js
@@ -48,7 +48,8 @@ function eth_node(networkName) {
     uri = uri.replace("{{networkName}}", networkName);
   }
   if (!uri || uri === "") {
-    throw new Error(`environment variable "ETH_NODE_URI" not configured `);
+    // throw new Error(`environment variable "ETH_NODE_URI" not configured `);
+    return "";
   }
   if (uri.indexOf("{{") >= 0) {
     throw new Error(`invalid uri or network not supported by nod eprovider : ${uri}`);
