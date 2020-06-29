4436d73325752af2e8157210ce357a7850a03b2b
Nick Furfaro <nfurfaro33@gmail.com>
2020-06-29 08:23:39 -0600

    Initial tests around contract setup

--- test/starter-pack/testStarterPack.js
+++ test/starter-pack/testStarterPack.js
@@ -1,58 +1,39 @@
 const {setupStarterPack} = require("./fixtures");
 const {assert} = require("chai");
-const {ethers} = require("@nomiclabs/buidler");
+const {ethers, getNamedAccounts} = require("@nomiclabs/buidler");
 const starterPackJSON = require("../../artifacts/StarterPack.json");
 const starterPackABI = starterPackJSON.abi;
 const starterPackbytecode = starterPackJSON.bytecode;
 
-let starterPack;
-
-async function deployStarterPack() {
-  const [wallet, admin] = await ethers.getSigners();
-  const StarterPack = await ethers.getContractFactory(starterPackABI, starterPackbytecode, wallet);
-  const adminAddress = await admin.getAddress();
-  starterPack = await StarterPack.deploy(adminAddress);
-  await starterPack.deployed();
-  return {starterPack};
-}
-
 describe("StarterPack: Setup", function () {
   it.only("Returns a starterPack contract", async function () {
-    const starterPack = await setupStarterPack();
-    console.log(`starterPack: ${starterPack}`);
-    // const {starterPack} = await deployStarterPack();
-    console.log(`starterPack deployed at: ${starterPack.address}`);
+    const {starterPackContract: starterPack} = await setupStarterPack();
     assert.notEqual(starterPack.address, undefined);
-  }); // Passing
-
-  it("should set the admin address correctly", async function () {
-    const [wallet, admin] = await ethers.getSigners();
-    const returnedAdmin = await starterPack.getAdmin();
-    const configAdminAddress = await admin.getAddress();
-    assert.equal(returnedAdmin, configAdminAddress);
-  }); // Passing
-});
-
-describe("Purchase", function () {
-  it.skip("can call the purchase function", async function () {
-    const [wallet, admin, from, to] = await ethers.getSigners();
-    const tx = await starterPack.purchase(from, to, [1, 1, 1, 1], [1, 2, 3, 4, 0], 11, ["0x0"]);
-    await tx.wait();
   });
 
-  it.skip("should invalidate the nonce after 1 use", async function () {});
+  it("should set the admin address correctly", async function () {
+    const {starterPackContract: starterPack} = await setupStarterPack();
+    const {starterPackAdmin} = await getNamedAccounts();
+    const returnedAdmin = await starterPack.getAdmin();
+    assert.equal(returnedAdmin, starterPackAdmin);
+  });
 
-  it.skip("should emit the Purchase event", async function () {});
-
-  it.skip("should fail if the nonce is reused", async function () {});
-
-  it.skip("should fail if ...", async function () {});
+  it.skip("should set the beneficiary address correctly", async function () {
+    const {starterPackContract: starterPack} = await setupStarterPack();
+    const {starterPackSaleBeneficiary} = await getNamedAccounts();
+    // not implemented yet
+    const returnedBeneficiary = await starterPack.beneficiary();
+    assert.equal(returnedAdmin, starterPackSaleBeneficiary);
+  });
 });
 
-describe("SetPrices", function () {
-  it.skip("should fail if called by other than the admin address", async function () {});
-});
+// describe("Purchase"...
+// it("should invalidate the nonce after 1 use", async function () {});
+// it("should emit the Purchase event", async function () {});
+// it("should fail if the nonce is reused", async function () {});
 
-describe("WithdrawAll", function () {
-  it.skip("should fail if called by other than the admin address", async function () {});
-});
+// describe("SetPrices"...
+// it("should fail if called by other than the admin address", async function () {});
+
+// describe("WithdrawAll"...
+// it("should fail if called by other than the admin address", async function () {});
