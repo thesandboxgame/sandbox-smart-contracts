3b76052184d3394f8b48887bd85b2ab0e64e5171
Nick Furfaro <nfurfaro33@gmail.com>
2020-06-29 12:17:37 -0600

    Update deploy script to have all constructor args

--- deploy/071_deploy_starterPack.js
+++ deploy/071_deploy_starterPack.js
@@ -3,14 +3,51 @@ const {guard} = require("../lib");
 module.exports = async ({getNamedAccounts, deployments}) => {
   const {deployIfDifferent, log} = deployments;
 
-  const {deployer, starterPackAdmin} = await getNamedAccounts();
+  const {deployer, starterPackAdmin, backendReferralWallet} = await getNamedAccounts();
+
+  const sandContract = await deployments.getOrNull("Sand");
+  if (!sandContract) {
+    throw new Error("no SAND contract deployed");
+  }
+
+  const metaTxContract = await deployments.getOrNull("NativeMetaTransactionProcessor");
+  if (!sandContract) {
+    throw new Error("no META-TX contract deployed");
+  }
+
+  const daiContract = await deployments.getOrNull("DAI");
+  if (!daiContract) {
+    throw new Error("no DAI contract deployed");
+  }
+
+  const daiMedianizer = await deployments.getOrNull("DAIMedianizer");
+  if (!daiMedianizer) {
+    throw new Error("no DAI-MEDIANIZER contract deployed");
+  }
+
+  const catalystGroup = await deployments.getOrNull("Catalyst");
+  if (!catalystGroup) {
+    throw new Error("no CATALYST contract deployed");
+  }
+
+  const gemGroup = await deployments.getOrNull("Gem");
+  if (!sandContract) {
+    throw new Error("no GEM contract deployed");
+  }
 
   const deployResult = await deployIfDifferent(
     ["data"],
-    "StarterPack",
+    "StarterPackV1",
     {from: deployer, gas: 3000000},
-    "StarterPack",
-    starterPackAdmin
+    "StarterPackV1",
+    starterPackAdmin,
+    sandContract.address,
+    metaTxContract.address,
+    backendReferralWallet,
+    daiMedianizer.address,
+    daiContract.address,
+    catalystGroup.address,
+    gemGroup.address
   );
 
   if (deployResult.newlyDeployed) {
@@ -21,3 +58,11 @@ module.exports = async ({getNamedAccounts, deployments}) => {
 };
 
 module.exports.skip = guard(["1", "4", "314159"], "StarterPack");
+module.exports.dependencies = [
+  "Sand",
+  "NativeMetaTransactionProcessor",
+  "DAIMedianizer",
+  "DAI",
+  "ERC20GroupCatalyst",
+  "Gem",
+];
