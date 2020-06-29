4c042e23662b5c9a22338a803a3e4e4e1593f307
Nick Furfaro <nfurfaro33@gmail.com>
2020-06-29 08:22:39 -0600

    Clean up unused stuff from fixture

--- test/starter-pack/fixtures.js
+++ test/starter-pack/fixtures.js
@@ -1,17 +1,10 @@
-const {bre, deployments, getNamedAccounts} = require("@nomiclabs/buidler");
+const {ethers, deployments, getNamedAccounts} = require("@nomiclabs/buidler");
 
-module.exports.setupStarterPack = deployments.createFixture(async (bre, options) => {
-  const {deployer, catalystAdmin} = await getNamedAccounts();
+module.exports.setupStarterPack = deployments.createFixture(async () => {
+  const {deployer} = await getNamedAccounts();
   await deployments.fixture();
-  options = options || {};
 
   const starterPackContract = await ethers.getContract("StarterPack", deployer);
-
-  await deployments.deploy("StarterPack", {
-    from: deployer,
-    gas: 3000000,
-    args: [catalystAdmin],
-  });
 
   return {
     starterPackContract,
