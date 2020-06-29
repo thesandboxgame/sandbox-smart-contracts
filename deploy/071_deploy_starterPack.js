e74c418496cfcd61f0e75fc01cb7438afafcc058
Nick Furfaro <nfurfaro33@gmail.com>
2020-06-29 06:15:32 -0600

    work on deploy script

--- deploy/071_deploy_starterPack.js
+++ deploy/071_deploy_starterPack.js
@@ -3,9 +3,15 @@ const {guard} = require("../lib");
 module.exports = async ({getNamedAccounts, deployments}) => {
   const {deployIfDifferent, log} = deployments;
 
-  const {deployer} = await getNamedAccounts();
+  const {deployer, catalystAdmin} = await getNamedAccounts();
 
-  const deployResult = await deployIfDifferent(["data"], "StarterPack", {from: deployer, gas: 3000000}, deployer);
+  const deployResult = await deployIfDifferent(
+    ["data"],
+    "StarterPack",
+    {from: deployer, gas: 3000000},
+    "StarterPack",
+    catalystAdmin
+  );
 
   if (deployResult.newlyDeployed) {
     log(" - StarterPack deployed at : " + deployResult.address + " for gas : " + deployResult.receipt.gasUsed);
@@ -15,4 +21,3 @@ module.exports = async ({getNamedAccounts, deployments}) => {
 };
 
 module.exports.skip = guard(["1", "4", "314159"], "StarterPack");
-module.exports.tags = ["StarterPack"];
