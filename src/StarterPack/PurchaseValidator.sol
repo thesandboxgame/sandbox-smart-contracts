c4ef95d3b97c6b9102c13d1afcf4c9cf3ac9b78a
Nick Furfaro <nfurfaro33@gmail.com>
2020-06-30 11:44:07 -0600

    Add imports for admin and sigUtil

--- src/StarterPack/PurchaseValidator.sol
+++ src/StarterPack/PurchaseValidator.sol
@@ -1,10 +1,26 @@
 pragma solidity 0.6.5;
 pragma experimental ABIEncoderV2;
 
+import "../contracts_common/src/Libraries/SigUtil.sol";
+import "../contracts_common/src/BaseWithStorage/Admin.sol";
 
-contract PurchaseValidator {
+
+contract PurchaseValidator is Admin {
     address private _signingWallet;
 
+    /**
+     * @notice Check if a purchase message is valid
+     * @param signature The signature to check (signed purchase message)
+     * @param from The address of the referrer
+     * @param to The address of the creator
+     * @param catalystIds The address of the creator
+     * @param catalystQuantities The address of the creator
+     * @param gemIds The address of the creator
+     * @param gemQuantities The address of the creator
+     * @param nonce The address of the creator
+     * @param expiryTime The expiry time of the referral
+     * @return True if the referral is valid
+     */
     function isPurchaseValid(
         bytes memory signature,
         address from,
@@ -16,7 +32,17 @@ contract PurchaseValidator {
         uint256 nonce,
         uint256 expiryTime
     ) public view returns (bool) {
-        return true;
+        // if (now > expiryTime) {
+        //     return false;
+        // }
+        // if (nonce already used) {
+        //   return false
+        // }
+        bytes32 hashedData = keccak256(abi.encodePacked(from, to, catalystIds, catalystQuantities, gemIds, gemQuantities, nonce, expiryTime));
+
+        address signer = SigUtil.recover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hashedData)), signature);
+
+        return signer == _signingWallet;
     }
 
     constructor(address initialSigningWallet) public {
