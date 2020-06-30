bd328cc38a35d91d178b0f81da9d6db99fa84bc9
atkinsonholly <atkinsonholly@gmail.com>
2020-06-30 13:04:14 +0100

    add admin, beneficiary, sand medianizer and dai to fixtures

--- test/starter-pack/fixtures.js
+++ test/starter-pack/fixtures.js
@@ -1,12 +1,36 @@
 const {ethers, deployments, getNamedAccounts} = require("@nomiclabs/buidler");
 
 module.exports.setupStarterPack = deployments.createFixture(async () => {
-  const {deployer} = await getNamedAccounts();
+  const {deployer, starterPackAdmin, starterPackSaleBeneficiary, others} = await getNamedAccounts();
   await deployments.fixture();
 
-  const starterPackContract = await ethers.getContract("StarterPackV1", deployer);
+  const sandContract = await ethers.getContract("Sand");
+  const medianizerContract = await ethers.getContract("DAIMedianizer");
+  const daiContract = await ethers.getContract("DAI");
+  const starterPackContract = await ethers.getContract("StarterPackV1");
+
+  const starterPackContractAsDeployer = await ethers.getContract("StarterPackV1", deployer);
+  const starterPackContractAsAdmin = await ethers.getContract("StarterPackV1", starterPackAdmin);
+  const starterPackContractAsBeneficiary = await ethers.getContract("StarterPackV1", starterPackSaleBeneficiary);
+
+  const users = [];
+  for (const other of others) {
+    users.push({
+      address: other,
+      StarterPack: starterPackContract.connect(starterPackContract.provider.getSigner(other)),
+      Sand: sandContract.connect(sandContract.provider.getSigner(other)),
+      Dai: daiContract.connect(daiContract.provider.getSigner(other)),
+    });
+  }
 
   return {
     starterPackContract,
+    starterPackContractAsDeployer,
+    starterPackContractAsAdmin,
+    starterPackContractAsBeneficiary,
+    sandContract,
+    medianizerContract,
+    daiContract,
+    users,
   };
 });
