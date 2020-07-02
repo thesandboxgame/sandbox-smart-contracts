d65cfdc79b3bdf535d59fda294ea83ebfd8f1403
Nick Furfaro <nfurfaro33@gmail.com>
2020-07-02 12:04:18 -0600

    Update interface for validating messages

--- src/StarterPack/PurchaseValidator.sol
+++ src/StarterPack/PurchaseValidator.sol
@@ -8,29 +8,31 @@ import "../contracts_common/src/BaseWithStorage/Admin.sol";
 contract PurchaseValidator is Admin {
     address private _signingWallet;
 
+    struct Message {
+        uint256[] catalystIds;
+        uint256[] catalystQuantities;
+        uint256[] gemIds;
+        uint256[] gemQuantities;
+        address buyer;
+        uint256 nonce;
+    }
+
     /**
      * @notice Check if a purchase message is valid
      * @param from The tx sender
-     * @param to The address of the creator
-     * @param catalystIds The types of catalysts to send
-     * @param catalystQuantities The amounts of each type of catalyst to send
-     * @param gemIds The types of gems to send
-     * @param gemQuantities The amounts of each type of gem to send
-     * @param nonce A per-creator nonce, incremented to avoid reuse of signatures
+     * @param message The specific parameters signed
+     * by the backend, of type "Message"
      * @param signature A signed message specifying tx details
      * @return True if the referral is valid
      */
     function isPurchaseValid(
         address from,
-        address to,
-        uint256[] memory catalystIds,
-        uint256[] memory catalystQuantities,
-        uint256[] memory gemIds,
-        uint256[] memory gemQuantities,
-        uint256 nonce,
+        Message memory message,
         bytes memory signature
     ) public view returns (bool) {
-        bytes32 hashedData = keccak256(abi.encodePacked(from, to, catalystIds, catalystQuantities, gemIds, gemQuantities, nonce));
+        bytes32 hashedData = keccak256(
+            abi.encodePacked(message.catalystIds, message.catalystQuantities, message.gemIds, message.gemQuantities, message.buyer, message.nonce)
+        );
 
         address signer = SigUtil.recover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hashedData)), signature);
 
