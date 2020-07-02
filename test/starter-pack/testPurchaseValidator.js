f34315ab48d2be555e176995367040e828099281
Nick Furfaro <nfurfaro33@gmail.com>
2020-07-02 12:09:32 -0600

    Add new test for from address

--- test/starter-pack/testPurchaseValidator.js
+++ test/starter-pack/testPurchaseValidator.js
@@ -10,7 +10,7 @@ const gemIds = [0, 1, 2, 3, 4];
 const gemAmounts = [0, 0, 0, 0, 4];
 
 describe("Validating Purchase Messages", function () {
-  it.only("Purchase validator function exists", async function () {
+  it("Purchase validator function exists", async function () {
     const {starterPackContract: starterPack} = await setupStarterPack();
     const {others} = await getNamedAccounts();
     const starterPackBuyer = others[0];
@@ -27,5 +27,22 @@ describe("Validating Purchase Messages", function () {
     const sig = await signPurchaseMessage(privateKey, purchaseMessage);
 
     assert.ok(await starterPack.isPurchaseValid(starterPackBuyer, purchaseMessage, sig));
+  });
+
+  it.only("should fail if the from address does not match", async function () {
+    const {starterPackContract: starterPack} = await setupStarterPack();
+    const {others} = await getNamedAccounts();
+    const starterPackBuyer = others[0];
+    const wrongFromAddress = others[1];
+    const purchaseMessage = {
+      catalystIds: catIds,
+      catalystQuantities: catAmounts,
+      gemIds: gemIds,
+      gemQuantities: gemAmounts,
+      buyer: starterPackBuyer,
+      nonce: 1,
+    };
+    const sig = await signPurchaseMessage(privateKey, purchaseMessage);
+    assert.notOk(await starterPack.isPurchaseValid(wrongFromAddress, purchaseMessage, sig));
   });
 });
