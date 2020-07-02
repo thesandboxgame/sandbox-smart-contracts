625e6f1cd413979b82710f42b5ef57a528cee644
atkinsonholly <atkinsonholly@gmail.com>
2020-07-02 15:33:41 +0100

    WIP purchaseWithSand function and tests

--- test/starter-pack/fixtures.js
+++ test/starter-pack/fixtures.js
@@ -1,7 +1,17 @@
 const {ethers, deployments, getNamedAccounts} = require("@nomiclabs/buidler");
+const {BigNumber} = require("ethers");
+const {toWei} = require("local-utils");
 
 module.exports.setupStarterPack = deployments.createFixture(async () => {
-  const {deployer, starterPackAdmin, starterPackSaleBeneficiary, others} = await getNamedAccounts();
+  const {
+    deployer,
+    starterPackAdmin,
+    starterPackSaleBeneficiary,
+    sandAdmin,
+    others,
+    gemAdmin,
+    catalystAdmin,
+  } = await getNamedAccounts();
   await deployments.fixture();
 
   const sandContract = await ethers.getContract("Sand");
@@ -10,9 +20,27 @@ module.exports.setupStarterPack = deployments.createFixture(async () => {
   const starterPackContract = await ethers.getContract("StarterPackV1");
   const metaTxContract = await ethers.getContract("NativeMetaTransactionProcessor");
 
+  const gemAsAdmin = await ethers.getContract("Gem", gemAdmin);
+  const catalystAsAdmin = await ethers.getContract("Catalyst", catalystAdmin);
+  const sandAsAdmin = await ethers.getContract("Sand", sandAdmin);
+
   const starterPackContractAsDeployer = await ethers.getContract("StarterPackV1", deployer);
   const starterPackContractAsAdmin = await ethers.getContract("StarterPackV1", starterPackAdmin);
   const starterPackContractAsBeneficiary = await ethers.getContract("StarterPackV1", starterPackSaleBeneficiary);
+
+  const SandAdmin = {
+    address: sandAdmin,
+    Sand: sandContract.connect(sandContract.provider.getSigner(sandAdmin)),
+  };
+
+  const DaiAdmin = {
+    address: deployer,
+    Dai: daiContract.connect(daiContract.provider.getSigner(deployer)),
+  };
+
+  await sandAsAdmin.setSuperOperator(starterPackContract.address, true);
+  await gemAsAdmin.setSuperOperator(starterPackContract.address, true);
+  await catalystAsAdmin.setSuperOperator(starterPackContract.address, true);
 
   const users = [];
   for (const other of others) {
@@ -24,6 +52,35 @@ module.exports.setupStarterPack = deployments.createFixture(async () => {
     });
   }
 
+  async function setupUser(StarterPack, SandAdmin, DaiAdmin, user, {hasSand, hasDAI}) {
+    if (hasDAI) {
+      await DaiAdmin.Dai.transfer(user.address, toWei("1000000"));
+      await user.Dai.approve(StarterPack.address, toWei("1000000"));
+    }
+    if (hasSand) {
+      await SandAdmin.Sand.transfer(user.address, BigNumber.from("1000000000000000000000000"));
+    }
+    if (!hasDAI && !hasSand) {
+      await user.Dai.approve(StarterPack.address, toWei("1000000"));
+    }
+    return user;
+  }
+
+  const userWithSAND = await setupUser(starterPackContract, SandAdmin, DaiAdmin, users[0], {
+    hasSand: true,
+    hasDAI: false,
+  });
+  const userWithoutSAND = users[2];
+
+  const userWithDAI = await setupUser(starterPackContract, SandAdmin, DaiAdmin, users[0], {
+    hasSand: false,
+    hasDAI: true,
+  });
+  const userWithoutDAI = await setupUser(starterPackContract, SandAdmin, DaiAdmin, users[2], {
+    hasSand: false,
+    hasDAI: false,
+  });
+
   return {
     starterPackContract,
     starterPackContractAsDeployer,
@@ -34,5 +91,11 @@ module.exports.setupStarterPack = deployments.createFixture(async () => {
     daiContract,
     metaTxContract,
     users,
+    SandAdmin,
+    DaiAdmin,
+    userWithSAND,
+    userWithoutSAND,
+    userWithDAI,
+    userWithoutDAI,
   };
 });
