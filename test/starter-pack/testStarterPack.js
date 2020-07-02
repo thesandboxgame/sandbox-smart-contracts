40ca09781ebe9fe7dd2d9c738c8afb57a2fa11fc
atkinsonholly <atkinsonholly@gmail.com>
2020-07-02 07:54:23 +0100

    WIP initial purchase test outline

--- test/starter-pack/testStarterPack.js
+++ test/starter-pack/testStarterPack.js
@@ -1,8 +1,11 @@
 const {setupStarterPack} = require("./fixtures");
 const {assert} = require("chai");
 const {getNamedAccounts} = require("@nomiclabs/buidler");
+const {waitFor, expectRevert} = require("local-utils");
 
-describe("StarterPack: Setup", function () {
+const emptySignature = "0x";
+
+describe("StarterPack:Setup", function () {
   it("Returns a starterPack contract", async function () {
     const {starterPackContract: starterPack} = await setupStarterPack();
     assert.notEqual(starterPack.address, undefined);
@@ -16,10 +19,71 @@ describe("StarterPack: Setup", function () {
   });
 });
 
-// describe("Purchase"...
-// it("should invalidate the nonce after 1 use", async function () {});
-// it("should emit the Purchase event", async function () {});
-// it("should fail if the nonce is reused", async function () {});
+describe("StarterPack:PurchaseWithSand", function () {
+  let setUp;
+  beforeEach(async function () {
+    setUp = await setupStarterPack();
+    await setUp.starterPackContractAsAdmin.setSANDEnabled(true);
+  });
+
+  it("should revert if the user does not have enough SAND", async function () {
+    const {users} = setUp;
+    expectRevert(
+      await users[0].StarterPack.purchaseWithSand(
+        users[0].address,
+        users[0].address,
+        [0, 1, 2, 3],
+        [1, 1, 1, 1],
+        [0, 1, 2, 3, 4],
+        [1, 1, 1, 1, 1],
+        1,
+        emptySignature
+      ),
+      "Not enough funds allowed"
+    );
+  });
+
+  it("should throw if purchases are not enabled", async function () {
+    const {users, starterPackContractAsAdmin} = setUp;
+    await starterPackContractAsAdmin.setSANDEnabled(false);
+    expectRevert(
+      await users[0].StarterPack.purchaseWithSand(
+        users[0].address,
+        users[0].address,
+        [0, 1, 2, 3],
+        [1, 1, 1, 1],
+        [0, 1, 2, 3, 4],
+        [1, 1, 1, 1, 1],
+        1,
+        emptySignature
+      ),
+      "sand payments not enabled"
+    );
+  });
+
+  it("should enable purchases for user with SAND", async function () {
+    const {users} = setUp;
+    const receipt = waitFor(
+      await users[0].StarterPack.purchaseWithSand(
+        users[0].address,
+        users[0].address,
+        [0, 1, 2, 3],
+        [1, 1, 1, 1],
+        [0, 1, 2, 3, 4],
+        [1, 1, 1, 1, 1],
+        1,
+        emptySignature
+      )
+    );
+    console.log(receipt);
+  });
+
+  // it("should throw if SAND is not enabled", async function () {});
+  // it("cannot enable/disable SAND if not admin", async function () {});
+  // it("should invalidate the nonce after 1 use", async function () {});
+  // it("should emit the Purchase event", async function () {});
+  // it("should fail if the nonce is reused", async function () {});
+});
 
 // describe("SetPrices"...
 // it("should fail if called by other than the admin address", async function () {});
