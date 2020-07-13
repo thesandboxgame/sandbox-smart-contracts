b46bec671e4912d7fa82cc2aa9e0f4f8a7fe264e
atkinsonholly <atkinsonholly@gmail.com>
2020-07-13 18:08:51 +0100

    Refactor purchaseValidator tests using new helper

--- test/starter-pack/testPurchaseValidator.js
+++ test/starter-pack/testPurchaseValidator.js
@@ -1,104 +1,77 @@
 const {assert} = require("chai");
-const {BigNumber} = require("ethers");
-const {getNamedAccounts} = require("@nomiclabs/buidler");
 const {setupStarterPack} = require("./fixtures");
-const {expectRevert, zeroAddress} = require("local-utils");
-const {signPurchaseMessage} = require("../../lib/purchaseMessageSigner");
-const {getMsgAndSignature} = require('./_testHelper')
-const privateKey = "0x4242424242424242424242424242424242424242424242424242424242424242";
-
-// Example queueIds and nonces:
-// const queueId1_nonce1 = BigNumber.from("0x0000000000000000000000000000000100000000000000000000000000000001");
-// const queueId42_nonce11 = BigNumber.from("0x0000000000000000000000000000002A0000000000000000000000000000000B");
-
-const catIds = [0, 1, 2, 3];
-const catAmounts = [0, 0, 0, 1];
-const gemIds = [0, 1, 2, 3, 4];
-const gemAmounts = [0, 0, 0, 0, 4];
-
-let purchaseMessage = {
-  catalystIds: catIds,
-  catalystQuantities: catAmounts,
-  gemIds: gemIds,
-  gemQuantities: gemAmounts,
-  buyer: zeroAddress,
-  nonce: 0, // queuId:0, nonce:0
-};
+const {expectRevert} = require("local-utils");
+const {getMsgAndSignature} = require("./_testHelper");
+const {getNamedAccounts} = require("@nomiclabs/buidler");
 
 describe("ValidatingPurchaseMessages", function () {
-  it("Purchase validator function exists", async function () {
-    const {starterPackContract: starterPack} = await setupStarterPack();
-    const {others} = await getNamedAccounts();
-    const starterPackBuyer = others[0];
-    const goodMsg = Object.assign({}, purchaseMessage, {buyer: starterPackBuyer});
-    const sig = await signPurchaseMessage(privateKey, goodMsg);
+  let starterPack;
+  let message;
+  let signature;
+  let roles;
 
+  beforeEach(async function () {
+    ({starterPackContract: starterPack} = await setupStarterPack());
+    ({message, signature} = await getMsgAndSignature());
+    roles = await getNamedAccounts();
+  });
+
+  it("Purchase validator function exists", async function () {
     assert.ok(
       await starterPack.isPurchaseValid(
-        starterPackBuyer,
-        purchaseMessage.catalystIds,
-        purchaseMessage.catalystQuantities,
-        purchaseMessage.gemIds,
-        purchaseMessage.gemQuantities,
-        starterPackBuyer,
-        purchaseMessage.nonce,
-        sig
+        message.buyer,
+        message.catalystIds,
+        message.catalystQuantities,
+        message.gemIds,
+        message.gemQuantities,
+        message.buyer,
+        message.nonce,
+        signature
       )
     );
   });
 
   it("should fail if the from address does not match", async function () {
-    const {starterPackContract: starterPack} = await setupStarterPack();
-    const {others} = await getNamedAccounts();
-    const starterPackBuyer = others[0];
-    const wrongFromAddress = others[1];
-    const goodMsg = Object.assign({}, purchaseMessage, {buyer: starterPackBuyer});
-    const sig = await signPurchaseMessage(privateKey, goodMsg);
-
+    const wrongFromAddress = roles.others[1];
     await expectRevert(
       starterPack.isPurchaseValid(
         wrongFromAddress,
-        purchaseMessage.catalystIds,
-        purchaseMessage.catalystQuantities,
-        purchaseMessage.gemIds,
-        purchaseMessage.gemQuantities,
-        starterPackBuyer,
-        purchaseMessage.nonce,
-        sig
+        message.catalystIds,
+        message.catalystQuantities,
+        message.gemIds,
+        message.gemQuantities,
+        message.buyer,
+        message.nonce,
+        signature
       ),
       "INVALID_SENDER"
     );
   });
 
   it("should fail if the nonce is re-used", async function () {
-    const {starterPackContract: starterPack} = await setupStarterPack();
-    const {others} = await getNamedAccounts();
-    const starterPackBuyer = others[0];
-    const goodMsg = Object.assign({}, purchaseMessage, {buyer: starterPackBuyer});
-    const sig = await signPurchaseMessage(privateKey, goodMsg);
     assert.ok(
       await starterPack.isPurchaseValid(
-        starterPackBuyer,
-        purchaseMessage.catalystIds,
-        purchaseMessage.catalystQuantities,
-        purchaseMessage.gemIds,
-        purchaseMessage.gemQuantities,
-        starterPackBuyer,
-        purchaseMessage.nonce,
-        sig
+        message.buyer,
+        message.catalystIds,
+        message.catalystQuantities,
+        message.gemIds,
+        message.gemQuantities,
+        message.buyer,
+        message.nonce,
+        signature
       )
     );
 
     await expectRevert(
       starterPack.isPurchaseValid(
-        starterPackBuyer,
-        purchaseMessage.catalystIds,
-        purchaseMessage.catalystQuantities,
-        purchaseMessage.gemIds,
-        purchaseMessage.gemQuantities,
-        starterPackBuyer,
-        purchaseMessage.nonce,
-        sig
+        message.buyer,
+        message.catalystIds,
+        message.catalystQuantities,
+        message.gemIds,
+        message.gemQuantities,
+        message.buyer,
+        message.nonce,
+        signature
       ),
       "INVALID_NONCE"
     );
