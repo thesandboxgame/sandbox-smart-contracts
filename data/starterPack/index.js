67eab97dd7035245e0e2e71be4b091d82594e68b
atkinsonholly <atkinsonholly@gmail.com>
2020-07-16 15:25:39 +0100

    update starterPack prices to larger numbers following conversation in slack about decimal places, and update associated tests

--- data/starterPack/index.js
+++ data/starterPack/index.js
@@ -1,1 +1,10 @@
-module.exports.starterPackPrices = [100, 200, 300, 1000];
+const ethers = require("ethers");
+const {BigNumber} = ethers;
+
+// sand price is in Sand unit (Sand has 18 decimals)
+module.exports.starterPackPrices = [
+  BigNumber.from(100),
+  BigNumber.from(200),
+  BigNumber.from(300),
+  BigNumber.from(1000),
+];
