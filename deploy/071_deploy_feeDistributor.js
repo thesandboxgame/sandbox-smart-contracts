7f399ae7577067a463847203ef34b64d6677e0bc
CloverGuy <arthemisP@protonmail.com>
2020-07-28 15:47:48 +0300

    added deployment file

---
+++ deploy/071_deploy_feeDistributor.js
@@ -0,0 +1,11 @@
+module.exports = async ({getNamedAccounts, deployments}) => {
+  const {deployer} = await getNamedAccounts();
+  const {deploy} = deployments;
+
+  await deploy("FeeDistributor", {
+    from: deployer,
+    gas: 3000000,
+    log: true,
+    args: [[deployer], [10000]],
+  });
+};
