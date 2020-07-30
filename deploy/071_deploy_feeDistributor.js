d421b2b0acb05a33768bdc5ec7fc5b1941bf4e24
CloverGuy <arthemisP@protonmail.com>
2020-07-30 14:17:26 +0300

    added percentages example

--- deploy/071_deploy_feeDistributor.js
+++ deploy/071_deploy_feeDistributor.js
@@ -1,12 +1,13 @@
 module.exports = async ({getNamedAccounts, deployments}) => {
   const {deployer, feeDistributionRecipients} = await getNamedAccounts();
   const {deploy} = deployments;
+  const percentages = require("../data/feeDistribution/percentages");
 
   await deploy("FeeDistributor", {
     from: deployer,
     gas: 3000000,
     log: true,
-    args: [feeDistributionRecipients, [10000]],
+    args: [feeDistributionRecipients, percentages],
   });
 };
 module.exports.tags = ["FeeDistributor"];
