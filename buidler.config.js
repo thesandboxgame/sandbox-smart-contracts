00c0eb2d20d88662229b0bd5a4b0464e89b61dda
Wighawag <wighawag@gmail.com>
2020-07-30 15:38:27 +0100

    setup for deployments

--- buidler.config.js
+++ buidler.config.js
@@ -127,7 +127,7 @@ module.exports = {
     sandboxAccount: {
       default: 4,
       1: "0x7A9fe22691c811ea339D9B73150e6911a5343DcA",
-      4: "0x3B4Ce74FB0793250AE211334a5Df00214D5aB807", // Leon asset minter
+      4: "0x5BC3D5A39a50BE2348b9C529f81aE79f00945897", // Leon asset minter
       42: "0x9a3b0D0B08fb71F1a5E0F248Ad3a42C341f7837c",
       314159: "0x9a3b0D0B08fb71F1a5E0F248Ad3a42C341f7837c",
     },
