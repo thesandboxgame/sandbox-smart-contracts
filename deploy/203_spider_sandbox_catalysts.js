978beae2263e151068d7e44bd5733ebef1c6debb
Wighawag <wighawag@gmail.com>
2020-08-01 06:24:46 +0100

    fix tests by removing tmp deploy scripts

--- deploy/203_spider_sandbox_catalysts.js
+++
@@ -1,6 +0,0 @@
-module.exports = async ({getNamedAccounts, deployments}) => {
-  const {execute} = deployments;
-  const {catalystMinter, sandboxAccount} = await getNamedAccounts();
-  await execute("Catalyst", {from: catalystMinter, skipUnknownSigner: true}, "batchMint", sandboxAccount, [2], [1]);
-  return true;
-};
--- deploy/204_spider_sandbox_gems.js
+++
@@ -1,6 +0,0 @@
-module.exports = async ({getNamedAccounts, deployments}) => {
-  const {execute} = deployments;
-  const {gemMinter, sandboxAccount} = await getNamedAccounts();
-  await execute("Gem", {from: gemMinter, skipUnknownSigner: true}, "batchMint", sandboxAccount, [0, 2], [1, 2]);
-  return true;
-};
