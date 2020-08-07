88405f5234df6fe98227fc3d670263489c5395db
atkinsonholly <atkinsonholly@gmail.com>
2020-08-07 12:13:13 +0100

    Converted format of EstateSaleWithFee contract and added updates to docs

--- src/EstateSale/EstateSaleWithFee.sol
+++ src/EstateSale/EstateSaleWithFee.sol
@@ -9,24 +9,10 @@ import "../contracts_common/src/BaseWithStorage/MetaTransactionReceiver.sol";
 import "../ReferralValidator/ReferralValidator.sol";
 
 
-/**
- * @title Estate Sale contract with referral that supports also DAI and ETH as payment
- * @notice This contract mananges the sale of our lands as Estates
- */
+/// @title Estate Sale contract with referral
+/// @notice This contract mananges the sale of our lands as Estates
 contract EstateSaleWithFee is MetaTransactionReceiver, ReferralValidator {
     using SafeMathWithRequire for uint256;
-
-    uint256 internal constant GRID_SIZE = 408; // 408 is the size of the Land
-
-    ERC1155 internal immutable _asset;
-    LandToken internal immutable _land;
-    ERC20 internal immutable _sand;
-    address internal immutable _estate;
-    address internal immutable _feeDistributor;
-
-    address payable internal _wallet;
-    uint256 internal immutable _expiryTime;
-    bytes32 internal immutable _merkleRoot;
 
     bool _sandEnabled = false;
 
@@ -40,33 +26,7 @@ contract EstateSaleWithFee is MetaTransactionReceiver, ReferralValidator {
         uint256 amountPaid
     );
 
-    constructor(
-        address landAddress,
-        address sandContractAddress,
-        address initialMetaTx,
-        address admin,
-        address payable initialWalletAddress,
-        bytes32 merkleRoot,
-        uint256 expiryTime,
-        address initialSigningWallet,
-        uint256 initialMaxCommissionRate,
-        address estate,
-        address asset,
-        address feeDistributor
-    ) public ReferralValidator(initialSigningWallet, initialMaxCommissionRate) {
-        _land = LandToken(landAddress);
-        _sand = ERC20(sandContractAddress);
-        _setMetaTransactionProcessor(initialMetaTx, true);
-        _wallet = initialWalletAddress;
-        _merkleRoot = merkleRoot;
-        _expiryTime = expiryTime;
-        _admin = admin;
-        _estate = estate;
-        _asset = ERC1155(asset);
-        _feeDistributor = feeDistributor;
-    }
-
-    /// @dev set the wallet receiving the proceeds
+    /// @notice set the wallet receiving the proceeds
     /// @param newWallet address of the new receiving wallet
     function setReceivingWallet(address payable newWallet) external {
         require(newWallet != address(0), "receiving wallet cannot be zero address");
@@ -74,7 +34,7 @@ contract EstateSaleWithFee is MetaTransactionReceiver, ReferralValidator {
         _wallet = newWallet;
     }
 
-    /// @dev enable/disable the specific SAND payment for Lands
+    /// @notice enable/disable the specific SAND payment for Lands
     /// @param enabled whether to enable or disable
     function setSANDEnabled(bool enabled) external {
         require(msg.sender == _admin, "only admin can enable/disable SAND");
@@ -85,6 +45,89 @@ contract EstateSaleWithFee is MetaTransactionReceiver, ReferralValidator {
     /// @return whether the specific SAND payments are enabled
     function isSANDEnabled() external view returns (bool) {
         return _sandEnabled;
+    }
+
+    /// @notice buy Land with SAND using the merkle proof associated with it
+    /// @param buyer address that perform the payment
+    /// @param to address that will own the purchased Land
+    /// @param reserved the reserved address (if any)
+    /// @param x x coordinate of the Land
+    /// @param y y coordinate of the Land
+    /// @param size size of the pack of Land to purchase
+    /// @param priceInSand price in SAND to purchase that Land
+    /// @param proof merkleProof for that particular Land
+    function buyLandWithSand(
+        address buyer,
+        address to,
+        address reserved,
+        uint256 x,
+        uint256 y,
+        uint256 size,
+        uint256 priceInSand,
+        bytes32 salt,
+        uint256[] calldata assetIds,
+        bytes32[] calldata proof,
+        bytes calldata referral
+    ) external {
+        require(_sandEnabled, "sand payments not enabled");
+        _checkValidity(buyer, reserved, x, y, size, priceInSand, salt, assetIds, proof);
+        _handleFeeAndReferral(buyer, priceInSand, referral);
+        _mint(buyer, to, x, y, size, priceInSand, address(_sand), priceInSand);
+        _sendAssets(to, assetIds);
+    }
+
+    /// @notice Gets the expiry time for the current sale
+    /// @return The expiry time, as a unix epoch
+    function getExpiryTime() external view returns (uint256) {
+        return _expiryTime;
+    }
+
+    /// @notice Gets the Merkle root associated with the current sale
+    /// @return The Merkle root, as a bytes32 hash
+    function getMerkleRoot() external view returns (bytes32) {
+        return _merkleRoot;
+    }
+
+    /// @notice enable Admin to withdraw remaining assets from EstateSaleWithFee contract
+    /// @param to intended recipient of the asset tokens
+    /// @param assetIds the assetIds to be transferred
+    /// @param values the quantities of the assetIds to be transferred
+    function withdrawAssets(
+        address to,
+        uint256[] calldata assetIds,
+        uint256[] calldata values
+    ) external {
+        require(msg.sender == _admin, "NOT_AUTHORIZED");
+        require(block.timestamp > _expiryTime, "SALE_NOT_OVER");
+        _asset.safeBatchTransferFrom(address(this), to, assetIds, values, "");
+    }
+
+    function onERC1155Received(
+        address, /*operator*/
+        address, /*from*/
+        uint256, /*id*/
+        uint256, /*value*/
+        bytes calldata /*data*/
+    ) external pure returns (bytes4) {
+        return 0xf23a6e61;
+    }
+
+    function onERC1155BatchReceived(
+        address, /*operator*/
+        address, /*from*/
+        uint256[] calldata, /*ids*/
+        uint256[] calldata, /*values*/
+        bytes calldata /*data*/
+    ) external pure returns (bytes4) {
+        return 0xbc197c81;
+    }
+
+    function _sendAssets(address to, uint256[] memory assetIds) internal {
+        uint256[] memory values = new uint256[](assetIds.length);
+        for (uint256 i = 0; i < assetIds.length; i++) {
+            values[i] = 1;
+        }
+        _asset.safeBatchTransferFrom(address(this), to, assetIds, values, "");
     }
 
     function _checkValidity(
@@ -125,63 +168,6 @@ contract EstateSaleWithFee is MetaTransactionReceiver, ReferralValidator {
         emit LandQuadPurchased(buyer, to, x + (y * GRID_SIZE), size, price, token, tokenAmount);
     }
 
-    /**
-     * @notice buy Land with SAND using the merkle proof associated with it
-     * @param buyer address that perform the payment
-     * @param to address that will own the purchased Land
-     * @param reserved the reserved address (if any)
-     * @param x x coordinate of the Land
-     * @param y y coordinate of the Land
-     * @param size size of the pack of Land to purchase
-     * @param priceInSand price in SAND to purchase that Land
-     * @param proof merkleProof for that particular Land
-     */
-    function buyLandWithSand(
-        address buyer,
-        address to,
-        address reserved,
-        uint256 x,
-        uint256 y,
-        uint256 size,
-        uint256 priceInSand,
-        bytes32 salt,
-        uint256[] calldata assetIds,
-        bytes32[] calldata proof,
-        bytes calldata referral
-    ) external {
-        require(_sandEnabled, "sand payments not enabled");
-        _checkValidity(buyer, reserved, x, y, size, priceInSand, salt, assetIds, proof);
-
-        _handleFeeAndReferral(buyer, priceInSand, referral);
-
-        _mint(buyer, to, x, y, size, priceInSand, address(_sand), priceInSand);
-        _sendAssets(to, assetIds);
-    }
-
-    /**
-     * @notice Gets the expiry time for the current sale
-     * @return The expiry time, as a unix epoch
-     */
-    function getExpiryTime() external view returns (uint256) {
-        return _expiryTime;
-    }
-
-    /**
-     * @notice Gets the Merkle root associated with the current sale
-     * @return The Merkle root, as a bytes32 hash
-     */
-    function merkleRoot() external view returns (bytes32) {
-        return _merkleRoot;
-    }
-
-    function _sendAssets(address to, uint256[] memory assetIds) internal {
-        uint256[] memory values = new uint256[](assetIds.length);
-        for (uint256 i = 0; i < assetIds.length; i++) {
-            values[i] = 1;
-        }
-        _asset.safeBatchTransferFrom(address(this), to, assetIds, values, "");
-    }
-
     function _generateLandHash(
         uint256 x,
         uint256 y,
@@ -215,49 +201,55 @@ contract EstateSaleWithFee is MetaTransactionReceiver, ReferralValidator {
         uint256 priceInSand,
         bytes memory referral
     ) internal {
-         // send 5% fee to a specially configured instance of FeeDistributor.sol
+        // send 5% fee to a specially configured instance of FeeDistributor.sol
         uint256 remainingAmountInSand = _handleSandFee(buyer, priceInSand);
 
         // calculate referral based on 95% of original priceInSand
         handleReferralWithERC20(buyer, remainingAmountInSand, referral, _wallet, address(_sand));
     }
 
-    function _handleSandFee(
-        address buyer,
-        uint256 priceInSand
-    ) internal returns (uint256) {
+    function _handleSandFee(address buyer, uint256 priceInSand) internal returns (uint256) {
         uint256 feeAmountInSand = priceInSand.div(100).mul(5);
         require(_sand.transferFrom(buyer, address(_feeDistributor), feeAmountInSand), "FEE_TRANSFER_FAILED");
         return priceInSand.sub(feeAmountInSand);
     }
 
-    function onERC1155Received(
-        address, /*operator*/
-        address, /*from*/
-        uint256, /*id*/
-        uint256, /*value*/
-        bytes calldata /*data*/
-    ) external pure returns (bytes4) {
-        return 0xf23a6e61;
+    uint256 internal constant GRID_SIZE = 408; // 408 is the size of the Land
+
+    ERC1155 internal immutable _asset;
+    LandToken internal immutable _land;
+    ERC20 internal immutable _sand;
+    address internal immutable _estate;
+    address internal immutable _feeDistributor;
+
+    address payable internal _wallet;
+    uint256 internal immutable _expiryTime;
+    bytes32 internal immutable _merkleRoot;
+
+    constructor(
+        address landAddress,
+        address sandContractAddress,
+        address initialMetaTx,
+        address admin,
+        address payable initialWalletAddress,
+        bytes32 merkleRoot,
+        uint256 expiryTime,
+        address initialSigningWallet,
+        uint256 initialMaxCommissionRate,
+        address estate,
+        address asset,
+        address feeDistributor
+    ) public ReferralValidator(initialSigningWallet, initialMaxCommissionRate) {
+        _land = LandToken(landAddress);
+        _sand = ERC20(sandContractAddress);
+        _setMetaTransactionProcessor(initialMetaTx, true);
+        _wallet = initialWalletAddress;
+        _merkleRoot = merkleRoot;
+        _expiryTime = expiryTime;
+        _admin = admin;
+        _estate = estate;
+        _asset = ERC1155(asset);
+        _feeDistributor = feeDistributor;
     }
 
-    function onERC1155BatchReceived(
-        address, /*operator*/
-        address, /*from*/
-        uint256[] calldata, /*ids*/
-        uint256[] calldata, /*values*/
-        bytes calldata /*data*/
-    ) external pure returns (bytes4) {
-        return 0xbc197c81;
-    }
-
-    function withdrawAssets(
-        address to,
-        uint256[] calldata assetIds,
-        uint256[] calldata values
-    ) external {
-        require(msg.sender == _admin, "NOT_AUTHORIZED");
-        require(block.timestamp > _expiryTime, "SALE_NOT_OVER");
-        _asset.safeBatchTransferFrom(address(this), to, assetIds, values, "");
-    }
 }
