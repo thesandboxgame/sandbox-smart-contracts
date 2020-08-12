7d1a99655b9d508edb9e8a8eb58f764ccfe3b081
Wighawag <wighawag@gmail.com>
2020-08-12 08:37:16 +0100

    fix typo

--- deploy/825_set_starterPack_sand.js
+++ deploy/825_set_starterPack_sand.js
@@ -4,7 +4,7 @@ module.exports = async ({deployments}) => {
 
   const isSandSuperOperator = await read("Sand", "isSuperOperator", starterPack.address);
   if (!isSandSuperOperator) {
-    log("setting AssetSignedAuction as super operator for Sand");
+    log("setting StarterPackV1 as super operator for Sand");
     const currentSandAdmin = await read("Sand", "getAdmin");
     await execute(
       "Sand",
