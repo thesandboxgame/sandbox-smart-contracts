48e6ad5ce0928c9eecc3981a4a69cbb6a8414adc
Nick Furfaro <nfurfaro33@gmail.com>
2020-08-13 10:51:57 -0600

    Update fixture to return sandContract

--- test/metaTx/fixtures.js
+++ test/metaTx/fixtures.js
@@ -3,7 +3,9 @@ const {deployments} = require("@nomiclabs/buidler");
 module.exports.setupTest = deployments.createFixture(async (bre) => {
   await deployments.fixture("MetaTxWrapper");
   const MetaTxWrapperContract = await bre.ethers.getContract("MetaTxWrapper");
+  const sandContract = await ethers.getContract("Sand");
   return {
     MetaTxWrapperContract,
+    sandContract,
   };
 });
--- test/metaTx/metaTxWrapper.js
+++ test/metaTx/metaTxWrapper.js
@@ -1,38 +1,38 @@
-const {ethers} = require("@nomiclabs/buidler");
-const {utils} = require("ethers");
+const {ethers, getNamedAccounts, deployments} = require("@nomiclabs/buidler");
+const {utils, BigNumber} = require("ethers");
 const {assert, expect} = require("local-chai");
 const {expectRevert} = require("local-utils");
-const {signPurchaseMessage} = require("../../lib/purchaseMessageSigner");
 const {setupTest} = require("./fixtures");
 
-const privateKey = "0x4242424242424242424242424242424242424242424242424242424242424242";
-
 let setUp;
-// let accounts;
 let dummyTrustedforwarder;
-let userWithEth;
+let userWithSand;
+let userWithoutSand;
 
 describe("MetaTxWrapper", function () {
   beforeEach(async function () {
     setUp = await setupTest();
     const signers = await ethers.getSigners();
     dummyTrustedforwarder = signers[11]; // 0x532792b73c0c6e7565912e7039c59986f7e1dd1f
-    userWithEth = await signers[1].getAddress();
+    userWithSand = await signers[1].getAddress();
+    userWithoutSand = await signers[2].getAddress();
   });
 
   it("can check trusted forwarder", async function () {
-    const {MetaTxWrapperContract} = setUp;
-    const trustedForwarder = await MetaTxWrapperContract.trustedForwarder();
+    const {metaWrapper} = setUp;
+    console.log(`metaTxWrapper: ${metaWrapper.abi}`);
+    const trustedForwarder = await metaWrapper.trustedForwarder();
 
     expect(trustedForwarder).to.equal(await dummyTrustedforwarder.getAddress());
-    assert.ok(await MetaTxWrapperContract.isTrustedForwarder(dummyTrustedforwarder.getAddress()));
+    assert.ok(await metaWrapper.isTrustedForwarder(dummyTrustedforwarder.getAddress()));
   });
 
   it("should revert if forwarded by Untrusted address", async function () {
-    const {MetaTxWrapperContract} = setUp;
+    const {metaWrapper} = setUp;
     const tx = {
-      to: MetaTxWrapperContract.address,
+      to: metaWrapper.address,
       value: utils.parseEther("0.0"),
+      data: "0x0000000000000000000000000000000000000000",
     };
     const signers = await ethers.getSigners();
     const untrustedforwarder = signers[13];
@@ -43,11 +43,11 @@ describe("MetaTxWrapper", function () {
   });
 
   it("should revert with incorrect or missing first param", async function () {
-    const {MetaTxWrapperContract} = setUp;
+    const {metaWrapper} = setUp;
     const senderAddress = "0x532792b73c0c6e7565912e7039c59986f7e1dd1f";
 
     const tx = {
-      to: MetaTxWrapperContract.address,
+      to: metaWrapper.address,
       value: utils.parseEther("0.0"),
       data: senderAddress,
     };
@@ -55,36 +55,15 @@ describe("MetaTxWrapper", function () {
   });
 
   it("can forward a call", async function () {
-    const {MetaTxWrapperContract} = setUp;
-    // const {others} = await getNamedAccounts();
-
-    const TestMessage = {
-      catalystIds: [0, 1, 2, 3],
-      catalystQuantities: [10, 10, 10, 10],
-      gemIds: [0, 1, 2, 3, 4],
-      gemQuantities: [20, 20, 20, 20, 20],
-      nonce: 0,
+    const {metaWrapper} = setUp;
+    const sandContract = await ethers.getContract("Sand");
+    const {sandAdmin} = await getNamedAccounts();
+    const SandAdmin = {
+      address: sandAdmin,
+      Sand: sandContract.connect(sandContract.provider.getSigner(sandAdmin)),
     };
-    const dummySignature = signPurchaseMessage(privateKey, TestMessage, userWithEth);
+    await SandAdmin.Sand.transfer(userWithSand, BigNumber.from("1000000000000000000000000"));
 
-    let ABI = ["function purchaseWithETH(address buyer, Message message, bytes signature)"];
-    let iface = new utils.Interface(ABI);
-    const encodedCallData = iface.encodeFunctionData("purchaseWithETH", [userWithEth, TestMessage, dummySignature]);
-
-    // const senderAddress = "0x532792b73c0c6e7565912e7039c59986f7e1dd1f";
-
-    const tx = {
-      to: MetaTxWrapperContract.address,
-      value: utils.parseEther("0.0"),
-      data: encodedCallData,
-    };
-    // @review need to set the forwardTo address in wrapper constructor
-    await dummyTrustedforwarder.sendTransaction(tx);
+    await metaWrapper.transferFrom(userWithSand, userWithoutSand, BigNumber.from("50000000000000000000000"));
   });
-  /**
-
-> let iface = new ethers.utils.Interface(ABI);
-> iface.encodeFunctionData("transfer", [ "0x1234567890123456789012345678901234567890", parseEther("1.0") ])
-'0xa9059cbb00000000000000000000000012345678901234567890123456789012345678900000000000000000000000000000000000000000000000000de0b6b3a7640000'
-   */
 });
