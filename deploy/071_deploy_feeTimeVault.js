1e4d30b83c0b211a15a193b1eaf8ae2b71b41994
CloverGuy <arthemisP@protonmail.com>
2020-08-14 12:38:01 +0300

    changes to deployment script

--- deploy/071_deploy_feeTimeVault.js
+++ deploy/071_deploy_feeTimeVault.js
@@ -1,12 +1,12 @@
 module.exports = async ({getNamedAccounts, deployments}) => {
   const {deployer} = await getNamedAccounts();
   const {deploy} = deployments;
-
+  const {lockPeriod} = require("../data/feeTimeVault/deploymentData.js");
   await deploy("FeeTimeVault", {
     from: deployer,
     gas: 3000000,
     log: true,
-    args: [],
+    args: [lockPeriod], //TODO: add sand address,
   });
 };
 module.exports.tags = ["FeeTimeVault"];
