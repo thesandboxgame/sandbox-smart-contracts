b61d8069347c8a978d2ebdbc0841d796eda1c465
Nick Furfaro <nfurfaro33@gmail.com>
2020-08-18 12:52:13 -0600

    Rename deploy script and add cat wrapper

---
+++ deploy/072_deploy_metaTxWrappers.js
@@ -0,0 +1,35 @@
+const {guard} = require("../lib");
+
+module.exports = async ({getNamedAccounts, deployments}) => {
+  const {deploy} = deployments;
+
+  const {deployer} = await getNamedAccounts();
+  const sandContract = await deployments.get("Sand");
+  const catalystMinterContract = await deployments.get("CatalystMinter");
+  const signers = await ethers.getSigners();
+  const fakeTrustedForwarder = await signers[11].getAddress();
+
+  const metaTxSand = await deploy("MetaTxWrapper", {
+    contractName: "SandWrapper",
+    from: deployer,
+    args: [fakeTrustedForwarder, sandContract.address],
+    log: true,
+  });
+
+  const metaTxCatalyst = await deploy("MetaTxWrapper", {
+    contractName: "CatalystWrapper",
+    from: deployer,
+    args: [fakeTrustedForwarder, catalystMinterContract.address],
+    log: true,
+  });
+
+  const sandWrapper = {...metaTxSand, abi: sandContract.abi};
+  const catalystWrapper = {...metaTxCatalyst, abi: catalystMinterContract.abi};
+
+  await deployments.save("SandWrapper", sandWrapper);
+  await deployments.save("CatalystWrapper", catalystWrapper);
+};
+
+module.exports.skip = guard(["1", "4", "314159"]);
+module.exports.dependencies = ["Sand", "CatalystMinter"];
+module.exports.tags = ["MetaTxWrapper"];
