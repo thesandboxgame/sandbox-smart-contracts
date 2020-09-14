0e1cf210250a8fef5e76c3cfb43fa371ef7db8c2
CloverGuy <arthemisP@protonmail.com>
2020-09-14 16:00:03 +0300

    stable version of kyber deployment scripts

--- deploy/203_deploy_kyber_reserve.js
+++ deploy/203_deploy_kyber_reserve.js
@@ -1,59 +1,57 @@
-const { guard } = require("../lib");
 const fs = require("fs");
 const path = require("path");
-const { BigNumber } = require("ethers");
-const configPath = path.join(__dirname, "../data/kyberReserve/apr_input.json");
-const configParams = JSON.parse(fs.readFileSync(configPath, "utf8"));
+const {BigNumber} = require("ethers");
+const configPath = path.join(__dirname, "../data/kyberReserve/apr_input_rinkeby.json");
 
-module.exports = async ({ getNamedAccounts, deployments }) => {
+module.exports = async ({getChainId, getNamedAccounts, deployments}) => {
+  const chainId = await getChainId();
+  if (chainId === "31337") {
+    return;
+  }
   let reserveAdmin;
   let whitelistedAddresses;
   let pricingOperator;
   let reserveOperators;
   let weiDepositAmount;
   let sandDepositAmount;
+  const configParams = JSON.parse(fs.readFileSync(configPath, "utf8"));
 
-  const { deploy, execute, read } = deployments;
-  const { deployer } = await getNamedAccounts();
+  const {deploy, execute, read, log} = deployments;
+  const {deployer} = await getNamedAccounts();
   const networkAddress = configParams.kyberNetworkAddress;
   parseInput(configParams);
   deployerAddress = deployer;
   const sandContract = await deployments.get("Sand");
-  console.log(`sand add = ${sandContract.address}`);
-  console.log("deploying pricing contract...");
+  log("deploying LiquidityConversionRates...");
   let lcr = await deploy("LiquidityConversionRates", {
     from: deployer,
     args: [deployerAddress, sandContract.address],
     log: true,
   });
-  console.log("deploying reserve contract...");
+  log("deploying KyberReserve...");
   let kyberReserve = await deploy("KyberReserve", {
     from: deployer,
     args: [networkAddress, lcr.address, deployerAddress],
     log: true,
   });
-  console.log(`reserve address: ${kyberReserve.address}`);
-
   // whitelist addresses
   await whitelistAddressesInReserve(kyberReserve);
 
   // transfer reserve permissions
   await setReservePermissions(kyberReserve);
 
-  // set reserve address
-  console.log("set reserve address in pricing");
+  // set reserve addressF
   await execute(
     "LiquidityConversionRates",
-    { from: deployer, skipUnknownSigner: true },
+    {from: deployer, skipUnknownSigner: true},
     "setReserveAddress",
     kyberReserve.address
   );
 
   // transfer admin rights to pricing operator
-  console.log(`transfer admin rights to ${pricingOperator}`);
   await execute(
     "LiquidityConversionRates",
-    { from: deployer, skipUnknownSigner: true },
+    {from: deployer, skipUnknownSigner: true},
     "transferAdminQuickly",
     pricingOperator
   );
@@ -61,8 +59,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   ethBalance = await ethers.provider.getBalance(kyberReserve.address);
   await depositSand(BigNumber.from(sandDepositAmount));
   sandBalance = await read("Sand", "balanceOf", kyberReserve.address);
-  console.log("APR setup completed!");
-  process.exit(0);
   function parseInput(jsonInput) {
     whitelistedAddresses = jsonInput["whitelistedAddresses"];
     reserveAdmin = jsonInput["reserveAdmin"];
@@ -73,16 +69,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   }
 
   async function whitelistAddressesInReserve() {
-    console.log("whitelisting addresses...");
-    let admin = await read("KyberReserve", "admin");
-    console.log(`admin = ${admin}`);
-    console.log(`deployer = ${deployer}`);
     for (let whitelistAddress of whitelistedAddresses) {
-      let tokw = await read("KyberReserve", "tokenWallet", sandContract.address);
-      console.log(`tokw = ${tokw}`);
       await execute(
         "KyberReserve",
-        { from: deployer, skipUnknownSigner: true },
+        {from: deployer, skipUnknownSigner: true},
         "approveWithdrawAddress",
         sandContract.address,
         whitelistAddress,
@@ -100,23 +90,23 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     await addAlerter(reserveAdmin);
 
-    await execute("KyberReserve", { from: deployer, skipUnknownSigner: true }, "transferAdminQuickly", reserveAdmin);
+    await execute("KyberReserve", {from: deployer, skipUnknownSigner: true}, "transferAdminQuickly", reserveAdmin);
   }
   async function addOperator(operator) {
     const operators = await read("KyberReserve", "getOperators");
     if (operators.indexOf(operator) !== -1) {
-      console.log(`${operator} was already set as an operator, skipping`);
+      log(`${operator} was already set as an operator, skipping`);
     } else {
-      await execute("KyberReserve", { from: deployer, skipUnknownSigner: true }, "addOperator", operator);
+      await execute("KyberReserve", {from: deployer, skipUnknownSigner: true}, "addOperator", operator);
     }
   }
 
   async function addAlerter(alerter) {
     const alerters = await read("KyberReserve", "getAlerters");
     if (alerters.indexOf(alerter) !== -1) {
-      console.log(`${alerter} was already set as an alerter, skipping`);
+      log(`${alerter} was already set as an alerter, skipping`);
     } else {
-      await execute("KyberReserve", { from: deployer, skipUnknownSigner: true }, "addAlerter", alerter);
+      await execute("KyberReserve", {from: deployer, skipUnknownSigner: true}, "addAlerter", alerter);
     }
   }
 
@@ -129,8 +119,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   }
 
   async function depositSand(value) {
-    await execute("Sand", { from: deployer, skipUnknownSigner: true }, "transfer", kyberReserve.address, value);
+    await execute("Sand", {from: deployer, skipUnknownSigner: true}, "transfer", kyberReserve.address, value);
   }
 };
 
 module.exports.tags = ["KyberReserve"];
+
+// module.exports = async ({ getChainId, getNamedAccounts, deployments, network }) => {
+// };
---
+++ deploy/905_set_kyber_liquidity.js
@@ -0,0 +1,106 @@
+const fs = require("fs");
+const path = require("path");
+const configPath = path.join(__dirname, "../data/kyberReserve/liquidity_settings.json");
+const {BigNumber} = require("ethers");
+
+let tokenAddress;
+
+let pricingAdmin;
+
+let tokenPriceInEth;
+let ethBalance;
+let liqRate;
+let minAllowablePrice;
+let maxAllowablePrice;
+let maxTxBuyAmtEth;
+let maxTxSellAmtEth;
+let feePercent;
+const formulaPrecision = 40;
+let _rInFp;
+let _pMinInFp;
+let _numFpBits;
+let _maxCapBuyInWei;
+let _maxCapSellInWei;
+let _feeInBps;
+let _maxTokenToEthRateInPrecision;
+let _minTokenToEthRateInPrecision;
+
+module.exports = async ({getChainId, deployments}) => {
+  const chainId = await getChainId();
+  if (chainId === "31337") {
+    return;
+  }
+  const configParams = JSON.parse(fs.readFileSync(configPath, "utf8"));
+  const {execute, read} = deployments;
+  const reserveAddress = (await deployments.get("KyberReserve")).address;
+  await instantiateContracts();
+  parseInput(configParams);
+  await fetchParams();
+  calculateParams();
+  await setLiquidityParams();
+  async function instantiateContracts() {
+    pricingAddress = await read("KyberReserve", "conversionRatesContract");
+    pricingAdmin = await read("LiquidityConversionRates", "admin");
+    tokenAddress = await read("LiquidityConversionRates", "token");
+  }
+
+  function parseInput(jsonInput) {
+    tokenPriceInEth = tokenPriceInEth ? tokenPriceInEth : jsonInput["tokenPriceInEth"];
+
+    minAllowablePrice = jsonInput["minAllowablePrice"] ? jsonInput["minAllowablePrice"] : 0.5;
+
+    maxAllowablePrice = jsonInput["maxAllowablePrice"] ? jsonInput["maxAllowablePrice"] : 2.0;
+
+    maxTxBuyAmtEth = jsonInput["maxTxBuyAmtEth"] ? jsonInput["maxTxBuyAmtEth"] : 10;
+
+    maxTxSellAmtEth = jsonInput["maxTxSellAmtEth"] ? jsonInput["maxTxSellAmtEth"] : 10;
+
+    feePercent = jsonInput["feePercent"] ? jsonInput["feePercent"] : 0.05;
+  }
+
+  async function fetchParams() {
+    let tokenDecimals = await read("Sand", "decimals");
+    ethBalance = (await ethers.provider.getBalance(reserveAddress)) / 10 ** 18;
+    liqRate = Math.log(1 / minAllowablePrice) / ethBalance;
+    try {
+      const tokenWallet = await read("KyberReserve", "tokenWallet", tokenAddress);
+      // tokenBalance = await token.balanceOf(tokenWallet) / 10 ** tokenDecimals;
+      tokenBalance = (await read("Sand", "balanceOf", tokenWallet)) / 10 ** tokenDecimals;
+    } catch (e) {
+      tokenBalance = (await read("Sand", "balanceOf", reserveAddress)) / 10 ** tokenDecimals;
+      // tokenBalance = await token.balanceOf(reserveAddress) / 10 ** tokenDecimals;
+    }
+  }
+
+  function calculateParams() {
+    const maxSupportPrice = maxAllowablePrice * tokenPriceInEth;
+    const minSupportPrice = minAllowablePrice * tokenPriceInEth;
+    _rInFp = liqRate * 2 ** formulaPrecision;
+    _rInFp = Math.floor(_rInFp);
+    _pMinInFp = minSupportPrice * 2 ** formulaPrecision;
+    _pMinInFp = Math.floor(_pMinInFp);
+    _numFpBits = formulaPrecision;
+    _maxCapBuyInWei = maxTxBuyAmtEth * 10 ** 18;
+    _maxCapSellInWei = maxTxSellAmtEth * 10 ** 18;
+    _feeInBps = feePercent * 100;
+    _maxTokenToEthRateInPrecision = maxSupportPrice * 10 ** 18;
+    _minTokenToEthRateInPrecision = minSupportPrice * 10 ** 18;
+  }
+
+  async function setLiquidityParams() {
+    await execute(
+      "LiquidityConversionRates",
+      {from: pricingAdmin, skipUnknownSigner: true},
+      "setLiquidityParams",
+      BigNumber.from(_rInFp.toString()),
+      BigNumber.from(_pMinInFp.toString()),
+      BigNumber.from(_numFpBits.toString()),
+      BigNumber.from(_maxCapBuyInWei.toString()),
+      BigNumber.from(_maxCapSellInWei.toString()),
+      BigNumber.from(_feeInBps.toString()),
+      BigNumber.from(_maxTokenToEthRateInPrecision.toString()),
+      BigNumber.from(_minTokenToEthRateInPrecision.toString())
+    );
+  }
+};
+module.exports.tags = ["SetLiquidity"];
