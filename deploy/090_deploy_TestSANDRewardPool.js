feb97674f99a3524ab3390460a7be8cc413cb3e3
Wighawag <wighawag@gmail.com>
2020-09-15 09:32:31 +0100

    fix reward pool script

--- deploy/090_deploy_TestSANDRewardPool.js
+++ deploy/090_deploy_TestSANDRewardPool.js
@@ -25,14 +25,15 @@ module.exports = async ({getNamedAccounts, deployments}) => {
     const receipt = await pairCreatorAsDeployer.functions
       .createPair("0xCc933a862fc15379E441F2A16Cb943D385a4695f", "0xc778417E063141139Fce010982780140Aa0cD5Ab", {
         gasLimit: 8000000,
-      }) //.then((tx) => tx.wait()); // makes deployment crash
+      })
+      .then((tx) => tx.wait()); // makes deployment crash
 
     // Rinkeby SAND token address, Rinkeby WETH token address
 
     console.log("pair", receipt);
 
     const events = receipt.events;
-    const pairCreationEvent = events.find(event => event.event === "PairCreated");
+    const pairCreationEvent = events.find((event) => event.event === "PairCreated");
     const pairAddress = pairCreationEvent.args[2];
 
     if (pairAddress) {
