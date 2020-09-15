09f1ae34bde8547d3407e249bc08083ede6dca00
atkinsonholly <atkinsonholly@gmail.com>
2020-09-15 13:51:07 +0100

    updates to deploy scripts

--- deploy/090_deploy_TestSANDRewardPool.js
+++ deploy/090_deploy_TestSANDRewardPool.js
@@ -14,41 +14,57 @@ module.exports = async ({getNamedAccounts, deployments}) => {
   const chainId = await getChainId();
 
   if (chainId === "4") {
-    const uniswapV2Factory = new Contract(
-      uniswapV2FactoryAddress,
-      IUniswapV2Factory.abi,
-      ethers.provider.getSigner(deployer)
-    );
+    let pair;
 
-    const pairCreatorAsDeployer = uniswapV2Factory.connect(ethers.provider.getSigner(deployer));
+    // // uncomment to add a new pair
+    // // -------------------------------
+    // const uniswapV2Factory = new Contract(
+    //   uniswapV2FactoryAddress,
+    //   IUniswapV2Factory.abi,
+    //   ethers.provider.getSigner(deployer)
+    // );
 
-    const receipt = await pairCreatorAsDeployer.functions
-      .createPair("0xCc933a862fc15379E441F2A16Cb943D385a4695f", "0xc778417E063141139Fce010982780140Aa0cD5Ab", {
-        gasLimit: 8000000,
-      })
-      .then((tx) => tx.wait()); // makes deployment crash
+    // const pairCreatorAsDeployer = uniswapV2Factory.connect(ethers.provider.getSigner(deployer));
 
-    // Rinkeby SAND token address, Rinkeby WETH token address
+    // // check if the pair already exists (note: tokens can be in any order)
+    // ({pair} = await pairCreatorAsDeployer.functions.getPair(
+    //   "0xCc933a862fc15379E441F2A16Cb943D385a4695f",
+    //   "0xc778417E063141139Fce010982780140Aa0cD5Ab"
+    // ));
 
-    console.log("pair", receipt);
+    // if (!pair) {
+    //   // createPair if the pair does not exist in the UniswapV2Factory
+    //   const receipt = await pairCreatorAsDeployer.functions
+    //     .createPair("0xCc933a862fc15379E441F2A16Cb943D385a4695f", "0xc778417E063141139Fce010982780140Aa0cD5Ab", {
+    //       gasLimit: 8000000,
+    //     })
+    //     .then((tx) => tx.wait());
 
-    const events = receipt.events;
-    const pairCreationEvent = events.find((event) => event.event === "PairCreated");
-    const pairAddress = pairCreationEvent.args[2];
+    //   // Rinkeby SAND token address, Rinkeby WETH token address
 
-    if (pairAddress) {
+    //   const events = receipt.events;
+    //   const pairCreationEvent = events.find((event) => event.event === "PairCreated");
+    //   pair = pairCreationEvent.args[2];
+    // }
+
+    // If pair contract address is known
+    pair = "0x57459003f480188204085A0F744ffEbcD53bcc5E";
+
+    if (pair) {
       log("Rinkeby SAND address: 0xCc933a862fc15379E441F2A16Cb943D385a4695f");
       log("Rinkeby WETH address: 0xc778417E063141139Fce010982780140Aa0cD5Ab");
-      log(`Rinkeby UniswapV2 SAND-ETH Pair Contract Address: ${pairAddress}`);
+      log(`Rinkeby UniswapV2 SAND-ETH Pair Contract Address: ${pair}`); // 0x57459003f480188204085A0F744ffEbcD53bcc5E
+
+      // -------------------------------
 
       await deploy("TestSANDRewardPool", {
         from: deployer,
-        args: [pairAddress],
+        args: [pair],
         log: true,
       });
     }
   }
 };
 
-module.exports.skip = guard(["1", "314159", "4"], "TestSANDRewardPool"); // Remove "TestSANDRewardPool"
+module.exports.skip = guard(["1", "314159", "4"]); //, "TestSANDRewardPool"); // Remove "TestSANDRewardPool"
 module.exports.tags = ["TestSANDRewardPool"];
