f3fc9b7810132f8e40d8d8c8ef4dc80362f4cd5b
Nick Furfaro <nfurfaro33@gmail.com>
2020-09-24 11:46:54 -0600

    Remove catalyst from fixture

--- test/metaTx/fixtures.js
+++ test/metaTx/fixtures.js
@@ -3,13 +3,9 @@ const {ethers, deployments} = require("@nomiclabs/buidler");
 module.exports.setupTest = deployments.createFixture(async () => {
   await deployments.fixture();
   const sandContract = await ethers.getContract("Sand");
-  const catalystContract = await ethers.getContract("CatalystMinter");
   const sandWrapper = await ethers.getContract("SandWrapper");
-  const catalystWrapper = await ethers.getContract("CatalystWrapper");
   return {
     sandWrapper,
-    catalystWrapper,
     sandContract,
-    catalystContract,
   };
 });
