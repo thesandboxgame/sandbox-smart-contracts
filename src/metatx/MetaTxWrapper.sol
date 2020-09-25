c458623a9b353bfcf4a2a516883529b10fdc49f1
Nick Furfaro <nfurfaro33@gmail.com>
2020-09-25 06:16:32 -0600

    Clean up console.logs

--- src/metatx/MetaTxWrapper.sol
+++ src/metatx/MetaTxWrapper.sol
@@ -1,7 +1,6 @@
 pragma solidity 0.6.5;
 
 import "./BaseRelayRecipient.sol";
-import "@nomiclabs/buidler/console.sol";
 
 
 contract MetaTxWrapper is BaseRelayRecipient {
@@ -16,16 +15,10 @@ contract MetaTxWrapper is BaseRelayRecipient {
         uint256 length = msg.data.length;
         // retrieve the msg sender as per EIP-2771
         address signer = _msgSender();
-        // @review delete all console logs
-        console.logBytes(msg.data);
         address firstParam = abi.decode(msg.data[4:], (address));
-        console.log("msg.sender: ", msg.sender);
-        console.log("signer: ", signer);
-        console.log("firstParam: ", firstParam);
         require(signer == firstParam, "INVALID_METATX_DATA");
         address target = _forwardTo;
         (bool success, ) = target.call{value: msg.value}(msg.data);
-        // require(success, "FORWARDED_CALL_FAILED");
         if (!success) {
             assembly {
                 let returnDataSize := returndatasize()
