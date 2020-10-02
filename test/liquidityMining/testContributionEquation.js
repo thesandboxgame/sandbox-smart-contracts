5eaac65ef2eb78a4b47f8cc5e4a42bf50f042410
Wighawag <wighawag@gmail.com>
2020-10-02 11:05:30 +0100

    more test values

--- test/liquidityMining/testContributionEquation.js
+++ test/liquidityMining/testContributionEquation.js
@@ -10,8 +10,38 @@ describe("LandWeightedSANDRewardPool computation", function () {
     const valuesToTests = [
       {
         amountStaked: 1000,
+        numLands: 0,
+        expectedContribution: 1000,
+      },
+      {
+        amountStaked: 1000,
+        numLands: 1,
+        expectedContribution: 2000,
+      },
+      {
+        amountStaked: 1000,
+        numLands: 3,
+        expectedContribution: 2044,
+      },
+      {
+        amountStaked: 1000,
         numLands: 9,
         expectedContribution: 2108,
+      },
+      {
+        amountStaked: 1000,
+        numLands: 10,
+        expectedContribution: 2115,
+      },
+      {
+        amountStaked: 1000,
+        numLands: 122,
+        expectedContribution: 2395,
+      },
+      {
+        amountStaked: 1000,
+        numLands: 10000,
+        expectedContribution: 4054,
       },
     ];
     for (const values of valuesToTests) {
