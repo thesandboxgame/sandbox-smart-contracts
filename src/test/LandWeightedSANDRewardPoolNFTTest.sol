ace0d8719155940cbd290cc7d5f8f8eef744f378
atkinsonholly <atkinsonholly@gmail.com>
2020-10-05 14:28:17 +0100

    WIP check land balances for reward pool

---
+++ src/test/LandWeightedSANDRewardPoolNFTTest.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.6.5;
+
+import "../LiquidityMining/LandWeightedSANDRewardPool.sol";
+
+
+contract LandWeightedSANDRewardPoolNFTTest is LandWeightedSANDRewardPool {
+    constructor(address stakeTokenContract, address rewardTokenContract, address nftContract, uint256 nftFactor6, uint256 nftConstant6)
+        public
+        LandWeightedSANDRewardPool(IERC20(stakeTokenContract), IERC20(rewardTokenContract), ERC721(nftContract), nftFactor6, nftConstant6)
+    {}
+
+    function testComputeContribution(uint256 amountStaked, uint256 numLands) external view returns (uint256) {
+        return computeContribution(amountStaked, numLands);
+    }
+}
--- test/liquidityMining/testMockSANDRewardPool.js
+++ test/liquidityMining/testMockSANDRewardPool.js
@@ -5,8 +5,8 @@ const {mine} = require("local-utils");
 
 const STAKE_TOKEN = "UNI_SAND_ETH";
 const REWARD_TOKEN = "Sand";
-const MULTIPLIER_NFToken = "Land";
-const POOL = "LandWeightedSANDRewardPool";
+const MULTIPLIER_NFToken = "MockLand";
+const POOL = "LandWeightedSANDRewardPoolNFTTest";
 const REWARD_DURATION = 2592000; // 30 days in seconds
 const REWARD_AMOUNT = BigNumber.from(1500000).mul("1000000000000000000");
 const ACTUAL_REWARD_AMOUNT = REWARD_AMOUNT.div(REWARD_DURATION).mul(REWARD_DURATION);
@@ -14,7 +14,7 @@ const ACTUAL_REWARD_AMOUNT = REWARD_AMOUNT.div(REWARD_DURATION).mul(REWARD_DURAT
 const NEW_REWARD_AMOUNT = BigNumber.from(2000000).mul("1000000000000000000");
 const STAKE_AMOUNT = BigNumber.from(10000).mul("1000000000000000000");
 
-describe("SANDRewardPool", function () {
+describe("MockSANDRewardPool", function () {
   let deployer;
   let others;
   let sandAdmin;
@@ -37,18 +37,24 @@ describe("SANDRewardPool", function () {
       landAdmin,
     } = await getNamedAccounts());
 
+    // Deploy mock contracts
+    rewardToken = await ethers.getContract(REWARD_TOKEN);
+    await deployments.deploy(MULTIPLIER_NFToken, {from: deployer, args: [rewardToken.address, landAdmin]});
+    multiplierNFToken = await ethers.getContract(MULTIPLIER_NFToken);
+    stakeToken = await ethers.getContract(STAKE_TOKEN);
+    await deployments.deploy(POOL, {
+      from: deployer,
+      args: [stakeToken.address, rewardToken.address, multiplierNFToken.address, "100000", "9000000"],
+    });
+    rewardPool = await ethers.getContract(POOL);
+
     // Define token admins
     const rewardTokenAdmin = sandAdmin;
     const stakeTokenAdmin = deployer;
     const multiplierNFTokenAdmin = landAdmin;
 
-    // Contracts
-    rewardToken = await ethers.getContract(REWARD_TOKEN);
-    multiplierNFToken = await ethers.getContract(MULTIPLIER_NFToken);
-    stakeToken = await ethers.getContract(STAKE_TOKEN);
-
     // Get contract roles
-    rewardPool = await ethers.getContract(POOL);
+    rewardPoolAsDeployer = rewardPool.connect(ethers.provider.getSigner(deployer));
     rewardPoolAsAdmin = rewardPool.connect(ethers.provider.getSigner(liquidityRewardAdmin));
     rewardPoolAsUser = rewardPool.connect(ethers.provider.getSigner(others[0]));
     stakeTokenAsAdmin = stakeToken.connect(ethers.provider.getSigner(stakeTokenAdmin));
@@ -58,15 +64,24 @@ describe("SANDRewardPool", function () {
     multiplierNFTokenAsAdmin = multiplierNFToken.connect(ethers.provider.getSigner(multiplierNFTokenAdmin));
     multiplierNFTokenAsUser = multiplierNFToken.connect(ethers.provider.getSigner(others[0]));
 
+    // Send reward to pool
+    await rewardPoolAsDeployer.setRewardDistribution(liquidityRewardAdmin);
+    await rewardTokenAsAdmin.transfer(rewardPool.address, REWARD_AMOUNT);
+    await rewardPoolAsAdmin.notifyRewardAmount(REWARD_AMOUNT);
+
     // Give user some stakeTokens
     await stakeTokenAsAdmin.transfer(others[0], STAKE_AMOUNT);
     await stakeTokenAsUser.approve(rewardPool.address, STAKE_AMOUNT);
-  }
 
-  it("Contract should exist", async function () {
-    await createFixture();
-    await ethers.getContract(POOL);
-  });
+    // Set up user with NFTs (mockLands)
+    await multiplierNFTokenAsAdmin.transferFrom(multiplierNFToken.address, others[0], 0);
+    await multiplierNFTokenAsAdmin.transferFrom(multiplierNFToken.address, others[0], 1);
+    await multiplierNFTokenAsAdmin.transferFrom(multiplierNFToken.address, others[0], 2);
+
+    // TODO: check user's NFT balance
+    const balance = await multiplierNFToken.functions.balanceOf(others[0]);
+    console.log(balance);
+  }
 
   it("Pool contains reward tokens", async function () {
     await createFixture();
@@ -123,6 +138,9 @@ describe("SANDRewardPool", function () {
     expect(earned).to.equal(0);
   });
 
-  // Test what happens if rewardToken in pool is less than amount notified
-  // Test what happens if pool is notified before end of current reward period
+  // Multiple stakers with no LANDs receive rewards
+  // Total rewards add up to 100% of reward available
+
+  // Multiple stakers with LANDs receive rewards
+  // Total contributions add up to 100% of reward available
 });
