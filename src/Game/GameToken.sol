804266646c8a6540f4db91531eedb679ad242629
Nick Furfaro <nfurfaro33@gmail.com>
2020-10-07 14:48:54 -0600

    Add constructor

--- src/Game/GameToken.sol
+++ src/Game/GameToken.sol
@@ -3,4 +3,16 @@ pragma solidity 0.6.5;
 import "../BaseWithStorage/ERC721BaseToken.sol";
 
 
-contract GameToken is ERC721BaseToken {}
+contract GameToken is ERC721BaseToken {
+    uint256 public _number;
+
+    address _owner; // is this ownable?
+    address _gameEditor; // do we need more than 1?
+    uint256[] assets; // asset ids?
+
+    function setNumber(uint256 number) public {
+        _number = number;
+    }
+
+    constructor(address metaTransactionContract, address admin) public ERC721BaseToken(metaTransactionContract, admin) {}
+}
