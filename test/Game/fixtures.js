6971ba64dceef35f637e6a03ed8faccfe0b6b2dd
Nick Furfaro <nfurfaro33@gmail.com>
2020-10-08 12:26:44 -0600

    Update fixture with admin connected contract

--- test/Game/fixtures.js
+++ test/Game/fixtures.js
@@ -1,9 +1,14 @@
-const {ethers, deployments} = require("@nomiclabs/buidler");
+const {ethers, deployments, getNamedAccounts} = require("@nomiclabs/buidler");
 
 module.exports.setupTest = deployments.createFixture(async () => {
   await deployments.fixture();
+  const {gameTokenAdmin} = await getNamedAccounts();
+
   const gameToken = await ethers.getContract("GameToken");
+  const gameTokenAsAdmin = await ethers.getContract("GameToken", gameTokenAdmin);
+
   return {
     gameToken,
+    gameTokenAsAdmin,
   };
 });
