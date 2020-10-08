c89b46cd1145187203b158755f3d6681b17a682d
Nick Furfaro <nfurfaro33@gmail.com>
2020-10-08 12:40:27 -0600

    Add _mintGame func to assign gameId to owner

--- src/Game/GameToken.sol
+++ src/Game/GameToken.sol
@@ -5,28 +5,28 @@ import "../BaseWithStorage/ERC721BaseToken.sol";
 
 contract GameToken is ERC721BaseToken {
     /**
-     * @notice function to allow token owner to set game editors
-     * @param id the id of the GAME token owned by owner
-     * @param editor the address of the editor to set
-     * @param isEditor add or remove the ability to edit
+     * @notice Function to allow token owner to set game editors
+     * @param gameId The id of the GAME token owned by owner
+     * @param editor The address of the editor to set
+     * @param isEditor Add or remove the ability to edit
      */
     function setGameEditor(
-        uint256 id,
+        uint256 gameId,
         address editor,
         bool isEditor
     ) external {
-        require(msg.sender == _ownerOf(id), "EDITOR_ACCESS_DENIED");
-        _gameEditors[id][editor] = isEditor;
+        require(msg.sender == _ownerOf(gameId), "EDITOR_ACCESS_DENIED");
+        _gameEditors[gameId][editor] = isEditor;
     }
 
     /**
-     * @notice function to get game editor status
-     * @param id the id of the GAME token owned by owner
+     * @notice Function to get game editor status
+     * @param gameId the id of the GAME token owned by owner
      * @param editor the address of the editor to set
      * @return isEditor editor status of editor for given tokenId
      */
-    function isGameEditor(uint256 id, address editor) external returns (bool isEditor) {
-        return _gameEditors[id][editor];
+    function isGameEditor(uint256 gameId, address editor) external returns (bool isEditor) {
+        return _gameEditors[gameId][editor];
     }
 
     /**
@@ -47,27 +47,43 @@ contract GameToken is ERC721BaseToken {
 
     /**
      * @notice Return the URI of a specific token
-     * @param id The id of the token
+     * @param gameId The id of the token
      * @return tokenURI The URI of the token
      */
-    function tokenURI(uint256 id) public view returns (string memory tokenURI) {
-        require(_ownerOf(id) != address(0), "Id does not exist");
-        string memory URI = _metaData[id];
+    function tokenURI(uint256 gameId) public view returns (string memory tokenURI) {
+        require(_ownerOf(gameId) != address(0), "Id does not exist");
+        string memory URI = _metaData[gameId];
         return URI;
     }
 
     /**
-     * @notice Set the URI of a specific token
-     * @param id the id of the token
-     * @param URI the URI string for the token's metadata
+     * @notice Set the URI of a specific game token
+     * @param gameId The id of the game token
+     * @param URI The URI string for the token's metadata
      */
-    function setTokenURI(uint256 id, string memory URI) public {
-        require(msg.sender == _ownerOf(id) || _gameEditors[id][msg.sender], "URI_ACCESS_DENIED");
-        _metaData[id] = URI;
+    function setTokenURI(uint256 gameId, string memory URI) public {
+        require(msg.sender == _ownerOf(gameId) || _gameEditors[gameId][msg.sender], "URI_ACCESS_DENIED");
+        _metaData[gameId] = URI;
+    }
+
+    /**
+     * @dev Function to create a new gameId and associate it with an owner
+     * @param to The address of the Game owner
+     * @return id The newly created gameId
+     */
+    function _mintGame(address to) internal returns (uint256 id) {
+        uint256 gameId = _nextId++;
+        _owners[gameId] = uint256(to);
+        // _numNFTPerAddress[to]++;
+        emit Transfer(address(0), to, gameId);
+        return gameId;
     }
 
     mapping(uint256 => string) private _metaData;
     mapping(uint256 => mapping(address => bool)) private _gameEditors;
+    uint256 internal _nextId;
 
-    constructor(address metaTransactionContract, address admin) public ERC721BaseToken(metaTransactionContract, admin) {}
+    constructor(address metaTransactionContract, address admin) public ERC721BaseToken(metaTransactionContract, admin) {
+        _nextId = 1;
+    }
 }
