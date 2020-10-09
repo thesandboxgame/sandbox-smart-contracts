4ef7b76b4a01ca7f800e2d259950802dc41f7de1
CloverGuy <arthemisP@protonmail.com>
2020-10-09 16:27:01 +0300

    added documentation v2

--- src/FeeDistributor/FeeDistributor.sol
+++ src/FeeDistributor/FeeDistributor.sol
@@ -16,7 +16,7 @@ contract FeeDistributor {
     /// @notice Enables fee holder to withdraw its share
     /// @notice Zero address reserved for ether withdrawal
     /// @param token the token that fee should be distributed in
-    /// @param beneficiary
+    /// @param beneficiary the address that receives fees
     /// @return amount had withdrawn
     function withdraw(IERC20 token, address payable beneficiary) external returns (uint256 amount) {
         if (address(token) == address(0)) {
