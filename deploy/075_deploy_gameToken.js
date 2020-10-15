d3ed206a66181817bed62b5f5a2f8ee2314c4674
Nick Furfaro <nfurfaro33@gmail.com>
2020-10-15 05:42:31 -0600

    Add asset to deploy script

--- deploy/075_deploy_gameToken.js
+++ deploy/075_deploy_gameToken.js
@@ -5,14 +5,15 @@ module.exports = async ({getNamedAccounts, deployments}) => {
 
   const {deployer, gameTokenAdmin} = await getNamedAccounts();
   const metaTransactionContract = await deployments.get("NativeMetaTransactionProcessor");
+  const assetContract = await deployments.get("Asset");
 
   await deploy("GameToken", {
     from: deployer,
-    args: [metaTransactionContract.address, gameTokenAdmin],
+    args: [metaTransactionContract.address, gameTokenAdmin, assetContract.address],
     log: true,
   });
 };
 
 module.exports.skip = guard(["1", "4", "314159"]); // @todo "GameToken"
 module.exports.tags = ["GameToken"];
-module.exports.dependencies = ["NativeMetaTransactionProcessor"];
+module.exports.dependencies = ["NativeMetaTransactionProcessor", "Asset"];
