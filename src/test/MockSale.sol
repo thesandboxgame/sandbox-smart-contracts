5d5ec7577ee3a95e6be80bc285f56123b451b36a
Wighawag <wighawag@gmail.com>
2020-10-22 13:40:21 +0100

    format

--- src/test/MockSale.sol
+++ src/test/MockSale.sol
@@ -4,6 +4,7 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
+
 contract MockSale {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
