99f42e1b04c0b31601c1c7379665b16d48e01445
Nick Furfaro <nfurfaro33@gmail.com>
2020-11-03 06:05:56 -0700

    Move supplyAssets func to fixtures

--- test/Game/fixtures.js
+++ test/Game/fixtures.js
@@ -1,4 +1,26 @@
 const {ethers, deployments, getNamedAccounts} = require("@nomiclabs/buidler");
+const {emptyBytes} = require("local-utils");
+
+const {execute} = deployments;
+
+const rarity = 3;
+
+module.exports.supplyAssets = async (creator, packId, owner, supply, hash) => {
+  await execute("Asset", {from: assetBouncerAdmin, skipUnknownSigner: true}, "setBouncer", assetAdmin, true);
+  const assetReceipt = await execute(
+    "Asset",
+    {from: assetAdmin, skipUnknownSigner: true},
+    "mint",
+    creator,
+    packId,
+    hash,
+    supply,
+    rarity,
+    creator,
+    emptyBytes
+  );
+  return {assetReceipt};
+};
 
 module.exports.setupTest = deployments.createFixture(async () => {
   await deployments.fixture();
