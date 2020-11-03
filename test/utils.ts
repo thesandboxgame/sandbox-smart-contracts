3a99317c1989f5fe1a466e1b33b15542457f3bfd
atkinsonholly <atkinsonholly@gmail.com>
2020-11-03 17:07:05 +0000

    findEvents added to utils.ts

--- test/utils.ts
+++ test/utils.ts
@@ -52,3 +52,19 @@ export function cubeRoot6(bigNum: BigNumber): BigNumber {
   }
   return c;
 }
+
+// TODO: correct types
+type Contract = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  queryFilter: any,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  filters: any,
+}
+
+// TODO: correct types
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function findEvents(contract: Contract, event: string, blockHash: string): Promise<any[]> {
+  const filter = contract.filters[event]();
+  const events = await contract.queryFilter(filter, blockHash);
+  return events;
+}
