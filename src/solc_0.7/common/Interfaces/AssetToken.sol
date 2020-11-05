dd4d2f280eefa76095145dd08210f51aa6046c0f
Nick Furfaro <nfurfaro33@gmail.com>
2020-11-05 13:20:34 -0700

    Add funcs to asset token interface

--- src/solc_0.7/common/Interfaces/AssetToken.sol
+++ src/solc_0.7/common/Interfaces/AssetToken.sol
@@ -36,9 +36,38 @@ interface AssetToken {
         address to
     ) external returns (uint256 newId);
 
+    function transferFrom(
+        address from,
+        address to,
+        uint256 id
+    ) external;
+
     function safeTransferFrom(
         address from,
         address to,
         uint256 id
+    ) external;
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external;
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] calldata ids,
+        bytes calldata data
+    ) external;
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
     ) external;
 }
