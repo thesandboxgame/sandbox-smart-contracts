8d3074ad1051a1779ba954c5ccb00df690416bd0
atkinsonholly <atkinsonholly@gmail.com>
2020-11-05 13:50:34 +0000

    user addresses updated to correct sand balance for tests

--- test/permit/permit.test.js
+++ test/permit/permit.test.js
@@ -24,12 +24,14 @@ describe("Permit", function () {
     deadline = BigNumber.from(2582718400);
   });
 
+  // Note: on test network, others[1] is sandAdmin, others[2] is sandBeneficiary
+
   it("ERC20 Approval event is emitted when msg.sender == owner", async function () {
     const {permitContract, sandContract, others} = setUp;
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -41,13 +43,13 @@ describe("Permit", function () {
     const flatSig = sigUtil.signTypedData_v4(privateKeyAsBuffer, {data: permitData712});
     const sig = splitSignature(flatSig);
 
-    const receipt = await permitContract.permit(wallet.address, others[1], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s).then((tx) => tx.wait());;
+    const receipt = await permitContract.permit(wallet.address, others[3], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s).then((tx) => tx.wait());;
 
     const transferEvents = await findEvents(sandContract, "Approval", receipt.blockHash);
 
     const approvalEvent = transferEvents[0];
     expect(approvalEvent.args[0]).to.equal(wallet.address); // owner
-    expect(approvalEvent.args[1]).to.equal(others[1]); // spender
+    expect(approvalEvent.args[1]).to.equal(others[3]); // spender
     expect(approvalEvent.args[2]).to.equal(TEST_AMOUNT); // amount
   });
 
@@ -56,7 +58,7 @@ describe("Permit", function () {
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -71,7 +73,7 @@ describe("Permit", function () {
     const checkNonce = await permitContract.nonces(wallet.address);
     expect(checkNonce).to.equal(0);
 
-    await permitContract.permit(wallet.address, others[1], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s);
+    await permitContract.permit(wallet.address, others[3], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s);
 
     const nonceAfterApproval = await permitContract.nonces(wallet.address);
     expect(nonceAfterApproval).to.equal(1);
@@ -84,7 +86,7 @@ describe("Permit", function () {
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -96,7 +98,7 @@ describe("Permit", function () {
     const flatSig = sigUtil.signTypedData_v4(privateKeyAsBuffer, {data: permitData712});
     const sig = splitSignature(flatSig);
 
-    await expect(permitContract.permit(wallet.address, others[1], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s)).to.be.revertedWith("PAST_DEADLINE");
+    await expect(permitContract.permit(wallet.address, others[3], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s)).to.be.revertedWith("PAST_DEADLINE");
   });
 
   it("Permit function reverts if owner is zeroAddress", async function () {
@@ -104,7 +106,7 @@ describe("Permit", function () {
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -116,7 +118,7 @@ describe("Permit", function () {
     const flatSig = sigUtil.signTypedData_v4(privateKeyAsBuffer, {data: permitData712});
     const sig = splitSignature(flatSig);
 
-    await expect(permitContract.permit(zeroAddress, others[1], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s)).to.be.revertedWith("INVALID_SIGNATURE"
+    await expect(permitContract.permit(zeroAddress, others[3], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s)).to.be.revertedWith("INVALID_SIGNATURE"
     );
   });
 
@@ -125,7 +127,7 @@ describe("Permit", function () {
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -137,7 +139,7 @@ describe("Permit", function () {
     const flatSig = sigUtil.signTypedData_v4(privateKeyAsBuffer, {data: permitData712});
     const sig = splitSignature(flatSig);
 
-    await expect(permitContract.permit(others[2], others[1], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s)).to.be.revertedWith("INVALID_SIGNATURE"
+    await expect(permitContract.permit(others[4], others[3], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s)).to.be.revertedWith("INVALID_SIGNATURE"
     );
   });
 
@@ -146,7 +148,7 @@ describe("Permit", function () {
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -158,7 +160,7 @@ describe("Permit", function () {
     const flatSig = sigUtil.signTypedData_v4(privateKeyAsBuffer, {data: permitData712});
     const sig = splitSignature(flatSig);
 
-    await expect(permitContract.permit(wallet.address, others[2], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s)).to.be.revertedWith("INVALID_SIGNATURE"
+    await expect(permitContract.permit(wallet.address, others[4], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s)).to.be.revertedWith("INVALID_SIGNATURE"
     );
   });
 
@@ -168,7 +170,7 @@ describe("Permit", function () {
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -183,12 +185,12 @@ describe("Permit", function () {
 
   it("Non-approved operators cannot transfer ERC20 until approved", async function () {
     const {permitContract, sandContract, sandAdmin, sandBeneficiary, others} = setUp;
-    const receiverOriginalBalance = await sandContract.balanceOf(others[2]);
-    // TODO: expect(receiverOriginalBalance).to.equal(0);
+    const receiverOriginalBalance = await sandContract.balanceOf(others[4]);
+    expect(receiverOriginalBalance).to.equal(0);
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -204,19 +206,19 @@ describe("Permit", function () {
     const sandContractAsAdmin = await sandContract.connect(sandContract.provider.getSigner(sandAdmin));
     await sandContractAsAdmin.transferFrom(sandBeneficiary, wallet.address, TEST_AMOUNT);
 
-    const sandContractAsSpender = await sandContract.connect(sandContract.provider.getSigner(others[1]));
-    await expect(sandContractAsSpender.transferFrom(wallet.address, others[2], TEST_AMOUNT)).to.be.revertedWith(
+    const sandContractAsSpender = await sandContract.connect(sandContract.provider.getSigner(others[3]));
+    await expect(sandContractAsSpender.transferFrom(wallet.address, others[4], TEST_AMOUNT)).to.be.revertedWith(
       "Not enough funds allowed"
     );
-    await permitContract.permit(wallet.address, others[1], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s).then((tx) => tx.wait());;
-    const receipt = await sandContractAsSpender.transferFrom(wallet.address, others[2], TEST_AMOUNT);
-    const receiverNewBalance = await sandContract.balanceOf(others[2]);
+    await permitContract.permit(wallet.address, others[3], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s).then((tx) => tx.wait());;
+    const receipt = await sandContractAsSpender.transferFrom(wallet.address, others[4], TEST_AMOUNT);
+    const receiverNewBalance = await sandContract.balanceOf(others[4]);
     const transferEventsMatching = await findEvents(sandContract, "Transfer", receipt.blockHash);
     expect(transferEventsMatching.length).to.equal(1);
     expect(transferEventsMatching[0].args[0]).to.equal(wallet.address);
-    expect(transferEventsMatching[0].args[1]).to.equal(others[2]);
+    expect(transferEventsMatching[0].args[1]).to.equal(others[4]);
     expect(transferEventsMatching[0].args[2]).to.equal(TEST_AMOUNT);
-    // TODO: expect(receiverNewBalance).to.equal(TEST_AMOUNT.add(receiverOriginalBalance));
+    expect(receiverNewBalance).to.equal(TEST_AMOUNT.add(receiverOriginalBalance));
   });
 
   it("Approved operators cannot transfer more ERC20 than their allowance", async function () {
@@ -224,7 +226,7 @@ describe("Permit", function () {
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -240,9 +242,9 @@ describe("Permit", function () {
     const sandContractAsAdmin = await sandContract.connect(sandContract.provider.getSigner(sandAdmin));
     await sandContractAsAdmin.transferFrom(sandBeneficiary, wallet.address, TEST_AMOUNT.mul(2));
 
-    const sandContractAsSpender = await sandContract.connect(sandContract.provider.getSigner(others[1]));
-    await permitContract.permit(wallet.address, others[1], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s).then((tx) => tx.wait());;
-    await expect(sandContractAsSpender.transferFrom(wallet.address, others[2], TEST_AMOUNT.mul(2))).to.be.revertedWith(
+    const sandContractAsSpender = await sandContract.connect(sandContract.provider.getSigner(others[3]));
+    await permitContract.permit(wallet.address, others[3], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s).then((tx) => tx.wait());;
+    await expect(sandContractAsSpender.transferFrom(wallet.address, others[4], TEST_AMOUNT.mul(2))).to.be.revertedWith(
       "Not enough funds allowed"
     );
   });
@@ -252,7 +254,7 @@ describe("Permit", function () {
 
     const approve = {
       owner: wallet.address,
-      spender: others[1],
+      spender: others[3],
       value: TEST_AMOUNT._hex,
       nonce: nonce._hex,
       deadline: deadline._hex,
@@ -268,8 +270,8 @@ describe("Permit", function () {
     const sandContractAsAdmin = await sandContract.connect(sandContract.provider.getSigner(sandAdmin));
     await sandContractAsAdmin.transferFrom(sandBeneficiary, wallet.address, TEST_AMOUNT.div(2));
 
-    const sandContractAsSpender = await sandContract.connect(sandContract.provider.getSigner(others[1]));
-    await permitContract.permit(wallet.address, others[1], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s).then((tx) => tx.wait());;
-    await expect(sandContractAsSpender.transferFrom(wallet.address, others[2], TEST_AMOUNT)).to.be.revertedWith("not enough fund");
+    const sandContractAsSpender = await sandContract.connect(sandContract.provider.getSigner(others[3]));
+    await permitContract.permit(wallet.address, others[3], TEST_AMOUNT, deadline, sig.v, sig.r, sig.s).then((tx) => tx.wait());;
+    await expect(sandContractAsSpender.transferFrom(wallet.address, others[4], TEST_AMOUNT)).to.be.revertedWith("not enough fund");
   });
 });
