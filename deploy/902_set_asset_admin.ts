bf0533e5551a9c4b1a70afa4985b47b76c104e19
Nick Furfaro <nfurfaro33@gmail.com>
2020-11-10 07:20:03 -0700

    Migrate set asset admin script to ts/hardhat

---
+++ deploy/902_set_asset_admin.ts
@@ -0,0 +1,33 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {log, execute, read} = deployments;
+  const {assetAdmin, assetBouncerAdmin} = await getNamedAccounts();
+
+  const currentAdmin = await read('Asset', 'getAdmin');
+  if (currentAdmin.toLowerCase() !== assetAdmin.toLowerCase()) {
+    await execute(
+      'Asset',
+      {from: currentAdmin, log: true},
+      'changeAdmin',
+      assetAdmin
+    );
+  }
+
+  const currentBouncerAdmin = await read('Asset', 'getBouncerAdmin');
+  if (currentBouncerAdmin.toLowerCase() !== assetBouncerAdmin.toLowerCase()) {
+    log('setting Sand Execution Admin');
+    await execute(
+      'Asset',
+      {from: currentBouncerAdmin, log: true},
+      'changeExecutionAdmin',
+      assetBouncerAdmin
+    );
+  }
+};
+
+export default func;
+func.runAtTheEnd = true;
+func.dependencies = ['Asset'];
