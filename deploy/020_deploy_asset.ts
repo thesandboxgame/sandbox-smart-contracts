6a6e07425e815c9cc91e101dbeeeb72f0aece634
Nick Furfaro <nfurfaro33@gmail.com>
2020-11-10 06:57:17 -0700

    Migrate asset deploy script to ts/hardhat

---
+++ deploy/020_deploy_asset.ts
@@ -0,0 +1,23 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+
+  const {deployer} = await getNamedAccounts();
+  const sandContract = await deployments.get('Sand');
+
+  await deploy('Asset', {
+    from: deployer,
+    args: [
+      sandContract.address,
+      deployer, // is set to assetAdmin in a later stage
+      deployer, // is set to assetBouncerAdmin in a later stage]}, // , gasPrice: '10000000000'},
+    ],
+    log: true,
+  });
+};
+export default func;
+func.tags = ['Asset', 'Asset_deploy'];
+func.dependencies = ['Sand'];
