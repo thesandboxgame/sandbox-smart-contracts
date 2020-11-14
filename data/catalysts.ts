91e3ab6d5b6a4acc6ee6348165195fbd2f3f98f8
CloverGuy <arthemisP@protonmail.com>
2020-11-14 20:09:02 +0200

    tests using fixtures

--- data/catalysts.ts
+++ data/catalysts.ts
@@ -1,39 +1,22 @@
-import {BigNumber} from '@ethersproject/bignumber';
-function sandWei(amount: number) {
-  return BigNumber.from(amount).mul('1000000000000000000').toString();
-}
-
-export = [
+export =[
   {
     name: 'Common',
     symbol: 'COMMON',
-    sandMintingFee: sandWei(1),
-    sandUpdateFee: sandWei(1),
     maxGems: 1,
-    quantityRange: [4000, 20000],
   },
   {
     name: 'Rare',
     symbol: 'RARE',
-    sandMintingFee: sandWei(4),
-    sandUpdateFee: sandWei(4),
     maxGems: 2,
-    quantityRange: [1500, 4000],
   },
   {
     name: 'Epic',
     symbol: 'EPIC',
-    sandMintingFee: sandWei(10),
-    sandUpdateFee: sandWei(10),
     maxGems: 3,
-    quantityRange: [200, 1500],
   },
   {
     name: 'Legendary',
     symbol: 'LEGENDARY',
-    sandMintingFee: sandWei(200),
-    sandUpdateFee: sandWei(200),
     maxGems: 4,
-    quantityRange: [1, 200],
   },
 ];
--- test/erc677/ERC677.test.ts
+++ test/erc677/ERC677.test.ts
@@ -1,53 +1,53 @@
-import {ethers, getNamedAccounts} from 'hardhat';
-import {BigNumber} from '@ethersproject/bignumber';
-import {expect} from '../chai-setup';
-import {ContractFactory, Contract} from 'ethers';
+import { deployments, ethers, getNamedAccounts } from 'hardhat';
+import { BigNumber } from '@ethersproject/bignumber';
+import { expect } from '../chai-setup';
+import { Contract } from 'ethers';
 
 describe('ERC677Token', function () {
-  async function initContracts(name: string, symbol: string) {
+
+
+  let tokenReceiver: Contract;
+  let gemToken: Contract;
+  let emptyContract: Contract;
+  let fallbackContract: Contract;
+
+  async function createFixture() {
+    await deployments.fixture();
     const accounts = await getNamedAccounts();
-    const token: Contract = await initContract(
-      'ERC20Token',
-      accounts.deployer,
-      [name, symbol, accounts.deployer]
-    );
-    const tokenReceiver: Contract = await initContract(
-      'MockERC677Receiver',
-      accounts.deployer,
-      []
-    );
-    const tx = await token
+    gemToken = await ethers.getContract('Gem_Power');
+    await deployments.deploy('MockERC677Receiver', {
+      from: accounts.deployer,
+      args: [],
+    });
+    tokenReceiver = await ethers.getContract('MockERC677Receiver');
+    await deployments.deploy('EmptyContract', {
+      from: accounts.deployer,
+      args: [],
+    });
+    emptyContract = await ethers.getContract('EmptyContract');
+    await deployments.deploy('FallBackContract', {
+      from: accounts.deployer,
+      args: [],
+    });
+    fallbackContract = await ethers.getContract('FallBackContract');
+    const tx = await gemToken
       .connect(ethers.provider.getSigner(accounts.deployer))
       .mint(accounts.deployer, BigNumber.from('800000000000000000'));
     await tx.wait();
-    return {token, tokenReceiver};
-  }
-
-  async function initContract(
-    contractName: string,
-    deployer: string,
-    params: Array<string>
-  ): Promise<Contract> {
-    const ethersFactory: ContractFactory = await ethers.getContractFactory(
-      contractName,
-      ethers.provider.getSigner(deployer)
-    );
-    const contractRef: Contract = await ethersFactory.deploy(...params);
-    return contractRef;
   }
 
   it('Transfering tokens to ERC677Receiver contract should emit an OnTokenTransferEvent event', async function () {
-    const {token, tokenReceiver} = await initContracts('MOCK', 'MOCK');
+    await createFixture();
     const accounts = await getNamedAccounts();
-    const fromBalanceBefore = await token.balanceOf(accounts.deployer);
-    const toBalanceBefore = await token.balanceOf(tokenReceiver.address);
+    const fromBalanceBefore = await gemToken.balanceOf(accounts.deployer);
+    const toBalanceBefore = await gemToken.balanceOf(tokenReceiver.address);
     const amount = BigNumber.from('100000000000000000');
-    const tx = await token
+    const tx = await gemToken
       .connect(ethers.provider.getSigner(accounts.deployer))
       .transferAndCall(tokenReceiver.address, amount, Buffer.from('data'));
     await tx.wait();
-    const fromBalanceAfter = await token.balanceOf(accounts.deployer);
-    const toBalanceAfter = await token.balanceOf(tokenReceiver.address);
+    const fromBalanceAfter = await gemToken.balanceOf(accounts.deployer);
+    const toBalanceAfter = await gemToken.balanceOf(tokenReceiver.address);
     const tokenReceiverEvents = await tokenReceiver.queryFilter(
       tokenReceiver.filters.OnTokenTransferEvent()
     );
@@ -66,56 +66,46 @@ describe('ERC677Token', function () {
     }
   });
   it('Transfering tokens to EOA', async function () {
-    const {token} = await initContracts('MOCK', 'MOCK');
+    await createFixture();
     const accounts = await getNamedAccounts();
-    const fromBalanceBefore = await token.balanceOf(accounts.deployer);
-    const toBalanceBefore = await token.balanceOf(accounts.sandAdmin);
+    const fromBalanceBefore = await gemToken.balanceOf(accounts.deployer);
+    const toBalanceBefore = await gemToken.balanceOf(accounts.sandAdmin);
     const amount = BigNumber.from('100000000000000000');
-    const tx = await token
+    const tx = await gemToken
       .connect(ethers.provider.getSigner(accounts.deployer))
       .transferAndCall(accounts.sandAdmin, amount, Buffer.from('data'));
     await tx.wait();
-    const fromBalanceAfter = await token.balanceOf(accounts.deployer);
-    const toBalanceAfter = await token.balanceOf(accounts.sandAdmin);
+    const fromBalanceAfter = await gemToken.balanceOf(accounts.deployer);
+    const toBalanceAfter = await gemToken.balanceOf(accounts.sandAdmin);
     expect(fromBalanceBefore).to.equal(fromBalanceAfter.add(amount));
     expect(toBalanceAfter).to.equal(toBalanceBefore.add(amount));
   });
   it('Transfering tokens to a non receiver contract should fail', async function () {
+    await createFixture();
     const accounts = await getNamedAccounts();
-    const {token} = await initContracts('MOCK', 'MOCK');
-    const emptyContract = await initContract(
-      'EmptyContract',
-      accounts.deployer,
-      []
-    );
-    const toBalanceBefore = await token.balanceOf(emptyContract.address);
+    const toBalanceBefore = await gemToken.balanceOf(emptyContract.address);
 
     const amount = BigNumber.from('100000000000000000');
     expect(
-      token
+      gemToken
         .connect(ethers.provider.getSigner(accounts.deployer))
         .transferAndCall(emptyContract.address, amount, Buffer.from('data'))
     ).to.be.revertedWith('');
-    const toBalanceAfter = await token.balanceOf(emptyContract.address);
+    const toBalanceAfter = await gemToken.balanceOf(emptyContract.address);
     expect(toBalanceAfter).to.equal(toBalanceBefore);
   });
   it('Transfering tokens to a contract with fallback function should succeed', async function () {
+    await createFixture();
     const accounts = await getNamedAccounts();
-    const {token} = await initContracts('MOCK', 'MOCK');
-    const fallbackContract = await initContract(
-      'FallBackContract',
-      accounts.deployer,
-      []
-    );
-    const toBalanceBefore = await token.balanceOf(fallbackContract.address);
-    const fromBalanceBefore = await token.balanceOf(accounts.deployer);
+    const toBalanceBefore = await gemToken.balanceOf(fallbackContract.address);
+    const fromBalanceBefore = await gemToken.balanceOf(accounts.deployer);
 
     const amount = BigNumber.from('100000000000000000');
-    token
+    gemToken
       .connect(ethers.provider.getSigner(accounts.deployer))
       .transferAndCall(fallbackContract.address, amount, Buffer.from('data'));
-    const fromBalanceAfter = await token.balanceOf(accounts.deployer);
-    const toBalanceAfter = await token.balanceOf(fallbackContract.address);
+    const fromBalanceAfter = await gemToken.balanceOf(accounts.deployer);
+    const toBalanceAfter = await gemToken.balanceOf(fallbackContract.address);
     expect(fromBalanceBefore).to.equal(fromBalanceAfter.add(amount));
     expect(toBalanceAfter).to.equal(toBalanceBefore.add(amount));
   });
