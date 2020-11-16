af0d8c0d0fec29d3110c3e2480b701e93c8659c9
Nick Furfaro <nfurfaro33@gmail.com>
2020-11-16 11:49:09 -0700

    Update the game token interface

--- src/solc_0.7/common/Interfaces/GameToken.sol
+++ src/solc_0.7/common/Interfaces/GameToken.sol
@@ -7,24 +7,47 @@ interface GameTokenInterface {
     event AssetsAdded(uint256 indexed id, uint24[] list);
     event AssetsRemoved(uint256 indexed id, uint256 numRemoved);
 
+    function getMinter() external view returns (address);
+
+    function setMinter(address minter) external;
+
     function createGame(
         address from,
         address to,
         uint256[] calldata assetIds,
+        uint256[] calldata values,
         address[] calldata editors
-    ) external;
+    ) external returns (uint256 id);
 
-    function addAssets(
+    function addSingleAsset(
         address from,
         uint256 gameId,
-        uint256[] calldata assetIds
+        uint256 assetId
     ) external;
 
-    function removeAssets(
+    function addMultipleAssets(
         address from,
         uint256 gameId,
-        uint256[] calldata assetIds
+        uint256[] memory assetIds,
+        uint256[] memory values
     ) external;
+
+    function removeSingleAsset(
+        uint256 gameId,
+        uint256 assetId,
+        address to
+    ) external;
+
+    function removeMultipleAssets(
+        uint256 gameId,
+        uint256[] calldata assetIds,
+        uint256[] calldata values,
+        address to
+    ) external;
+
+    function getNumberOfAssets(uint256 gameId) external view returns (uint256);
+
+    function getGameAssets(uint256 gameId) external view returns (uint256[] memory, uint256[] memory);
 
     function setGameEditor(
         uint256 gameId,
@@ -32,21 +55,39 @@ interface GameTokenInterface {
         bool isEditor
     ) external;
 
-    function creatorOf(uint256 id) external view returns (address);
+    function isGameEditor(uint256 gameId, address editor) external view returns (bool isEditor);
 
-    function transferCreatorship(
-        address sender,
-        address original,
-        address to
-    ) external;
+    // @review re-enable this !!!
+    // function creatorOf(uint256 id) external view returns (address);
 
-    function isGameEditor(uint256 gameId, address editor) external returns (bool isEditor);
+    // @review re-enable this !!!
+    // function transferCreatorship(
+    //     address sender,
+    //     address original,
+    //     address to
+    // ) external;
 
     function name() external pure returns (string memory);
 
     function symbol() external pure returns (string memory);
 
-    function tokenURI(uint256 gameId) external view returns (string memory tokenURI);
+    function tokenURI(uint256 gameId) external view returns (string memory uri);
 
     function setTokenURI(uint256 gameId, string calldata URI) external;
+
+    function onERC1155Received(
+        address, /*operator*/
+        address, /*from*/
+        uint256, /*id*/
+        uint256, /*value*/
+        bytes calldata /*data*/
+    ) external pure returns (bytes4);
+
+    function onERC1155BatchReceived(
+        address, /*operator*/
+        address, /*from*/
+        uint256[] calldata, /*ids*/
+        uint256[] calldata, /*values*/
+        bytes calldata /*data*/
+    ) external view returns (bytes4);
 }
