27766de3ef20bc3b7e8a3f5aa6a5f4bed5796bda
Nick Furfaro <nfurfaro33@gmail.com>
2020-11-17 11:24:53 -0700

    Remove unneeded file import from deploy script

--- deploy/0075_deploy_gameToken.ts
+++ deploy/0075_deploy_gameToken.ts
@@ -1,7 +1,6 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const func: DeployFunction = async function (hre) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
   const {deployer, gameTokenAdmin} = await getNamedAccounts();
