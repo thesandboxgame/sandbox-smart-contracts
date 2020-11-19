47aa3088e193dd21bb0c0cec837ff0e7d2708710
Nick Furfaro <nfurfaro33@gmail.com>
2020-11-19 06:42:30 -0700

    Add data712.ts

---
+++ test/Game/data712.ts
@@ -0,0 +1,131 @@
+type Message = {
+  to: string | undefined;
+  data: string | undefined;
+  value: string;
+  from: string;
+  nonce: string;
+  gas: number;
+};
+
+type Contract = {
+  address: string;
+};
+
+type Data712 = {
+  types: {
+    EIP712DomainType: [
+      {
+        name: 'name';
+        type: 'string';
+      },
+      {
+        name: 'version';
+        type: 'string';
+      },
+      {
+        name: 'chainId';
+        type: 'uint256';
+      },
+      {
+        name: 'verifyingContract';
+        type: 'address';
+      }
+    ];
+    ForwardRequestType: [
+      {
+        name: 'from';
+        type: 'address';
+      },
+      {
+        name: 'to';
+        type: 'address';
+      },
+      {
+        name: 'value';
+        type: 'uint256';
+      },
+      {
+        name: 'gas';
+        type: 'uint256';
+      },
+      {
+        name: 'nonce';
+        type: 'uint256';
+      },
+      {
+        name: 'data';
+        type: 'bytes';
+      }
+    ];
+  };
+  primaryType: 'ForwardRequest';
+  domain: {
+    name: 'The Sandbox';
+    version: '1';
+    chainId: '1234';
+    verifyingContract: string;
+  };
+  message: Message;
+};
+
+export const data712 = function (
+  verifyingContract: Contract,
+  message: Message
+): Data712 {
+  return {
+    types: {
+      EIP712DomainType: [
+        {
+          name: 'name',
+          type: 'string',
+        },
+        {
+          name: 'version',
+          type: 'string',
+        },
+        {
+          name: 'chainId',
+          type: 'uint256',
+        },
+        {
+          name: 'verifyingContract',
+          type: 'address',
+        },
+      ],
+      ForwardRequestType: [
+        {
+          name: 'from',
+          type: 'address',
+        },
+        {
+          name: 'to',
+          type: 'address',
+        },
+        {
+          name: 'value',
+          type: 'uint256',
+        },
+        {
+          name: 'gas',
+          type: 'uint256',
+        },
+        {
+          name: 'nonce',
+          type: 'uint256',
+        },
+        {
+          name: 'data',
+          type: 'bytes',
+        },
+      ],
+    },
+    primaryType: 'ForwardRequest',
+    domain: {
+      name: 'The Sandbox',
+      version: '1',
+      chainId: '1234',
+      verifyingContract: verifyingContract.address,
+    },
+    message: message,
+  };
+};
