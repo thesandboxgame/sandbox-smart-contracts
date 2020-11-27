15f3c39cdf5acf32fdaa459ebb54a28ada057088
CloverGuy <arthemisP@protonmail.com>
2020-11-27 10:47:40 +0200

    formatting according to standard

--- src/solc_0.7/catalyst/CatalystToken.sol
+++ src/solc_0.7/catalyst/CatalystToken.sol
@@ -7,7 +7,6 @@ import "./ERC20Token.sol";
 
 abstract contract CatalystToken is ERC20Token {
     uint16 public immutable catalystId;
-    uint8 internal immutable _maxGems;
 
     function getMaxGems() external view virtual returns (uint8);
 
@@ -16,6 +15,12 @@ abstract contract CatalystToken is ERC20Token {
         view
         virtual
         returns (uint32[] memory values);
+
+    // //////////////////////// DATA /////////////////////
+
+    uint8 internal immutable _maxGems;
+
+    // /////////////////// CONSTRUCTOR ////////////////////
 
     constructor(
         string memory name,
