9b49978842331f23d3d364b2b13e91809b0a0a6e
Wighawag <wighawag@gmail.com>
2020-12-01 14:24:00 +0000

    fix solhint warning

--- src/solc_0.7/Base/TheSandbox712.sol
+++ src/solc_0.7/Base/TheSandbox712.sol
@@ -5,6 +5,7 @@ contract TheSandbox712 {
     bytes32 internal constant EIP712DOMAIN_TYPEHASH = keccak256(
         "EIP712Domain(string name,string version,address verifyingContract)"
     );
+    // solhint-disable-next-line var-name-mixedcase
     bytes32 public immutable DOMAIN_SEPARATOR;
 
     constructor() {
