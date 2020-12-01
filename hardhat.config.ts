519e5465b0b0788d7b52488bb882fa15225cd91a
atkinsonholly <atkinsonholly@gmail.com>
2020-12-01 17:51:11 +0000

    disable ts warning no-explicit-any in smock subtask section

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -212,6 +212,7 @@ import {subtask} from 'hardhat/config';
 import {TASK_COMPILE_SOLIDITY_GET_ARTIFACT_FROM_COMPILATION_OUTPUT} from 'hardhat/builtin-tasks/task-names';
 subtask(
   TASK_COMPILE_SOLIDITY_GET_ARTIFACT_FROM_COMPILATION_OUTPUT,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async ({contractOutput}: {contractOutput: any}, __, runSuper) => {
     const artifact = await runSuper();
     artifact.storageLayout = contractOutput.storageLayout;
