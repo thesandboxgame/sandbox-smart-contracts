177d233eb6226c5d6140cd055c0ae05a09528d6e
Wighawag <wighawag@gmail.com>
2020-12-01 14:08:04 +0000

    test warning failure

--- src/solc_0.7/Base/TheSandbox712.sol
+++ src/solc_0.7/Base/TheSandbox712.sol
@@ -5,7 +5,6 @@ contract TheSandbox712 {
     bytes32 internal constant EIP712DOMAIN_TYPEHASH = keccak256(
         "EIP712Domain(string name,string version,address verifyingContract)"
     );
-    // solhint-disable-next-line var-name-mixedcase
     bytes32 public immutable DOMAIN_SEPARATOR;
 
     constructor() {
