3ca9e38a8c552959b61937dc0a80629561a07316
Nick Furfaro <nfurfaro33@gmail.com>
2020-12-03 06:22:09 -0700

    Add IGameManager

---
+++ src/solc_0.7/common/Interfaces/IGameManager.sol
@@ -0,0 +1,52 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.7.1;
+
+interface IGameManager {
+    function createGame(
+        address from,
+        address to,
+        uint256[] memory assetIds,
+        uint256[] memory values,
+        address[] memory editors,
+        string memory uri,
+        uint96 randomId
+    ) external returns (uint256 gameId);
+
+    function addAssets(
+        address from,
+        uint256 gameId,
+        uint256[] memory assetIds,
+        uint256[] memory values,
+        string memory uri
+    ) external;
+
+    function removeAssets(
+        uint256 gameId,
+        uint256[] memory assetIds,
+        uint256[] memory values,
+        address to,
+        string memory uri
+    ) external;
+
+    function destroyAndRecover(
+        address from,
+        address to,
+        uint256 gameId,
+        uint256[] calldata assetIds,
+        uint256[] calldata values
+    ) external;
+
+    function destroyGame(
+        address from,
+        address to,
+        uint256 gameId
+    ) external;
+
+    function recoverAssets(
+        address from,
+        address to,
+        uint256 gameId,
+        uint256[] memory assetIds,
+        uint256[] memory values
+    ) external;
+}
