4ab3e794707f227c37ffe0c17aa43006f38a12c0
Nick Furfaro <nfurfaro33@gmail.com>
2020-12-04 11:19:34 -0700

    Add function documentation to GameManager contract

--- src/solc_0.7/GameManager/GameManager.sol
+++ src/solc_0.7/GameManager/GameManager.sol
@@ -41,7 +41,14 @@ contract GameManager is WithMetaTransaction, IGameManager {
         _admin = admin;
     }
 
-    // @review do we want to support metaTxs for these functions?
+    /// @notice Function to create a new GAME token
+    /// @param from The address of the one creating the game, included in the gameId
+    /// @param to The address who will be assigned ownership of this game
+    /// @param assetIds The ids of the assets to add to this game
+    /// @param values the amount of each token id to add to game
+    /// @param editors The addresses to allow to edit (can also be set later)
+    /// @param randomId A random id created on the backend.
+    /// @return id The id of the new GAME token (erc721)
     function createGame(
         address from,
         address to,
@@ -55,6 +62,12 @@ contract GameManager is WithMetaTransaction, IGameManager {
       gameToken.createGame(from, to, assetIds, values, editors,uri, randomId);
     }
 
+    /// @notice Function to add assets to an existing GAME
+    /// @param from The address of the current owner of assets
+    /// @param gameId The id of the GAME to add asset to
+    /// @param assetIds The id of the asset to add to GAME
+    /// @param values The amount of each asset to add to GAME
+    /// @param uri The new uri to set
     function addAssets(
         address from,
         uint256 gameId,
@@ -66,7 +79,12 @@ contract GameManager is WithMetaTransaction, IGameManager {
         gameToken.addAssets(from, gameId, assetIds, values, uri);
     }
 
-    // @review on gameToken, should this func have a "from" param ?
+    /// @notice Function to remove assets from a GAME
+    /// @param gameId The GAME to remove assets from
+    /// @param assetIds An array of asset Ids to remove
+    /// @param values An array of the number of each asset id to remove
+    /// @param to The address to send removed assets to
+    /// @param uri The URI string to update the GAME token's URI
     function removeAssets(
         address from,
         uint256 gameId,
