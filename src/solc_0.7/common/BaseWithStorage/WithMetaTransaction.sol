e3564917460ba0380ed04415abae9b5725e4a49e
Nick Furfaro <nfurfaro33@gmail.com>
2020-12-07 16:20:00 -0700

    Update errors to snake_case in WithMetaTransaction

--- src/solc_0.7/common/BaseWithStorage/WithMetaTransaction.sol
+++ src/solc_0.7/common/BaseWithStorage/WithMetaTransaction.sol
@@ -17,7 +17,7 @@ contract WithMetaTransaction is WithAdmin {
     /// @param metaTransactionProcessor address that will be given/removed metaTransactionProcessor rights.
     /// @param processorType set the metaTransactionProcessor type
     function setMetaTransactionProcessor(address metaTransactionProcessor, uint8 processorType) public {
-        require(msg.sender == _admin, "only admin can setup metaTransactionProcessors");
+        require(msg.sender == _admin, "SET_PROCESSOR_ACCESS_DENIED");
         _setMetaTransactionProcessor(metaTransactionProcessor, processorType);
     }
 
