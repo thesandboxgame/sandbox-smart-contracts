4d6f9e288bab3b8da6b7f2b12a1bc4465813966e
Nick Furfaro <nfurfaro33@gmail.com>
2020-12-09 11:16:57 -0700

    Natspec revisions & additions

--- src/solc_0.7/GameToken/GameToken.sol
+++ src/solc_0.7/GameToken/GameToken.sol
@@ -29,10 +29,34 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
 
     ///////////////////////////////  Events //////////////////////////////
 
+    /// @dev Emits when assets are added to a game.
+    /// @param id The id of the erc721 GAME token.
+    /// @param assets An array of asset Ids.
+    /// @param values An array of asset values.
     event AssetsAdded(uint256 indexed id, uint256[] assets, uint256[] values);
+
+    /// @dev Emits when assets are removed from a game.
+    /// @param id The id of the erc721 GAME token.
+    /// @param assets An array of asset Ids.
+    /// @param values An array of asset values.
+    /// @param to The receiving address for the removed assets.
     event AssetsRemoved(uint256 indexed id, uint256[] assets, uint256[] values, address to);
+
+    /// @dev Emits when creatorship of a GAME token is transferred.
+    /// @param original The original creator of the GAME token.
+    /// @param from The current 'creator' of the token.
+    /// @param to The new 'creator' of the token.
     event CreatorshipTransfer(address indexed original, address indexed from, address indexed to);
+
+    /// @dev Emits when an address has its gameEditor status changed.
+    /// @param id The original creator of the GAME token.
+    /// @param gameEditor The address whose editor rights to update.
+    /// @param isEditor WHether the address 'gameEditor' should be an editor.
     event GameEditorSet(uint256 indexed id, address gameEditor, bool isEditor);
+
+    /// @dev Emits when a GAME token has it's metadata URI modified.
+    /// @param id The GAME token to update metadata URI for.
+    /// @param uri The new URI to set for the token.
     event TokenURIChanged(uint256 indexed id, string uri);
 
     constructor(
@@ -59,9 +83,9 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
 
     ///////////////////////////////  Functions //////////////////////////////
 
-    /// @notice Function to get the amount of each assetId in a GAME
-    /// @param gameId The game to query
-    /// @param assetIds The assets to get balances for
+    /// @notice Get the amount of each assetId in a GAME.
+    /// @param gameId The game to query.
+    /// @param assetIds The assets to get balances for.
     function getAssetBalances(uint256 gameId, uint256[] calldata assetIds)
         external
         view
@@ -77,11 +101,11 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         return assets;
     }
 
-    /// @notice Function to allow token owner to set game editors
-    /// @param from The address of the one creating the game (may be different from msg.sender if metaTx)
-    /// @param gameId The id of the GAME token owned by owner
-    /// @param editor The address of the editor to set
-    /// @param isEditor Add or remove the ability to edit
+    /// @notice Allow token owner to set game editors.
+    /// @param from The address of the one creating the game (may be different from msg.sender if metaTx).
+    /// @param gameId The id of the GAME token owned by owner.
+    /// @param editor The address of the editor to set.
+    /// @param isEditor Add or remove the ability to edit.
     function setGameEditor(
         address from,
         uint256 gameId,
@@ -93,9 +117,9 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
     }
 
     /// @notice Transfers creatorship of `original` from `sender` to `to`.
-    /// @param sender address of current registered creator.
-    /// @param original address of the original creator whose creation are saved in the ids themselves.
-    /// @param to address which will be given creatorship for all tokens originally minted by `original`.
+    /// @param sender The address of current registered creator.
+    /// @param original The address of the original creator whose creation are saved in the ids themselves.
+    /// @param to The address which will be given creatorship for all tokens originally minted by `original`.
     function transferCreatorship(
         address sender,
         address original,
@@ -120,14 +144,14 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         emit CreatorshipTransfer(original, current, to);
     }
 
-    /// @notice Function to create a new GAME token
-    /// @param from The address of the one creating the game (may be different from msg.sender if metaTx)
-    /// @param to The address who will be assigned ownership of this game
-    /// @param assetIds The ids of the assets to add to this game
-    /// @param values the amount of each token id to add to game
-    /// @param editor The address to allow to edit (can also be set later)
+    /// @notice Create a new GAME token.
+    /// @param from The address of the one creating the game (may be different from msg.sender if metaTx).
+    /// @param to The address who will be assigned ownership of this game.
+    /// @param assetIds The ids of the assets to add to this game.
+    /// @param values the amount of each token id to add to game.
+    /// @param editor The address to allow to edit (can also be set later).
     /// @param randomId A random id created on the backend.
-    /// @return id The id of the new GAME token (erc721)
+    /// @return id The id of the new GAME token (erc721).
     function createGame(
         address from,
         address to,
@@ -151,14 +175,17 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         return gameId;
     }
 
-    /// @notice Function to get game editor status
-    /// @param gameId The id of the GAME token owned by owner
-    /// @param editor The address of the editor to set
-    /// @return isEditor Editor status of editor for given tokenId
+    /// @notice Get game editor status.
+    /// @param gameId The id of the GAME token owned by owner.
+    /// @param editor The address of the editor to set.
+    /// @return isEditor Editor status of editor for given tokenId.
     function isGameEditor(uint256 gameId, address editor) external view override returns (bool isEditor) {
         return _gameEditors[gameId][editor];
     }
 
+    /// @notice Called by other contracts to check if this can receive erc1155 batch.
+    /// @param operator The address of the operator in the current tx.
+    /// @return the bytes4 value 0xbc197c81.
     function onERC1155BatchReceived(
         address operator,
         address, /*from*/
@@ -172,6 +199,9 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         revert("ERC1155_BATCH_REJECTED");
     }
 
+    /// @notice Called by other contracts to check if this can receive erc1155 tokens.
+    /// @param operator The address of the operator in the current tx.
+    /// @return the bytes4 value 0xf23a6e61.
     function onERC1155Received(
         address operator,
         address, /*from*/
@@ -185,30 +215,30 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         revert("ERC1155_REJECTED");
     }
 
-    /// @notice Return the name of the token contract
-    /// @return The name of the token contract
+    /// @notice Return the name of the token contract.
+    /// @return The name of the token contract.
     function name() external pure override returns (string memory) {
         return "The Sandbox: GAME token";
     }
 
-    /// @notice Function to get the symbol of the token contract
-    /// @return The symbol of the token contract
+    /// @notice Get the symbol of the token contract.
+    /// @return the symbol of the token contract.
     function symbol() external pure override returns (string memory) {
         return "GAME";
     }
 
-    /// @notice Set the URI of a specific game token
-    /// @param gameId The id of the game token
-    /// @param uri The uri string for the token's metadata
+    /// @notice Set the URI of a specific game token.
+    /// @param gameId The id of the game token.
+    /// @param uri The uri string for the token's metadata.
     function setTokenURI(uint256 gameId, string calldata uri) external override onlyMinter() {
         _setTokenURI(gameId, uri);
     }
 
-    /// @notice Function to burn a GAME token and recover assets
-    /// @param from The address of the one destroying the game
-    /// @param to The address to send all GAME assets to
-    /// @param gameId The id of the GAME to destroy
-    /// @param assetIds The assets to recover from the burnt GAME
+    /// @notice Burn a GAME token and recover assets.
+    /// @param from The address of the one destroying the game.
+    /// @param to The address to send all GAME assets to.
+    /// @param gameId The id of the GAME to destroy.
+    /// @param assetIds The assets to recover from the burnt GAME.
     function destroyAndRecover(
         address from,
         address to,
@@ -219,10 +249,10 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         _recoverAssets(from, to, gameId, assetIds);
     }
 
-    /// @notice Function to burn a GAME token
-    /// @param from The address of the one destroying the game
-    /// @param to The address to send all GAME assets to
-    /// @param gameId The id of the GAME to destroy
+    /// @notice Burn a GAME token.
+    /// @param from The address of the one destroying the game.
+    /// @param to The address to send all GAME assets to.
+    /// @param gameId The id of the GAME to destroy.
     function destroyGame(
         address from,
         address to,
@@ -231,12 +261,12 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         _destroyGame(from, to, gameId);
     }
 
-    /// @notice Function to add assets to an existing GAME
-    /// @param from The address of the current owner of assets
-    /// @param gameId The id of the GAME to add asset to
-    /// @param assetIds The id of the asset to add to GAME
-    /// @param values The amount of each asset to add to GAME
-    /// @param uri The new uri to set
+    /// @notice Add assets to an existing GAME.
+    /// @param from The address of the current owner of assets.
+    /// @param gameId The id of the GAME to add asset to.
+    /// @param assetIds The id of the asset to add to GAME.
+    /// @param values The amount of each asset to add to GAME.
+    /// @param uri The new uri to set.
     function addAssets(
         address from,
         uint256 gameId,
@@ -259,13 +289,12 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         emit AssetsAdded(gameId, assetIds, values);
     }
 
-    /// @notice Function to remove assets from a GAME
-    /// @param gameId The GAME to remove assets from
-    /// @param assetIds An array of asset Ids to remove
-    /// @param values An array of the number of each asset id to remove
-    /// @param to The address to send removed assets to
-    /// @param uri The URI string to update the GAME token's URI
-
+    /// @notice Remove assets from a GAME.
+    /// @param gameId The GAME to remove assets from.
+    /// @param assetIds An array of asset Ids to remove.
+    /// @param values An array of the number of each asset id to remove.
+    /// @param to The address to send removed assets to.
+    /// @param uri The URI string to update the GAME token's URI.
     function removeAssets(
         uint256 gameId,
         uint256[] memory assetIds,
@@ -292,7 +321,7 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
     }
 
     /// @notice Get the creator of the token type `id`.
-    /// @param id the id of the token to get the creator of.
+    /// @param id The id of the token to get the creator of.
     /// @return the creator of the token type `id`.
     function creatorOf(uint256 id) public view override returns (address) {
         require(id != uint256(0), "GAME_NEVER_MINTED");
@@ -304,19 +333,19 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         return originalCreator;
     }
 
-    /// @notice Return the URI of a specific token
-    /// @param gameId The id of the token
-    /// @return uri The URI of the token
+    /// @notice Return the URI of a specific token.
+    /// @param gameId The id of the token.
+    /// @return uri The URI of the token.
     function tokenURI(uint256 gameId) public view override returns (string memory uri) {
         require(_ownerOf(gameId) != address(0), "BURNED_OR_NEVER_MINTED");
         return _metaData[gameId];
     }
 
-    /// @notice Function to transfer assets from a burnt GAME
-    /// @param from Previous owner of the burnt game
-    /// @param to Address that will receive the assets
-    /// @param gameId Id of the burnt GAME token
-    /// @param assetIds The assets to recover from the burnt GAME
+    /// @notice Transfer assets from a burnt GAME.
+    /// @param from Previous owner of the burnt game.
+    /// @param to Address that will receive the assets.
+    /// @param gameId Id of the burnt GAME token.
+    /// @param assetIds The assets to recover from the burnt GAME.
     function recoverAssets(
         address from,
         address to,
@@ -326,22 +355,22 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         _recoverAssets(from, to, gameId, assetIds);
     }
 
-    /**
-     * @notice Check if the contract supports an interface
-     * 0x01ffc9a7 is ERC-165
-     * 0x80ac58cd is ERC-721
-     * @param id The id of the interface
-     * @return True if the interface is supported
-     */
+    /// @notice Check if the contract supports an interface.
+    /// 0x01ffc9a7 is ERC-165.
+    /// 0x80ac58cd is ERC-721.
+    /// @param id The id of the interface.
+    /// @return if the interface is supported.
     function supportsInterface(bytes4 id) public pure override returns (bool) {
         return id == 0x01ffc9a7 || id == 0x80ac58cd || id == 0x5b5e139f;
     }
 
+    /// @dev See setTokenURI.
     function _setTokenURI(uint256 gameId, string memory uri) internal {
         _metaData[gameId] = uri;
         emit TokenURIChanged(gameId, uri);
     }
 
+    /// @dev See destroyGame.
     function _destroyGame(
         address from,
         address to,
@@ -355,6 +384,7 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         _burn(from, owner, gameId);
     }
 
+    /// @dev See recoverAssets.
     function _recoverAssets(
         address from,
         address to,
@@ -378,11 +408,20 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         emit AssetsRemoved(gameId, assetIds, values, to);
     }
 
+    /// @dev Check if a withdrawal is allowed.
+    /// @param from The address requesting the withdrawal.
+    /// @param gameId The id of the GAME token to withdraw assets from.
     function _check_withdrawal_authorized(address from, uint256 gameId) internal view {
         require(from != address(0), "SENDER_ZERO_ADDRESS");
         require(from == _withdrawalOwnerOf(gameId), "LAST_OWNER_NOT_EQUAL_SENDER");
     }
 
+    /// @dev Get the address allowed to withdraw assets from the GAME token.
+    /// If too many assets in GAME, block.gaslimit won't allow detroy and withdraw in 1 tx.
+    /// A game owner may destroy their GAME token, then withdraw assets in a later tx (even
+    /// though ownerOf(id) would be address(0) after burning.)
+    /// @param id The id of the GAME token to query.
+    /// @return the address of the owner before burning.
     function _withdrawalOwnerOf(uint256 id) internal view returns (address) {
         uint256 data = _owners[id];
         if ((data & (2**160)) == 2**160) {
@@ -391,11 +430,11 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         return address(0);
     }
 
-    /// @dev Function to create a new gameId and associate it with an owner
-    /// @param from The address of the Game creator
-    /// @param to The address of the Game owner
-    /// @param randomId The id to use when generating the new GameId
-    /// @return id The newly created gameId
+    /// @dev Create a new gameId and associate it with an owner.
+    /// @param from The address of the Game creator.
+    /// @param to The address of the Game owner.
+    /// @param randomId The id to use when generating the new GameId.
+    /// @return id The newly created gameId.
     function _mintGame(
         address from,
         address to,
@@ -408,17 +447,17 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         return gameId;
     }
 
-    /// @dev Function to create a new gameId and associate it with an owner
-    /// @param creator The address of the Game creator
-    /// @param randomId The id to use when generating the new GameId
+    /// @dev Create a new gameId and associate it with an owner.
+    /// @param creator The address of the Game creator.
+    /// @param randomId The id to use when generating the new GameId.
     function generateGameId(address creator, uint96 randomId) internal pure returns (uint256) {
         return uint256(creator) * CREATOR_OFFSET_MULTIPLIER + uint96(randomId);
     }
 
-    /// @dev Function to allow token owner to set game editors
-    /// @param gameId The id of the GAME token owned by owner
-    /// @param editor The address of the editor to set
-    /// @param isEditor Add or remove the ability to edit
+    /// @dev Allow token owner to set game editors.
+    /// @param gameId The id of the GAME token owned by owner.
+    /// @param editor The address of the editor to set.
+    /// @param isEditor Add or remove the ability to edit.
     function _setGameEditor(
         uint256 gameId,
         address editor,
