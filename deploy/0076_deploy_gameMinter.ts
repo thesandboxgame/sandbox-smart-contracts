38d66f863a6cffec19f0059cc259168500776ec3
Nick Furfaro <nfurfaro33@gmail.com>
2020-12-09 14:15:24 -0700

    Rename deploy script for GameMinter

--- deploy/0076_deploy_gameMinter.ts
+++ deploy/0076_deploy_gameMinter.ts
@@ -1,16 +1,23 @@
 import {DeployFunction} from 'hardhat-deploy/types';
+import {gameMintingFee, gameModificationFee} from '../data/gameMinterFees';
 
 const func: DeployFunction = async function (hre) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
-  const {deployer, gameMinterAdmin} = await getNamedAccounts();
+  const {deployer, mintingFeeCollector} = await getNamedAccounts();
   const gameContract = await deployments.get('GameToken');
   const sandContract = await deployments.get('Sand');
 
   await deploy('GameMinter', {
     from: deployer,
     log: true,
-    args: [gameContract.address, gameMinterAdmin, sandContract.address],
+    args: [
+      gameContract.address,
+      sandContract.address,
+      gameMintingFee,
+      gameModificationFee,
+      mintingFeeCollector,
+    ],
   });
 };
 
