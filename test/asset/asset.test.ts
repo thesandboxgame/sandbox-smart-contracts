b7058028ce03fb8e699488943b2774464ed4dfc8
Wighawag <wighawag@gmail.com>
2021-01-04 23:39:57 +0000

    add test for owner to owner balance expansion

---
+++ test/asset/asset.test.ts
@@ -0,0 +1,24 @@
+import {setupAsset} from './fixtures';
+import {waitFor} from '../utils';
+import {expect} from '../chai-setup';
+
+describe('Asset.sol', function () {
+  it('user sending asset to itself keep the same balance', async function () {
+    const {Asset, users, mintAsset} = await setupAsset();
+    const tokenId = await mintAsset(users[0].address, 20);
+    await waitFor(
+      users[0].Asset.safeBatchTransferFrom(
+        users[0].address,
+        users[0].address,
+        [tokenId],
+        [10],
+        '0x'
+      )
+    );
+    const balance = await Asset['balanceOf(address,uint256)'](
+      users[0].address,
+      tokenId
+    );
+    expect(balance).to.be.equal(20);
+  });
+});
--- test/utils.ts
+++ test/utils.ts
@@ -105,3 +105,19 @@ export function waitFor(
 ): Promise<ContractReceipt> {
   return p.then((tx) => tx.wait());
 }
+
+export async function setupUsers<T extends {[contractName: string]: Contract}>(
+  addresses: string[],
+  contracts: T
+): Promise<({address: string} & T)[]> {
+  const users: ({address: string} & T)[] = [];
+  for (const address of addresses) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const user: any = {address};
+    for (const key of Object.keys(contracts)) {
+      user[key] = contracts[key].connect(await ethers.getSigner(address));
+    }
+    users.push(user);
+  }
+  return users;
+}
