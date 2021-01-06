a101af4e817eae12ab4ddc9b1581cf4a9dadcca1
Nick Furfaro <nfurfaro33@gmail.com>
2021-01-06 11:09:12 -0700

    Add conditional to call to _burn in GameToken._bumpGameVersion

--- src/solc_0.7/Game/GameToken.sol
+++ src/solc_0.7/Game/GameToken.sol
@@ -504,7 +504,14 @@ contract GameToken is ERC721BaseToken, WithMinter, IGameToken {
         uint32 version = uint32(gameId);
         version++;
         address owner = _ownerOf(gameId);
-        _burn(from, owner, gameId);
+        if (from == owner) {
+            // caller is owner or metaTx on owner's behalf
+            _burn(from, owner, gameId);
+        } else if (_gameEditors[owner][from]) {
+            // caller is editor or metaTx on editor's behalf, so we need to pass owner
+            // instead of from or _burn will fail
+            _burn(owner, owner, gameId);
+        }
         (uint256 newId, ) = _mintGame(originalCreator, owner, subId, version, false);
         address newOwner = _ownerOf(newId);
         assert(owner == newOwner);
