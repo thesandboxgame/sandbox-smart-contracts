4c2240e6ad0b191fe055b986b4151b8cf9411763
Nick Furfaro <nfurfaro33@gmail.com>
2021-01-13 12:05:12 -0700

    Restore Asset

--- src/solc_0.5/Asset.sol
+++ src/solc_0.5/Asset.sol
@@ -3,11 +3,9 @@ pragma solidity 0.5.9;
 import "./Asset/ERC1155ERC721.sol";
 
 contract Asset is ERC1155ERC721 {
-    function init(
+    constructor(
         address metaTransactionContract,
         address assetAdmin,
         address bouncerAdmin
-    ) public {
-      ERC1155ERC721.init(metaTransactionContract, assetAdmin, bouncerAdmin);
-    }
+    ) public ERC1155ERC721(metaTransactionContract, assetAdmin, bouncerAdmin) {}
 }
