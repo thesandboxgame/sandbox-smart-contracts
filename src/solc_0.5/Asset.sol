1bc0194d27fedd9bc63f6bc132beecc1f0a2f450
Nick Furfaro <nfurfaro33@gmail.com>
2021-01-13 06:33:50 -0700

    Add function keyword

--- src/solc_0.5/Asset.sol
+++ src/solc_0.5/Asset.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.9;
 import "./Asset/ERC1155ERC721.sol";
 
 contract Asset is ERC1155ERC721 {
-    init(
+    function init(
         address metaTransactionContract,
         address assetAdmin,
         address bouncerAdmin
