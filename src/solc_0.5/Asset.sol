aca0da3d37e0823229af975969fbacb7b65f5c81
Nick Furfaro <nfurfaro33@gmail.com>
2021-01-13 09:54:13 -0700

    Add _init var to Asset.sol

--- src/solc_0.5/Asset.sol
+++ src/solc_0.5/Asset.sol
@@ -3,11 +3,15 @@ pragma solidity 0.5.9;
 import "./Asset/ERC1155ERC721.sol";
 
 contract Asset is ERC1155ERC721 {
+    bool internal _init;
+
     function init(
         address metaTransactionContract,
         address assetAdmin,
         address bouncerAdmin
     ) public {
+      require(!_init, "ALREADY_INITIALISED");
+        _init = true;
       ERC1155ERC721.init_ERC1155ERC721(metaTransactionContract, assetAdmin, bouncerAdmin);
     }
 }
