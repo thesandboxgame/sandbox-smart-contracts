c91c7cc3d55749aaefac02604c40866c61198fca
Nick Furfaro <nfurfaro33@gmail.com>
2021-02-18 11:07:38 -0700

    Add the IAssetAttributesRegistry.sol

--- src/solc_0.7/catalyst/interfaces/IAssetAttributesRegistry.sol
+++ src/solc_0.7/catalyst/interfaces/IAssetAttributesRegistry.sol
@@ -3,14 +3,41 @@ pragma solidity 0.7.5;
 pragma experimental ABIEncoderV2;
 
 interface IAssetAttributesRegistry {
+    struct GemEvent {
+        uint16[] gemIds;
+        bytes32 blockHash;
+    }
+
     function getRecord(uint256 assetId)
         external
         view
+        virtual
         returns (
             bool exists,
             uint16 catalystId,
             uint16[] memory gemIds
         );
 
-    function getAttributes(uint256 assetId, GemEvent[] calldata events) external view returns (uint32[] memory values);
+    function getAttributes(uint256 assetId, GemEvent[] calldata events)
+        external
+        view
+        virtual
+        returns (uint32[] memory values);
+
+    function setCatalyst(
+        uint256 assetId,
+        uint16 catalystId,
+        uint16[] calldata gemIds
+    ) external virtual;
+
+    function setCatalystWithBlockNumber(
+        uint256 assetId,
+        uint16 catalystId,
+        uint16[] calldata gemIds,
+        uint64 blockNumber
+    ) external virtual;
+
+    function addGems(uint256 assetId, uint16[] calldata gemIds) external virtual;
+
+    function setMigrationContract(address _migrationContract) external virtual;
 }
