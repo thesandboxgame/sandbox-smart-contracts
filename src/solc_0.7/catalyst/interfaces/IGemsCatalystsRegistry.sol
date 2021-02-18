7233f1ec3e4acec1f2b62cf65367f91c2dd90e7d
Nick Furfaro <nfurfaro33@gmail.com>
2021-02-18 11:56:22 -0700

    Add IGemsCatalystsRegistry.sol

---
+++ src/solc_0.7/catalyst/interfaces/IGemsCatalystsRegistry.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.7.5;
+pragma experimental ABIEncoderV2;
+
+interface IGemsCatalystsRegistry {
+    function getAttributes(
+        uint16 catalystId,
+        uint256 assetId,
+        IAssetAttributesRegistry.GemEvent[] calldata events
+    ) external view returns (uint32[] memory values);
+
+    function getMaxGems(uint16 catalystId) external view returns (uint8);
+
+    function burnDifferentGems(
+        address from,
+        uint16[] calldata gemIds,
+        uint256 amount
+    ) external;
+
+    function burnDifferentCatalysts(
+        address from,
+        uint16[] calldata catalystIds,
+        uint256 amount
+    ) external;
+
+    function batchBurnGems(
+        address from,
+        uint16[] calldata gemIds,
+        uint256[] calldata amounts
+    ) external;
+
+    function batchBurnCatalysts(
+        address from,
+        uint16[] calldata catalystIds,
+        uint256[] calldata amounts
+    ) external;
+
+    function addGemsAndCatalysts(Gem[] calldata gems, Catalyst[] calldata catalysts) external;
+
+    function doesGemExist(uint16 gemId) external view returns (bool);
+
+    function burnCatalyst(
+        address from,
+        uint16 catalystId,
+        uint256 amount
+    ) external;
+
+    function burnGem(
+        address from,
+        uint16 gemId,
+        uint256 amount
+    ) external;
+}
