4ce048b1b1196e0103a8b137e76012a3e2785d33
Nick Furfaro <nfurfaro33@gmail.com>
2021-02-23 06:19:52 -0700

    Update AssetUpgraderFeeBUrner deploy script w/ mock assetAttributesRgistry

--- deploy/13_test_deployments/9042_deploy_assetUpgreder_feeBurner.ts
+++ deploy/13_test_deployments/9042_deploy_assetUpgreder_feeBurner.ts
@@ -6,24 +6,35 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts, getChainId} = hre;
   const {deploy, execute, read} = deployments;
 
-  const AssetAttributesRegistry = await deployments.get(
-    'AssetAttributesRegistry'
-  );
   const Sand = await deployments.get('Sand');
   const Asset = await deployments.get('Asset');
   const GemsCatalystsRegistry = await deployments.get('GemsCatalystsRegistry');
 
-  const {deployer} = await getNamedAccounts();
+  const {deployer, assetAttributesRegistryAdmin} = await getNamedAccounts();
   const BURN_ADDRESS = '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF';
 
   // @note For testing fee-burning only
   const chainId = await getChainId();
   if (chainId == '31337') {
+    await deploy(`MockAssetAttributesRegistry`, {
+      from: deployer,
+      log: true,
+      args: [
+        GemsCatalystsRegistry.address,
+        assetAttributesRegistryAdmin,
+        assetAttributesRegistryAdmin,
+        assetAttributesRegistryAdmin,
+      ],
+    });
+
+    const MockAssetAttributesRegistry = await deployments.get(
+      'MockAssetAttributesRegistry'
+    );
     await deploy(`AssetUpgraderFeeBurner`, {
       from: deployer,
       log: true,
       args: [
-        AssetAttributesRegistry.address,
+        MockAssetAttributesRegistry.address,
         Sand.address,
         Asset.address,
         GemsCatalystsRegistry.address,
