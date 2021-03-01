1e775270ae4d9c68bc25232acaa7f25693874181
Nick Furfaro <nfurfaro33@gmail.com>
2021-03-01 06:19:08 -0700

    Refactor fixture to premint cats & gems

--- test/catalyst/assetAttributesRegistry/fixtures.ts
+++ test/catalyst/assetAttributesRegistry/fixtures.ts
@@ -27,6 +27,37 @@ export const setupAssetAttributesRegistry = deployments.createFixture(
       user0,
       BigNumber.from(100000).mul('1000000000000000000')
     );
+    const power: Contract = await ethers.getContract('Gem_POWER');
+    const defense: Contract = await ethers.getContract('Gem_DEFENSE');
+    const speed: Contract = await ethers.getContract('Gem_SPEED');
+    const magic: Contract = await ethers.getContract('Gem_MAGIC');
+    const luck: Contract = await ethers.getContract('Gem_LUCK');
+    const common: Contract = await ethers.getContract('Catalyst_COMMON');
+    const rare: Contract = await ethers.getContract('Catalyst_RARE');
+    const epic: Contract = await ethers.getContract('Catalyst_EPIC');
+    const legendary: Contract = await ethers.getContract('Catalyst_LEGENDARY');
+
+    const gemsCatalystsUnit = '1000000000000000000';
+    const mintingAmount = BigNumber.from('8').mul(
+      BigNumber.from(gemsCatalystsUnit)
+    );
+
+    const gemContracts = [power, defense, speed, luck, magic];
+    const catContracts = [common, rare, epic, legendary];
+
+    async function mintAllGems(user: string, amount: BigNumber): Promise<void> {
+      for (const contract of gemContracts) {
+        await mintGem(contract, amount, user);
+      }
+    }
+    async function mintAllCats(user: string, amount: BigNumber): Promise<void> {
+      for (const contract of catContracts) {
+        await mintCatalyst(contract, amount, user);
+      }
+    }
+
+    await mintAllGems(user0, mintingAmount);
+    await mintAllCats(user0, mintingAmount);
 
     const assetAttributesRegistryAsUser0 = await assetAttributesRegistry.connect(
       ethers.provider.getSigner(user0)
