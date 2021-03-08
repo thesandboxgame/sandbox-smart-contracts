97e2e20a2e3e535d495dba67ffdfabf83630e4d1
Nick Furfaro <nfurfaro33@gmail.com>
2021-03-08 13:15:05 -0700

    Add Asset contract & imports to v8 folder

--- src/solc_0.8/common/BaseWithStorage/WithAdmin.sol
+++ src/solc_0.8/common/BaseWithStorage/WithAdmin.sol
@@ -1,26 +1,30 @@
-pragma solidity ^0.5.2;
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
 
 contract WithAdmin {
     address internal _admin;
 
+    /// @dev Emits when the contract administrator is changed.
+    /// @param oldAdmin The address of the previous administrator.
+    /// @param newAdmin The address of the new administrator.
     event AdminChanged(address oldAdmin, address newAdmin);
 
-    /// @notice gives the current administrator of this contract.
-    /// @return the current administrator of this contract.
+    modifier onlyAdmin() {
+        require(msg.sender == _admin, "ADMIN_ONLY");
+        _;
+    }
+
+    /// @dev Get the current administrator of this contract.
+    /// @return The current administrator of this contract.
     function getAdmin() external view returns (address) {
         return _admin;
     }
 
-    /// @notice change the administrator to be `newAdmin`.
-    /// @param newAdmin address of the new administrator.
+    /// @dev Change the administrator to be `newAdmin`.
+    /// @param newAdmin The address of the new administrator.
     function changeAdmin(address newAdmin) external {
-        require(msg.sender == _admin, "only admin can change admin");
+        require(msg.sender == _admin, "ADMIN_ACCESS_DENIED");
         emit AdminChanged(_admin, newAdmin);
         _admin = newAdmin;
-    }
-
-    modifier onlyAdmin() {
-        require(msg.sender == _admin, "only admin allowed");
-        _;
     }
 }
