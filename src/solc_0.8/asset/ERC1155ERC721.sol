1e4fdd230c6cd79d173b79d5fb3a382ec757de45
Nick Furfaro <nfurfaro33@gmail.com>
2021-03-10 06:26:03 -0700

    Use onlyAdmin modifier

--- src/solc_0.8/asset/ERC1155ERC721.sol
+++ src/solc_0.8/asset/ERC1155ERC721.sol
@@ -113,8 +113,7 @@ contract ERC1155ERC721 is WithSuperOperators, IERC1155, IERC721 {
     /// @notice Enable or disable the ability of `metaTransactionProcessor` to perform meta-tx (metaTransactionProcessor rights).
     /// @param metaTransactionProcessor address that will be given/removed metaTransactionProcessor rights.
     /// @param enabled set whether the metaTransactionProcessor is enabled or disabled.
-    function setMetaTransactionProcessor(address metaTransactionProcessor, bool enabled) external {
-        require(msg.sender == _admin, "!ADMIN");
+    function setMetaTransactionProcessor(address metaTransactionProcessor, bool enabled) external onlyAdmin() {
         _metaTransactionContracts[metaTransactionProcessor] = enabled;
         emit MetaTransactionProcessor(metaTransactionProcessor, enabled);
     }
