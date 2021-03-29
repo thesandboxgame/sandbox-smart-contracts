cb4f77aefdc3fe970adcb907c6038ffcbc71a10c
Nick Furfaro <nfurfaro33@gmail.com>
2021-03-29 14:07:24 -0600

    Fix ObjectLib32.sol by adding unchecked block

--- src/solc_0.8/common/Libraries/ObjectLib32.sol
+++ src/solc_0.8/common/Libraries/ObjectLib32.sol
@@ -2,6 +2,7 @@
 // solhint-disable-next-line compiler-version
 pragma solidity 0.8.2;
 
+
 library ObjectLib32 {
     enum Operations {ADD, SUB, REPLACE}
     // Constants regarding bin or chunk sizes for balance packing
@@ -17,7 +18,9 @@ library ObjectLib32 {
     /// @return bin Bin number.
     /// @return index ID's index within that bin.
     function getTokenBinIndex(uint256 tokenId) internal pure returns (uint256 bin, uint256 index) {
-        bin = (tokenId * TYPES_BITS_SIZE) / 256;
+         unchecked {
+          bin = (tokenId * TYPES_BITS_SIZE) / 256;
+        }
         index = tokenId % TYPES_PER_UINT256;
         return (bin, index);
     }
