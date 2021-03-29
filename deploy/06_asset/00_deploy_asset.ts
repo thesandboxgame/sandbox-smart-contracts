7b8e736fb859dc88be77265b05033638de2ba24d
Nick Furfaro <nfurfaro33@gmail.com>
2021-03-29 15:35:30 -0600

    Update  proxy deployment to use hardhat-deploy w/ oz-transparentProxy

--- deploy/06_asset/00_deploy_asset.ts
+++ deploy/06_asset/00_deploy_asset.ts
@@ -1,100 +1,26 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import fs from 'fs-extra';
-import {DeployFunction, DeploymentSubmission} from 'hardhat-deploy/types';
-import {BigNumber} from '@ethersproject/bignumber';
-import {getAddress} from '@ethersproject/address';
+import {DeployFunction} from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
 ): Promise<void> {
-  const {deployments, getNamedAccounts, getChainId, upgrades, ethers} = hre;
+  const {deployments, getNamedAccounts} = hre;
   const {deployer} = await getNamedAccounts();
-  const {log} = deployments;
-
-  const chainId = await getChainId();
+  const {deploy} = deployments;
 
   const sandContract = await deployments.get('Sand');
 
-  const assetContract = await deployments.getOrNull('Asset');
-  if (assetContract) {
-    log(`Asset already deployed at ${assetContract.address}`);
-    return;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const Asset = await (ethers as any).getContractFactory('Asset', deployer); // TODO check types with hardhat-ethers and hardhat-deploy-ethers, for now use `any`
-
-  // Problem : cannot use deterministic deployment with openzepelin proxies ?
-  const asset = await upgrades.deployProxy(
-    Asset,
-    [sandContract.address, deployer, deployer],
-    {initializer: 'init', unsafeAllowCustomTypes: false}
-  );
-  await asset.deployed();
-  const implementationStorage = await ethers.provider.getStorageAt(
-    asset.address,
-    '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
-  );
-  const implementationAddress = getAddress(
-    BigNumber.from(implementationStorage).toHexString()
-  );
-
-  log(
-    `Asset deployed as Proxy at : ${asset.address}, implementation: ${implementationAddress}`
-  );
-
-  const AssetArtifact = await deployments.getExtendedArtifact('Asset');
-  const assetAsDeployment: DeploymentSubmission = {
-    address: asset.address,
-    ...AssetArtifact,
-    // TODO :transactionHash: transactionHash for Proxy deployment
-    // args ?
-    // linkedData ?
-    // receipt?
-    // libraries ?
-  };
-  await deployments.save('Asset', assetAsDeployment);
-
-  const assetImplementation = {
-    address: implementationAddress,
-    ...AssetArtifact,
-    // TODO :transactionHash: transactionHash for Proxy deployment
-    // args ?
-    // linkedData ?
-    // receipt?
-    // libraries ?
-  };
-  await deployments.save('Asset_Implementation', assetImplementation);
-
-  // TODO:
-  // await deployments.save('Asset_Proxy', assetProxy); // How do we access the Proxy abi/metadata from openzepeelin. if openzeppelin lib do not provide it, we will need to fetch manually from hre artifacts
-  // await deployments.save('Asset_ProxyAdmin', assetImplementation); // how do we access the Proxy abi/metadata from openzepeelin. if openzeppelin lib do not provide it, we will need to fetch manually from hre artifacts
-
-  // --------------------------------------------------
-  // Logging Proxy Admin
-  // --------------------------------------------------
-  try {
-    // Problems:
-    // - openzeppelin do not support multiple network name for same network chainId
-    let openzeppelinNetworkName = `unknown-${chainId}`;
-    switch (chainId) {
-      case '4':
-        openzeppelinNetworkName = 'rinkeby';
-        break;
-      case '1':
-        openzeppelinNetworkName = 'mainnet';
-        break;
-      // TODO more + move to lib
-    }
-    const openzeppelinFileName = `.openzeppelin/${openzeppelinNetworkName}.json`;
-    const networkFile = JSON.parse(
-      fs.readFileSync(openzeppelinFileName).toString()
-    );
-    const proxyAdminAddress = networkFile.admin.address;
-    log('Proxy Admin Contract deployed to:', proxyAdminAddress);
-  } catch (e) {
-    console.error(e);
-  }
+  await deploy('Asset', {
+    from: deployer,
+    contract: 'Asset',
+    args: [sandContract.address, deployer, deployer],
+    proxy: {
+      owner: deployer,
+      proxyContract: 'OpenZeppelinTransparentProxy',
+      methodName: 'init',
+    },
+    log: true,
+  });
 };
 
 export default func;
