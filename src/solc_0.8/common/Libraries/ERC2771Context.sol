295aa4299370abc64a54bcb21046b6c90f508ba4
Nick Furfaro <nfurfaro33@gmail.com>
2021-04-27 05:21:59 -0600

    Fixup

--- src/solc_0.8/common/Libraries/ERC2771Context.sol
+++ src/solc_0.8/common/Libraries/ERC2771Context.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-
+// solhint-disable-next-line compiler-version
 pragma solidity ^0.8.0;
 
 /// @dev upgradable variant of openzeppelin contract.
@@ -12,20 +12,21 @@ contract ERC2771Context {
       _trustedForwarder = forwarder;
     }
 
-    function isTrustedForwarder(address forwarder) public view virtual returns(bool) {
+    function isTrustedForwarder(address forwarder) public view returns(bool) {
         return forwarder == _trustedForwarder;
     }
 
-    function _msgSender() internal view virtual returns (address sender) {
+    function _msgSender() internal view returns (address sender) {
         if (isTrustedForwarder(msg.sender)) {
             // The assembly code is more direct than the Solidity version using `abi.decode`.
+            // solhint-disable-next-line no-inline-assembly
             assembly { sender := shr(96, calldataload(sub(calldatasize(), 20))) }
         } else {
             return msg.sender;
         }
     }
 
-    function _msgData() internal view virtual returns (bytes calldata) {
+    function _msgData() internal view returns (bytes calldata) {
         if (isTrustedForwarder(msg.sender)) {
             return msg.data[:msg.data.length-20];
         } else {
