7d1c5e3d75d7fba59e2b9a4291180feb7fe5cb93
Nick Furfaro <nfurfaro33@gmail.com>
2021-04-28 06:31:38 -0600

    temp: comment out extra mint functions

--- src/solc_0.8/asset/AssetV2.sol
+++ src/solc_0.8/asset/AssetV2.sol
@@ -8,26 +8,27 @@ import "./ERC1155ERC721.sol";
 contract AssetV2 is ERC1155ERC721 {
     // polygon-specific layer 1 functionality
     // IBurnableERC1155, IMintableERC1155
-    function mint(
-        address account,
-        uint256 id,
-        uint256 amount,
-        bytes calldata data
-    ) external {
-        // @review
-        // predicate-only !
-        // _mint(account, id, amount, data);
-    }
 
-    function mintBatch(
-        address to,
-        uint256[] calldata ids,
-        uint256[] calldata amounts,
-        bytes calldata data
-    ) external {
-      // @review
-        // predicate-only !
-        // _mintBatch(to, ids, amounts, data);
-    }
+    // function mint(
+    //     address account,
+    //     uint256 id,
+    //     uint256 amount,
+    //     bytes calldata data
+    // ) external {
+    //     // @review
+    //     // predicate-only !
+    //     // _mint(account, id, amount, data);
+    // }
+
+    // function mintBatch(
+    //     address to,
+    //     uint256[] calldata ids,
+    //     uint256[] calldata amounts,
+    //     bytes calldata data
+    // ) external {
+    //   // @review
+    //     // predicate-only !
+    //     // _mintBatch(to, ids, amounts, data);
+    // }
 
 }
