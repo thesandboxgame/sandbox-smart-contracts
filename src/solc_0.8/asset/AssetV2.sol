7ec1481ed3827936148ff8617b990e3b6996436b
Nick Furfaro <nfurfaro33@gmail.com>
2021-04-28 06:15:21 -0600

    new: Start to add polygon functionality to AssetV2.sol

--- src/solc_0.8/asset/AssetV2.sol
+++ src/solc_0.8/asset/AssetV2.sol
@@ -6,5 +6,28 @@ import "./ERC1155ERC721.sol";
 
 // solhint-disable-next-line no-empty-blocks
 contract AssetV2 is ERC1155ERC721 {
-// @note Add L1 polygon speecific stuff here.
+    // polygon-specific layer 1 functionality
+    // IBurnableERC1155, IMintableERC1155
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) external {
+        // @review
+        // predicate-only !
+        // _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external {
+      // @review
+        // predicate-only !
+        // _mintBatch(to, ids, amounts, data);
+    }
+
 }
