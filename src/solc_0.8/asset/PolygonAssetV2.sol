0257fbdb6e4d6236502ce75a7da5413cdc6eb8cc
Nick Furfaro <nfurfaro33@gmail.com>
2021-04-28 06:15:52 -0600

    new: Add PolygonAssetV2.sol

---
+++ src/solc_0.8/asset/PolygonAssetV2.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+// solhint-disable-next-line compiler-version
+pragma solidity 0.8.2;
+
+import "./ERC1155ERC721.sol";
+
+// solhint-disable-next-line no-empty-blocks
+contract PolygonAssetV2 is ERC1155ERC721 {
+// @note Add L2 polygon specific stuff here.
+}
