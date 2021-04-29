b5d7d9dbb63eaa1b03b645e5534ac77e72ac5236
Nick Furfaro <nfurfaro33@gmail.com>
2021-04-29 13:50:41 -0600

    new: Setup AssetV2 with basic L1 matic functionality

--- src/solc_0.8/asset/AssetV2.sol
+++ src/solc_0.8/asset/AssetV2.sol
@@ -7,28 +7,19 @@ import "./ERC1155ERC721.sol";
 // solhint-disable-next-line no-empty-blocks
 contract AssetV2 is ERC1155ERC721 {
     // polygon-specific layer 1 functionality
-    // IBurnableERC1155, IMintableERC1155
-
-    // function mint(
-    //     address account,
-    //     uint256 id,
-    //     uint256 amount,
-    //     bytes calldata data
-    // ) external {
-    //     // @review
-    //     // predicate-only !
-    //     // _mint(account, id, amount, data);
-    // }
-
-    // function mintBatch(
-    //     address to,
-    //     uint256[] calldata ids,
-    //     uint256[] calldata amounts,
-    //     bytes calldata data
-    // ) external {
-    //   // @review
-    //     // predicate-only !
-    //     // _mintBatch(to, ids, amounts, data);
-    // }
+    function mintBatch(
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external {
+      // @review
+        require(_msgSender() == _predicate, "!PREDICATE");
+        // operator here could be predicate... is there a benefit?
+        // hash should be the bytes32 metaData-hash.
+        bytes32 dummyHash = bytes32("0x00");
+        address operator = address(uint160(0));
+        _mint(dummyHash, amounts[0], 0, operator, to, ids[0], data, false);
+    }
 
 }
