15f2e67dac0e75a84c5967cf0a4747d3a2a65822
Nick Furfaro <nfurfaro33@gmail.com>
2021-04-30 13:47:56 -0600

    new: Add EstateV1.sol

---
+++ src/solc_0.8/estate/EstateV1.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+
+import "./EstateBaseToken.sol";
+
+// solhint-disable-next-line no-empty-blocks
+contract EstateV1 is EstateBaseToken {
+
+}
