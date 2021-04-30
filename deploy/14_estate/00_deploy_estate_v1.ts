99931bba3a42204a9d4b627f4cad929d35d502a4
Nick Furfaro <nfurfaro33@gmail.com>
2021-04-30 14:08:49 -0600

    new: Add estate deploy script

---
+++ deploy/14_estate/00_deploy_estate_v1.ts
@@ -0,0 +1,25 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+): Promise<void> {
+  const {deployments, getNamedAccounts} = hre;
+  const {deployer} = await getNamedAccounts();
+  const {deploy} = deployments;
+
+  const landContract = await deployments.get('Land');
+  const forwarder = await deployments.get('TestMetaTxForwarder');
+
+  await deploy('EstateV1', {
+    from: deployer,
+    args: [forwarder.address, landContract.address],
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+};
+
+export default func;
+func.tags = ['Asset', 'Asset_deploy'];
+func.dependencies = ['Land_deploy', 'TestMetaTxForwarder_deploy'];
+func.skip = async (hre) => hre.network.name !== 'hardhat';
