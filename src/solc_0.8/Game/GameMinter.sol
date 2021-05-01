9ee690003f2b7eb27257b6aabf53882a98971158
Nick Furfaro <nfurfaro33@gmail.com>
2021-05-01 07:48:46 -0600

    improvement: Change name of struct from Update to GameData

--- src/solc_0.8/Game/GameMinter.sol
+++ src/solc_0.8/Game/GameMinter.sol
@@ -44,7 +44,7 @@ contract GameMinter is ERC2771Context, IGameMinter {
     /// @return gameId The id of the new GAME token (erc721)
     function createGame(
         address to,
-        GameToken.Update calldata creation,
+        GameToken.GameData calldata creation,
         address editor,
         uint64 subId
     ) external override returns (uint256 gameId) {
@@ -58,7 +58,7 @@ contract GameMinter is ERC2771Context, IGameMinter {
     /// @param gameId The current id of the GAME token.
     /// @param update The values to use for the update.
     /// @return newId The new gameId.
-    function updateGame(uint256 gameId, GameToken.Update memory update) external override returns (uint256 newId) {
+    function updateGame(uint256 gameId, GameToken.GameData memory update) external override returns (uint256 newId) {
         address gameOwner = _gameToken.ownerOf(gameId);
         address msgSender = _msgSender();
         require(msgSender == gameOwner || _gameToken.isGameEditor(gameOwner, msgSender), "AUTH_ACCESS_DENIED");
