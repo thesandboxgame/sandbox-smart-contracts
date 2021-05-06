eae7dcf5fed658d10e918f24f7d8afe3c5b90640
Nick Furfaro <nfurfaro33@gmail.com>
2021-05-06 13:37:50 -0600

    new: Add new deployment for Trusted Forwarder

---
+++ deploy/01_external/03_deploy_metaTx_forwarder_if_needed.ts
@@ -0,0 +1,20 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+
+  const {deployer} = await getNamedAccounts();
+
+  let TRUSTED_FORWARDER = await deployments.getOrNull('TRUSTED_FORWARDER');
+  if (!TRUSTED_FORWARDER) {
+    TRUSTED_FORWARDER = await deploy('TRUSTED_FORWARDER', {
+      from: deployer,
+      contract: 'TestMetaTxForwarder',
+      log: true,
+    });
+  }
+};
+export default func;
+func.tags = ['TRUSTED_FORWARDER'];
