a95d2f850064ec91c26237e747c33103737fad7f
Nick Furfaro <nfurfaro33@gmail.com>
2021-05-07 15:32:26 -0600

    fix: Update import name

--- src/solc_0.8/Game/GameMinter.sol
+++ src/solc_0.8/Game/GameMinter.sol
@@ -2,7 +2,7 @@
 // solhint-disable-next-line compiler-version
 pragma solidity 0.8.2;
 
-import "./GameToken.sol";
+import "./BaseGameToken.sol";
 import "../common/interfaces/IGameMinter.sol";
 import "@openzeppelin/contracts-0.8/token/ERC20/IERC20.sol";
 import "../common/BaseWithStorage/ERC2771Handler.sol";
@@ -10,7 +10,7 @@ import "../common/BaseWithStorage/ERC2771Handler.sol";
 contract GameMinter is ERC2771Handler, IGameMinter {
     ///////////////////////////////  Data //////////////////////////////
 
-    GameToken internal immutable _gameToken;
+    BaseGameToken internal immutable _gameToken;
     // @todo confirm actual fees
     uint256 internal immutable _gameMintingFee;
     uint256 internal immutable _gameUpdateFee;
@@ -20,7 +20,7 @@ contract GameMinter is ERC2771Handler, IGameMinter {
     ///////////////////////////////  Functions /////////////////////////
 
     constructor(
-        GameToken gameTokenContract,
+        BaseGameToken gameTokenContract,
         address trustedForwarder,
         uint256 gameMintingFee,
         uint256 gameUpdateFee,
@@ -44,7 +44,7 @@ contract GameMinter is ERC2771Handler, IGameMinter {
     /// @return gameId The id of the new GAME token (erc721)
     function createGame(
         address to,
-        GameToken.GameData calldata creation,
+        BaseGameToken.GameData calldata creation,
         address editor,
         uint64 subId
     ) external override returns (uint256 gameId) {
@@ -58,7 +58,7 @@ contract GameMinter is ERC2771Handler, IGameMinter {
     /// @param gameId The current id of the GAME token.
     /// @param update The values to use for the update.
     /// @return newId The new gameId.
-    function updateGame(uint256 gameId, GameToken.GameData memory update) external override returns (uint256 newId) {
+    function updateGame(uint256 gameId, BaseGameToken.GameData memory update) external override returns (uint256 newId) {
         address gameOwner = _gameToken.ownerOf(gameId);
         address msgSender = _msgSender();
         require(msgSender == gameOwner || _gameToken.isGameEditor(gameOwner, msgSender), "AUTH_ACCESS_DENIED");
