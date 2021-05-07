02300b2a08794bc857189895144ae03a7bcd7ebb
Nick Furfaro <nfurfaro33@gmail.com>
2021-05-07 07:06:40 -0600

    Fixup

--- deploy/01_external/03_deploy_trusted_forwarder_if_needed.ts
+++ deploy/01_external/03_deploy_trusted_forwarder_if_needed.ts
@@ -1,6 +1,6 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
-import {skipUnlessTest} from '../utils/network';
+import {skipUnlessTest} from '../../utils/network';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
