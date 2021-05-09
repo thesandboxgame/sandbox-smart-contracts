956f54fee662b242846124b29bd60d5147ba9fca
CloverGuy <arthemisP@protonmail.com>
2021-05-09 18:21:47 +0300

    first estate test work

--- test/estate/estate.test.ts
+++ test/estate/estate.test.ts
@@ -1,10 +1,29 @@
 import {setupEstate} from './fixtures';
 import {waitFor} from '../utils';
 import {expect} from '../chai-setup';
+import {ethers} from 'hardhat';
+const emptyBytes = Buffer.from('');
 
 describe('Estate.sol', function () {
-  it('Can read the chainIndex from the tokenId', async function () {
-    const {Estate, users, mintEstate} = await setupEstate();
-    const chainIndex = await Estate.chainIndex(tokenId);
+  it('creating from Land Quad', async function () {
+    const {estateContract, landContractAsMinter, user0} = await setupEstate();
+    const size = 6;
+    const x = 6;
+    const y = 12;
+    await waitFor(landContractAsMinter.mintQuad(user0, size, x, y, emptyBytes));
+    await waitFor(
+      estateContract
+        .connect(ethers.provider.getSigner(user0))
+        .createFromQuad(user0, user0, size, x, y)
+    );
+    for (let sx = 0; sx < size; sx++) {
+      for (let sy = 0; sy < size; sy++) {
+        const id = x + sx + (y + sy) * 408;
+        const landOwner = await landContractAsMinter.callStatic.ownerOf(id);
+        expect(landOwner).to.equal(estateContract.address);
+      }
+    }
+    const estateOwner = await estateContract.callStatic.ownerOf(1);
+    expect(estateOwner).to.equal(user0);
   });
 });
