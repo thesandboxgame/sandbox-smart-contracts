25b7729efff7e43d63b33e5a33a0935a22acab3d
Nick Furfaro <nfurfaro33@gmail.com>
2021-05-10 10:52:15 -0600

    new: Add ChildEstateToken

---
+++ src/solc_0.8/polygon/child/ChildEstateTokenV1.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+
+import "../../estate/BaseEstateToken.sol";
+
+// solhint-disable-next-line no-empty-blocks
+contract ChildEstateTokenV1 is BaseEstateToken {
+
+}
