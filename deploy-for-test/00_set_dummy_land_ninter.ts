4288f89a6579411d37b9035ca1e5670ce8e6075f
Nick Furfaro <nfurfaro33@gmail.com>
2021-05-11 13:24:44 -0600

    test: Add new script for setup of dummy land-minter

---
+++ deploy-for-test/00_set_dummy_land_ninter.ts
@@ -0,0 +1,28 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+import {skipUnlessTest} from '../utils/network';
+
+const func: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+): Promise<void> {
+  const {deployments, getUnnamedAccounts} = hre;
+  const {log, execute, read} = deployments;
+
+  const others = await getUnnamedAccounts();
+  const dummyMinter = others[7];
+
+    const currentLandAdmin = await read('Land', 'getAdmin');
+    await execute(
+        'Land',
+        {from: currentLandAdmin, log: true},
+        'setMinter',
+        dummyMinter,
+        true
+      )
+
+
+export default func;
+func.runAtTheEnd = true;
+func.tags = ['Land', 'Land_setup'];
+func.dependencies = ['Land_deploy'];
+func.skip = skipUnlessTest;
