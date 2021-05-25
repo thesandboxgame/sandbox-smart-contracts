6aa44668e1651089456c820d6aeec07777d5c938
Arhaam Patvi <thearhaam@gmail.com>
2021-05-25 21:34:47 +0530

    feat: implement mintBatch for predicate contract

--- src/solc_0.8/asset/AssetV2.sol
+++ src/solc_0.8/asset/AssetV2.sol
@@ -6,5 +6,30 @@ import "./ERC1155ERC721.sol";
 
 // solhint-disable-next-line no-empty-blocks
 contract AssetV2 is ERC1155ERC721 {
-
+    /// @notice called by predicate to mint tokens transferred from L2
+    /// @param to address to mint to
+    /// @param ids ids to mint
+    /// @param amounts supply for each token type
+    /// @param data extra data to accompany the minting call
+    function mintBatch(
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external {
+        require(_msgSender() == _predicate, "!PREDICATE");
+        bytes32 hash = abi.decode(data, (bytes32));
+        for (uint256 i = 0; i < ids.length; i++) {
+            // ERC-721
+            if ((amounts[i] == 1) && (ids[i] & IS_NFT > 0)) {
+                uint8 rarity = 0;
+                _mint(hash, amounts[i], rarity, _predicate, to, ids[i], data, false);
+            }
+            // ERC-1155
+            else {
+                uint8 rarity = 0;
+                _mint(hash, amounts[i], rarity, _predicate, to, ids[i], data, false);
+            }
+        }
+    }
 }
