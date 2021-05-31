a36983ec4f039bd797493c45b4fe87f29ab4e9ee
Arhaam Patvi <thearhaam@gmail.com>
2021-05-31 21:00:56 +0530

    feat: add deployment for fake ChildChainManager

---
+++ deploy/01_external/04_deploy_child_chain_manager_if_needed.ts
@@ -0,0 +1,22 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+import {skipUnlessTest} from '../../utils/network';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+
+  const {deployer} = await getNamedAccounts();
+
+  let CHILD_CHAIN_MANAGER = await deployments.getOrNull('CHILD_CHAIN_MANAGER');
+  if (!CHILD_CHAIN_MANAGER) {
+    CHILD_CHAIN_MANAGER = await deploy('CHILD_CHAIN_MANAGER', {
+      from: deployer,
+      contract: 'FakeChildChainManager',
+      log: true,
+    });
+  }
+};
+export default func;
+func.tags = ['CHILD_CHAIN_MANAGER'];
+func.skip = skipUnlessTest;
