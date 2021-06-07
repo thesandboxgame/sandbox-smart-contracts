3e655f0b277dd06446acfc96fd639d03d8bb44ac
Arhaam Patvi <thearhaam@gmail.com>
2021-06-07 21:37:24 +0530

    feat: polygon sand contract

---
+++ src/solc_0.8/polygon/child/sand/PolygonSand.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: MIT
+// solhint-disable-next-line compiler-version
+pragma solidity 0.8.2;
+
+import "@openzeppelin/contracts-0.8/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-0.8/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts-0.8/access/Ownable.sol";
+
+contract PolygonSand is ERC20, Ownable {
+    using SafeMath for uint256;
+    address public childChainManagerProxy;
+
+    constructor(address _childChainManagerProxy) ERC20("SAND", "SAND") {
+        require(_childChainManagerProxy != address(0), "Bad ChildChainManagerProxy address");
+        childChainManagerProxy = _childChainManagerProxy;
+    }
+
+    /// @notice update the ChildChainManager Proxy address
+    /// @param newChildChainManagerProxy address of the new childChainManagerProxy
+    function updateChildChainManager(address newChildChainManagerProxy) external {
+        require(newChildChainManagerProxy != address(0), "Bad ChildChainManagerProxy address");
+        require(msg.sender == owner(), "You're not allowed");
+        childChainManagerProxy = newChildChainManagerProxy;
+    }
+
+    /// @notice called when tokens are deposited on root chain
+    /// @param user user address for whom deposit is being done
+    /// @param depositData abi encoded amount
+    function deposit(address user, bytes calldata depositData) external {
+        require(msg.sender == childChainManagerProxy, "You're not allowed to deposit");
+        uint256 amount = abi.decode(depositData, (uint256));
+        _mint(user, amount);
+    }
+
+    /// @notice called when user wants to withdraw tokens back to root chain
+    /// @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
+    /// @param amount amount to withdraw
+    function withdraw(uint256 amount) external {
+        _burn(_msgSender(), amount);
+    }
+}
