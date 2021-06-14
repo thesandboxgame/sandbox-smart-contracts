1bcc1088b3a3572570f07d6e3acbaacf0e4ac536
Lucas MASSONI SGUERRA <lucasmsguerra@gmail.com>
2021-06-14 10:06:52 +0200

    fixed unused variable

--- deploy_polygon/03_sandbasetoken/01_deploy_base_sand_token.ts
+++ deploy_polygon/03_sandbasetoken/01_deploy_base_sand_token.ts
@@ -17,3 +17,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 export default func;
 func.tags = ['SandBaseToken', 'SandBaseToken_deploy'];
+func.skip = skipUnlessTest;
