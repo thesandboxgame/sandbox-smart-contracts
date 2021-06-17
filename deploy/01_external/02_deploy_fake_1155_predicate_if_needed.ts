372ecb1511b2c8620ed26e2147e022860be18f90
Arhaam Patvi <thearhaam@gmail.com>
2021-06-17 13:09:13 +0530

    temp: disable old erc1155 predicate

--- deploy/01_external/02_deploy_fake_1155_predicate_if_needed.ts
+++ deploy/01_external/02_deploy_fake_1155_predicate_if_needed.ts
@@ -1,20 +1,20 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
+// import {HardhatRuntimeEnvironment} from 'hardhat/types';
+// import {DeployFunction} from 'hardhat-deploy/types';
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments, getNamedAccounts} = hre;
-  const {deploy} = deployments;
+// const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+//   const {deployments, getNamedAccounts} = hre;
+//   const {deploy} = deployments;
 
-  const {deployer} = await getNamedAccounts();
+//   const {deployer} = await getNamedAccounts();
 
-  let ERC1155_PREDICATE = await deployments.getOrNull('ERC1155_PREDICATE');
-  if (!ERC1155_PREDICATE) {
-    ERC1155_PREDICATE = await deploy('ERC1155_PREDICATE', {
-      from: deployer,
-      contract: 'FakePredicateForwarder',
-      log: true,
-    });
-  }
-};
-export default func;
-func.tags = ['ERC1155_PREDICATE'];
+//   let ERC1155_PREDICATE = await deployments.getOrNull('ERC1155_PREDICATE');
+//   if (!ERC1155_PREDICATE) {
+//     ERC1155_PREDICATE = await deploy('ERC1155_PREDICATE', {
+//       from: deployer,
+//       contract: 'FakePredicateForwarder',
+//       log: true,
+//     });
+//   }
+// };
+// export default func;
+// func.tags = ['ERC1155_PREDICATE'];
