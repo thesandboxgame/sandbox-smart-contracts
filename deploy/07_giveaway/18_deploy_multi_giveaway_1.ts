a48227e39cd64aa9241fc848930fdba5cdf65e91
León <leon.isla.22@gmail.com>
2021-06-17 15:40:37 -0300

    Remove unused import to make CI happy

--- deploy/07_giveaway/18_deploy_multi_giveaway_1.ts
+++ deploy/07_giveaway/18_deploy_multi_giveaway_1.ts
@@ -1,6 +1,5 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
-import {skipUnlessTest} from '../../utils/network';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
