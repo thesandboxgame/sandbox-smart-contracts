816e0871097e730f60aa6c52efd67e18c3f3c40c
Arhaam Patvi <thearhaam@gmail.com>
2021-06-18 13:12:33 +0530

    feat: added on received functions

--- src/solc_0.8/test/FakeERC1155Predicate.sol
+++ src/solc_0.8/test/FakeERC1155Predicate.sol
@@ -2,11 +2,12 @@
 pragma solidity 0.8.2;
 
 import {IERC1155} from "@openzeppelin/contracts-0.8/token/ERC1155/IERC1155.sol";
+import {ERC1155Receiver} from "@openzeppelin/contracts-0.8/token/ERC1155/utils/ERC1155Receiver.sol";
 
 /// @dev This is NOT a secure ChildChainManager contract implementation!
 /// DO NOT USE in production.
 
-contract ERC1155Predicate {
+contract ERC1155Predicate is ERC1155Receiver {
     address private asset;
 
     function setAsset(address _asset) external {
@@ -28,5 +29,25 @@ contract ERC1155Predicate {
         uint256[] memory amounts
     ) public {
         IERC1155(asset).safeBatchTransferFrom(address(this), withdrawer, ids, amounts, bytes(""));
+    }
+    
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external override pure returns (bytes4) {
+        return 0;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external override pure returns (bytes4) {
+        return ERC1155Receiver(address(0)).onERC1155BatchReceived.selector;
     }
 }
