e091f067e9f196ec1c858b4733cdbb1ab6c2ab03
León <leon.isla.22@gmail.com>
2021-06-22 08:46:09 -0300

    Add deployment script + testnet config

--- data/landSales/deadlines.ts
+++ data/landSales/deadlines.ts
@@ -3,5 +3,10 @@ const deadlines: {[sector: number]: number} =  {
   17: 1614776400, // Wednesday, 3 March 2021 13:00:00 GMT+00:00
   18: 1619701200, // Thursday, 29 April 2021 13:00:00 GMT+00:00
   19: Date.UTC(2021, 5, 10, 13) / 1000, // Thursday, 10 June 2021 13:00:00 GMT+00:00
+  20: Date.UTC(2021, 6, 1, 13) / 1000,  // Thursday, 01 July 2021 13:00:00 GMT+00:00
+  21: Date.UTC(2021, 6, 8, 13) / 1000,  // Thursday, 08 July 2021 13:00:00 GMT+00:00
+  22: Date.UTC(2021, 6, 15, 13) / 1000, // Thursday, 15 July 2021 13:00:00 GMT+00:00
+  23: Date.UTC(2021, 6, 22, 13) / 1000, // Thursday, 22 July 2021 13:00:00 GMT+00:00
+  24: Date.UTC(2021, 6, 29, 13) / 1000, // Thursday, 29 July 2021 13:00:00 GMT+00:00
 };
 export default deadlines;
