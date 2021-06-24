660608df3064586f33747d5aa12643f40f4bb80b
Arhaam Patvi <thearhaam@gmail.com>
2021-06-24 16:34:14 +0530

    refactor: remove old erc1155 predicate deployment

--- deploy/01_external/02_deploy_fake_1155_predicate_if_needed.ts
+++ deploy/01_external/02_deploy_fake_1155_predicate_if_needed.ts
@@ -1,24 +1,3 @@
-// import {HardhatRuntimeEnvironment} from 'hardhat/types';
-// import {DeployFunction} from 'hardhat-deploy/types';
-
-// const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-//   const {deployments, getNamedAccounts} = hre;
-//   const {deploy} = deployments;
-
-//   const {deployer} = await getNamedAccounts();
-
-//   let ERC1155_PREDICATE = await deployments.getOrNull('ERC1155_PREDICATE');
-//   if (!ERC1155_PREDICATE) {
-//     ERC1155_PREDICATE = await deploy('ERC1155_PREDICATE', {
-//       from: deployer,
-//       contract: 'FakePredicateForwarder',
-//       log: true,
-//     });
-//   }
-// };
-// export default func;
-// func.tags = ['ERC1155_PREDICATE'];
-
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 import {skipUnlessTest} from '../../utils/network';
