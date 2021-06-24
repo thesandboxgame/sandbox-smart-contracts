48180f810d403814800e109d410b92f69dfc0379
Arhaam Patvi <thearhaam@gmail.com>
2021-06-24 16:33:08 +0530

    feat: fake erc1155 predicate based on chainexit predicate

--- src/solc_0.8/test/FakeERC1155Predicate.sol
+++ src/solc_0.8/test/FakeERC1155Predicate.sol
@@ -46,34 +46,52 @@ contract ERC1155Predicate is ERC1155Receiver {
         bytes memory data
     ) public {
         IMintableERC1155 token = IMintableERC1155(asset);
-        token.mintBatch(
-            address(this),
-            ids,
-            calculateAmountsToBeMinted(
-                token.balanceOfBatch(makeArrayWithAddress(address(this), ids.length), ids),
-                amounts
-            ),
-            data
-        );
-        token.safeBatchTransferFrom(address(this), withdrawer, ids, amounts, bytes(""));
+        uint256[] memory balances = token.balanceOfBatch(makeArrayWithAddress(address(this), ids.length), ids);
+        (uint256[] memory toBeMinted, bool needMintStep, bool needTransferStep) =
+            calculateAmountsToBeMinted(balances, amounts);
+        if (needMintStep) {
+            token.mintBatch(
+                withdrawer,
+                ids,
+                toBeMinted,
+                data // passing data when minting to withdrawer
+            );
+        }
+        if (needTransferStep) {
+            token.safeBatchTransferFrom(
+                address(this),
+                withdrawer,
+                ids,
+                balances,
+                data // passing data when transferring unlocked tokens to withdrawer
+            );
+        }
     }
 
-    function calculateAmountsToBeMinted(uint256[] memory tokenBalances, uint256[] memory amountsToBeExited)
+    function calculateAmountsToBeMinted(uint256[] memory balances, uint256[] memory exitAmounts)
         internal
         pure
-        returns (uint256[] memory)
+        returns (
+            uint256[] memory,
+            bool,
+            bool
+        )
     {
-        require(
-            tokenBalances.length == amountsToBeExited.length,
-            "MintableERC1155Predicate: Array length mismatch found"
-        );
-        uint256[] memory toBeMintedAmounts = new uint256[](tokenBalances.length);
-        for (uint256 i = 0; i < tokenBalances.length; i++) {
-            if (tokenBalances[i] < amountsToBeExited[i]) {
-                toBeMintedAmounts[i] = amountsToBeExited[i] - tokenBalances[i];
+        uint256 count = balances.length;
+        require(count == exitAmounts.length, "ChainExitERC1155Predicate: Array length mismatch found");
+        uint256[] memory toBeMinted = new uint256[](count);
+        bool needMintStep;
+        bool needTransferStep;
+        for (uint256 i = 0; i < count; i++) {
+            if (balances[i] < exitAmounts[i]) {
+                toBeMinted[i] = exitAmounts[i] - balances[i];
+                needMintStep = true;
+            }
+            if (balances[i] != 0) {
+                needTransferStep = true;
             }
         }
-        return toBeMintedAmounts;
+        return (toBeMinted, needMintStep, needTransferStep);
     }
 
     function makeArrayWithAddress(address addr, uint256 size) internal pure returns (address[] memory) {
