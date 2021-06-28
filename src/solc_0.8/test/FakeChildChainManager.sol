e168388405867615fd95b954ad5a308066c569ce
Arhaam Patvi <thearhaam@gmail.com>
2021-06-28 15:11:33 +0530

    feat: fake childChainManager sand support

--- src/solc_0.8/test/FakeChildChainManager.sol
+++ src/solc_0.8/test/FakeChildChainManager.sol
@@ -2,12 +2,14 @@
 pragma solidity 0.8.2;
 
 import "../polygon/child/asset/PolygonAssetV2.sol";
+import "../polygon/child/sand/PolygonSand.sol";
 
 /// @dev This is NOT a secure ChildChainManager contract implementation!
 /// DO NOT USE in production.
 
 contract FakeChildChainManager {
     address public polygonAsset;
+    address public polygonSand;
 
     // solhint-disable-next-line no-empty-blocks
     constructor() {}
@@ -15,8 +17,16 @@ contract FakeChildChainManager {
     function setPolygonAsset(address _polygonAsset) external {
         polygonAsset = _polygonAsset;
     }
+    
+    function setPolygonSand(address _polygonSand) external {
+        polygonSand = _polygonSand;
+    }
 
     function callDeposit(address user, bytes calldata depositData) external {
         PolygonAssetV2(polygonAsset).deposit(user, depositData);
+    }
+
+    function callSandDeposit(address user, bytes calldata depositData) external {
+        PolygonSand(polygonSand).deposit(user, depositData);
     }
 }
