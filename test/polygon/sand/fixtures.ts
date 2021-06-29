1445947d4d8fa3d42d587839badd0d20f17cd57a
Arhaam Patvi <thearhaam@gmail.com>
2021-06-29 13:57:05 +0530

    test: setup sand beneficiary for polygon sand

--- test/polygon/sand/fixtures.ts
+++ test/polygon/sand/fixtures.ts
@@ -1,12 +1,21 @@
-import {ethers, deployments, getUnnamedAccounts} from 'hardhat';
+import {
+  ethers,
+  deployments,
+  getNamedAccounts,
+  getUnnamedAccounts,
+} from 'hardhat';
 
-import {setupUsers} from '../../utils';
+import {setupUsers, setupUser} from '../../utils';
 
 export const setupSand = deployments.createFixture(async () => {
   await deployments.fixture('PolygonSand');
   const Sand = await ethers.getContract('PolygonSand');
+  const accounts = await getNamedAccounts();
   const unnamedAccounts = await getUnnamedAccounts();
   const users = await setupUsers(unnamedAccounts, {Sand});
   const childChainManager = await ethers.getContract('CHILD_CHAIN_MANAGER');
-  return {Sand, users, childChainManager};
+  const sandBeneficiary = await setupUser(accounts.sandBeneficiary, {
+    Sand,
+  });
+  return {Sand, users, sandBeneficiary, childChainManager};
 });
