aec6d45a52a3b72b0cc8499f4d55228a731f10f5
Arhaam Patvi <thearhaam@gmail.com>
2021-06-29 13:57:40 +0530

    test: transfer sand L2 to L1

--- test/polygon/sand/sand.test.ts
+++ test/polygon/sand/sand.test.ts
@@ -22,7 +22,7 @@ describe('PolygonSand.sol', function () {
         )
       );
       const polygon_balance = BigNumber.from(
-        await polygon.users[0].Sand.balanceOf(mainnet.sandBeneficiary.address)
+        await polygon.users[0].Sand.balanceOf(polygon.sandBeneficiary.address)
       );
 
       // Grant approval to ERC20 predicate contract
@@ -38,7 +38,7 @@ describe('PolygonSand.sol', function () {
       await waitFor(
         mainnet.predicate.lockTokens(
           mainnet.sandBeneficiary.address,
-          mainnet.sandBeneficiary.address,
+          polygon.sandBeneficiary.address,
           data
         )
       );
@@ -47,8 +47,74 @@ describe('PolygonSand.sol', function () {
       await waitFor(
         polygon.childChainManager.callSandDeposit(
           polygon.Sand.address,
+          polygon.sandBeneficiary.address,
+          data
+        )
+      );
+
+      // Ensure balance is updated on Mainnet & Polygon
+      const updated_mainnet_balance = BigNumber.from(
+        await mainnet.sandContractAsUser0.balanceOf(
+          mainnet.sandBeneficiary.address
+        )
+      );
+      const updated_polygon_balance = BigNumber.from(
+        await polygon.users[0].Sand.balanceOf(polygon.sandBeneficiary.address)
+      );
+
+      expect(updated_mainnet_balance).to.be.equal(
+        mainnet_balance.sub(transferAmount)
+      );
+      expect(updated_polygon_balance).to.be.equal(
+        polygon_balance.add(transferAmount)
+      );
+    });
+    it('should be able to transfer SAND: L2 to L1', async function () {
+      const polygon = await setupPolygonSand();
+      const mainnet = await setupMainnetSand();
+
+      const transferAmount = ethers.utils.parseEther('20').toString();
+
+      // Transfer: L1 to L2
+      await waitFor(
+        mainnet.sandBeneficiary.sandContract.approve(
+          mainnet.predicate.address,
+          transferAmount
+        )
+      );
+      const data = abiCoder.encode(['uint256'], [transferAmount]);
+      await waitFor(
+        mainnet.predicate.lockTokens(
+          mainnet.sandBeneficiary.address,
           mainnet.sandBeneficiary.address,
           data
+        )
+      );
+      await waitFor(
+        polygon.childChainManager.callSandDeposit(
+          polygon.Sand.address,
+          mainnet.sandBeneficiary.address,
+          data
+        )
+      );
+
+      const mainnet_balance = BigNumber.from(
+        await mainnet.sandContractAsUser0.balanceOf(
+          mainnet.sandBeneficiary.address
+        )
+      );
+      const polygon_balance = BigNumber.from(
+        await polygon.users[0].Sand.balanceOf(mainnet.sandBeneficiary.address)
+      );
+
+      // Withdraw tokens from PolygonSand
+      await waitFor(polygon.sandBeneficiary.Sand.withdraw(transferAmount));
+
+      // Emulate exit token
+      await waitFor(
+        mainnet.predicate.exitTokens(
+          mainnet.sandBeneficiary.address,
+          transferAmount
         )
       );
 
@@ -61,11 +127,15 @@ describe('PolygonSand.sol', function () {
       const updated_polygon_balance = BigNumber.from(
         await polygon.users[0].Sand.balanceOf(mainnet.sandBeneficiary.address)
       );
+      console.log(mainnet_balance.toString());
+      console.log(polygon_balance.toString());
+      console.log(updated_mainnet_balance.toString());
+      console.log(updated_polygon_balance.toString());
       expect(updated_mainnet_balance).to.be.equal(
-        mainnet_balance.sub(transferAmount)
+        mainnet_balance.add(transferAmount)
       );
       expect(updated_polygon_balance).to.be.equal(
-        polygon_balance.add(transferAmount)
+        polygon_balance.sub(transferAmount)
       );
     });
   });
