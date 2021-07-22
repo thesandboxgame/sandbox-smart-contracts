14308c6ba6edde3a84101dfd52a47eb65d5625ee
Lucas <luc4rd@gmail.com>
2021-07-22 17:41:56 +0200

    fixed warnings

--- test/polygon/landBase/land.test.ts
+++ test/polygon/landBase/land.test.ts
@@ -1,10 +1,5 @@
 import {expect} from '../../chai-setup';
-import {
-  ethers,
-  deployments,
-  getNamedAccounts,
-  getUnnamedAccounts,
-} from 'hardhat';
+import {ethers, deployments, getUnnamedAccounts} from 'hardhat';
 import {Contract} from 'ethers';
 import {setupUsers, waitFor} from '../../utils';
 
@@ -13,49 +8,23 @@ type User = {
   MockLandWithMint: Contract;
 };
 
-/*const setupTest = deployments.createFixture(async function () {
-  await deployments.fixture(['MockLandWithMint']);
-
-  const accounts = await getNamedAccounts();
-  const others = await getUnnamedAccounts();
-  const user0 = others[0];
-  const user1 = others[1];
-
-
-  const MockLandWithMint = await ethers.getContract('MockLandWithMint');
-  await deployments.deploy('MockERC20BasicApprovalTarget', {
-    from: accounts.deployer,
-    args: [],
-  });
-
-  return {MockLandWithMint, accounts, others, user0, user1};
-});*/
-
 const setupTest = deployments.createFixture(
   async (): Promise<{
     MockLandWithMint: Contract;
     landOwners: User[];
-    //user0: User;
-    //user1: User;
   }> => {
     await deployments.fixture('MockLandWithMint');
     const MockLandWithMint = await ethers.getContract('MockLandWithMint');
     const unnamedAccounts = await getUnnamedAccounts();
     const landOwners = await setupUsers(unnamedAccounts, {MockLandWithMint});
-    //const user0 = landOwners[0];
-    //const user1 = landOwners[1];
-
-    return {MockLandWithMint, landOwners /*, user0, user1*/};
+    return {MockLandWithMint, landOwners};
   }
 );
 
 describe('MockLandWithMint.sol', function () {
   describe('Mint and transfer', function () {
     it('testing transferQuad', async function () {
-      const {
-        MockLandWithMint,
-        landOwners /*, user0, user1*/,
-      } = await setupTest();
+      const {landOwners} = await setupTest();
       const bytes = '0x3333';
       await waitFor(
         landOwners[0].MockLandWithMint.mintQuad(
@@ -80,16 +49,20 @@ describe('MockLandWithMint.sol', function () {
           bytes
         )
       );
-      const num1 = await MockLandWithMint.balanceOf(landOwners[0].address);
+      const num1 = await landOwners[0].MockLandWithMint.balanceOf(
+        landOwners[0].address
+      );
       expect(num1).to.equal(0);
-      const num2 = await MockLandWithMint.balanceOf(landOwners[1].address);
+      const num2 = await landOwners[0].MockLandWithMint.balanceOf(
+        landOwners[1].address
+      );
       expect(num2).to.equal(144);
     });
   });
 
   describe('transfer batch', function () {
     it('testing batchTransferQuad', async function () {
-      const {MockLandWithMint, landOwners} = await setupTest();
+      const {landOwners} = await setupTest();
       const bytes = '0x3333';
       await waitFor(
         landOwners[0].MockLandWithMint.mintQuad(
@@ -127,9 +100,6 @@ describe('MockLandWithMint.sol', function () {
           bytes
         )
       );
-      const num = await landOwners[0].MockLandWithMint.balanceOf(
-        landOwners[0].address
-      );
       await waitFor(
         landOwners[0].MockLandWithMint.batchTransferQuad(
           landOwners[0].address,
@@ -151,16 +121,3 @@ describe('MockLandWithMint.sol', function () {
     });
   });
 });
-
-//address to, uint256 x, uint256 y, uint256 size, uint256 price
-
-/*
-batchTransferQuad(
-        address from,
-        address to,
-        uint256[] calldata sizes,
-        uint256[] calldata xs,
-        uint256[] calldata ys,
-        bytes calldata data
-    ) external {
-*/
