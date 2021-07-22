96a265b72db92e5761bff5ab2136fe43bc7727e5
Lucas <luc4rd@gmail.com>
2021-07-22 17:30:39 +0200

    final version

--- test/polygon/landBase/land.test.ts
+++ test/polygon/landBase/land.test.ts
@@ -6,82 +6,147 @@ import {
   getUnnamedAccounts,
 } from 'hardhat';
 import {Contract} from 'ethers';
-import {waitFor} from '../../utils';
+import {setupUsers, waitFor} from '../../utils';
 
 type User = {
   address: string;
   MockLandWithMint: Contract;
 };
 
+/*const setupTest = deployments.createFixture(async function () {
+  await deployments.fixture(['MockLandWithMint']);
+
+  const accounts = await getNamedAccounts();
+  const others = await getUnnamedAccounts();
+  const user0 = others[0];
+  const user1 = others[1];
+
+
+  const MockLandWithMint = await ethers.getContract('MockLandWithMint');
+  await deployments.deploy('MockERC20BasicApprovalTarget', {
+    from: accounts.deployer,
+    args: [],
+  });
+
+  return {MockLandWithMint, accounts, others, user0, user1};
+});*/
+
 const setupTest = deployments.createFixture(
-  /*async (): Promise<{
+  async (): Promise<{
     MockLandWithMint: Contract;
+    landOwners: User[];
+    //user0: User;
+    //user1: User;
   }> => {
     await deployments.fixture('MockLandWithMint');
-    const MockLandWithMint: Contract = await ethers.getContract(
-      'MockLandWithMint'
-    );*/
-  async function () {
-    await deployments.fixture(['MockLandWithMint']);
-
-    const accounts = await getNamedAccounts();
-    const others = await getUnnamedAccounts();
-    const user0 = others[0];
-    const user1 = others[1];
-
     const MockLandWithMint = await ethers.getContract('MockLandWithMint');
-    await deployments.deploy('MockERC20BasicApprovalTarget', {
-      from: accounts.deployer,
-      args: [],
-    });
+    const unnamedAccounts = await getUnnamedAccounts();
+    const landOwners = await setupUsers(unnamedAccounts, {MockLandWithMint});
+    //const user0 = landOwners[0];
+    //const user1 = landOwners[1];
 
-    return {MockLandWithMint, accounts, others, user0, user1};
+    return {MockLandWithMint, landOwners /*, user0, user1*/};
   }
 );
 
 describe('MockLandWithMint.sol', function () {
-  /*describe('Deployment', function () {
-    it('testing', async function () {
-      const {MockLandWithMint} = await setupTest();
-      const symbol = await MockLandWithMint.symbol();
-      expect(symbol).to.equal(name);
-    });
-  });*/
   describe('Mint and transfer', function () {
     it('testing transferQuad', async function () {
-      const {MockLandWithMint, user0, user1} = await setupTest();
-      await waitFor(MockLandWithMint.mintQuad(user0, 12, 0, 0, ''));
-      const num = await MockLandWithMint.balanceOf(user0);
+      const {
+        MockLandWithMint,
+        landOwners /*, user0, user1*/,
+      } = await setupTest();
+      const bytes = '0x3333';
+      await waitFor(
+        landOwners[0].MockLandWithMint.mintQuad(
+          landOwners[0].address,
+          12,
+          0,
+          0,
+          bytes
+        )
+      );
+      const num = await landOwners[0].MockLandWithMint.balanceOf(
+        landOwners[0].address
+      );
       expect(num).to.equal(144);
-      //await waitFor(MockLandWithMint.transferQuad(user0, user1, 12, 0, 0));
-      //const num1 = await MockLandWithMint.balanceOf(user0);
-      //expect(num1).to.equal(0);
-      //const num2 = await MockLandWithMint.balanceOf(user1);
-      //expect(num2).to.equal(144);
+      await waitFor(
+        landOwners[0].MockLandWithMint.transferQuad(
+          landOwners[0].address,
+          landOwners[1].address,
+          12,
+          0,
+          0,
+          bytes
+        )
+      );
+      const num1 = await MockLandWithMint.balanceOf(landOwners[0].address);
+      expect(num1).to.equal(0);
+      const num2 = await MockLandWithMint.balanceOf(landOwners[1].address);
+      expect(num2).to.equal(144);
     });
   });
 
   describe('transfer batch', function () {
     it('testing batchTransferQuad', async function () {
-      const {MockLandWithMint, user0, user1} = await setupTest();
-      await waitFor(MockLandWithMint.mintQuad(user0, 24, 0, 0));
-      await waitFor(MockLandWithMint.mintQuad(user0, 12, 300, 300));
-      await waitFor(MockLandWithMint.mintQuad(user0, 6, 30, 30));
-      await waitFor(MockLandWithMint.mintQuad(user0, 3, 24, 24));
-      const num = await MockLandWithMint.balanceOf(user0);
-      //expect(num).to.equal(720);
+      const {MockLandWithMint, landOwners} = await setupTest();
+      const bytes = '0x3333';
       await waitFor(
-        MockLandWithMint.batchTransferQuad(
-          user0,
-          user1,
-          [24, 12, 6, 3],
-          [0, 300, 30, 24],
-          [0, 300, 30, 24]
+        landOwners[0].MockLandWithMint.mintQuad(
+          landOwners[0].address,
+          24,
+          0,
+          0,
+          bytes
         )
       );
-      const num1 = await MockLandWithMint.balanceOf(user0);
+      await waitFor(
+        landOwners[0].MockLandWithMint.mintQuad(
+          landOwners[0].address,
+          12,
+          300,
+          300,
+          bytes
+        )
+      );
+      await waitFor(
+        landOwners[0].MockLandWithMint.mintQuad(
+          landOwners[0].address,
+          6,
+          30,
+          30,
+          bytes
+        )
+      );
+      await waitFor(
+        landOwners[0].MockLandWithMint.mintQuad(
+          landOwners[0].address,
+          3,
+          24,
+          24,
+          bytes
+        )
+      );
+      const num = await landOwners[0].MockLandWithMint.balanceOf(
+        landOwners[0].address
+      );
+      await waitFor(
+        landOwners[0].MockLandWithMint.batchTransferQuad(
+          landOwners[0].address,
+          landOwners[1].address,
+          [24, 12, 6, 3],
+          [0, 300, 30, 24],
+          [0, 300, 30, 24],
+          bytes
+        )
+      );
+      const num1 = await landOwners[0].MockLandWithMint.balanceOf(
+        landOwners[0].address
+      );
       expect(num1).to.equal(0);
-      const num2 = await MockLandWithMint.balanceOf(user1);
+      const num2 = await landOwners[0].MockLandWithMint.balanceOf(
+        landOwners[1].address
+      );
       expect(num2).to.equal(765);
     });
   });
