aa101248217ce38790be05bc825c711aaa48e649
Arhaam Patvi <thearhaam@gmail.com>
2021-08-04 17:02:16 +0530

    feat: mint batch for multiple assets

--- src/solc_0.8/asset/AssetV2.sol
+++ src/solc_0.8/asset/AssetV2.sol
@@ -21,12 +21,17 @@ contract AssetV2 is ERC1155ERC721 {
         bytes32[] memory hashes = abi.decode(data, (bytes32[]));
         for (uint256 i = 0; i < ids.length; i++) {
             uint256 uriId = ids[i] & URI_ID;
-            // @review - why does this fail even though nothing has been minted yet?
-            // require(uint256(_metadataHash[uriId]) == 0, "ID_TAKEN");
             _metadataHash[uriId] = hashes[i];
             _rarityPacks[uriId] = "0x00";
+            uint16 numNFTs = 0;
+            if ((ids[i] & IS_NFT) > 0) {
+                numNFTs = 1;
+            }
+            uint256[] memory singleId = new uint256[](1);
+            singleId[0] = ids[i];
+            uint256[] memory singleAmount = new uint256[](1);
+            singleAmount[0] = amounts[i];
+            _mintBatches(singleAmount, to, singleId, numNFTs);
         }
-        // @todo - handle numNFTs
-        _mintBatches(amounts, to, ids, 0);
     }
 }
