9b1f6b91654d7b1ed5f48da78e7a17e045df99da
León <leon.isla.22@gmail.com>
2021-08-13 15:38:18 -0300

    fix: format

--- test/land-sale/AuthValidator.test.ts
+++ test/land-sale/AuthValidator.test.ts
@@ -2,25 +2,29 @@ import {expect} from 'chai';
 import {ethers, getNamedAccounts} from 'hardhat';
 import {setupAuthValidator, backendAuthWallet} from './fixtures';
 
-describe.only("AuthValidator", function () {
-  it("signature should be valid", async function () {
+describe('AuthValidator', function () {
+  it('signature should be valid', async function () {
     const {authValidatorContract} = await setupAuthValidator();
-    const {deployer} = await getNamedAccounts()
+    const {deployer} = await getNamedAccounts();
     const hashedData = ethers.utils.solidityKeccak256(['address'], [deployer]);
-    const signature = await backendAuthWallet.signMessage(ethers.utils.arrayify(hashedData))
+    const signature = await backendAuthWallet.signMessage(
+      ethers.utils.arrayify(hashedData)
+    );
 
     const res = await authValidatorContract.isAuthValid(signature, hashedData);
     expect(res).to.equal(true);
-  })
+  });
 
-  it("signature should be invalid", async function () {
+  it('signature should be invalid', async function () {
     const {authValidatorContract} = await setupAuthValidator();
-    const {deployer} = await getNamedAccounts()
+    const {deployer} = await getNamedAccounts();
     const wallet = await ethers.getSigner(deployer);
     const hashedData = ethers.utils.solidityKeccak256(['address'], [deployer]);
-    const signature = await wallet.signMessage(ethers.utils.arrayify(hashedData))
+    const signature = await wallet.signMessage(
+      ethers.utils.arrayify(hashedData)
+    );
 
     const res = await authValidatorContract.isAuthValid(signature, hashedData);
     expect(res).to.equal(false);
-  })
-})
+  });
+});
--- test/land-sale/fixtures.ts
+++ test/land-sale/fixtures.ts
@@ -1,51 +1,72 @@
 import {ethers, deployments, getNamedAccounts} from 'hardhat';
-import fs from "fs-extra";
+import fs from 'fs-extra';
 import {SaltedProofSaleLandInfo} from '../../lib/merkleTreeHelper';
 import {Wallet} from 'ethers';
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 
-export const backendAuthWallet = new ethers.Wallet("0x4242424242424242424242424242424242424242424242424242424242424242")
-export const zeroAddress = "0x0000000000000000000000000000000000000000";
+export const backendAuthWallet = new ethers.Wallet(
+  '0x4242424242424242424242424242424242424242424242424242424242424242'
+);
+export const zeroAddress = '0x0000000000000000000000000000000000000000';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const signAuthMessageAs = async (wallet: Wallet | SignerWithAddress, ...args: any[]): Promise<string> => {
+export const signAuthMessageAs = async (
+  wallet: Wallet | SignerWithAddress,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ...args: any[]
+): Promise<string> => {
   const hashedData = ethers.utils.solidityKeccak256(
-    ['address', 'address', 'uint256', 'uint256', 'uint256', 'uint256', 'bytes32', 'uint256[]', 'bytes32[]'],
+    [
+      'address',
+      'address',
+      'uint256',
+      'uint256',
+      'uint256',
+      'uint256',
+      'bytes32',
+      'uint256[]',
+      'bytes32[]',
+    ],
     args
   );
   return wallet.signMessage(ethers.utils.arrayify(hashedData));
-}
+};
 
-export const setupAuthValidator = deployments.createFixture(async function (hre) {
+export const setupAuthValidator = deployments.createFixture(async function (
+  hre
+) {
   await deployments.fixture(['AuthValidator']);
   const authValidatorContract = await ethers.getContract('AuthValidator');
   return {
     authValidatorContract,
     hre,
-    getNamedAccounts
+    getNamedAccounts,
   };
 });
 
 export const setupEstateSale = deployments.createFixture(async function (hre) {
   await deployments.fixture(['EstateSaleWithAuth', 'AuthValidator', 'Sand']);
   const authValidatorContract = await ethers.getContract('AuthValidator');
-  const estateSaleWithAuthContract = await ethers.getContract('EstateSaleWithAuth_0_0');
+  const estateSaleWithAuthContract = await ethers.getContract(
+    'EstateSaleWithAuth_0_0'
+  );
   const sandContract = await ethers.getContract('Sand');
-  const proofs: SaltedProofSaleLandInfo[] = fs.readJSONSync('./secret/estate-sale/hardhat/.proofs_EstateSaleWithAuth_0_0.json')
+  const proofs: SaltedProofSaleLandInfo[] = fs.readJSONSync(
+    './secret/estate-sale/hardhat/.proofs_EstateSaleWithAuth_0_0.json'
+  );
   await transferSandToDeployer(proofs);
   const approveSandForEstateSale = async (address: string, price: string) => {
     const sandContractAsUser = await sandContract.connect(
       ethers.provider.getSigner(address)
     );
     await sandContractAsUser.approve(estateSaleWithAuthContract.address, price);
-  }
+  };
   return {
     authValidatorContract,
     estateSaleWithAuthContract,
     approveSandForEstateSale,
     proofs,
     hre,
-    getNamedAccounts
+    getNamedAccounts,
   };
 });
 
