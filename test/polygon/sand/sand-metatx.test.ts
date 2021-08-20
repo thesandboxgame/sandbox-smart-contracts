3dd8ae33ba9235c2485af2d28c46573142c02582
Andres Adjimann <andres@sandbox.game>
2021-08-20 17:42:03 -0300

    test: some more tests for PolygonSand

--- test/polygon/sand/sand-metatx.test.ts
+++ test/polygon/sand/sand-metatx.test.ts
@@ -3,7 +3,7 @@ import {Fixtures, setupSand} from './fixtures';
 import {AbiCoder} from 'ethers/lib/utils';
 import {sendMetaTx} from '../../sendMetaTx';
 import {expectEventWithArgsFromReceipt, toWei, waitFor} from '../../utils';
-import {BigNumber, BigNumberish} from 'ethers';
+import {BigNumber, BigNumberish, PopulatedTransaction} from 'ethers';
 
 const abiCoder = new AbiCoder();
 
@@ -26,24 +26,43 @@ async function depositViaCildChainManager(
   expect(await fixtures.Sand.balanceOf(user)).to.be.equal(pre.add(amount));
 }
 
+async function sendMeta(
+  fixtures: Fixtures,
+  signer: string,
+  populatedTx: Promise<PopulatedTransaction>
+) {
+  const {data} = await populatedTx;
+  // users[3] pay for the gas, of a message signed by signer.
+  const receipt = await sendMetaTx(
+    fixtures.Sand.address,
+    fixtures.users[3].TrustedForwarder,
+    data,
+    signer
+  );
+  const event = await expectEventWithArgsFromReceipt(
+    fixtures.TrustedForwarder,
+    receipt,
+    'TXResult'
+  );
+  expect(event.args.success).to.be.true;
+}
+
 describe('PolygonSand.sol Meta TX', function () {
   let fixtures: Fixtures;
   let users: string[];
+  let sandBeneficiary: string;
   beforeEach(async function () {
     fixtures = await setupSand();
+    sandBeneficiary = fixtures.sandBeneficiary.address;
     users = fixtures.users.map((x) => x.address);
     const amount = toWei(123);
-    await depositViaCildChainManager(
-      fixtures,
-      fixtures.sandBeneficiary.address,
-      amount
-    );
+    await depositViaCildChainManager(fixtures, sandBeneficiary, amount);
   });
   describe('transfer', function () {
     it('without metatx', async function () {
       const amount = 123;
       const pre = BigNumber.from(await fixtures.Sand.balanceOf(users[0]));
-      fixtures.sandBeneficiary.Sand.transfer(users[0], amount);
+      await fixtures.sandBeneficiary.Sand.transfer(users[0], amount);
       expect(await fixtures.Sand.balanceOf(users[0])).to.be.equal(
         pre.add(amount)
       );
@@ -52,26 +71,104 @@ describe('PolygonSand.sol Meta TX', function () {
       const amount = 123;
       const pre = BigNumber.from(await fixtures.Sand.balanceOf(users[0]));
 
-      const {data} = await fixtures.Sand.populateTransaction.transfer(
-        users[0],
-        amount
-      );
       // users[3] pay for the gas, of a message signed by sandBeneficiary.
-      const receipt = await sendMetaTx(
-        fixtures.Sand.address,
-        fixtures.users[3].TrustedForwarder,
-        data,
-        fixtures.sandBeneficiary.address
+      await sendMeta(
+        fixtures,
+        sandBeneficiary,
+        fixtures.Sand.populateTransaction.transfer(users[0], amount)
       );
-      const event = await expectEventWithArgsFromReceipt(
-        fixtures.TrustedForwarder,
-        receipt,
-        'TXResult'
-      );
-
-      expect(event.args.success).to.be.true;
       expect(await fixtures.Sand.balanceOf(users[0])).to.be.equal(
         pre.add(amount)
+      );
+    });
+  });
+
+  describe('approve and transferFrom', function () {
+    it('without metatx', async function () {
+      const amount = 123;
+      const preBalance = BigNumber.from(
+        await fixtures.Sand.balanceOf(users[0])
+      );
+      const preAllowance = BigNumber.from(
+        await fixtures.Sand.allowance(sandBeneficiary, users[0])
+      );
+      fixtures.sandBeneficiary.Sand.approve(users[0], amount);
+      expect(
+        await fixtures.Sand.allowance(
+          fixtures.sandBeneficiary.address,
+          users[0]
+        )
+      ).to.be.equal(preAllowance.add(amount));
+      fixtures.users[0].Sand.transferFrom(sandBeneficiary, users[0], amount);
+      expect(await fixtures.Sand.balanceOf(users[0])).to.be.equal(
+        preBalance.add(amount)
+      );
+    });
+    it('with metatx', async function () {
+      const amount = 123;
+      const preBalance = BigNumber.from(
+        await fixtures.Sand.balanceOf(users[0])
+      );
+      const preAllowance = BigNumber.from(
+        await fixtures.Sand.allowance(sandBeneficiary, users[0])
+      );
+      await sendMeta(
+        fixtures,
+        sandBeneficiary,
+        fixtures.Sand.populateTransaction.approve(users[0], amount)
+      );
+      expect(
+        await fixtures.Sand.allowance(
+          fixtures.sandBeneficiary.address,
+          users[0]
+        )
+      ).to.be.equal(preAllowance.add(amount));
+      await sendMeta(
+        fixtures,
+        users[0],
+        fixtures.Sand.populateTransaction.transferFrom(
+          sandBeneficiary,
+          users[0],
+          amount
+        )
+      );
+      expect(await fixtures.Sand.balanceOf(users[0])).to.be.equal(
+        preBalance.add(amount)
+      );
+    });
+  });
+
+  describe(' burn', function () {
+    it('without metatx', async function () {
+      const amount = 123;
+      const preSupply = BigNumber.from(await fixtures.Sand.totalSupply());
+      const pre = BigNumber.from(
+        await fixtures.Sand.balanceOf(sandBeneficiary)
+      );
+      await fixtures.sandBeneficiary.Sand.burn(amount);
+      expect(await fixtures.Sand.balanceOf(sandBeneficiary)).to.be.equal(
+        pre.sub(amount)
+      );
+      expect(await fixtures.Sand.totalSupply()).to.be.equal(
+        preSupply.sub(amount)
+      );
+    });
+    it('with metatx', async function () {
+      const amount = 123;
+      const preSupply = BigNumber.from(await fixtures.Sand.totalSupply());
+      const pre = BigNumber.from(
+        await fixtures.Sand.balanceOf(sandBeneficiary)
+      );
+      await sendMeta(
+        fixtures,
+        sandBeneficiary,
+        fixtures.Sand.populateTransaction.burn(amount)
+      );
+      expect(await fixtures.Sand.balanceOf(sandBeneficiary)).to.be.equal(
+        pre.sub(amount)
+      );
+      expect(await fixtures.Sand.totalSupply()).to.be.equal(
+        preSupply.sub(amount)
       );
     });
   });
