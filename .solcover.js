26a11076400e625b4ff208d21a1608833791b0cc
Andres Adjimann <andres@sandbox.game>
2021-08-26 17:50:28 -0300

    test: exclude solc_0.8/test from coverage

--- .solcover.js
+++ .solcover.js
@@ -3,5 +3,5 @@ module.exports = {
     grep: '@skip-on-coverage', // Find everything with this tag
     invert: true, // Run the grep's inverse set.
   },
-  skipFiles: ['solc_0.5', 'solc_0.6', 'solc_0.7'],
+  skipFiles: ['solc_0.8/test', 'solc_0.5', 'solc_0.6', 'solc_0.7'],
 };
