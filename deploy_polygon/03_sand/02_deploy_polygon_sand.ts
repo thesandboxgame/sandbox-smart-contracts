40d801e4ab210c8d28714bac3901a9fbe8c2f9b5
León <leon.isla.22@gmail.com>
2021-09-09 13:15:45 -0300

    fix: format

--- deploy_polygon/03_sand/02_deploy_polygon_sand.ts
+++ deploy_polygon/03_sand/02_deploy_polygon_sand.ts
@@ -6,7 +6,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
 
-  const {deployer,sandAdmin,sandExecutionAdmin} = await getNamedAccounts();
+  const {deployer, sandAdmin, sandExecutionAdmin} = await getNamedAccounts();
 
   const TRUSTED_FORWARDER = await deployments.get('TRUSTED_FORWARDER');
   const CHILD_CHAIN_MANAGER = await deployments.get('CHILD_CHAIN_MANAGER');
