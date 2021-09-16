82157a31959f59c6f70538383dc7976d13419153
León Isla <leon.isla.22@gmail.com>
2021-09-16 08:11:23 -0300

    Task/deploy twd land sales (#384)
    * add: rinkeby deployment
    
    * add: mainnet deployment wave 1
    
    * fix: format
    
    * update: redeploy wave 1 mainnet
    
    * update: wave 1 deployment
    
    * add: sector 28 deployment data (#390)
    
    * add: rinkeby deployment
    
    * add: mainnet deployment
    
    * Task/add wave 3 sector 29 data deployment (#398)
    
    * add: new sector data
    
    * add: wave 3 deployments
    
    * Chore/twd wave 4 sector 30 (#405)
    
    * add: deadline + sector data
    
    * add: wave 4 rinkeby deployment
    
    * add: wave 4 mainnet deployment
    
    Co-authored-by: mvanmeerbeck <mvanmeerbeck@theblockchainxdev.com>

---
+++ deploy/09_landsale/06_deploy_land_sale_10.ts
@@ -0,0 +1,73 @@
+import {
+  getDeadline,
+  getLandSales,
+  LandSale,
+  writeProofs,
+  setAsLandMinter,
+} from '../../data/landSales/getLandSales';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const LANDSALE_NAME = 'LandPreSale_10';
+
+const func: DeployFunction = async function (hre) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+  const {
+    deployer,
+    landSaleBeneficiary,
+    backendReferralWallet,
+    landSaleFeeRecipient,
+    landSaleAdmin,
+  } = await getNamedAccounts();
+
+  const sandContract = await deployments.get('Sand');
+  const landContract = await deployments.get('Land');
+  const assetContract = await deployments.get('Asset');
+
+  async function deployLandSale(landSale: LandSale) {
+    const {lands, merkleRootHash, sector} = landSale;
+
+    const landSaleName = `${LANDSALE_NAME}_${sector}`;
+    const deadline = getDeadline(hre, sector);
+
+    const landSaleDeployment = await deploy(landSaleName, {
+      from: deployer,
+      linkedData: lands,
+      contract: 'EstateSaleWithFee',
+      args: [
+        landContract.address,
+        sandContract.address,
+        sandContract.address,
+        landSaleAdmin,
+        landSaleBeneficiary,
+        merkleRootHash,
+        deadline,
+        backendReferralWallet,
+        2000,
+        '0x0000000000000000000000000000000000000000',
+        assetContract.address,
+        landSaleFeeRecipient,
+      ],
+      skipIfAlreadyDeployed: true,
+      log: true,
+    });
+
+    writeProofs(hre, landSaleName, landSale);
+
+    await setAsLandMinter(hre, landSaleDeployment.address);
+  }
+
+  const landSales = await getLandSales(
+    LANDSALE_NAME,
+    hre.network.name,
+    hre.network.live
+  );
+
+  for (const landSale of landSales) {
+    await deployLandSale(landSale);
+  }
+};
+
+export default func;
+func.tags = [LANDSALE_NAME, LANDSALE_NAME + '_deploy'];
+func.dependencies = ['Sand_deploy', 'Land_deploy', 'Asset_deploy'];
