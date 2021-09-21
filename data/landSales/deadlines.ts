da18349ab4459f65d85b8a6b4e6791502e7a88d8
León <leon.isla.22@gmail.com>
2021-09-21 10:34:52 -0300

    add: pororo wave deploy preparation

--- data/landSales/deadlines.ts
+++ data/landSales/deadlines.ts
@@ -15,5 +15,6 @@ const deadlines: {[sector: number]: number} =  {
   28: Date.UTC(2021, 8, 9, 13) / 1000, // Thursday, 9 September 2021 13:00:00 GMT+00:00
   29: Date.UTC(2021, 8, 16, 13) / 1000, // Thursday, 16 September 2021 13:00:00 GMT+00:00
   30: Date.UTC(2021, 8, 23, 13) / 1000, // Thursday, 23 September 2021 13:00:00 GMT+00:00
+  31: Date.UTC(2021, 8, 30, 13) / 1000, // Thursday, 30 September 2021 13:00:00 GMT+00:00
 };
 export default deadlines;
