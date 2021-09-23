e978ed5b7864023acb47abf08c889b6e01997aac
Arhaam Patvi <thearhaam@gmail.com>
2021-09-24 05:01:26 +0530

    feat: PolygonLand interface

---
+++ src/solc_0.8/common/interfaces/IPolygonLand.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+
+import "./ILandToken.sol";
+
+interface IPolygonLand is LandToken {
+    function mint(
+        address user,
+        uint256 size,
+        uint256 x,
+        uint256 y,
+        bytes memory data
+    ) external;
+
+    // @temp - Will remove once locking mechanism has been tested
+    // function exit(uint256 tokenId) external;
+}
