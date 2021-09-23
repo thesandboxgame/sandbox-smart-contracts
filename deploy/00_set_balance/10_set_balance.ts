edd16d7c73b38564bb892da6b0d2fcfc33245520
Andres Adjimann <andres@sandbox.game>
2021-09-23 16:10:16 -0300

    chore: lint

--- deploy/00_set_balance/10_set_balance.ts
+++ deploy/00_set_balance/10_set_balance.ts
@@ -1,25 +1,24 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
-import {ethers, getNamedAccounts, getUnnamedAccounts} from 'hardhat';
 import {skipUnlessTest} from '../../utils/network';
 import {toWei} from '../../test/utils';
 
-// Set the balance for forked networks.
+// Set a fake balance for forked networks.
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const initialAmount = toWei('1000').toHexString();
 
-  const named = await getNamedAccounts();
+  const named = await hre.getNamedAccounts();
   for (const c in named) {
     console.log('Setting', c, 'balance', initialAmount);
-    await ethers.provider.send('hardhat_setBalance', [
+    await hre.ethers.provider.send('hardhat_setBalance', [
       named[c],
       initialAmount.toString(),
     ]);
   }
-  const others = await getUnnamedAccounts();
+  const others = await hre.getUnnamedAccounts();
   for (const c of others) {
     console.log('Setting', c, 'balance', initialAmount);
-    await ethers.provider.send('hardhat_setBalance', [
+    await hre.ethers.provider.send('hardhat_setBalance', [
       c,
       initialAmount.toString(),
     ]);
@@ -28,5 +27,5 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 export default func;
 func.tags = ['SET_BALANCE'];
 func.skip = async (hre) => {
-  return (await skipUnlessTest(hre)) || !!process.env.SKIP_BALANCE_SETTING;
+  return (await skipUnlessTest(hre)) || !process.env.HARDHAT_FORK;
 };
