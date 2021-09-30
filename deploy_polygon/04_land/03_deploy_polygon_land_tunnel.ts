f54483e82c7b51b814935d8e61589ce2ad404d4e
Arhaam Patvi <thearhaam@gmail.com>
2021-09-30 17:45:47 +0530

    feat: setFxChildTunnel

--- deploy_polygon/04_land/03_deploy_polygon_land_tunnel.ts
+++ deploy_polygon/04_land/03_deploy_polygon_land_tunnel.ts
@@ -10,13 +10,25 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const FXCHILD = await deployments.get('FXCHILD');
   const PolygonLand = await deployments.get('PolygonLand');
 
-  await deploy('PolygonLandTunnel', {
+  const PolygonLandTunnel = await deploy('PolygonLandTunnel', {
     from: deployer,
     contract: 'PolygonLandTunnel',
     args: [FXCHILD.address, PolygonLand.address],
     log: true,
     skipIfAlreadyDeployed: true,
   });
+
+  const LandTunnel = await hre.companionNetworks['l1'].deployments.getOrNull(
+    'LandTunnel'
+  );
+  if (LandTunnel) {
+    await hre.companionNetworks['l1'].deployments.execute(
+      'LandTunnel',
+      {from: deployer},
+      'setFxChildTunnel',
+      [PolygonLandTunnel.address]
+    );
+  }
 };
 
 export default func;
