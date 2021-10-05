b37919f29493805ea8ad9d0456a72f6c8a1fd805
Andres Adjimann <andres@sandbox.game>
2021-10-05 14:35:25 -0300

    test: use withSnapshot

--- test/estate/fixtures.ts
+++ test/estate/fixtures.ts
@@ -7,7 +7,7 @@ import {
 import {Contract} from 'ethers';
 import {Address} from 'hardhat-deploy/dist/types';
 import {BigNumber} from '@ethersproject/bignumber';
-import {setupUser} from '../utils';
+import {setupUser, withSnapshot} from '../utils';
 
 type User = {address: string; childEstateTokenV1: Contract};
 export type Fixtures = {
@@ -19,34 +19,36 @@ export type Fixtures = {
   others: string[];
   accounts: {[name: string]: Address};
 };
-export const setupFixtures = deployments.createFixture(async function () {
-  await deployments.fixture(['Land', 'TRUSTED_FORWARDER']);
-  const trustedForwarder = await ethers.getContract('TRUSTED_FORWARDER');
-  const landContract: Contract = await ethers.getContract('Land');
-  const accounts = await getNamedAccounts();
-  const others = await getUnnamedAccounts();
-  await deployments.deploy('ChildEstateTokenV1', {
-    from: accounts.deployer,
-    args: [],
-  });
-  const childEstateTokenV1 = await ethers.getContract('ChildEstateTokenV1');
-  const chainIndex = BigNumber.from(213);
-  await childEstateTokenV1.initV1(
-    trustedForwarder.address,
-    landContract.address,
-    chainIndex
-  );
-  const deployer = await setupUser(accounts.deployer, {
-    childEstateTokenV1,
-    trustedForwarder,
-  });
-  return {
-    deployer,
-    trustedForwarder,
-    landContract,
-    chainIndex,
-    childEstateTokenV1,
-    others,
-    accounts,
-  };
-});
+export const setupFixtures = withSnapshot(
+  ['Land', 'TRUSTED_FORWARDER'],
+  async function () {
+    const trustedForwarder = await ethers.getContract('TRUSTED_FORWARDER');
+    const landContract: Contract = await ethers.getContract('Land');
+    const accounts = await getNamedAccounts();
+    const others = await getUnnamedAccounts();
+    await deployments.deploy('ChildEstateTokenV1', {
+      from: accounts.deployer,
+      args: [],
+    });
+    const childEstateTokenV1 = await ethers.getContract('ChildEstateTokenV1');
+    const chainIndex = BigNumber.from(213);
+    await childEstateTokenV1.initV1(
+      trustedForwarder.address,
+      landContract.address,
+      chainIndex
+    );
+    const deployer = await setupUser(accounts.deployer, {
+      childEstateTokenV1,
+      trustedForwarder,
+    });
+    return {
+      deployer,
+      trustedForwarder,
+      landContract,
+      chainIndex,
+      childEstateTokenV1,
+      others,
+      accounts,
+    };
+  }
+);
