42e416fda54f60357af59bd46913b00de1447b4b
Ayush Tiwari <ayush@sandbox.game>
2021-10-18 17:09:22 +0530

    feat: added comments to PolygonSandBatchDeposit contract

--- src/solc_0.8/polygon/root/PolygonSandBatchDeposit.sol
+++ src/solc_0.8/polygon/root/PolygonSandBatchDeposit.sol
@@ -11,6 +11,11 @@ contract PolygonSandBatchDeposit is Ownable {
         _polygonSand = polygonSand;
     }
 
+    /**
+     * @notice Mints the given value of sand to respective holder on polygonSand contract
+     * @param holders Array of token holder addresses
+     * @param values Array of Amount of tokens to be minted for corresponding addresses
+     */
     function batchMint(address[] calldata holders, uint256[] calldata values) external onlyOwner {
         require(holders.length == values.length, "Number of holders should be equal to number of values");
         for (uint256 i = 0; i < holders.length; i++) {
