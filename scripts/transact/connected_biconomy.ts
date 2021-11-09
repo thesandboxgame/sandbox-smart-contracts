bc914eb264d227a2ca87b6d27d496cebc37f4ff8
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2021-11-09 18:02:21 +0100

    wait for initialize

--- scripts/transact/connected_biconomy.ts
+++ scripts/transact/connected_biconomy.ts
@@ -1,55 +1,64 @@
-import {getNamedAccounts, ethers, deployments, hre} from 'hardhat';
+import {getNamedAccounts, ethers} from 'hardhat';
 import * as bico from '@biconomy/mexa';
 
-// this script allow to stop pending transaction
+/**
+ *  Testing Biconomy
+ *  yarn execute mumbai ./scripts/transact/connected_biconomy.ts
+ * */
 (async () => {
-  console.log('DSFSDFDFDF');
   const biconomy = new bico.Biconomy(ethers.provider, {
     apiKey: 'TVWVnKizm.0d4d3808-c6ce-4612-bbe1-a6b85f47d892',
     debug: true,
   });
-  console.log('LAALALLA');
+
   const ethersProvider = new ethers.providers.Web3Provider(biconomy);
 
   const {deployer, sandAdmin} = await getNamedAccounts();
 
-  // Initialize Constants
-  const sandContract = await ethers.getContract(
-    'PolygonSand',
-    biconomy.getSignerByAddress(deployer)
-  );
+  biconomy
+    .onEvent(biconomy.READY, async () => {
+      // Initialize Constants
+      const sandContract = await ethers.getContract(
+        'PolygonSand',
+        biconomy.getSignerByAddress(deployer)
+      );
 
-  const balance = await sandContract.balanceOf(deployer);
+      // Create your target method signature.. here we are calling setQuote() method of our contract
+      const {data} = await sandContract.populateTransaction.transfer(
+        sandAdmin,
+        10
+      );
+      const provider = biconomy.getEthersProvider();
 
-  console.log(balance);
+      const gasLimit = await provider.estimateGas({
+        to: sandContract.address,
+        from: deployer,
+        data: data,
+      });
+      console.log('Gas limit : ', gasLimit);
 
-  // Create your target method signature.. here we are calling setQuote() method of our contract
-  const {data} = await sandContract.populateTransaction.transfer(sandAdmin, 10);
-  const provider = biconomy.getEthersProvider();
+      const txParams = {
+        data: data,
+        to: sandContract.address,
+        from: deployer,
+        gasLimit: gasLimit, // optional
+        signatureType: 'EIP712_SIGN', // Or omit this because by default mexa will consider personal sign
+      };
 
-  const gasLimit = await provider.estimateGas({
-    to: sandContract.address,
-    from: deployer,
-    data: data,
-  });
-  console.log('Gas limit : ', gasLimit);
+      const tx = await provider.send('eth_sendTransaction', [txParams]);
+      console.log('Transaction hash : ', tx);
 
-  const txParams = {
-    data: data,
-    to: sandContract.address,
-    from: deployer,
-    gasLimit: gasLimit, // optional
-    signatureType: 'PERSONAL_SIGN', // Or omit this because by default mexa will consider personal sign
-  };
-
-  const tx = await provider.send('eth_sendTransaction', [txParams]);
-  console.log('Transaction hash : ', tx);
-
-  //event emitter methods
-  provider.once(tx, (transaction: any) => {
-    // Emitted when the transaction has been mined
-    //show success message
-    console.log(transaction);
-    //do something with transaction hash
-  });
+      //event emitter methods
+      provider.once(tx, (transaction: any) => {
+        // Emitted when the transaction has been mined
+        //show success message
+        console.log(transaction);
+        //do something with transaction hash
+      });
+    })
+    .onEvent(biconomy.ERROR, (error: string, message: string) => {
+      // Handle error while initializing mexa
+      console.log(message);
+      console.log(error);
+    });
 })();
