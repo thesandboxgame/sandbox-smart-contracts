a57e36249fa7ba2d367cc290dc77dca3ce98c12b
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-12-13 13:16:43 +0530

    chore: uncomment tests

--- test/polygon/land/land.test.ts
+++ test/polygon/land/land.test.ts
@@ -43,209 +43,209 @@ describe('PolygonLand.sol', function () {
           plotCount
         );
       });
-      //   it('should be able to tranfer 3x3 Land', async function () {
-      //     const {
-      //       Land,
-      //       landMinter,
-      //       users,
-      //       LandTunnel,
-      //       PolygonLand,
-      //     } = await setupLand();
-      //     const landHolder = users[0];
-      //     const size = 3;
-      //     const x = 0;
-      //     const y = 0;
-      //     const bytes = '0x00';
-      //     const plotCount = size * size;
+      it('should be able to tranfer 3x3 Land', async function () {
+        const {
+          Land,
+          landMinter,
+          users,
+          LandTunnel,
+          PolygonLand,
+        } = await setupLand();
+        const landHolder = users[0];
+        const size = 3;
+        const x = 0;
+        const y = 0;
+        const bytes = '0x00';
+        const plotCount = size * size;
 
-      //     // Mint LAND on L1
-      //     await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
+        // Mint LAND on L1
+        await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
 
-      //     // Transfer to L1 Tunnel
-      //     await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
-      //     await landHolder.LandTunnel.batchTransferQuadToL2(
-      //       landHolder.address,
-      //       size,
-      //       x,
-      //       y,
-      //       bytes
-      //     );
+        // Transfer to L1 Tunnel
+        await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
+        await landHolder.LandTunnel.batchTransferQuadToL2(
+          landHolder.address,
+          [size],
+          [x],
+          [y],
+          bytes
+        );
 
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
-      //     expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
-      //     expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
-      //       plotCount
-      //     );
-      //   });
-      //   it('should be able to tranfer 6x6 Land', async function () {
-      //     const {
-      //       Land,
-      //       landMinter,
-      //       users,
-      //       LandTunnel,
-      //       PolygonLand,
-      //     } = await setupLand();
-      //     const landHolder = users[0];
-      //     const size = 6;
-      //     const x = 0;
-      //     const y = 0;
-      //     const bytes = '0x00';
-      //     const plotCount = size * size;
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
+        expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
+        expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
+          plotCount
+        );
+      });
+      it('should be able to tranfer 6x6 Land', async function () {
+        const {
+          Land,
+          landMinter,
+          users,
+          LandTunnel,
+          PolygonLand,
+        } = await setupLand();
+        const landHolder = users[0];
+        const size = 6;
+        const x = 0;
+        const y = 0;
+        const bytes = '0x00';
+        const plotCount = size * size;
 
-      //     // Mint LAND on L1
-      //     await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
+        // Mint LAND on L1
+        await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
 
-      //     // Transfer to L1 Tunnel
-      //     await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
-      //     await landHolder.LandTunnel.batchTransferQuadToL2(
-      //       landHolder.address,
-      //       size,
-      //       x,
-      //       y,
-      //       bytes
-      //     );
+        // Transfer to L1 Tunnel
+        await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
+        await landHolder.LandTunnel.batchTransferQuadToL2(
+          landHolder.address,
+          [size],
+          [x],
+          [y],
+          bytes
+        );
 
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
-      //     expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
-      //     expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
-      //       plotCount
-      //     );
-      //   });
-      //   it('should be able to tranfer 12x12 Land', async function () {
-      //     const {
-      //       Land,
-      //       landMinter,
-      //       users,
-      //       LandTunnel,
-      //       PolygonLand,
-      //     } = await setupLand();
-      //     const landHolder = users[0];
-      //     const size = 12;
-      //     const x = 0;
-      //     const y = 0;
-      //     const bytes = '0x00';
-      //     const plotCount = size * size;
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
+        expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
+        expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
+          plotCount
+        );
+      });
+      it('should be able to tranfer 12x12 Land', async function () {
+        const {
+          Land,
+          landMinter,
+          users,
+          LandTunnel,
+          PolygonLand,
+        } = await setupLand();
+        const landHolder = users[0];
+        const size = 12;
+        const x = 0;
+        const y = 0;
+        const bytes = '0x00';
+        const plotCount = size * size;
 
-      //     // Mint LAND on L1
-      //     await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
+        // Mint LAND on L1
+        await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
 
-      //     // Transfer to L1 Tunnel
-      //     await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
-      //     await landHolder.LandTunnel.batchTransferQuadToL2(
-      //       landHolder.address,
-      //       size,
-      //       x,
-      //       y,
-      //       bytes
-      //     );
+        // Transfer to L1 Tunnel
+        await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
+        await landHolder.LandTunnel.batchTransferQuadToL2(
+          landHolder.address,
+          [size],
+          [x],
+          [y],
+          bytes
+        );
 
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
-      //     expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
-      //     expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
-      //       plotCount
-      //     );
-      //   });
-      //   it('should be able to tranfer 24x24 Land', async function () {
-      //     const {
-      //       Land,
-      //       landMinter,
-      //       users,
-      //       LandTunnel,
-      //       PolygonLand,
-      //     } = await setupLand();
-      //     const landHolder = users[0];
-      //     const size = 24;
-      //     const x = 0;
-      //     const y = 0;
-      //     const bytes = '0x00';
-      //     const plotCount = size * size;
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
+        expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
+        expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
+          plotCount
+        );
+      });
+      it('should be able to tranfer 24x24 Land', async function () {
+        const {
+          Land,
+          landMinter,
+          users,
+          LandTunnel,
+          PolygonLand,
+        } = await setupLand();
+        const landHolder = users[0];
+        const size = 24;
+        const x = 0;
+        const y = 0;
+        const bytes = '0x00';
+        const plotCount = size * size;
 
-      //     // Mint LAND on L1
-      //     await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
+        // Mint LAND on L1
+        await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
 
-      //     // Transfer to L1 Tunnel
-      //     await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
-      //     await landHolder.LandTunnel.batchTransferQuadToL2(
-      //       landHolder.address,
-      //       size,
-      //       x,
-      //       y,
-      //       bytes
-      //     );
+        // Transfer to L1 Tunnel
+        await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
+        await landHolder.LandTunnel.batchTransferQuadToL2(
+          landHolder.address,
+          [size],
+          [x],
+          [y],
+          bytes
+        );
 
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
-      //     expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
-      //     expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
-      //       plotCount
-      //     );
-      //   });
-      // });
-      // describe('L2 to L1', function () {
-      //   it('should be able to tranfer 1x1 Land', async function () {
-      //     const {
-      //       deployer,
-      //       Land,
-      //       landMinter,
-      //       users,
-      //       LandTunnel,
-      //       PolygonLand,
-      //       PolygonLandTunnel,
-      //       CheckpointManager,
-      //     } = await setupLand();
-      //     const landHolder = users[0];
-      //     const size = 1;
-      //     const x = 0;
-      //     const y = 0;
-      //     const bytes = '0x00';
-      //     const plotCount = size * size;
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
+        expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
+        expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
+          plotCount
+        );
+      });
+    });
+    describe('L2 to L1', function () {
+      it('should be able to tranfer 1x1 Land', async function () {
+        const {
+          deployer,
+          Land,
+          landMinter,
+          users,
+          LandTunnel,
+          PolygonLand,
+          PolygonLandTunnel,
+          CheckpointManager,
+        } = await setupLand();
+        const landHolder = users[0];
+        const size = 1;
+        const x = 0;
+        const y = 0;
+        const bytes = '0x00';
+        const plotCount = size * size;
 
-      //     // Mint LAND on L1
-      //     await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
+        // Mint LAND on L1
+        await landMinter.Land.mintQuad(landHolder.address, size, x, y, bytes);
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
 
-      //     // Transfer to L1 Tunnel
-      //     await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
-      //     await landHolder.LandTunnel.batchTransferQuadToL2(
-      //       landHolder.address,
-      //       size,
-      //       x,
-      //       y,
-      //       bytes
-      //     );
+        // Transfer to L1 Tunnel
+        await landHolder.Land.setApprovalForAll(LandTunnel.address, true);
+        await landHolder.LandTunnel.batchTransferQuadToL2(
+          landHolder.address,
+          [size],
+          [x],
+          [y],
+          bytes
+        );
 
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
-      //     expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
-      //     expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
-      //       plotCount
-      //     );
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(0);
+        expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(plotCount);
+        expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(
+          plotCount
+        );
 
-      //     // Transfer to L2 Tunnel
-      //     await landHolder.PolygonLand.setApprovalForAll(
-      //       PolygonLandTunnel.address,
-      //       true
-      //     );
-      //     const tx = await landHolder.PolygonLandTunnel.transferQuadToL1(
-      //       landHolder.address,
-      //       size,
-      //       x,
-      //       y,
-      //       bytes
-      //     );
-      //     await tx.wait();
+        // Transfer to L2 Tunnel
+        await landHolder.PolygonLand.setApprovalForAll(
+          PolygonLandTunnel.address,
+          true
+        );
+        const tx = await landHolder.PolygonLandTunnel.transferQuadToL1(
+          landHolder.address,
+          size,
+          x,
+          y,
+          bytes
+        );
+        await tx.wait();
 
-      //     console.log('DUMMY CHECKPOINT. moving on...');
+        console.log('DUMMY CHECKPOINT. moving on...');
 
-      //     // Release on L1
+        // Release on L1
 
-      //     await deployer.LandTunnel.receiveMessage({});
-      //     expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
-      //     expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(0);
-      //     expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(0);
-      //   });
+        await deployer.LandTunnel.receiveMessage({});
+        expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
+        expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(0);
+        expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(0);
+      });
     });
   });
 });
