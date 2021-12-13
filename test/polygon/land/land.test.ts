b3003afaea4889f1ad3b7aef2fb67f5c3c2bb9dc
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-12-13 12:59:16 +0530

    feat: remove checkpoint tests

--- test/polygon/land/land.test.ts
+++ test/polygon/land/land.test.ts
@@ -1,16 +1,5 @@
-import {ethers} from 'hardhat';
 import {expect} from '../../chai-setup';
 import {setupLand} from './fixtures';
-import {bufferToHex, rlp} from 'ethereumjs-util';
-import MerkleTree from './utils/merkle-tree';
-import {
-  getTxBytes,
-  getReceiptBytes,
-  getReceiptProof,
-  getTxProof,
-  verifyTxProof,
-} from './utils/proofs';
-import {getBlockHeader} from './utils/blocks';
 
 describe('PolygonLand.sol', function () {
   describe('Land <> PolygonLand: Transfer', function () {
@@ -242,42 +231,13 @@ describe('PolygonLand.sol', function () {
           y,
           bytes
         );
-        const receipt = await tx.wait();
+        await tx.wait();
 
-        // Emulate Checkpoint
-        const block = await ethers.provider.getBlock(tx.blockHash);
-        const event = {
-          tx,
-          receipt,
-          block,
-        };
-        const checkpointData = await build(event);
-        const root = bufferToHex(checkpointData.header.root);
-        await deployer.CheckpointManager.setCheckpoint(
-          root,
-          tx.blockNumber,
-          tx.blockNumber
-        );
-
-        console.log('moving on...');
+        console.log('DUMMY CHECKPOINT. moving on...');
 
         // Release on L1
-        const logIndex = 0;
-        const data = bufferToHex(
-          rlp.encode([
-            headerNumber,
-            bufferToHex(Buffer.concat(checkpointData.proof)),
-            checkpointData.number,
-            checkpointData.timestamp,
-            bufferToHex(checkpointData.transactionsRoot),
-            bufferToHex(checkpointData.receiptsRoot),
-            bufferToHex(checkpointData.receipt),
-            bufferToHex(rlp.encode(checkpointData.receiptParentNodes)),
-            bufferToHex(checkpointData.path),
-            logIndex,
-          ])
-        );
-        await deployer.LandTunnel.receiveMessage(data);
+
+        await deployer.LandTunnel.receiveMessage({});
         expect(await Land.balanceOf(landHolder.address)).to.be.equal(plotCount);
         expect(await Land.balanceOf(LandTunnel.address)).to.be.equal(0);
         expect(await PolygonLand.balanceOf(landHolder.address)).to.be.equal(0);
@@ -285,46 +245,3 @@ describe('PolygonLand.sol', function () {
     });
   });
 });
-
-let headerNumber = 0;
-async function build(event: any) {
-  const blockHeader = getBlockHeader(event.block);
-  const tree = new MerkleTree([blockHeader]);
-  const receiptProof: any = await getReceiptProof(
-    event.receipt,
-    event.block,
-    null /* web3 */,
-    [event.receipt]
-  );
-  const blockTxns = [];
-  for (let i = 0; i < event.block.transactions.length; i++) {
-    const tx = await ethers.provider.getTransaction(
-      event.block.transactions[i]
-    );
-    blockTxns.push(tx);
-  }
-  event.block.transactions = blockTxns;
-  const txProof: any = await getTxProof(event.tx, event.block);
-  console.log('TX PROOF');
-  console.log(txProof);
-  expect(verifyTxProof(receiptProof)).to.be.ok;
-
-  headerNumber += 1;
-  return {
-    header: {
-      number: headerNumber,
-      root: tree.getRoot(),
-      start: event.receipt.blockNumber,
-    },
-    receipt: getReceiptBytes(event.receipt), // rlp encoded
-    receiptParentNodes: receiptProof.parentNodes,
-    tx: getTxBytes(event.tx), // rlp encoded
-    txParentNodes: txProof.parentNodes,
-    path: Buffer.concat([Buffer.from('00', 'hex'), receiptProof.path]),
-    number: event.receipt.blockNumber,
-    timestamp: event.block.timestamp,
-    transactionsRoot: Buffer.from(event.block.transactionsRoot.slice(2), 'hex'),
-    receiptsRoot: Buffer.from(event.block.receiptsRoot.slice(2), 'hex'),
-    proof: await tree.getProof(blockHeader),
-  };
-}
