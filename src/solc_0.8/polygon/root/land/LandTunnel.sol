106a7959cd66b99e28399e7d3748920df0172699
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-01-10 11:19:47 +0530

    feat: added max gas limit on l2

--- src/solc_0.8/polygon/root/land/LandTunnel.sol
+++ src/solc_0.8/polygon/root/land/LandTunnel.sol
@@ -4,14 +4,43 @@ pragma solidity 0.8.2;
 import "fx-portal/contracts/tunnel/FxBaseRootTunnel.sol";
 import "../../../common/interfaces/ILandToken.sol";
 import "../../../common/interfaces/IERC721TokenReceiver.sol";
+import "@openzeppelin/contracts-0.8/access/Ownable.sol";
 
 // @todo - natspec comments
 
-contract LandTunnel is FxBaseRootTunnel, IERC721TokenReceiver {
+contract LandTunnel is FxBaseRootTunnel, IERC721TokenReceiver, Ownable {
     address public rootToken;
+    uint32 public maxGasLimitOnL2 = 500;
+    mapping(uint8 => uint32) public gasLimits;
 
     event Deposit(address user, uint256 size, uint256 x, uint256 y, bytes data);
     event Withdraw(address user, uint256 size, uint256 x, uint256 y, bytes data);
+
+    event SetGasLimit(uint8 size, uint32 limit);
+    event SetMaxGasLimit(uint32 maxGasLimit);
+
+    function setMaxLimitOnL2(uint32 _maxGasLimit) external onlyOwner {
+        maxGasLimitOnL2 = _maxGasLimit;
+        emit SetMaxGasLimit(_maxGasLimit);
+    }
+
+    function _setLimit(uint8 size, uint32 limit) internal {
+        gasLimits[size] = limit;
+        emit SetGasLimit(size, limit);
+    }
+
+    function setLimit(uint8 size, uint32 limit) external onlyOwner {
+        _setLimit(size, limit);
+    }
+
+    // setupLimits([5, 10, 20, 90, 340]);
+    function setupLimits(uint32[5] calldata limits) external onlyOwner {
+        _setLimit(1, limits[0]);
+        _setLimit(3, limits[1]);
+        _setLimit(6, limits[2]);
+        _setLimit(12, limits[3]);
+        _setLimit(24, limits[4]);
+    }
 
     constructor(
         address _checkpointManager,
@@ -39,6 +68,14 @@ contract LandTunnel is FxBaseRootTunnel, IERC721TokenReceiver {
     ) public {
         require(sizes.length == xs.length && xs.length == ys.length, "l2: invalid data");
         LandToken(rootToken).batchTransferQuad(msg.sender, address(this), sizes, xs, ys, data);
+
+        uint32 gasLimit = 0;
+        for (uint256 i = 0; i < sizes.length; i++) {
+            gasLimit += gasLimits[uint8(sizes[i])];
+        }
+
+        require(gasLimit < maxGasLimitOnL2, "Exceeds gas limit on L2.");
+
         for (uint256 index = 0; index < sizes.length; index++) {
             bytes memory message = abi.encode(to, sizes[index], xs[index], ys[index], data);
             _sendMessageToChild(message);
