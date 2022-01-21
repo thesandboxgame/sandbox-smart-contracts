59ecac713d0e5a45158750fc7726c4e57ee9e5f7
Ayush Tiwari <ayush@sandbox.game>
2022-01-21 14:09:21 +0530

    feat: modified polygon land deployment script for deployment via proxy

--- deploy_polygon/04_land/02_deploy_polygon_land.ts
+++ deploy_polygon/04_land/02_deploy_polygon_land.ts
@@ -4,13 +4,21 @@ import {DeployFunction} from 'hardhat-deploy/types';
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
-  const {deployer} = await getNamedAccounts();
+  const {deployer, upgradeAdmin} = await getNamedAccounts();
 
   await deploy('PolygonLand', {
     from: deployer,
     contract: 'PolygonLand',
+    proxy: {
+      owner: upgradeAdmin,
+      proxyContract: 'OpenZeppelinTransparentProxy',
+      execute: {
+        methodName: 'initialize',
+        args: [],
+      },
+      upgradeIndex: 0,
+    },
     log: true,
-    skipIfAlreadyDeployed: true,
   });
 };
 export default func;
