44c9f47ac9a9f512eff7b6915314fbf9db4026e6
León <leon.isla.22@gmail.com>
2022-02-08 17:22:46 -0300

    add: mainnet deployment

--- scripts/transact/connected_cancel_tx.ts
+++ scripts/transact/connected_cancel_tx.ts
@@ -5,6 +5,8 @@ const {rawTx} = deployments;
 // this script allow to stop pending transaction
 (async () => {
   const {deployer} = await getNamedAccounts();
+  console.log("Fetching nonce for account:", deployer);
   const nonce = await ethers.provider.getTransactionCount(deployer, 'latest');
+  console.log("Nonce:", nonce);
   await rawTx({from: deployer, to: deployer, nonce});
 })();
