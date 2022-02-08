443a32ce3616887d4458b1dc40d15b7f4fd9a9ed
León <leon.isla.22@gmail.com>
2022-02-08 17:25:09 -0300

    fix: format

--- scripts/transact/connected_cancel_tx.ts
+++ scripts/transact/connected_cancel_tx.ts
@@ -5,8 +5,8 @@ const {rawTx} = deployments;
 // this script allow to stop pending transaction
 (async () => {
   const {deployer} = await getNamedAccounts();
-  console.log("Fetching nonce for account:", deployer);
+  console.log('Fetching nonce for account:', deployer);
   const nonce = await ethers.provider.getTransactionCount(deployer, 'latest');
-  console.log("Nonce:", nonce);
+  console.log('Nonce:', nonce);
   await rawTx({from: deployer, to: deployer, nonce});
 })();
