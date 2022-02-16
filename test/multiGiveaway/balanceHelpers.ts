c94f2312cd0bf4c37b57a66413523d108d5d4232
atkinsonholly <atkinsonholly@gmail.com>
2022-02-16 09:25:24 +0000

    loop balance helpers

--- test/multiGiveaway/balanceHelpers.ts
+++ test/multiGiveaway/balanceHelpers.ts
@@ -19,34 +19,17 @@ export const testInitialAssetAndLandBalances = async (
   landContract: Contract,
   giveawayContract: Contract
 ): Promise<void> => {
-  const initBalanceAssetId1 = await assetContract['balanceOf(address,uint256)'](
-    giveawayContract.address,
-    claim.erc1155[0].ids[0]
-  );
-  expect(initBalanceAssetId1).to.equal(claim.erc1155[0].values[0]);
-  const initBalanceAssetId2 = await assetContract['balanceOf(address,uint256)'](
-    giveawayContract.address,
-    claim.erc1155[0].ids[1]
-  );
-  expect(initBalanceAssetId2).to.equal(claim.erc1155[0].values[1]);
-  const initBalanceAssetId3 = await assetContract['balanceOf(address,uint256)'](
-    giveawayContract.address,
-    claim.erc1155[0].ids[2]
-  );
-  expect(initBalanceAssetId3).to.equal(claim.erc1155[0].values[2]);
+  for (let i = 0; i < 3; i++) {
+    const initBalanceAssetId = await assetContract[
+      'balanceOf(address,uint256)'
+    ](giveawayContract.address, claim.erc1155[0].ids[i]);
+    expect(initBalanceAssetId).to.equal(claim.erc1155[0].values[i]);
+  }
 
-  const originalOwnerLandId1 = await landContract.ownerOf(0);
-  expect(originalOwnerLandId1).to.equal(giveawayContract.address);
-  const originalOwnerLandId2 = await landContract.ownerOf(1);
-  expect(originalOwnerLandId2).to.equal(giveawayContract.address);
-  const originalOwnerLandId3 = await landContract.ownerOf(2);
-  expect(originalOwnerLandId3).to.equal(giveawayContract.address);
-  const originalOwnerLandId4 = await landContract.ownerOf(3);
-  expect(originalOwnerLandId4).to.equal(giveawayContract.address);
-  const originalOwnerLandId5 = await landContract.ownerOf(4);
-  expect(originalOwnerLandId5).to.equal(giveawayContract.address);
-  const originalOwnerLandId6 = await landContract.ownerOf(5);
-  expect(originalOwnerLandId6).to.equal(giveawayContract.address);
+  for (let i = 0; i < 6; i++) {
+    const originalOwnerLandId = await landContract.ownerOf(i);
+    expect(originalOwnerLandId).to.equal(giveawayContract.address);
+  }
 };
 
 export const testInitialERC20Balance = async (
@@ -63,34 +46,17 @@ export const testFinalAssetAndLandBalances = async (
   assetContract: Contract,
   landContract: Contract
 ): Promise<void> => {
-  const balanceAssetId1 = await assetContract['balanceOf(address,uint256)'](
-    user,
-    claim.erc1155[0].ids[0]
-  );
-  expect(balanceAssetId1).to.equal(claim.erc1155[0].values[0]);
-  const balanceAssetId2 = await assetContract['balanceOf(address,uint256)'](
-    user,
-    claim.erc1155[0].ids[1]
-  );
-  expect(balanceAssetId2).to.equal(claim.erc1155[0].values[1]);
-  const balanceAssetId3 = await assetContract['balanceOf(address,uint256)'](
-    user,
-    claim.erc1155[0].ids[2]
-  );
-  expect(balanceAssetId3).to.equal(claim.erc1155[0].values[2]);
-
-  const ownerLandId1 = await landContract.ownerOf(0);
-  expect(ownerLandId1).to.equal(claim.to);
-  const ownerLandId2 = await landContract.ownerOf(1);
-  expect(ownerLandId2).to.equal(claim.to);
-  const ownerLandId3 = await landContract.ownerOf(2);
-  expect(ownerLandId3).to.equal(claim.to);
-  const ownerLandId4 = await landContract.ownerOf(3);
-  expect(ownerLandId4).to.equal(claim.to);
-  const ownerLandId5 = await landContract.ownerOf(4);
-  expect(ownerLandId5).to.equal(claim.to);
-  const ownerLandId6 = await landContract.ownerOf(5);
-  expect(ownerLandId6).to.equal(claim.to);
+  for (let i = 0; i < 3; i++) {
+    const balanceAssetId = await assetContract['balanceOf(address,uint256)'](
+      user,
+      claim.erc1155[0].ids[i]
+    );
+    expect(balanceAssetId).to.equal(claim.erc1155[0].values[i]);
+  }
+  for (let i = 0; i < 6; i++) {
+    const ownerLandId = await landContract.ownerOf(i);
+    expect(ownerLandId).to.equal(claim.to);
+  }
 };
 
 export const testUpdatedERC20Balance = async (
