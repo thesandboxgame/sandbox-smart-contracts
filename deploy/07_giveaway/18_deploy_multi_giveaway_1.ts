033c4edc40fb21276c8a45eb6ee998262f838156
atkinsonholly <atkinsonholly@gmail.com>
2022-02-22 10:16:35 +0000

    use trusted forwarder contracts for deployments

--- deploy/07_giveaway/18_deploy_multi_giveaway_1.ts
+++ deploy/07_giveaway/18_deploy_multi_giveaway_1.ts
@@ -6,12 +6,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deploy} = deployments;
   const {deployer, multiGiveawayAdmin} = await getNamedAccounts();
 
+  const TRUSTED_FORWARDER_V2 = await deployments.get('TRUSTED_FORWARDER_V2');
+
   await deploy('Multi_Giveaway_1', {
     contract: 'MultiGiveaway',
     from: deployer,
     log: true,
     skipIfAlreadyDeployed: true,
-    args: [multiGiveawayAdmin, multiGiveawayAdmin], // admin, trustedForwarder
+    args: [multiGiveawayAdmin, TRUSTED_FORWARDER_V2.address], // admin, trustedForwarder
   });
 };
 export default func;
