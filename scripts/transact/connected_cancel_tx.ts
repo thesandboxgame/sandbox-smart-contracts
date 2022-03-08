30f66256f4de1496432edddf8b34e9bd5d72035f
León Isla <leon.isla.22@gmail.com>
2022-03-08 09:14:48 -0300

    add: land sale 39 (#585)

--- scripts/transact/connected_cancel_tx.ts
+++ scripts/transact/connected_cancel_tx.ts
@@ -2,10 +2,17 @@ import {getNamedAccounts, ethers, deployments} from 'hardhat';
 
 const {rawTx} = deployments;
 
+const args = process.argv.slice(2);
+const txHash = args[0];
+
 // this script allow to stop pending transaction
 (async () => {
   const {deployer} = await getNamedAccounts();
   console.log('Fetching nonce for account:', deployer);
+  if (txHash) {
+    const tx = await ethers.provider.getTransaction(txHash);
+    console.log(tx);
+  }
   const nonce = await ethers.provider.getTransactionCount(deployer, 'latest');
   console.log('Nonce:', nonce);
   await rawTx({from: deployer, to: deployer, nonce});
