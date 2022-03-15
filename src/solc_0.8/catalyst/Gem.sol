c161d1f2a884593ccd8a2f79352adabb77383ce7
Lucas <luc4rd@gmail.com>
2022-03-15 15:48:20 +0100

    taking off OZ ERC20

--- src/solc_0.8/catalyst/Gem.sol
+++ src/solc_0.8/catalyst/Gem.sol
@@ -1,26 +1,18 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.2;
 
-//import "../common/BaseWithStorage/ERC20/ERC20Token.sol";
-import "@openzeppelin/contracts-0.8/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts-0.8/access/AccessControl.sol";
+import "../common/BaseWithStorage/ERC20/ERC20Token.sol";
 
-contract Gem is ERC20Burnable, AccessControl {
+contract Gem is ERC20Token {
     uint16 public immutable gemId;
 
     constructor(
         string memory name,
         string memory symbol,
         address admin,
-        uint16 _gemId
-    )
-        /* address operator */
-        ERC20(
-            name,
-            symbol /* admin, operator */
-        )
-    {
-        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+        uint16 _gemId,
+        address operator
+    ) ERC20Token(name, symbol, admin, operator) {
         gemId = _gemId;
     }
 }
--- src/solc_0.8/catalyst/GemsCatalystsRegistry.sol
+++ src/solc_0.8/catalyst/GemsCatalystsRegistry.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+git //SPDX-License-Identifier: MIT
 pragma solidity 0.8.2;
 pragma experimental ABIEncoderV2;
 
