0af1065b045668fb094f205464be2f8be62f1e4a
Ayush Tiwari <ayush@sandbox.game>
2022-03-16 18:49:25 +0530

    feat: added MintableERC1155 interface to AssetERC1155

--- src/solc_0.8/assetERC1155/AssetERC1155.sol
+++ src/solc_0.8/assetERC1155/AssetERC1155.sol
@@ -1,12 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.2;
 
-import "../common/interfaces/IAssetAttributesRegistry.sol";
 import "./AssetBaseERC1155.sol";
 import "../asset/libraries/AssetHelper.sol";
+import "../common/interfaces/IAssetAttributesRegistry.sol";
+import "../common/interfaces/@maticnetwork/pos-portal/root/RootToken/IMintableERC1155.sol";
 
 // solhint-disable-next-line no-empty-blocks
-contract AssetERC1155 is AssetBaseERC1155 {
+contract AssetERC1155 is AssetBaseERC1155, IMintableERC1155 {
     /// @notice fulfills the purpose of a constructor in upgradeable contracts
     function initialize(
         address trustedForwarder,
@@ -16,6 +17,28 @@ contract AssetERC1155 is AssetBaseERC1155 {
         uint8 chainIndex
     ) external {
         init(trustedForwarder, admin, bouncerAdmin, predicate, chainIndex);
+    }
+
+    /**
+     * @notice Creates `amount` tokens of token type `id`, and assigns them to `account`.
+     * @dev Should be callable only by MintableERC1155Predicate
+     * Make sure minting is done only by this function
+     * @param account user address for whom token is being minted
+     * @param id token which is being minted
+     * @param amount amount of token being minted
+     * @param data extra byte data to be accompanied with minted tokens
+     */
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) external override {
+        require(_msgSender() == _predicate, "!PREDICATE");
+        uint256 uriId = id & ERC1155ERC721Helper.URI_ID;
+        // _metadataHash[uriId] = hashes[i];  // TODO
+        _rarityPacks[uriId] = "0x00";
+        _mint(_msgSender(), account, id, amount, data);
     }
 
     /// @notice called by predicate to mint tokens transferred from L2
@@ -28,7 +51,7 @@ contract AssetERC1155 is AssetBaseERC1155 {
         uint256[] calldata ids,
         uint256[] calldata amounts,
         bytes calldata data
-    ) external {
+    ) external override {
         require(_msgSender() == _predicate, "!PREDICATE");
         for (uint256 i = 0; i < ids.length; i++) {
             uint256 uriId = ids[i] & ERC1155ERC721Helper.URI_ID;
