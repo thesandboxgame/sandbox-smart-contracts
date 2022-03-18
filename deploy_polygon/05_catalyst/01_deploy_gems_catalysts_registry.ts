29c07b4f3b6e3e6a6784e6d520f082f22b4cd1bf
Lucas <luc4rd@gmail.com>
2022-03-18 09:52:24 +0100

    deployments with initializer

--- deploy_polygon/05_catalyst/01_deploy_gems_catalysts_registry.ts
+++ deploy_polygon/05_catalyst/01_deploy_gems_catalysts_registry.ts
@@ -4,6 +4,7 @@ import {DeployFunction} from 'hardhat-deploy/types';
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
+  const {upgradeAdmin} = await getNamedAccounts();
 
   const TRUSTED_FORWARDER = await deployments.get('TRUSTED_FORWARDER');
 
@@ -11,8 +12,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   await deploy(`PolygonGemsCatalystsRegistry`, {
     from: deployer,
     log: true,
-    args: [deployer, TRUSTED_FORWARDER.address],
     contract: `GemsCatalystsRegistry`,
+    proxy: {
+      owner: upgradeAdmin, //is this correct?
+      proxyContract: 'OpenZeppelinTransparentProxy',
+      execute: {
+        methodName: 'initV1',
+        args: [deployer, TRUSTED_FORWARDER.address],
+      },
+      upgradeIndex: 0,
+    },
+    skipIfAlreadyDeployed: true,
   });
 };
 export default func;
