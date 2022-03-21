b03fc295b939915ba850a5838d293953cb28641d
atkinsonholly <atkinsonholly@gmail.com>
2022-03-21 14:25:11 +0000

    ERC721 mint function minter role updates

--- src/solc_0.8/polygon/child/asset/PolygonAssetERC721.sol
+++ src/solc_0.8/polygon/child/asset/PolygonAssetERC721.sol
@@ -30,7 +30,7 @@ contract PolygonAssetERC721 is BaseERC721 {
 
     /// @notice Creates a new token for `to`
     /// @param to The address that will receive a new token
-    /// @dev Minting is only permitted to MINTER_ROLE
+    /// @dev Minting is only permitted to MINTER_ROLE TODO: confirm roles
     /// @param id The id of the new token
     /// @param data Associated token metadata, which is decoded & used to set the token's metadata hash.
     function mint(
@@ -40,17 +40,6 @@ contract PolygonAssetERC721 is BaseERC721 {
     ) public override onlyRole(MINTER_ROLE) {
         require(!withdrawnTokens[id], "TOKEN_EXISTS_ON_ROOT_CHAIN");
         BaseERC721.mint(to, id, data);
-        emit Minted(to, id);
-    }
-
-    /// TODO: remove as mint without data not needed?
-    /// @notice Creates a new token for `to`
-    /// @param to The address that will receive a new token
-    /// @dev Minting is only permitted to MINTER_ROLE
-    /// @param id The id of the new token
-    function mint(address to, uint256 id) public override onlyRole(MINTER_ROLE) {
-        require(!withdrawnTokens[id], "TOKEN_EXISTS_ON_ROOT_CHAIN");
-        BaseERC721.mint(to, id);
         emit Minted(to, id);
     }
 
