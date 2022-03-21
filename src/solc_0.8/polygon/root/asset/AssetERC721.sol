c05d5783d0b96126fe58f8e0bb8663a54b5be6b8
atkinsonholly <atkinsonholly@gmail.com>
2022-03-21 11:37:25 +0000

    feat: setMetadata and tokenURI for erc721

--- src/solc_0.8/polygon/root/asset/AssetERC721.sol
+++ src/solc_0.8/polygon/root/asset/AssetERC721.sol
@@ -9,6 +9,9 @@ import {IRootERC721} from "../../../common/interfaces/@maticnetwork/pos-portal/r
 /// @dev This contract supports meta transactions.
 /// @dev This contract is final, don't inherit from it.
 contract AssetERC721 is BaseERC721, IRootERC721 {
+    bytes32 public constant METADATA_ROLE = keccak256("METADATA_ROLE");
+    uint256 public constant PACK_INDEX = 0x00000000000000000000000000000000000000000000000000000000000007FF;
+
     /// @notice fulfills the purpose of a constructor in upgradeable contracts
     function initialize(address trustedForwarder, address admin) public initializer {
         _setupRole(DEFAULT_ADMIN_ROLE, admin);
@@ -16,11 +19,69 @@ contract AssetERC721 is BaseERC721, IRootERC721 {
         __ERC721_init("Sandbox's ASSETs ERC721", "ASSETERC721");
     }
 
-    /// @dev Decode metadata obtained from L2 and attempt to set it for this `tokenId`
-    /// @dev pos-portal default implementation
-    function setTokenMetadata(uint256 tokenId, bytes memory data) external override {
-        // TODO: role
-        string memory uri = abi.decode(data, (string));
-        // TODO: _setTokenURI(tokenId, uri);
+    /// @notice Set the metadatahash for a given token id.
+    /// @dev The metadata hash for the ERC721 may need to be manually set or overridden.
+    /// @param id The token id.
+    /// @param data The metadatahash to be used for the token id.
+    function setTokenMetadata(uint256 id, bytes memory data) external override onlyRole(METADATA_ROLE) {
+        _setTokenMetadataHash(id, data);
+    }
+
+    /// @notice A distinct Uniform Resource Identifier (URI) for a given asset.
+    /// @param id The token to get the uri of.
+    /// @return URI The token's URI string.
+    function tokenURI(uint256 id) public view override returns (string memory) {
+        require(ownerOf(id) != address(0), "ZERO_ADDRESS");
+        return
+            string(
+                abi.encodePacked(
+                    "ipfs://bafybei",
+                    hash2base32(metadataHashes[id]),
+                    "/",
+                    uint2str(id & PACK_INDEX),
+                    ".json"
+                )
+            );
+    }
+
+    /// @dev Helper functions to obtain full tokenURI found below
+
+    bytes32 private constant base32Alphabet = 0x6162636465666768696A6B6C6D6E6F707172737475767778797A323334353637;
+
+    // solium-disable-next-line security/no-assign-params
+    function hash2base32(bytes32 hash) private pure returns (string memory _uintAsString) {
+        uint256 _i = uint256(hash);
+        uint256 k = 52;
+        bytes memory bstr = new bytes(k);
+        bstr[--k] = base32Alphabet[uint8((_i % 8) << 2)]; // uint8 s = uint8((256 - skip) % 5);  // (_i % (2**s)) << (5-s)
+        _i /= 8;
+        while (k > 0) {
+            bstr[--k] = base32Alphabet[_i % 32];
+            _i /= 32;
+        }
+        return string(bstr);
+    }
+
+    // solium-disable-next-line security/no-assign-params
+    function uint2str(uint256 _i) public pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+
+        bytes memory bstr = new bytes(len);
+        uint256 k = len;
+        while (_i != 0) {
+            bstr[--k] = bytes1(uint8(48 + uint8(_i % 10)));
+            _i /= 10;
+        }
+
+        return string(bstr);
     }
 }
