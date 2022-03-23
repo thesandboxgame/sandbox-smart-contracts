fce54c29af36227fa16edf844ae8061de4492d75
León <leon.isla.22@gmail.com>
2022-03-23 12:55:59 -0300

    update: redeploy sandbox minter with 0 fees

---
+++ deploy/12_old_catalysts/08_setup_catalysts_data.ts
@@ -0,0 +1,76 @@
+import {DeployFunction} from 'hardhat-deploy/types';
+import {BigNumber} from '@ethersproject/bignumber';
+
+const catalysts: {[key: string]: string}[] = [
+  {
+    name: 'Common',
+    sandMintingFee: '0',
+    sandUpdateFee: '0',
+    minQuantity: '1',
+    maxQuantity: '65535',
+  },
+  {
+    name: 'Rare',
+    sandMintingFee: '0',
+    sandUpdateFee: '0',
+    minQuantity: '1',
+    maxQuantity: '65535',
+  },
+  {
+    name: 'Epic',
+    sandMintingFee: '0',
+    sandUpdateFee: '0',
+    minQuantity: '1',
+    maxQuantity: '65535',
+  },
+  {
+    name: 'Legendary',
+    sandMintingFee: '0',
+    sandUpdateFee: '0',
+    minQuantity: '1',
+    maxQuantity: '65535',
+  },
+];
+
+const func: DeployFunction = async function (hre) {
+  const {deployments, getNamedAccounts} = hre;
+  const {read, execute, catchUnknownSigner} = deployments;
+
+  const {deployer} = await getNamedAccounts();
+
+  for (let i = 0; i < 4; i++) {
+    const config = await read('OldCatalysts', 'getMintData', i);
+    const setup = catalysts[i];
+    console.log(`${i} ${setup.name}`);
+    const keys = [
+      'minQuantity',
+      'maxQuantity',
+      'sandMintingFee',
+      'sandUpdateFee',
+    ];
+    if (
+      keys.some((key) => {
+        console.log(`${key} ${config[key]} ${setup[key]}`);
+        return !BigNumber.from(setup[key]).eq(BigNumber.from(config[key]));
+      })
+    ) {
+      console.log(`Updating minting data for ${setup.name}`);
+      await catchUnknownSigner(
+        execute(
+          'OldCatalysts',
+          {from: deployer},
+          'setConfiguration',
+          i,
+          setup.minQuantity,
+          setup.maxQuantity,
+          setup.sandMintingFee,
+          setup.sandUpdateFee
+        )
+      );
+    }
+  }
+};
+export default func;
+func.tags = ['OldCatalysts', 'OldCatalysts_setup'];
+func.dependencies = ['OldCatalysts_deploy'];
+func.skip = async () => true; // skip running as this is not to be used, require putting the whole Gem/Catalyst deployment back
