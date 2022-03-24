d62321a47531922ce23c61c2bd4a1e0c79c694b6
Lucas <luc4rd@gmail.com>
2022-03-24 17:51:50 +0100

    super operator changes

--- deploy_polygon/05_catalyst/10_set_gems_catalysts_super_operators.ts
+++ deploy_polygon/05_catalyst/10_set_gems_catalysts_super_operators.ts
@@ -5,11 +5,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {execute, read} = deployments;
 
-<<<<<<< HEAD
   const AssetMinter = await deployments.get('PolygonAssetMinter');
-=======
-  const AssetMinter = await deployments.get('AssetMinter');
->>>>>>> fixes in deployments
   const {gemsCatalystsRegistryAdmin} = await getNamedAccounts();
 
   const superOperatorRole = await read(
@@ -36,7 +32,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 export default func;
 func.runAtTheEnd = true;
-<<<<<<< HEAD
 func.tags = [
   'PolygonGemsCatalystsRegistry',
   'PolygonGemsCatalystsRegistry_setup',
@@ -46,7 +41,3 @@ func.dependencies = [
   'PolygonGemsCatalystsRegistry_deploy',
   'PolygonAssetMinter_deploy',
 ];
-=======
-func.tags = ['GemsCatalystsRegistry', 'GemsCatalystsRegistry_setup', 'L2'];
-func.dependencies = ['GemsCatalystsRegistry_deploy', 'AssetMinter_deploy'];
->>>>>>> fixes in deployments
