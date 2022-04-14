2425e799c2dd0342793bc13766370892cc1bc6a0
atkinsonholly <atkinsonholly@gmail.com>
2022-04-14 15:03:18 +0100

    1155 interfaces and tunnel data param removed and replaced with tokenURI call

--- src/solc_0.8/common/interfaces/IPolygonAssetERC721.sol
+++ src/solc_0.8/common/interfaces/IPolygonAssetERC721.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.2;
 
-import {IERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import {IERC721Base} from "./IERC721Base.sol";
 
 interface IPolygonAssetERC721 is IERC721Base {
@@ -13,7 +12,7 @@ interface IPolygonAssetERC721 is IERC721Base {
         bytes calldata data
     ) external override;
 
-    function setTokenMetadata(uint256 id, bytes memory data) external;
+    function setTokenURI(uint256 id, string memory uri) external;
 
     function tokenURI(uint256 id) external view returns (string memory);
 }
