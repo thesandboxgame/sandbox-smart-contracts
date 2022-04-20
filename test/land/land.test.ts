6d59f5ef89b09b599add8c8e2c608608cd41b246
Ayush Tiwari <ayush@sandbox.game>
2022-04-20 23:40:56 +0530

    test: added missing double transfer test

--- test/land/land.test.ts
+++ test/land/land.test.ts
@@ -8,6 +8,31 @@ describe('Land Transfer quad', function () {
   sizes.forEach((size1) => {
     sizes.forEach((size2) => {
       if (size2 >= size1) return;
+      it(`should NOT be able to transfer ${size2}x${size2} quad twice from ${size1}x${size1} quad`, async function () {
+        const {
+          landContract,
+          getNamedAccounts,
+          ethers,
+          mintQuad,
+        } = await setupLand();
+        const {deployer, landAdmin} = await getNamedAccounts();
+        const contract = landContract.connect(
+          ethers.provider.getSigner(deployer)
+        );
+        await mintQuad(deployer, size1, 0, 0);
+        // function transferQuad(address from, address to, uint256 size, uint256 x, uint256 y, bytes calldata data) external {
+        await contract.transferQuad(deployer, landAdmin, size2, 0, 0, '0x');
+        await expect(
+          contract.transferQuad(deployer, landAdmin, size2, 0, 0, '0x')
+        ).to.be.reverted;
+      });
+    });
+  });
+
+  // eslint-disable-next-line mocha/no-setup-in-describe
+  sizes.forEach((size1) => {
+    sizes.forEach((size2) => {
+      if (size2 >= size1) return;
       it(`should NOT be able to transfer burned ${size2}x${size2} quad twice from ${size1}x${size1} quad`, async function () {
         const {
           landContract,
