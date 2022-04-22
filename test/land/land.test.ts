38341681fbb10dde918c48e20cd289798cfaa585
Ayush Tiwari <ayush@sandbox.game>
2022-04-22 14:24:50 +0530

    fix: changed double spend test and removed redundant test

--- test/land/land.test.ts
+++ test/land/land.test.ts
@@ -20,10 +20,9 @@ describe('Land Transfer quad', function () {
           ethers.provider.getSigner(deployer)
         );
         await mintQuad(deployer, size1, 0, 0);
-        // function transferQuad(address from, address to, uint256 size, uint256 x, uint256 y, bytes calldata data) external {
         await contract.transferQuad(deployer, landAdmin, size2, 0, 0, '0x');
         await expect(
-          contract.transferQuad(deployer, landAdmin, size2, 0, 0, '0x')
+          contract.transferQuad(deployer, landAdmin, size1, 0, 0, '0x')
         ).to.be.reverted;
       });
     });
@@ -56,43 +55,5 @@ describe('Land Transfer quad', function () {
         ).to.be.revertedWith('not owner');
       });
     });
-  });
-
-  it(`should NOT be able to transfer burned 1x1 quad`, async function () {
-    const {
-      landContract,
-      getNamedAccounts,
-      ethers,
-      mintQuad,
-    } = await setupLand();
-    const {deployer, landAdmin} = await getNamedAccounts();
-    const contract = landContract.connect(ethers.provider.getSigner(deployer));
-    const x = 0;
-    const y = 0;
-    await mintQuad(deployer, 3, x, y);
-    const tokenId = x + y * GRID_SIZE;
-    await contract.burn(tokenId);
-    await expect(
-      contract.transferQuad(deployer, landAdmin, 1, 0, 0, '0x')
-    ).to.be.revertedWith('token does not exist');
-  });
-
-  it(`should NOT be able to transfer burned 1x1 quad through a parent quad`, async function () {
-    const {
-      landContract,
-      getNamedAccounts,
-      ethers,
-      mintQuad,
-    } = await setupLand();
-    const {deployer, landAdmin} = await getNamedAccounts();
-    const contract = landContract.connect(ethers.provider.getSigner(deployer));
-    const x = 0;
-    const y = 0;
-    await mintQuad(deployer, 3, x, y);
-    const tokenId = x + y * GRID_SIZE;
-    await contract.burn(tokenId);
-    await expect(
-      contract.transferQuad(deployer, landAdmin, 3, 0, 0, '0x')
-    ).to.be.revertedWith('not owner');
   });
 });
