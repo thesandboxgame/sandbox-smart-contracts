08f0a6347412be0b4852877f104cc81e53ae16cd
atkinsonholly <atkinsonholly@gmail.com>
2022-04-29 10:53:18 +0100

    Fix: cats & gems superoperators so tests work

--- deploy_polygon/05_catalyst/09_set_gems_catalysts_super_operators.ts
+++ deploy_polygon/05_catalyst/09_set_gems_catalysts_super_operators.ts
@@ -1,12 +1,21 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+import catalysts from '../../data/catalysts';
+import gems from '../../data/gems';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {execute, read} = deployments;
-
+  const GemsCatalystsRegistry = await deployments.get(
+    'PolygonGemsCatalystsRegistry'
+  );
+  const AssetAttributesRegistry = await deployments.get(
+    'PolygonAssetAttributesRegistry'
+  );
   const AssetMinter = await deployments.get('PolygonAssetMinter');
-  const {deployer} = await getNamedAccounts();
+  const AssetUpgrader = await deployments.get('PolygonAssetUpgrader');
+
+  const {deployer, catalystAdmin, gemAdmin} = await getNamedAccounts();
 
   const superOperatorRole = await read(
     'PolygonGemsCatalystsRegistry',
@@ -28,6 +37,140 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       superOperatorRole,
       AssetMinter.address
     );
+  }
+  // Set GemsCatalystsRegistry, AssetAttributesRegistry, AssetMinter and AssetUpgrader as catalyst superoperators
+  for (const catalyst of catalysts) {
+    const isGemsCatalystsRegistrySuperOperator = await read(
+      `PolygonCatalyst_${catalyst.symbol}`,
+      'hasRole',
+      superOperatorRole,
+      GemsCatalystsRegistry.address
+    );
+
+    if (!isGemsCatalystsRegistrySuperOperator) {
+      await execute(
+        `PolygonCatalyst_${catalyst.symbol}`,
+        {from: catalystAdmin, log: true},
+        'grantRole',
+        superOperatorRole,
+        GemsCatalystsRegistry.address
+      );
+    }
+    const isAssetAttributesRegistrySuperOperator = await read(
+      `PolygonCatalyst_${catalyst.symbol}`,
+      'hasRole',
+      superOperatorRole,
+      AssetAttributesRegistry.address
+    );
+
+    if (!isAssetAttributesRegistrySuperOperator) {
+      await execute(
+        `PolygonCatalyst_${catalyst.symbol}`,
+        {from: catalystAdmin, log: true},
+        'grantRole',
+        superOperatorRole,
+        AssetAttributesRegistry.address
+      );
+    }
+    const isAssetMinterSuperOperator = await read(
+      `PolygonCatalyst_${catalyst.symbol}`,
+      'hasRole',
+      superOperatorRole,
+      AssetMinter.address
+    );
+
+    if (!isAssetMinterSuperOperator) {
+      await execute(
+        `PolygonCatalyst_${catalyst.symbol}`,
+        {from: catalystAdmin, log: true},
+        'grantRole',
+        superOperatorRole,
+        AssetMinter.address
+      );
+    }
+    const isAssetUpgraderSuperOperator = await read(
+      `PolygonCatalyst_${catalyst.symbol}`,
+      'hasRole',
+      superOperatorRole,
+      AssetUpgrader.address
+    );
+
+    if (!isAssetUpgraderSuperOperator) {
+      await execute(
+        `PolygonCatalyst_${catalyst.symbol}`,
+        {from: catalystAdmin, log: true},
+        'grantRole',
+        superOperatorRole,
+        AssetUpgrader.address
+      );
+    }
+  }
+  // Set GemsCatalystsRegistry, AssetAttributesRegistry, AssetMinter and AssetUpgrader as gem superoperators
+  for (const gem of gems) {
+    const isGemsCatalystsRegistrySuperOperator = await read(
+      `PolygonGem_${gem.symbol}`,
+      'hasRole',
+      superOperatorRole,
+      GemsCatalystsRegistry.address
+    );
+
+    if (!isGemsCatalystsRegistrySuperOperator) {
+      await execute(
+        `PolygonGem_${gem.symbol}`,
+        {from: gemAdmin, log: true},
+        'grantRole',
+        superOperatorRole,
+        GemsCatalystsRegistry.address
+      );
+    }
+    const isAssetAttributesRegistrySuperOperator = await read(
+      `PolygonGem_${gem.symbol}`,
+      'hasRole',
+      superOperatorRole,
+      AssetAttributesRegistry.address
+    );
+
+    if (!isAssetAttributesRegistrySuperOperator) {
+      await execute(
+        `PolygonGem_${gem.symbol}`,
+        {from: gemAdmin, log: true},
+        'grantRole',
+        superOperatorRole,
+        AssetAttributesRegistry.address
+      );
+    }
+    const isAssetMinterSuperOperator = await read(
+      `PolygonGem_${gem.symbol}`,
+      'hasRole',
+      superOperatorRole,
+      AssetMinter.address
+    );
+
+    if (!isAssetMinterSuperOperator) {
+      await execute(
+        `PolygonGem_${gem.symbol}`,
+        {from: gemAdmin, log: true},
+        'grantRole',
+        superOperatorRole,
+        AssetMinter.address
+      );
+    }
+    const isAssetUpgraderSuperOperator = await read(
+      `PolygonGem_${gem.symbol}`,
+      'hasRole',
+      superOperatorRole,
+      AssetUpgrader.address
+    );
+
+    if (!isAssetUpgraderSuperOperator) {
+      await execute(
+        `PolygonGem_${gem.symbol}`,
+        {from: gemAdmin, log: true},
+        'grantRole',
+        superOperatorRole,
+        AssetUpgrader.address
+      );
+    }
   }
 };
 export default func;
