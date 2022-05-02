7bd6d4e1008571d69e60dbed1933842eee134cd3
León Isla <leon.isla.22@gmail.com>
2022-05-02 11:58:18 -0300

    add: method for land forced migration (#19)
    * add: method for land forced migration
    
    * fix: add skip true
    
    * fix: change skip to correct deploy script
    
    * fix: missing parameter
    
    * fix: format
    
    * upgrade: land swap on goerli
    
    * update: mainnet land migration batch executor address
    
    * update: mainnet deployments
    
    * update: test deploy script
    
    * update: test script
    
    * add: helper script to mint lands

--- deploy/02_land/09_upgrade_land_swap_v2.ts
+++ deploy/02_land/09_upgrade_land_swap_v2.ts
@@ -5,36 +5,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy, read, execute, catchUnknownSigner} = deployments;
 
-  const {deployer, sandAdmin, upgradeAdmin} = await getNamedAccounts();
-  const LAND_SWAP_TRUSTED_FORWARDER = await deployments.get(
-    'LAND_SWAP_TRUSTED_FORWARDER'
-  );
-  const LandOld = await deployments.get('Land_Old');
-  const Land = await deployments.get('Land');
+  const {deployer, upgradeAdmin} = await getNamedAccounts();
+  const LandMigrationBatch = await deployments.get('LandMigrationBatch');
 
   const LandSwap = await deploy('LandSwap', {
     from: deployer,
-    contract: 'LandSwap',
+    contract: 'LandSwapV2',
     proxy: {
       owner: upgradeAdmin,
       proxyContract: 'OpenZeppelinTransparentProxy',
-      execute: {
-        methodName: 'initialize',
-        args: [
-          sandAdmin,
-          LAND_SWAP_TRUSTED_FORWARDER.address,
-          LandOld.address,
-          Land.address,
-        ],
-      },
-      upgradeIndex: 0,
+      upgradeIndex: 1,
     },
     log: true,
   });
 
-  const admin = await read('Land', 'getAdmin');
   const isMinter = await read('Land', 'isMinter', LandSwap.address);
   if (!isMinter) {
+    const admin = await read('Land', 'getAdmin');
     await catchUnknownSigner(
       execute('Land', {from: admin}, 'setMinter', LandSwap.address, true)
     );
@@ -57,12 +44,26 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       )
     );
   }
+
+  const _batchMigration = await read('LandSwap', '_batchMigration');
+  if (_batchMigration !== LandMigrationBatch.address) {
+    const landSwapAdmin = await read('LandSwap', 'getAdmin');
+    await catchUnknownSigner(
+      execute(
+        'LandSwap',
+        {from: landSwapAdmin},
+        'setBatchMigration',
+        LandMigrationBatch.address
+      )
+    );
+  }
 };
 
 export default func;
-func.tags = ['LandSwap', 'LandSwap_deploy'];
+func.tags = ['LandSwap', 'LandSwapV2', 'LandSwap_deploy'];
 func.dependencies = [
   'Land_deploy',
   'Land_Old_deploy',
   'LAND_SWAP_TRUSTED_FORWARDER',
+  'LandMigrationBatch_deploy',
 ];
