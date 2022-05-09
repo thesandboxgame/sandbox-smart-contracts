b212227e39a438b69a390fa0533ccf3c76414fe7
atkinsonholly <atkinsonholly@gmail.com>
2022-05-09 17:36:53 +0100

    Deploy: scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -339,6 +339,7 @@ const config: HardhatUserConfig = {
       url: node_url('goerli'),
       accounts: accounts('goerli_test'),
       tags: ['testnet', 'L1'],
+      deploy: ['deploy'],
       // gasPrice: 600000000000, // Uncomment in case of pending txs, and adjust gas
       companionNetworks: {
         l2: 'mumbai_test',
