9d4bc76ac1c83d4c88836447804c523e7bce41fc
Andres Adjimann <andres@sandbox.game>
2022-05-12 14:59:20 -0300

    test: add a specific delete test for EstateGameRecordLib

--- test/estate/estateGameRecordLib.test.ts
+++ test/estate/estateGameRecordLib.test.ts
@@ -18,7 +18,21 @@ describe('Estate Game Records Test', function () {
       expect(await tester.contains(0, gameId)).to.be.true;
     }
   });
+
   it('deleteGame', async function () {
+    const {tester} = await setupEstateGameRecordLibTest();
+    const gameId = 123;
+    await tester.createGame(0, gameId);
+    expect(await tester.getGameIdAt(0, 0)).to.be.equal(gameId);
+    await tester.deleteGame(0, gameId);
+
+    await expect(tester.getGameIdAt(0, 0)).to.revertedWith('out-of-bounds');
+    expect(await tester.cant(0)).to.be.equal(0);
+    expect(await tester.contains(0, gameId)).to.be.false;
+    await expect(tester.deleteGame(0, gameId)).to.revertedWith('not found');
+  });
+
+  it('delete one game from a list', async function () {
     const {tester} = await setupEstateGameRecordLibTest();
     const cant = 10;
     const gameIdBase = 12;
@@ -32,7 +46,6 @@ describe('Estate Game Records Test', function () {
 
     expect(await tester.getGameIdAt(0, 4)).to.be.equal(gameId);
     await tester.deleteGame(0, gameId);
-
     expect(await tester.getGameIdAt(0, 4)).to.not.be.equal(gameId);
     expect(await tester.cant(0)).to.be.equal(cant - 1);
     expect(await tester.contains(0, gameId)).to.be.false;
