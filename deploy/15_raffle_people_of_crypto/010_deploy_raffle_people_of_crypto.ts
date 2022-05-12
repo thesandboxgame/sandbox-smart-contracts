05a211e98d51316c09b0e18a0a64afdcffcb8ad3
Ioana Verebi <ioanaverebi@gmail.com>
2022-05-12 17:42:05 +0300

    Personalization at the smartcontract level. People of Crypto contract

---
+++ deploy/15_raffle_people_of_crypto/010_deploy_raffle_people_of_crypto.ts
@@ -0,0 +1,30 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+  const {
+    deployer,
+    /* upgradeAdmin, */ treasury,
+    raffleSignWallet,
+  } = await getNamedAccounts();
+
+  let metadataUrl;
+  if (hre.network.name === 'mainnet') {
+    metadataUrl =
+      'https://www.sandbox.game/peopleofcrypto/unrevealed.json?tokenId=';
+  } else {
+    metadataUrl = 'https://api-demo.sandbox.game/collections/26/metadata/';
+  }
+
+  await deploy('RafflePeopleOfCrypto', {
+    from: deployer,
+    contract: 'PeopleOfCrypto',
+    args: [metadataUrl, 'People Of Crypto', 'POC', treasury, raffleSignWallet],
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+};
+export default func;
+func.tags = ['RafflePeopleOfCrypto', 'RafflePeopleOfCrypto_deploy'];
---
+++ test/raffle/PeopleOfCrypto.test.ts
@@ -0,0 +1,250 @@
+// import {expect} from 'chai';
+import {
+  raffleSignWallet,
+  setupRaffle,
+  zeroAddress,
+  assert,
+} from './PeopleOfCrypto.fixtures';
+
+// eslint-disable-next-line mocha/no-skipped-tests
+describe('RafflePeopleOfCrypto', function () {
+  it('should be able to mint with valid signature', async function () {
+    const {
+      rafflePeopleOfCryptoContract,
+      transferSand,
+      setupWave,
+      getNamedAccounts,
+      hre,
+      mint,
+    } = await setupRaffle();
+    const {deployer} = await getNamedAccounts();
+    await transferSand(deployer, '1000');
+    await setupWave(
+      rafflePeopleOfCryptoContract,
+      0,
+      20,
+      5,
+      '10',
+      zeroAddress,
+      0
+    );
+    await mint(
+      raffleSignWallet,
+      deployer,
+      0,
+      rafflePeopleOfCryptoContract.address,
+      hre.network.config.chainId || 31337,
+      '10',
+      1
+    );
+
+    const transferEvents = await rafflePeopleOfCryptoContract.queryFilter(
+      rafflePeopleOfCryptoContract.filters.Transfer()
+    );
+    assert.equal(transferEvents.length, 1);
+  });
+
+  it('should be able to mint 20_000 different tokens', async function () {
+    const {
+      rafflePeopleOfCryptoContract,
+      transferSand,
+      setupWave,
+      getNamedAccounts,
+      hre,
+      mint,
+    } = await setupRaffle();
+    const {deployer} = await getNamedAccounts();
+    await transferSand(deployer, '20000');
+    await setupWave(
+      rafflePeopleOfCryptoContract,
+      0,
+      10000,
+      10000,
+      '1',
+      zeroAddress,
+      0
+    );
+    const tokens = [];
+    for (let i = 0; i < 10000; i++) {
+      if (i % 10 === 0) console.log('minting token', i);
+      const receipt = await mint(
+        raffleSignWallet,
+        deployer,
+        i,
+        rafflePeopleOfCryptoContract.address,
+        hre.network.config.chainId || 31337,
+        '1',
+        1
+      );
+      const transferEvents = await rafflePeopleOfCryptoContract.queryFilter(
+        rafflePeopleOfCryptoContract.filters.Transfer(),
+        receipt.blockNumber
+      );
+      assert.equal(transferEvents.length, 1);
+      assert.exists(transferEvents[0].args);
+      if (transferEvents.length > 0 && transferEvents[0].args) {
+        const tokenId = transferEvents[0].args.tokenId.toString();
+        const exists = tokens.find((token) => token === tokenId);
+        assert.notExists(exists);
+        tokens.push(tokenId);
+      }
+    }
+  });
+
+  it('should be able to mint 20_000 different tokens in 3 waves', async function () {
+    const {
+      rafflePeopleOfCryptoContract,
+      transferSand,
+      setupWave,
+      getNamedAccounts,
+      hre,
+      mint,
+    } = await setupRaffle();
+    const {deployer} = await getNamedAccounts();
+    await transferSand(deployer, '20000');
+    const waves = [50, 6950, 3000];
+    const tokens = [];
+    let signatureId = 0;
+    for (const amount of waves) {
+      await setupWave(
+        rafflePeopleOfCryptoContract,
+        0,
+        amount,
+        amount,
+        '1',
+        zeroAddress,
+        0
+      );
+      for (let i = 0; i < amount; i++) {
+        if (signatureId % 10 === 0) console.log('minting token', i);
+        signatureId++;
+        const receipt = await mint(
+          raffleSignWallet,
+          deployer,
+          signatureId,
+          rafflePeopleOfCryptoContract.address,
+          hre.network.config.chainId || 31337,
+          '1',
+          1
+        );
+        const transferEvents = await rafflePeopleOfCryptoContract.queryFilter(
+          rafflePeopleOfCryptoContract.filters.Transfer(),
+          receipt.blockNumber
+        );
+        assert.equal(transferEvents.length, 1);
+        assert.exists(transferEvents[0].args);
+        if (transferEvents.length > 0 && transferEvents[0].args) {
+          const tokenId = transferEvents[0].args.tokenId.toString();
+          const exists = tokens.find((token) => token === tokenId);
+          assert.notExists(exists);
+          tokens.push(tokenId);
+        }
+      }
+    }
+    assert.equal(tokens.length, 10000);
+  });
+
+  it('should be able to mint 20_000 different tokens in 3 waves in 3 txs', async function () {
+    const {
+      rafflePeopleOfCryptoContract,
+      transferSand,
+      setupWave,
+      getNamedAccounts,
+      hre,
+      mint,
+    } = await setupRaffle();
+    const {deployer} = await getNamedAccounts();
+    await transferSand(deployer, '20000');
+    // const waves = [50, 200, 500, 1000, 2000, 4000, 6750, 3000];
+    const waves = [50, 6950, 3000];
+    const tokens = [];
+    let signatureId = 0;
+    for (const amount of waves) {
+      await setupWave(
+        rafflePeopleOfCryptoContract,
+        0,
+        amount,
+        amount,
+        '1',
+        zeroAddress,
+        0
+      );
+      signatureId++;
+      const receipt = await mint(
+        raffleSignWallet,
+        deployer,
+        signatureId,
+        rafflePeopleOfCryptoContract.address,
+        hre.network.config.chainId || 31337,
+        amount,
+        amount
+      );
+      const transferEvents = await rafflePeopleOfCryptoContract.queryFilter(
+        rafflePeopleOfCryptoContract.filters.Transfer(),
+        receipt.blockNumber
+      );
+      console.log(transferEvents.length);
+      assert.equal(transferEvents.length, amount);
+
+      for (const transferEvent of transferEvents) {
+        assert.exists(transferEvent.args);
+        if (transferEvent.args) {
+          const tokenId = transferEvent.args.tokenId.toString();
+          const exists = tokens.find((token) => token === tokenId);
+          assert.notExists(exists);
+          tokens.push(tokenId);
+        }
+      }
+    }
+    assert.equal(tokens.length, 20000);
+  });
+
+  it('should be able to personalize with valid signature', async function () {
+    const {
+      rafflePeopleOfCryptoContract,
+      transferSand,
+      setupWave,
+      getNamedAccounts,
+      hre,
+      mint,
+      personalize,
+    } = await setupRaffle();
+    const {deployer} = await getNamedAccounts();
+    await transferSand(deployer, '1000');
+    await setupWave(
+      rafflePeopleOfCryptoContract,
+      0,
+      20,
+      5,
+      '10',
+      zeroAddress,
+      0
+    );
+    await mint(
+      raffleSignWallet,
+      deployer,
+      0,
+      rafflePeopleOfCryptoContract.address,
+      hre.network.config.chainId || 31337,
+      '10',
+      1
+    );
+
+    const transferEvents = await rafflePeopleOfCryptoContract.queryFilter(
+      rafflePeopleOfCryptoContract.filters.Transfer()
+    );
+
+    console.log('Transfer events: ', transferEvents);
+    assert.equal(transferEvents.length, 1);
+
+    // await personalize(
+    //   raffleSignWallet,
+    //   deployer,
+    //   0,
+    //   rafflePeopleOfCryptoContract.address,
+    //   hre.network.config.chainId || 31337,
+    //   '10',
+    //   1
+    // );
+  });
+});
