2b2c171f0c50424f6bcad96f048e21afa8b31865
León <leon.isla.22@gmail.com>
2022-05-31 12:22:34 -0300

    deploy: asset signed auction with auth on goerli

--- deploy/06_asset/02_deploy_asset_signed_auction_with_auth.ts
+++ deploy/06_asset/02_deploy_asset_signed_auction_with_auth.ts
@@ -1,6 +1,6 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
-import {skipUnlessTest} from '../../utils/network';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {skipUnlessTestnet} from '../../utils/network';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts, getUnnamedAccounts} = hre;
@@ -34,4 +34,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 export default func;
 func.tags = ['AssetSignedAuctionWithAuth', 'AssetSignedAuctionWithAuth_deploy'];
 func.dependencies = ['Asset_deploy', 'AuthValidator_deploy'];
-func.skip = skipUnlessTest;
+func.skip = skipUnlessTestnet;
