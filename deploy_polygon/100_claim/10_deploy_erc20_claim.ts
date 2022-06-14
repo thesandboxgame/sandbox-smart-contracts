85cc96c88ffb862da674171b0ae5a2b9aaa258fe
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2022-06-14 18:06:10 +0200

    trusted forwarder v2

--- deploy_polygon/100_claim/10_deploy_erc20_claim.ts
+++ deploy_polygon/100_claim/10_deploy_erc20_claim.ts
@@ -7,7 +7,7 @@ const func: DeployFunction = async function (
 ): Promise<void> {
   const {deployments, getNamedAccounts} = hre;
   const {deployer, upgradeAdmin, sandAdmin} = await getNamedAccounts();
-  const TRUSTED_FORWARDER = await deployments.get('TRUSTED_FORWARDER');
+  const TRUSTED_FORWARDER = await deployments.get('TRUSTED_FORWARDER_V2');
   const contract = await deployments.getOrNull('ERC20SignedClaim');
   if (contract) {
     console.warn('reusing ERC20SignedClaim', contract.address);
@@ -32,5 +32,5 @@ const func: DeployFunction = async function (
 
 export default func;
 func.tags = ['ERC20SignedClaim', 'ERC20SignedClaim_deploy'];
-func.dependencies = ['TRUSTED_FORWARDER'];
+func.dependencies = ['TRUSTED_FORWARDER_V2'];
 func.skip = skipUnlessTestnet;
