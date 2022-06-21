db29ac0ed532fe3ea8dc76aebd0b49a9693f4665
Ayush Tiwari <ayush@sandbox.game>
2022-06-21 11:30:59 +0530

    fix: fixed floating pragma and removed ABIEncoderV2 pragma

--- src/solc_0.8/asset/AssetMinter.sol
+++ src/solc_0.8/asset/AssetMinter.sol
@@ -1,6 +1,5 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.2;
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts-0.8/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts-0.8/access/Ownable.sol";
