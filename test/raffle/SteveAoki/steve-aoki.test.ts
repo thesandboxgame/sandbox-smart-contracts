4c68decbc7c354f8770c6bb6c5a179a385f5898f
Marius Bălaj <balajmarius@gmail.com>
2022-06-23 13:51:27 +0300

    steve aoki: test contract

---
+++ test/raffle/SteveAoki/steve-aoki.test.ts
@@ -0,0 +1,192 @@
+// import {expect} from 'chai';
+import {expect} from 'chai';
+import {ethers} from 'hardhat';
+
+import {waitFor} from '../../utils';
+
+import {raffleSignWallet, setupRaffle, zeroAddress, assert} from './fixtures';
+
+// eslint-disable-next-line mocha/no-skipped-tests
+describe.only('RaffleSteveAoki', function () {
+  it('should be able to mint with valid signature', async function () {
+    const {
+      raffleSteveAokiContract,
+      transferSand,
+      setupWave,
+      getNamedAccounts,
+      hre,
+      mint,
+    } = await setupRaffle();
+    const {deployer} = await getNamedAccounts();
+    await transferSand(deployer, '1000');
+    await setupWave(raffleSteveAokiContract, 0, 20, 5, '10', zeroAddress, 0);
+    await mint(
+      raffleSignWallet,
+      deployer,
+      0,
+      raffleSteveAokiContract.address,
+      hre.network.config.chainId || 31337,
+      '10',
+      1
+    );
+
+    const transferEvents = await raffleSteveAokiContract.queryFilter(
+      raffleSteveAokiContract.filters.Transfer()
+    );
+    assert.equal(transferEvents.length, 1);
+  });
+
+  it('should be able to mint 3_333 different tokens', async function () {
+    const {
+      raffleSteveAokiContract,
+      transferSand,
+      setupWave,
+      getNamedAccounts,
+      hre,
+      mint,
+    } = await setupRaffle();
+    const {deployer} = await getNamedAccounts();
+    await transferSand(deployer, '10000');
+    await setupWave(
+      raffleSteveAokiContract,
+      0,
+      3333,
+      3333,
+      '1',
+      zeroAddress,
+      0
+    );
+    const tokens = [];
+    for (let i = 0; i < 3333; i++) {
+      if (i % 10 === 0) console.log('minting token', i);
+      const receipt = await mint(
+        raffleSignWallet,
+        deployer,
+        i,
+        raffleSteveAokiContract.address,
+        hre.network.config.chainId || 31337,
+        '1',
+        1
+      );
+      const transferEvents = await raffleSteveAokiContract.queryFilter(
+        raffleSteveAokiContract.filters.Transfer(),
+        receipt.blockNumber
+      );
+      assert.equal(transferEvents.length, 1);
+      assert.exists(transferEvents[0].args);
+      if (transferEvents.length > 0 && transferEvents[0].args) {
+        const tokenId = transferEvents[0].args.tokenId.toString();
+        const exists = tokens.find((token) => token === tokenId);
+        assert.notExists(exists);
+        tokens.push(tokenId);
+      }
+    }
+  });
+
+  it('should be able to mint 3_333 different tokens in 3 waves', async function () {
+    const {
+      raffleSteveAokiContract,
+      transferSand,
+      setupWave,
+      getNamedAccounts,
+      hre,
+      mint,
+    } = await setupRaffle();
+    const {deployer} = await getNamedAccounts();
+    await transferSand(deployer, '10000');
+    const waves = [33, 300, 3000];
+    const tokens = [];
+    let signatureId = 0;
+    for (const amount of waves) {
+      await setupWave(
+        raffleSteveAokiContract,
+        0,
+        amount,
+        amount,
+        '1',
+        zeroAddress,
+        0
+      );
+      for (let i = 0; i < amount; i++) {
+        if (signatureId % 10 === 0) console.log('minting token', i);
+        signatureId++;
+        const receipt = await mint(
+          raffleSignWallet,
+          deployer,
+          signatureId,
+          raffleSteveAokiContract.address,
+          hre.network.config.chainId || 31337,
+          '1',
+          1
+        );
+        const transferEvents = await raffleSteveAokiContract.queryFilter(
+          raffleSteveAokiContract.filters.Transfer(),
+          receipt.blockNumber
+        );
+        assert.equal(transferEvents.length, 1);
+        assert.exists(transferEvents[0].args);
+        if (transferEvents.length > 0 && transferEvents[0].args) {
+          const tokenId = transferEvents[0].args.tokenId.toString();
+          const exists = tokens.find((token) => token === tokenId);
+          assert.notExists(exists);
+          tokens.push(tokenId);
+        }
+      }
+    }
+    assert.equal(tokens.length, 10000);
+  });
+
+  it('should be able to mint 3_333 different tokens in 3 waves in 3 txs', async function () {
+    const {
+      raffleSteveAokiContract,
+      transferSand,
+      setupWave,
+      getNamedAccounts,
+      hre,
+      mint,
+    } = await setupRaffle();
+    const {deployer} = await getNamedAccounts();
+    await transferSand(deployer, '10000');
+    const waves = [33, 300, 3000];
+    const tokens = [];
+    let signatureId = 0;
+    for (const amount of waves) {
+      await setupWave(
+        raffleSteveAokiContract,
+        0,
+        amount,
+        amount,
+        '1',
+        zeroAddress,
+        0
+      );
+      signatureId++;
+      const receipt = await mint(
+        raffleSignWallet,
+        deployer,
+        signatureId,
+        raffleSteveAokiContract.address,
+        hre.network.config.chainId || 31337,
+        amount,
+        amount
+      );
+      const transferEvents = await raffleSteveAokiContract.queryFilter(
+        raffleSteveAokiContract.filters.Transfer(),
+        receipt.blockNumber
+      );
+      console.log(transferEvents.length);
+      assert.equal(transferEvents.length, amount);
+
+      for (const transferEvent of transferEvents) {
+        assert.exists(transferEvent.args);
+        if (transferEvent.args) {
+          const tokenId = transferEvent.args.tokenId.toString();
+          const exists = tokens.find((token) => token === tokenId);
+          assert.notExists(exists);
+          tokens.push(tokenId);
+        }
+      }
+    }
+    assert.equal(tokens.length, 10000);
+  });
+});
