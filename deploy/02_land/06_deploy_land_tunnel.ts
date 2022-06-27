942f67874373ff917c54caf0ddeebce233ac23bb
atkinsonholly <atkinsonholly@gmail.com>
2022-06-27 18:28:58 +0100

    PolygonLandTunnel and LandTunnel skipUnlessTestnet

--- deploy/02_land/06_deploy_land_tunnel.ts
+++ deploy/02_land/06_deploy_land_tunnel.ts
@@ -1,6 +1,6 @@
 import {DeployFunction} from 'hardhat-deploy/types';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {skipUnlessTest} from '../../utils/network';
+import {skipUnlessTestnet} from '../../utils/network';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
@@ -84,4 +84,4 @@ func.dependencies = [
   'CHECKPOINTMANAGER',
   'TRUSTED_FORWARDER',
 ];
-func.skip = skipUnlessTest;
+func.skip = skipUnlessTestnet;
--- deploy_polygon/04_land/03_deploy_polygon_land_tunnel.ts
+++ deploy_polygon/04_land/03_deploy_polygon_land_tunnel.ts
@@ -1,6 +1,6 @@
 import {DeployFunction} from 'hardhat-deploy/types';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {skipUnlessTest} from '../../utils/network';
+import {skipUnlessTestnet} from '../../utils/network';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
@@ -85,4 +85,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 export default func;
 func.tags = ['PolygonLandTunnel', 'PolygonLandTunnel_deploy', 'L2'];
 func.dependencies = ['PolygonLand', 'FXCHILD'];
-func.skip = skipUnlessTest;
+func.skip = skipUnlessTestnet;
