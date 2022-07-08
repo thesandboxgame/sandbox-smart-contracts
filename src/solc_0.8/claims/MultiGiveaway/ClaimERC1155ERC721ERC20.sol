7fc13eb188e01134538a2d61147d73c1ced836e5
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2022-07-08 11:41:41 +0200

    natspec, getter for expirytime, index on events

--- src/solc_0.8/claims/MultiGiveaway/ClaimERC1155ERC721ERC20.sol
+++ src/solc_0.8/claims/MultiGiveaway/ClaimERC1155ERC721ERC20.sol
@@ -41,7 +41,7 @@ contract ClaimERC1155ERC721ERC20 {
     /// @param erc20 The ERC20Claim struct containing the amounts and ERC20 contract addresses.
     /// @param merkleRoot The merkle root hash for the specific set of items being claimed.
     event ClaimedMultipleTokens(
-        address to,
+        address indexed to,
         ERC1155Claim[] erc1155,
         ERC721Claim[] erc721,
         ERC20Claim erc20,
