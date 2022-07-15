8b71806ffa597fe925acbc2b3dec76e057875347
León Isla <leon.isla.22@gmail.com>
2022-07-15 16:16:39 -0300

    fix: trusted forwarder is admin (#728)

--- deploy_utils/CheckAndSetTrustedForwarder.ts
+++ deploy_utils/CheckAndSetTrustedForwarder.ts
@@ -26,13 +26,35 @@ export default async function (
     console.log(
       `Setting ${trustedForwarderName} as trusted forwarder for ${contractName}`
     );
-    await catchUnknownSigner(
-      execute(
-        contractName,
-        {from: admin, log: true},
-        'setTrustedForwarder',
-        TRUSTED_FORWARDER.address
-      )
+    const adminIsTrustedForwarder = await read(
+      contractName,
+      'isTrustedForwarder',
+      admin
     );
+    if (adminIsTrustedForwarder) {
+      const {ethers} = hre;
+      const contract = await ethers.getContract(contractName);
+      const signer = await ethers.getSigner(admin);
+      const {data} = await contract.populateTransaction[
+        'setTrustedForwarder(address)'
+      ](TRUSTED_FORWARDER.address);
+      console.log(`Sending raw transaction since admin is trusted forwarder`);
+      await catchUnknownSigner(
+        signer.sendTransaction({
+          from: admin,
+          to: contract.address,
+          data: data + admin.slice(2),
+        })
+      );
+    } else {
+      await catchUnknownSigner(
+        execute(
+          contractName,
+          {from: admin, log: true},
+          'setTrustedForwarder',
+          TRUSTED_FORWARDER.address
+        )
+      );
+    }
   }
 }
