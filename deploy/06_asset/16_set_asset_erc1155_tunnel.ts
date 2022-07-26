dd7af84d36841b54e2dc67c451f2b693f7df54b9
León <leon.isla.22@gmail.com>
2022-07-26 16:56:23 -0300

    fix: setPredicate on L1

--- deploy/06_asset/16_set_asset_erc1155_tunnel.ts
+++ deploy/06_asset/16_set_asset_erc1155_tunnel.ts
@@ -1,32 +1,20 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {skipUnlessTestnet} from '../../utils/network';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments, getNamedAccounts} = hre;
-  const {execute, read} = deployments;
-  const {upgradeAdmin} = await getNamedAccounts();
-
+  const {deployments} = hre;
+  const {execute, read, catchUnknownSigner} = deployments;
   const AssetERC1155Tunnel = await deployments.get('AssetERC1155Tunnel');
-
-  let currentAdmin;
-  try {
-    currentAdmin = await read('Asset', 'getAdmin');
-  } catch (e) {
-    // no admin
-  }
-
-  // setting up predicate on L1
-  if (currentAdmin) {
-    if (currentAdmin.toLowerCase() !== upgradeAdmin.toLowerCase()) {
-      await execute(
-        'Asset',
-        {from: currentAdmin},
-        'setPredicate',
-        AssetERC1155Tunnel.address
-      );
-    }
-  }
+  const currentAdmin = await read('Asset', 'getAdmin');
+  await catchUnknownSigner(
+    execute(
+      'Asset',
+      {from: currentAdmin, log: true},
+      'setPredicate',
+      AssetERC1155Tunnel.address
+    )
+  );
 };
 
 export default func;
