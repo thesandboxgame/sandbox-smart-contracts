0afa99420579629d8fd45a243f410bc2de250765
atkinsonholly <atkinsonholly@gmail.com>
2022-09-16 17:45:21 +0100

    WIP: Asset ERC1155 regeneration script

--- setup/asset_remint_from_id.ts
+++ setup/asset_remint_from_id.ts
@@ -3,6 +3,10 @@ import fs from 'fs-extra';
 import hre from 'hardhat';
 import {DeployFunction} from 'hardhat-deploy/types';
 import {isContract} from '../utils/address';
+import {toHash} from '../utils/asset-uri-to-hash';
+import setupBatchDeployerAsAssetBouncer from '../deploy/10_helpers/04_asset_bouncer_enable_deployer_batch_and_predicate';
+import removeBatchDeployerAsAssetBouncer from '../deploy/10_helpers/05_asset_bouncer_disable_deployer_batch_and_predicate';
+
 const abiCoder = new AbiCoder();
 
 const MINT_BATCH_SIZE = 100;
@@ -15,13 +19,12 @@ type MintBatch = {
 };
 
 // File mints AssetERC1155 from file containing { id, owner, supply, uri?, isContract? }
-// Necessary to migrate AssetERC1155 from previous version on Goerli ("OldAsset") to new contract replacement
-// This is to ensure aligned upgrade version on Goerli and Mainnet
+// Necessary to migrate AssetERC1155 from previous version on Goerli ("OldAsset") to new contract (replacement not upgrade;
+// this is to ensure aligned upgrade version on Goerli and Mainnet)
 
 const func: DeployFunction = async function () {
   const {deployments, ethers, getNamedAccounts} = hre;
-  const {read, execute, catchUnknownSigner} = deployments;
-  const DeployerBatch = await deployments.get('DeployerBatch');
+  const {read, execute} = deployments;
   const Asset = await ethers.getContract('Asset');
   console.log('Asset address: ' + Asset.address);
 
@@ -33,7 +36,7 @@ const func: DeployFunction = async function () {
   );
 
   const gasPrice = await (await ethers.provider.getGasPrice()).toString();
-  const {deployer, assetAdmin} = await getNamedAccounts();
+  const {deployer} = await getNamedAccounts();
 
   const batchMints: {
     id: string;
@@ -49,8 +52,9 @@ const func: DeployFunction = async function () {
       batches.push(currentBatch);
       currentBatch = [];
     }
-    const exists = await read('Asset', 'exists', batch.id); // function only on contract after upgrade
-    if (!exists) {
+    // Make sure the Asset contract has been upgraded by this point
+    const hashExists = await read('Asset', 'doesHashExist', batch.id);
+    if (!hashExists) {
       const uri = await OldAsset.tokenURI(batch.id);
       batch.isContract = batch.isContract || (await isContract(batch.owner));
 
@@ -63,21 +67,10 @@ const func: DeployFunction = async function () {
         });
         continue;
       }
-      // We have the uri but we need to encode just the metadatahash, not the full uri, so we convert the uri to hash
-      // uri is formed of: "ipfs://bafybei",hash2base32(hash),"/",uint2str(id & PACK_INDEX),".json"
-
-      // Get hash2base32(hash)
-      const array = uri.split('/');
-      const hash = array[2].slice(7);
-      // TODO:
-      console.log('hash: ' + hash);
-
-      // Convert hash2base32(hash) to actual hash for encoding
-      // TODO:
 
       // Encoded bytes32 metadata hash must be provided as data
       // Hash must have length for tx to succeed
-      const metadata = abiCoder.encode(['bytes32'], [hash]);
+      const metadata = abiCoder.encode(['bytes32'], [toHash(uri)]);
       currentBatch.push({
         to: batch.owner,
         id: batch.id,
@@ -88,51 +81,39 @@ const func: DeployFunction = async function () {
       console.log(`id (${batch.id}) already minted`);
     }
   }
-  // fs.outputJSONSync('tmp/assets_for_remint.json', batchMints);
-  // if (currentBatch.length > 0) {
-  //   batches.push(currentBatch);
-  // }
-  // console.log({batches: batches.length});
-  // if (batches.length === 0) return;
-  // const mintRole = await read('Asset', 'MINTER_ROLE');
-  // const isMinter = await read(
-  //   'Asset',
-  //   'hasRole',
-  //   mintRole,
-  //   DeployerBatch.address
-  // );
-  // if (!isMinter) {
-  //   console.log('DeployerBatch is not a minter');
-  //   await catchUnknownSigner(
-  //     execute(
-  //       'Asset',
-  //       {from: assetAdmin, log: true},
-  //       'grantRole',
-  //       mintRole,
-  //       DeployerBatch.address
-  //     )
-  //   );
-  // }
-  // const datas = [];
-  // for (const batch of batches) {
-  //   for (const {to, id, supply, data} of batch) {
-  //     const populatedTx = await Asset.populateTransaction[
-  //       'mint(address,uint256,uint256,bytes)'
-  //     ](to, id, supply, data);
-  //     datas.push(populatedTx.data);
-  //   }
-  //   console.log({minting: batch.length});
-  //   const tx = await execute(
-  //     'DeployerBatch',
-  //     {from: deployer, gasPrice},
-  //     'singleTargetAtomicBatch',
-  //     Asset.address,
-  //     datas
-  //   );
-  //   console.log(`batchMint`, {
-  //     tx: tx.transactionHash,
-  //   });
-  // }
+  fs.outputJSONSync('tmp/assets_for_remint.json', batchMints);
+  if (currentBatch.length > 0) {
+    batches.push(currentBatch);
+  }
+  console.log({batches: batches.length});
+  if (batches.length === 0) return;
+
+  // Set up BatchDeployer as bouncer and set predicate as DeployerBatch (only bridge is allowed to mint on L1)
+  await setupBatchDeployerAsAssetBouncer(hre);
+
+  const datas = [];
+  for (const batch of batches) {
+    for (const {to, id, supply, data} of batch) {
+      const populatedTx = await Asset.populateTransaction[
+        'mint(address,uint256,uint256,bytes)'
+      ](to, id, supply, data);
+      datas.push(populatedTx.data);
+    }
+    console.log({minting: batch.length});
+    const tx = await execute(
+      'DeployerBatch',
+      {from: deployer, gasPrice},
+      'singleTargetAtomicBatch',
+      Asset.address,
+      datas
+    );
+    console.log(`batchMint`, {
+      tx: tx.transactionHash,
+    });
+  }
+
+  // Disable BatchDeployer as bouncer and set predicate as AssetERC1155Tunnel again (only bridge is allowed to mint on L1)
+  await removeBatchDeployerAsAssetBouncer(hre);
 };
 
 export default func;
