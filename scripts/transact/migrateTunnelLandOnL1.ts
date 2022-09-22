84d921576c29dc6f273272253e9585d8a7022010
Ayush Tiwari <ayush@sandbox.game>
2022-09-22 14:18:52 +0530

    fix: fixed land migration scripts

--- scripts/transact/migrateTunnelLandOnL1.ts
+++ scripts/transact/migrateTunnelLandOnL1.ts
@@ -1,7 +1,9 @@
 import fs from 'fs-extra';
-import {ethers,getNamedAccounts} from 'hardhat';
+import {ethers, getNamedAccounts} from 'hardhat';
 
-const tokensSnapshotL1 = [{id:"2045"},{id:"2863"}]
+const tokensSnapshotL1 =JSON.parse(
+  fs.readFileSync('./tunnel_mainnet.json').toString()
+);
 
 const maxIdInTransaction = 20;
 
@@ -15,7 +17,9 @@ void (async () => {
   let index = 0;
   const tokenIdsOnL1Length = tokenIdsOnL1.length;
   const numberOfCalls = Math.ceil(tokenIdsOnL1Length / maxIdInTransaction);
-  const landTunnelMigrationAsAdmin = await landTunnelMigration.connect(ethers.provider.getSigner(deployer));
+  const landTunnelMigrationAsAdmin = await landTunnelMigration.connect(
+    ethers.provider.getSigner(deployer)
+  );
   for (let i = 0; i < numberOfCalls; i++) {
     const argument = tokenIdsOnL1.slice(index, index + maxIdInTransaction);
     await landTunnelMigrationAsAdmin.migrateToTunnel(argument);
