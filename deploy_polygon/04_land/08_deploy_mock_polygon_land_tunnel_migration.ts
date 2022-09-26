cf4238bbe975d6a9a714fbcdae4325b6422b8edc
Ayush Tiwari <ayush@sandbox.game>
2022-09-26 17:41:05 +0530

    feat: added mock migration contract deployment scripts

---
+++ deploy_polygon/04_land/08_deploy_mock_polygon_land_tunnel_migration.ts
@@ -0,0 +1,34 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+
+  const {deployer} = await getNamedAccounts();
+
+  const mockPolygonLandTunnel = await deployments.get('MockPolygonLandTunnel');
+  const polygonLand = await deployments.get('PolygonLand');
+  const mockPolygonLandTunnelV2 = await deployments.get(
+    'MockPolygonLandTunnelV2'
+  );
+
+  await deploy('MockPolygonLandTunnelMigration', {
+    from: deployer,
+    contract: 'PolygonLandTunnelMigration',
+    args: [
+      polygonLand.address,
+      mockPolygonLandTunnelV2.address,
+      mockPolygonLandTunnel.address,
+      deployer,
+    ],
+    log: true,
+  });
+};
+export default func;
+func.tags = ['MockPolygonLandTunnelMigration'];
+func.dependencies = [
+  'MockPolygonLandTunnel',
+  'MockPolygonLandTunnelV2',
+  'PolygonLand',
+];
