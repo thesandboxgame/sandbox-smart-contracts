a66955738be2fefc50f8083305ed29f83f6aac4f
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2022-10-11 18:32:22 +0200

    only initializing on fx portal

--- src/solc_0.8/common/fx-portal/FxBaseChildTunnelUpgradeable.sol
+++ src/solc_0.8/common/fx-portal/FxBaseChildTunnelUpgradeable.sol
@@ -2,12 +2,13 @@
 pragma solidity 0.8.2;
 
 import "@maticnetwork/fx-portal/contracts/tunnel/FxBaseChildTunnel.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-abstract contract FxBaseChildTunnelUpgradeable is FxBaseChildTunnel {
+abstract contract FxBaseChildTunnelUpgradeable is FxBaseChildTunnel, Initializable {
     // solhint-disable-next-line no-empty-blocks
     constructor() FxBaseChildTunnel(address(0)) {}
 
-    function __FxBaseChildTunnelUpgradeable_initialize(address _fxChild) internal {
+    function __FxBaseChildTunnelUpgradeable_initialize(address _fxChild) internal onlyInitializing {
         fxChild = _fxChild;
     }
 
