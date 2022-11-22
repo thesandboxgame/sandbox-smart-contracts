a917b21a52c9efa833d5a602c4d526ead7be67fd
atkinsonholly <atkinsonholly@gmail.com>
2022-11-22 20:41:55 +0000

    deployment mumbai new StarterPack using new cats gems registry

--- data/starterPackPrices.ts
+++ data/starterPackPrices.ts
@@ -8,10 +8,10 @@ export const prices = {
       BigNumber.from(4)
     ],
   catalystPrices: [
-    BigNumber.from('20000000000000000000'),
-    BigNumber.from('60000000000000000000'),
-    BigNumber.from('200000000000000000000'),
-    BigNumber.from('800000000000000000000')
+    BigNumber.from('1'),
+    BigNumber.from('2'),
+    BigNumber.from('3'),
+    BigNumber.from('4')
   ],
   gemIds: [
     BigNumber.from(1),
@@ -21,10 +21,10 @@ export const prices = {
     BigNumber.from(5)
   ],
   gemPrices: [
-    BigNumber.from('20000000000000000000'),
-    BigNumber.from('20000000000000000000'),
-    BigNumber.from('20000000000000000000'),
-    BigNumber.from('20000000000000000000'),
-    BigNumber.from('20000000000000000000')
+    BigNumber.from('1'),
+    BigNumber.from('1'),
+    BigNumber.from('1'),
+    BigNumber.from('1'),
+    BigNumber.from('1')
   ]
 }
