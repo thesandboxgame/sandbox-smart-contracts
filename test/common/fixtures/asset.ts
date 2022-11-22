3903181950a63d12c0f5d061a56c8cf9a86af6a1
atkinsonholly <atkinsonholly@gmail.com>
2022-11-22 18:03:09 +0000

    test: AssetV1 test update for new tag

--- test/common/fixtures/asset.ts
+++ test/common/fixtures/asset.ts
@@ -286,11 +286,14 @@ export const originalAssetFixtures = async function () {
   const minter = otherAccounts[0];
   otherAccounts.splice(0, 1);
 
-  const {deployer} = await getNamedAccounts();
+  const {deployer, assetBouncerAdmin} = await getNamedAccounts();
 
-  // deployer is the original assetBouncerAdmin
   const originalAsset = await ethers.getContract('Asset', deployer);
-  await waitFor(originalAsset.setBouncer(minter, true));
+  const originalAssetAsBouncerAdmin = await ethers.getContract(
+    'Asset',
+    assetBouncerAdmin
+  );
+  await waitFor(originalAssetAsBouncerAdmin.setBouncer(minter, true));
 
   const Asset = await ethers.getContract('Asset', minter);
 
