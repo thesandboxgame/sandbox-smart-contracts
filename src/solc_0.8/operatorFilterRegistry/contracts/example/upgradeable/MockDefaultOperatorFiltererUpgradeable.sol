df9df53cdbd2f9a14578bd4c6c24473a4981a815
Rishabh Sharma <rishabh.sharma@sandbox.game>
2022-12-06 19:54:35 +0530

    feat: added contracts

---
+++ src/solc_0.8/operatorFilterRegistry/contracts/example/upgradeable/MockDefaultOperatorFiltererUpgradeable.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {MockOperatorFiltererUpgradeable} from "./MockOperatorFiltererUpgradeable.sol";
+
+abstract contract MockDefaultOperatorFiltererUpgradeable is MockOperatorFiltererUpgradeable {
+    address public constant DEFAULT_SUBSCRIPTION = address(0x3cc6CddA760b79bAfa08dF41ECFA224f810dCeB6);
+
+    function __DefaultOperatorFilterer_init(bool subscribe, address _operatorFilterRegistry) internal {
+        __MockOperatorFilterer_init(DEFAULT_SUBSCRIPTION, subscribe, _operatorFilterRegistry);
+    }
+}
