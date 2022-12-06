c7a87a622165505cd5be3c6d04916574b8515129
León <leon.isla.22@gmail.com>
2022-12-06 16:22:17 -0300

    fix: exclude minted

--- data/landSales/excludeMinted.ts
+++ data/landSales/excludeMinted.ts
@@ -3,7 +3,22 @@ import hre from 'hardhat';
 import {TheGraph} from "../../scripts/utils/thegraph";
 import {SectorData, SectorLand} from "./getLandSales";
 
-export async function excludeMinted({sector, lands}: SectorData): Promise<SectorData> {
+let l1, l2;
+if (hre.network.tags.testnet) {
+  // l1 = "GOERLI"
+  // l2 = "MUMBAI"
+  l1 = "MAINNET"
+  l2 = "POLYGON"
+} else {
+  l1 = "MAINNET"
+  l2 = "POLYGON"
+}
+const graphUrlL1 = process.env[`SANDBOX_GRAPH_URL_${l1}`]
+const graphUrlL2 = process.env[`SANDBOX_GRAPH_URL_${l2}`]
+const skipExcludeMinted = process.env.CI || !graphUrlL1 || !graphUrlL2 || process.env.NODE_ENV === "test";
+
+export async function excludeMinted({sector, lands, estates}: SectorData): Promise<SectorData> {
+  if (skipExcludeMinted) return {sector, lands, estates}
   const result: SectorData = {
     sector,
     lands: [],
@@ -19,7 +34,7 @@ export async function excludeMinted({sector, lands}: SectorData): Promise<Sector
   const mintedLands = await getMintedLands({minX, minY, maxX, maxY})
   lands.forEach(land => {
     if (mintedLands.find(m => m.coordinateX === land.coordinateX && m.coordinateY === land.coordinateY)) {
-      console.log("minted", land)
+      console.log("minted", JSON.stringify(land))
       return
     }
     result.lands.push(land)
@@ -28,17 +43,6 @@ export async function excludeMinted({sector, lands}: SectorData): Promise<Sector
 }
 
 async function getMintedLands({minX, minY, maxX, maxY}: {minX: number, minY: number, maxX: number, maxY: number}): Promise<SectorLand[]> {
-  let l1, l2;
-  if (hre.network.tags.testnet) {
-    // l1 = "GOERLI"
-    // l2 = "MUMBAI"
-    l1 = "MAINNET"
-    l2 = "POLYGON"
-  } else {
-    l1 = "MAINNET"
-    l2 = "POLYGON"
-  }
-  console.log(l1, l2)
   console.log({minX, minY, maxX, maxY})
   const query = `{
     landTokens(where: {x_gte: ${minX + 204} y_gte: ${minY + 204} x_lte:${maxX + 204} y_lte: ${maxY + 204}}) {
@@ -48,13 +52,10 @@ async function getMintedLands({minX, minY, maxX, maxY}: {minX: number, minY: num
       owner { id }
     }
   }`
-  const graphUrlL1 = process.env[`SANDBOX_GRAPH_URL_${l1}`]
-  const graphUrlL2 = process.env[`SANDBOX_GRAPH_URL_${l2}`]
-  if (!graphUrlL1 || !graphUrlL2) return [];
   const landMap: {[id: string]: SectorLand} = {}
   const landChains = await Promise.all([
-    new TheGraph(graphUrlL1).query<{id: string, x: number, y: number, owner: {id: string}}>(query, "landTokens", {}),
-    new TheGraph(graphUrlL2).query<{id: string, x: number, y: number, owner: {id: string}}>(query, "landTokens", {})
+    new TheGraph(graphUrlL1!).query<{id: string, x: number, y: number, owner: {id: string}}>(query, "landTokens", {}),
+    new TheGraph(graphUrlL2!).query<{id: string, x: number, y: number, owner: {id: string}}>(query, "landTokens", {})
   ])
   landChains.forEach(lands => lands.forEach(land => {
     if (landMap[land.id]) return;
