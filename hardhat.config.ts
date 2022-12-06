be5d048d1cc2b0f674bd3b97660077bac6323cbc
Rishabh Sharma <rishabh.sharma@sandbox.game>
2022-12-06 19:55:03 +0530

    fix: updated compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,7 +24,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.14',
+        version: '0.8.13',
         settings: {
           optimizer: {
             enabled: true,
