fb631ceb1a23b99b470a004142ea094f2118d19a
Rishabh Sharma <rishabh.sharma@sandbox.game>
2022-12-08 18:49:47 +0530

    fix: added contracts

---
+++ src/solc_0.8/operatorFilterRegistry/contracts/example/MockMarketPlace3.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+
+import {IERC1155} from "@openzeppelin/contracts-0.8/interfaces/IERC1155.sol";
+
+contract MockMarketPlace3 {
+    function transferToken(
+        address asset,
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) external {
+        IERC1155(asset).safeTransferFrom(from, to, id, amount, data);
+    }
+
+    function batchTransferToken(
+        address asset,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) external {
+        IERC1155(asset).safeBatchTransferFrom(from, to, ids, amounts, data);
+    }
+}
