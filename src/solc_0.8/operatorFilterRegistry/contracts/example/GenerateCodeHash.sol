3dc7fb0f22e2e869e1dd9ca1ce603de4cb3d7979
Rishabh Sharma <rishabh.sharma@sandbox.game>
2022-12-21 17:36:56 +0530

    fix: removed contracts from operator filterer directory

--- src/solc_0.8/operatorFilterRegistry/contracts/example/GenerateCodeHash.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-contract GenerateCodeHash {
-    function getCodeHash(address _contract) external view returns (bytes32) {
-        return _contract.codehash;
-    }
-}
