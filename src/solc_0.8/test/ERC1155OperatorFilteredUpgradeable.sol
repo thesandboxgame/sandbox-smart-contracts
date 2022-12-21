bd01ebeb684582519eab926e60cea0b7e4958ae6
Rishabh Sharma <rishabh.sharma@sandbox.game>
2022-12-21 19:02:50 +0530

    fix: udpated solidity pragma

--- src/solc_0.8/test/ERC1155OperatorFilteredUpgradeable.sol
+++ src/solc_0.8/test/ERC1155OperatorFilteredUpgradeable.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.2;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "./MockDefaultOperatorFiltererUpgradeable.sol";
--- src/solc_0.8/test/GenerateCodeHash.sol
+++ src/solc_0.8/test/GenerateCodeHash.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.2;
 
 contract GenerateCodeHash {
     function getCodeHash(address _contract) external view returns (bytes32) {
