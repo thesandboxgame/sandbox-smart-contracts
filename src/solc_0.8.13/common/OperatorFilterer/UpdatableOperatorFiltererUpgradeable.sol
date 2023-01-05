fe1a84aebd470936f0247f5333cc4209fdf38066
Marius Bălaj <balajmarius@gmail.com>
2023-01-05 13:39:13 +0200

    fix: import Initializable

--- src/solc_0.8.13/common/OperatorFilterer/UpdatableOperatorFiltererUpgradeable.sol
+++ src/solc_0.8.13/common/OperatorFilterer/UpdatableOperatorFiltererUpgradeable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Initializable} from "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable-0.8.13/proxy/utils/Initializable.sol";
 
 import {IOperatorFilterRegistry} from "./IOperatorFilterRegistry.sol";
 
