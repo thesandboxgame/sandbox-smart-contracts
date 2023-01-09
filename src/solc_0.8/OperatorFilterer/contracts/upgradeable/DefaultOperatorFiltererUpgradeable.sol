9814ee2c12cfc79d56abe0a56d31d7f861e5cf5a
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-01-09 17:22:09 +0530

    feat: added operator filterer contracts

---
+++ src/solc_0.8/OperatorFilterer/contracts/upgradeable/DefaultOperatorFiltererUpgradeable.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {OperatorFiltererUpgradeable} from "./OperatorFiltererUpgradeable.sol";
+
+abstract contract DefaultOperatorFiltererUpgradeable is OperatorFiltererUpgradeable {
+    address public constant DEFAULT_SUBSCRIPTION = address(0x3cc6CddA760b79bAfa08dF41ECFA224f810dCeB6);
+
+    function __DefaultOperatorFilterer_init(bool subscribe) internal {
+        __OperatorFilterer_init(DEFAULT_SUBSCRIPTION, subscribe);
+    }
+}
