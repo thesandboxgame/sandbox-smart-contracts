a25a20d4ab290a5c139ed1d20d28c802f9557014
Felipe Faria <felipeasf@gmail.com>
2023-01-13 15:20:14 -0300

    adding mock contract to coverage ignore array

--- .solcover.js
+++ .solcover.js
@@ -9,6 +9,7 @@ module.exports = {
     'solc_0.8/defi/rewardCalculation/TwoPeriodsRewardCalculator.sol', // legacy file
     'solc_0.8/common/BaseWithStorage/ERC2771HandlerV2.sol', // legacy file
     'solc_0.8/defi/rules', // legacy files
+    'solc_0.8/polygon/child/asset/MockPolygonAssetERC1155Tunnel.sol', // mock
     'solc_0.5',
     'solc_0.6',
     'solc_0.7',
