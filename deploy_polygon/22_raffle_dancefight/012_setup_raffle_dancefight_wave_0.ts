7c707898c43c275a8e9362d825ca61764cd4b834
Marius Bălaj <balajmarius@gmail.com>
2023-01-26 09:40:49 +0200

    fix: lint

--- deploy_polygon/22_raffle_dancefight/012_setup_raffle_dancefight_wave_0.ts
+++ deploy_polygon/22_raffle_dancefight/012_setup_raffle_dancefight_wave_0.ts
@@ -1,6 +1,5 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
-import {ethers} from 'hardhat';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments} = hre;
