00fea0419f394628da7b43a265f7c1f4c588e6d0
León <leon.isla.22@gmail.com>
2023-02-10 13:03:47 -0300

    deploy: rabbids & madballs contracts

--- deploy_polygon/21_raffle_madballs/014_change_owner_madballs.ts
+++ deploy_polygon/21_raffle_madballs/014_change_owner_madballs.ts
@@ -1,20 +1,20 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {catchUnknownSigner, execute, read} = deployments;
-  const {sandAdmin} = await getNamedAccounts();
+  const {nftCollectionAdmin} = await getNamedAccounts();
 
   const owner = await read('MadBalls', 'owner');
 
-  if (sandAdmin?.toLocaleLowerCase() !== owner?.toLocaleLowerCase()) {
+  if (nftCollectionAdmin?.toLocaleLowerCase() !== owner?.toLocaleLowerCase()) {
     await catchUnknownSigner(
       execute(
         'MadBalls',
         {from: owner, log: true},
         'transferOwnership',
-        sandAdmin
+        nftCollectionAdmin
       )
     );
   }
