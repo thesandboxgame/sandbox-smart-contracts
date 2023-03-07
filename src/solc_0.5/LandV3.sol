af8d2489be1f27033d7de35a1df1962fde7e93f5
Ayush Tiwari <ayush@sandbox.game>
2023-03-08 01:32:41 +0530

    feat: added operator filterer to LandV3

--- src/solc_0.5/LandV3.sol
+++ src/solc_0.5/LandV3.sol
@@ -3,8 +3,10 @@
 pragma solidity 0.5.9;
 
 import "./Land/erc721/LandBaseTokenV3.sol";
+import "./Land/erc721/ERC721BaseTokenV2.sol";
+import "./OperatorFilterer/contracts/upgradeable/OperatorFiltererUpgradeable.sol";
 
-contract LandV3 is LandBaseTokenV3 {
+contract LandV3 is LandBaseTokenV3, OperatorFiltererUpgradeable {
     /**
      * @notice Return the name of the token contract
      * @return The name of the token contract
@@ -68,5 +70,145 @@ contract LandV3 is LandBaseTokenV3 {
      */
     function supportsInterface(bytes4 id) external pure returns (bool) {
         return id == 0x01ffc9a7 || id == 0x80ac58cd || id == 0x5b5e139f;
+    }
+
+    /// @notice This function is used to register Land on the Operator filterer Registry of Opensea.can only be called by admin.
+    /// @dev used to register contract and subscribe to the subscriptionOrRegistrantToCopy's black list. 
+    /// @param subscriptionOrRegistrantToCopy registration address of the list to subscribe.
+    /// @param subscribe bool to signify subscription "true"" or to copy the list "false".
+    function register(address subscriptionOrRegistrantToCopy, bool subscribe) external onlyAdmin{
+        __OperatorFilterer_init(subscriptionOrRegistrantToCopy, subscribe);
+    }
+
+     /**
+     * @notice Approve an operator to spend tokens on the sender behalf
+     * @param sender The address giving the approval
+     * @param operator The address receiving the approval
+     * @param id The id of the token
+     */
+    function approveFor(
+        address sender,
+        address operator,
+        uint256 id
+    ) external onlyAllowedOperatorApproval(operator){
+        address owner = _ownerOf(id);
+        require(sender != address(0), "sender is zero address");
+        require(
+            msg.sender == sender ||
+            _metaTransactionContracts[msg.sender] ||
+            _operatorsForAll[sender][msg.sender] ||
+            _superOperators[msg.sender],
+            "not authorized to approve"
+        );
+        require(owner == sender, "owner != sender");
+        _approveFor(owner, operator, id);
+    }
+
+    /**
+     * @notice Set the approval for an operator to manage all the tokens of the sender
+     * @param operator The address receiving the approval
+     * @param approved The determination of the approval
+     */
+    function setApprovalForAll(address operator, bool approved) external onlyAllowedOperatorApproval(operator){
+        _setApprovalForAll(msg.sender, operator, approved);
+    }
+
+    /**
+     * @notice Set the approval for an operator to manage all the tokens of the sender
+     * @param sender The address giving the approval
+     * @param operator The address receiving the approval
+     * @param approved The determination of the approval
+     */
+    function setApprovalForAllFor(
+        address sender,
+        address operator,
+        bool approved
+    ) external onlyAllowedOperatorApproval(operator){
+        require(sender != address(0), "Invalid sender address");
+        require(
+            msg.sender == sender ||
+            _metaTransactionContracts[msg.sender] ||
+            _superOperators[msg.sender],
+            "not authorized to approve for all"
+        );
+
+        _setApprovalForAll(sender, operator, approved);
+    }
+
+    /**
+     * @notice Approve an operator to spend tokens on the sender behalf
+     * @param operator The address receiving the approval
+     * @param id The id of the token
+     */
+    function approve(address operator, uint256 id) external onlyAllowedOperatorApproval(operator){
+        address owner = _ownerOf(id);
+        require(owner != address(0), "token does not exist");
+        require(
+            owner == msg.sender ||
+            _operatorsForAll[owner][msg.sender] ||
+            _superOperators[msg.sender],
+            "not authorized to approve"
+        );
+        _approveFor(owner, operator, id);
+    }
+
+    /**
+     * @notice Transfer a token between 2 addresses
+     * @param from The sender of the token
+     * @param to The recipient of the token
+     * @param id The id of the token
+    */
+    function transferFrom(address from, address to, uint256 id) external onlyAllowedOperator(from){
+        bool metaTx = _checkTransfer(from, to, id);
+        _transferFrom(from, to, id);
+        if (to.isContract() && _checkInterfaceWith10000Gas(to, ERC721_MANDATORY_RECEIVER)) {
+            require(
+                _checkOnERC721Received(metaTx ? from : msg.sender, from, to, id, ""),
+                "erc721 transfer rejected by to"
+            );
+        }
+    }
+
+     /**
+     * @notice Transfer a token between 2 addresses letting the receiver knows of the transfer
+     * @param from The sender of the token
+     * @param to The recipient of the token
+     * @param id The id of the token
+     * @param data Additional data
+     */
+    function safeTransferFrom(address from, address to, uint256 id, bytes memory data) public onlyAllowedOperator(from){
+        super.safeTransferFrom(from, to, id, data);
+    }
+
+    /**
+     * @notice Transfer a token between 2 addresses letting the receiver knows of the transfer
+     * @param from The send of the token
+     * @param to The recipient of the token
+     * @param id The id of the token
+     */
+    function safeTransferFrom(address from, address to, uint256 id) external onlyAllowedOperator(from){
+        super.safeTransferFrom(from, to, id, "");
+    }
+
+    /**
+     * @notice Transfer many tokens between 2 addresses
+     * @param from The sender of the token
+     * @param to The recipient of the token
+     * @param ids The ids of the tokens
+     * @param data additional data
+    */
+    function batchTransferFrom(address from, address to, uint256[] calldata ids, bytes calldata data) external  onlyAllowedOperator(from){
+        _batchTransferFrom(from, to, ids, data, false);
+    }
+
+    /**
+     * @notice Transfer many tokens between 2 addresses ensuring the receiving contract has a receiver method
+     * @param from The sender of the token
+     * @param to The recipient of the token
+     * @param ids The ids of the tokens
+     * @param data additional data
+    */
+    function safeBatchTransferFrom(address from, address to, uint256[] calldata ids, bytes calldata data) external onlyAllowedOperator(from){
+        _batchTransferFrom(from, to, ids, data, true);
     }
 }
