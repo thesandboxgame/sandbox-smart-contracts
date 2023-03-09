3f514c58210422216062c3522a32bc0ef8cd81fe
León <leon.isla.22@gmail.com>
2023-03-09 13:00:51 -0300

    add: dancefight & hellskitchen deployments

--- deploy_polygon/23_raffle_hells_kitchen/014_change_owner_hells_kitchen.ts
+++ deploy_polygon/23_raffle_hells_kitchen/014_change_owner_hells_kitchen.ts
@@ -1,20 +1,20 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {catchUnknownSigner, execute, read} = deployments;
-  const {sandAdmin} = await getNamedAccounts();
+  const {nftCollectionAdmin} = await getNamedAccounts();
 
   const owner = await read('HellsKitchen', 'owner');
 
-  if (sandAdmin?.toLocaleLowerCase() !== owner?.toLocaleLowerCase()) {
+  if (nftCollectionAdmin?.toLocaleLowerCase() !== owner?.toLocaleLowerCase()) {
     await catchUnknownSigner(
       execute(
         'HellsKitchen',
         {from: owner, log: true},
         'transferOwnership',
-        sandAdmin
+        nftCollectionAdmin
       )
     );
   }
