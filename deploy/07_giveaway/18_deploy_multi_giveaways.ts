4c3c922bdbd2cef208ce18fdcbe0b48c3092c18e
León <leon.isla.22@gmail.com>
2023-03-13 10:37:10 -0300

    fix: format

--- deploy/07_giveaway/18_deploy_multi_giveaways.ts
+++ deploy/07_giveaway/18_deploy_multi_giveaways.ts
@@ -8,18 +8,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deploy} = deployments;
   const {deployer, multiGiveawayAdmin} = await getNamedAccounts();
 
-  const adminByNetwork: {[n: number]: {[name: string]: string | undefined, default: string}} = {
+  const adminByNetwork: {
+    [n: number]: {[name: string]: string | undefined; default: string};
+  } = {
     1: {
       default: deployer,
       mainnet: multiGiveawayAdmin,
-      polygon: multiGiveawayAdmin
+      polygon: multiGiveawayAdmin,
     },
     2: {
       default: deployer,
       mainnet: '0x2e0405eaC370F83d6A0085D69F77C37dAf8d901F',
-      polygon: '0x44baA6A480401717ed72762958eE2742b47F8b77'
-    }
-  }
+      polygon: '0x44baA6A480401717ed72762958eE2742b47F8b77',
+    },
+  };
 
   for (const n of multigiveaways) {
     await deploy(`Multi_Giveaway_${n}`, {
@@ -27,10 +29,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       from: deployer,
       log: true,
       skipIfAlreadyDeployed: true,
-      args: [adminByNetwork[n][hre.network.name] || adminByNetwork[n].default, '0x0000000000000000000000000000000000000000'], // admin, trustedForwarder
+      args: [
+        adminByNetwork[n][hre.network.name] || adminByNetwork[n].default,
+        '0x0000000000000000000000000000000000000000',
+      ], // admin, trustedForwarder
     });
   }
 };
 
 export default func;
-func.tags = ["Multi_Giveaway", "Multi_Giveaway_deploy"];
+func.tags = ['Multi_Giveaway', 'Multi_Giveaway_deploy'];
