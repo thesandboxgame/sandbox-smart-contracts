e6eb5f2889f49f4b1dea635a8a9ed1c168e521ad
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2023-04-11 15:55:44 +0200

    natspec FxBaseRootTunnelUpgradeable

--- src/solc_0.8/common/fx-portal/FxBaseRootTunnelUpgradeable.sol
+++ src/solc_0.8/common/fx-portal/FxBaseRootTunnelUpgradeable.sol
@@ -7,10 +7,20 @@ import {
     IFxStateSender
 } from "@maticnetwork/fx-portal/contracts/tunnel/FxBaseRootTunnel.sol";
 
+/**
+ * @title FxBaseRootTunnelUpgradeable
+ * @author The Sandbox
+ * @dev Upgradeable version of the fx-portal tunnel for the root chain
+ */
 abstract contract FxBaseRootTunnelUpgradeable is FxBaseRootTunnel {
     // solhint-disable-next-line no-empty-blocks
     constructor() FxBaseRootTunnel(address(0), address(0)) {}
 
+    /**
+     * @dev Initializes the contract
+     * @param _checkpointManager checkpoint manager address
+     * @param _fxRoot root token address
+     */
     function __FxBaseRootTunnelUpgradeable_initialize(address _checkpointManager, address _fxRoot) internal {
         checkpointManager = ICheckpointManager(_checkpointManager);
         fxRoot = IFxStateSender(_fxRoot);
