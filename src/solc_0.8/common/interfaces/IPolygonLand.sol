ff81acb6424c928fa7e5a80debce5b95af08ec39
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2023-04-11 18:04:38 +0200

    natspec IPolygonLand

--- src/solc_0.8/common/interfaces/IPolygonLand.sol
+++ src/solc_0.8/common/interfaces/IPolygonLand.sol
@@ -3,7 +3,20 @@ pragma solidity 0.8.2;
 
 import {ILandToken} from "./ILandToken.sol";
 
+/**
+ * @title IPolygonLand
+ * @author The Sandbox
+ * @notice Interface of the LAND token on the child chain
+ */
 interface IPolygonLand is ILandToken {
+    /**
+     * @notice Mint a new quad (aligned to a quad tree with size 1, 3, 6, 12 or 24 only)
+     * @param user The recipient of the new quad
+     * @param size The size of the new quad
+     * @param x The top left x coordinate of the new quad
+     * @param y The top left y coordinate of the new quad
+     * @param data extra data to pass to the transfer
+     */
     function mintQuad(
         address user,
         uint256 size,
@@ -12,6 +25,13 @@ interface IPolygonLand is ILandToken {
         bytes memory data
     ) external;
 
+    /**
+     * @notice Checks if a LAND exists by its coordinates
+     * @param size size of the quad
+     * @param x x coordinate
+     * @param y y coordinate
+     * @return does the LAND exist
+     */
     function exists(
         uint256 size,
         uint256 x,
