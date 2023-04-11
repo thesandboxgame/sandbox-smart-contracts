87bc0d1a5cdb7b605fe35b8655ee87c83513f1a1
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2023-04-11 15:05:28 +0200

    natspec AddressUtils

--- src/solc_0.5/contracts_common/Libraries/AddressUtils.sol
+++ src/solc_0.5/contracts_common/Libraries/AddressUtils.sol
@@ -1,12 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.5.9;
 
+/**
+ * @title AddressUtils
+ * @author The Sandbox
+ * @notice Helper to manipulate addresses
+ */
 library AddressUtils {
 
-    function toPayable(address _address) internal pure returns (address payable) {
+    /**
+     * @dev Cast the address to be payable
+     * @param _address target address
+     * @return a payable address
+     */
+    function toPayable(address _address) internal pure returns (address payable _payable) {
         return address(uint160(_address));
     }
 
+    /**
+     * @dev Check if the address is a contract
+     * @param addr target address
+     * @return is it a contract
+     */
     function isContract(address addr) internal view returns (bool) {
         // for accounts without code, i.e. `keccak256('')`:
         bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
