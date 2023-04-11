bc42adf63f9e67b58d3bef0540652587081f2742
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2023-04-11 15:52:58 +0200

    natspec FxBaseChildTunnelUpgradeable

--- src/solc_0.8/common/fx-portal/FxBaseChildTunnelUpgradeable.sol
+++ src/solc_0.8/common/fx-portal/FxBaseChildTunnelUpgradeable.sol
@@ -3,10 +3,19 @@ pragma solidity 0.8.2;
 
 import {FxBaseChildTunnel} from "@maticnetwork/fx-portal/contracts/tunnel/FxBaseChildTunnel.sol";
 
+/**
+ * @title FxBaseChildTunnelUpgradeable
+ * @author The Sandbox
+ * @dev Upgradeable version of the fx-portal tunnel for the child chain
+ */
 abstract contract FxBaseChildTunnelUpgradeable is FxBaseChildTunnel {
     // solhint-disable-next-line no-empty-blocks
     constructor() FxBaseChildTunnel(address(0)) {}
 
+    /**
+     * @dev Initializes the contract
+     * @param _fxChild child token address
+     */
     function __FxBaseChildTunnelUpgradeable_initialize(address _fxChild) internal {
         fxChild = _fxChild;
     }
