fb8e378635f37f78b5c0e38497b7b7ca41866f37
Ayush Tiwari <ayush@sandbox.game>
2023-04-13 15:24:04 +0530

    fix: updated require statements

--- src/solc_0.5/Land/erc721/LandBaseTokenV3.sol
+++ src/solc_0.5/Land/erc721/LandBaseTokenV3.sol
@@ -48,8 +48,8 @@ contract LandBaseTokenV3 is ERC721BaseTokenV2 {
         require(to != address(0), "to is zero address");
         require(size != 0, "size cannot be zero");
         require(isMinter(msg.sender), "Only a minter can mint");
-        require(x % size == 0 && y % size == 0, "Invalid coordinates");
-        require(x <= GRID_SIZE - size && y <= GRID_SIZE - size, "Out of bounds");
+
+        _isValidCoordinates(x, y, size);
 
         (uint256 layer, , ) = _getQuadLayer(size);
         uint256 quadId = _getQuadId(layer, x, y);
@@ -145,7 +145,8 @@ contract LandBaseTokenV3 is ERC721BaseTokenV2 {
     ) external {
         require(from != address(0), "from is zero address");
         require(to != address(0), "can't send to zero address");
-        require(sizes.length == xs.length && xs.length == ys.length, "invalid data");
+        require(sizes.length == xs.length, "LandBaseTokenV3: sizes's and x's length are different");
+        require(xs.length == ys.length, "LandBaseTokenV3: x's and y's length are different");
         bool metaTx = msg.sender != from && _metaTransactionContracts[msg.sender];
         if (msg.sender != from && !metaTx) {
             require(
@@ -232,10 +233,15 @@ contract LandBaseTokenV3 is ERC721BaseTokenV2 {
         uint256 x,
         uint256 y
     ) public view returns (bool) {
-        require(size != 0, "size cannot be zero");
-        require(x % size == 0 && y % size == 0, "Invalid coordinates");
-        require(x <= GRID_SIZE - size && y <= GRID_SIZE - size, "Out of bounds");
+        _isValidCoordinates(x, y, size);
         return _ownerOfQuad(size, x, y) != address(0);
+    }
+
+    function _isValidCoordinates(uint256 x, uint256 y, uint256 size) internal pure {
+        require(x % size == 0, "Invalid x coordinate");
+        require(y % size == 0, "Invalid y coordinate");
+        require(x <= GRID_SIZE - size, "x out of bounds");
+        require(y <= GRID_SIZE - size, "y out of bounds");
     }
 
     /**
@@ -466,9 +472,7 @@ contract LandBaseTokenV3 is ERC721BaseTokenV2 {
         uint256 x,
         uint256 y
     ) internal {
-        require(size != 0, "size cannot be zero");
-        require(x % size == 0 && y % size == 0, "Invalid coordinates");
-        require(x <= GRID_SIZE - size && y <= GRID_SIZE - size, "Out of bounds");
+        _isValidCoordinates(x, y, size);
         if (size == 3 || size == 6 || size == 12 || size == 24) {
             _regroupQuad(from, to, Land({x: x, y: y, size: size}), true, size / 2);
         } else {
@@ -744,7 +748,8 @@ contract LandBaseTokenV3 is ERC721BaseTokenV2 {
     /// @return address of the owner
     function _ownerOf(uint256 id) internal view returns (address) {
         (uint256 size, uint256 x, uint256 y) = _getQuadById(id);
-        require(x % size == 0 && y % size == 0, "Invalid token id");
+        require(x % size == 0, "x coordinate: Invalid token id");
+        require(y % size == 0, "y coordinate: Invalid token id");
         return _ownerOfQuad(size, x, y);
     }
 
--- src/solc_0.8/polygon/child/land/PolygonLandBaseTokenV2.sol
+++ src/solc_0.8/polygon/child/land/PolygonLandBaseTokenV2.sol
@@ -52,7 +52,8 @@ abstract contract PolygonLandBaseTokenV2 is IPolygonLand, Initializable, ERC721B
     ) external override {
         require(from != address(0), "from is zero address");
         require(to != address(0), "can't send to zero address");
-        require(sizes.length == xs.length && xs.length == ys.length, "invalid data");
+        require(sizes.length == xs.length, "PolygonLandBaseTokenV2: sizes's and x's length are different");
+        require(xs.length == ys.length, "PolygonLandBaseTokenV2: x's and y's length are different");
         if (_msgSender() != from) {
             require(
                 _operatorsForAll[from][_msgSender()] || _superOperators[_msgSender()],
@@ -139,7 +140,7 @@ abstract contract PolygonLandBaseTokenV2 is IPolygonLand, Initializable, ERC721B
         uint256 y,
         bytes memory data
     ) external virtual override {
-        _isValidCoordinates(x, y, size);
+        _isValidQuad(size, x, y);
         require(isMinter(_msgSender()), "!AUTHORIZED");
         _mintQuad(user, size, x, y, data);
     }
@@ -235,7 +236,7 @@ abstract contract PolygonLandBaseTokenV2 is IPolygonLand, Initializable, ERC721B
         uint256 x,
         uint256 y
     ) public view override returns (bool) {
-        _isValidCoordinates(x, y, size);
+        _isValidQuad(size, x, y);
         return _ownerOfQuad(size, x, y) != address(0);
     }
 
@@ -283,6 +284,18 @@ abstract contract PolygonLandBaseTokenV2 is IPolygonLand, Initializable, ERC721B
             );
     }
 
+    function _isValidQuad(
+        uint256 size,
+        uint256 x,
+        uint256 y
+    ) internal view {
+        require(size == 1 || size == 3 || size == 6 || size == 12 || size == 24, "Invalid size");
+        require(x % size == 0, "Invalid x coordinate");
+        require(y % size == 0, "Invalid y coordinate");
+        require(x <= GRID_SIZE - size, "Out of bounds");
+        require(y <= GRID_SIZE - size, "Out of bounds");
+    }
+
     function _transferQuad(
         address from,
         address to,
@@ -290,7 +303,7 @@ abstract contract PolygonLandBaseTokenV2 is IPolygonLand, Initializable, ERC721B
         uint256 x,
         uint256 y
     ) internal {
-        _isValidCoordinates(x, y, size);
+        _isValidQuad(size, x, y);
         if (size == 1) {
             uint256 id1x1 = _getQuadId(LAYER_1x1, x, y);
             address owner = _ownerOf(id1x1);
@@ -616,8 +629,10 @@ abstract contract PolygonLandBaseTokenV2 is IPolygonLand, Initializable, ERC721B
         uint256 x,
         uint256 y
     ) internal {
-        require(x % size == 0 && y % size == 0, "Invalid coordinates");
-        require(x <= GRID_SIZE - size && y <= GRID_SIZE - size, "Out of bounds");
+        require(x % size == 0, "Invalid x coordinate");
+        require(y % size == 0, "Invalid y coordinate");
+        require(x <= GRID_SIZE - size, "Out of bounds");
+        require(y <= GRID_SIZE - size, "Out of bounds");
         if (size == 3 || size == 6 || size == 12 || size == 24) {
             _regroupQuad(from, to, Land({x: x, y: y, size: size}), true, size / 2);
         } else {
@@ -736,7 +751,8 @@ abstract contract PolygonLandBaseTokenV2 is IPolygonLand, Initializable, ERC721B
 
     function _ownerOf(uint256 id) internal view override returns (address) {
         (uint256 size, uint256 x, uint256 y) = _getQuadById(id);
-        require(x % size == 0 && y % size == 0, "Invalid token id");
+        require(x % size == 0, "x coordinate: Invalid token id");
+        require(y % size == 0, "y coordinate: Invalid token id");
         if (size == 1) {
             uint256 owner1x1 = _owners[id];
             return (owner1x1 & BURNED_FLAG) == BURNED_FLAG ? address(0) : _ownerOfQuad(size, x, y);
