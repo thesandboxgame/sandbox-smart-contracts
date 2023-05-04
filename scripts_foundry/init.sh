4a7e8f6f0032cee45dba5cc8ecde2bf737e24d20
Alin Mihai Barbatei <alin.barbatei@gmail.com>
2023-05-04 12:11:23 +0300

    added foundry scripts

--- scripts_foundry/init.sh
+++ scripts_foundry/init.sh
@@ -2,7 +2,6 @@
 
 forge install foundry-rs/forge-std
 forge install OpenZeppelin/openzeppelin-contracts
-forge install vectorized/solady
 forge install OpenZeppelin/openzeppelin-contracts-upgradeable
 forge install ProjectOpenSea/operator-filter-registry
 
