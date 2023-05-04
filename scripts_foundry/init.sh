94a9e6831195ce3a888f656c8e17229d07f38557
Alin Mihai Barbatei <alin.barbatei@gmail.com>
2023-05-04 12:10:30 +0300

    added foundry scripts

---
+++ scripts_foundry/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+forge install foundry-rs/forge-std
+forge install OpenZeppelin/openzeppelin-contracts
+forge install vectorized/solady
+forge install OpenZeppelin/openzeppelin-contracts-upgradeable
+forge install ProjectOpenSea/operator-filter-registry
+
+sudo apt-get install lcov
