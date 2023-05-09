2ff8faf070f437ca7e8eafb923ab91960af913b8
Ayush Tiwari <ayush@sandbox.game>
2023-05-09 16:46:09 +0530

    refactor: removed gap from WithAdmin and WithSuperOperator due to size limit

--- src/solc_0.8/common/BaseWithStorage/WithAdminV2.sol
+++ src/solc_0.8/common/BaseWithStorage/WithAdminV2.sol
@@ -31,8 +31,4 @@ contract WithAdminV2 is ContextUpgradeable {
         emit AdminChanged(admin, newAdmin);
         _admin = newAdmin;
     }
-
-    // Empty storage space in contracts for future enhancements
-    // ref: https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/issues/13)
-    uint256[49] private __gap;
 }
