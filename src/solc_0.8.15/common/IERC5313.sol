2e18261c82ae7582e1eacfcce03027b7468c83ea
Alin Mihai Barbatei <alin.barbatei@gmail.com>
2023-05-11 14:41:23 +0300

    doc change in IERC5313

--- src/solc_0.8.15/common/IERC5313.sol
+++ src/solc_0.8.15/common/IERC5313.sol
@@ -2,9 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-// @note 
-// take from: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/interfaces/IERC5313.sol
-// as of 2023.04.25 version 4.9 of OpenZeppelin/openzeppelin-contracts, that holds this interface, is not released. Will change it when it will be
+// taken from: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/interfaces/IERC5313.sol
+// as of deployment, version 4.9 of OpenZeppelin/openzeppelin-contracts, that holds this interface, is not released. Will change it when it will be
 
 /**
  * @dev Interface for the Light Contract Ownership Standard.
