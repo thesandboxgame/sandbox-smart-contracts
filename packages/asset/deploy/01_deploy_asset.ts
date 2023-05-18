101237ed2f17f44e5260064d950a74961ad9385e
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-05-18 16:36:00 +0530

    fix: updated deployment script

--- packages/asset/deploy/01_deploy_asset.ts
+++ packages/asset/deploy/01_deploy_asset.ts
@@ -22,6 +22,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
           1, // chain index for polygon network
           [1, 2, 3, 4, 5, 6],
           [2, 4, 6, 8, 10, 12],
+          "ipfs://",
         ],
       },
       upgradeIndex: 0,
