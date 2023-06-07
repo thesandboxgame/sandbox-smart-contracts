25af25dcfa3cbf81c6eaa481d7f930507afa185e
atkinsonholly <atkinsonholly@gmail.com>
2023-06-07 11:55:05 +0100

    fix: fix test, import filterOperatorSubscription

--- packages/asset/deploy/01_deploy_asset.ts
+++ packages/asset/deploy/01_deploy_asset.ts
@@ -3,14 +3,17 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { OPERATOR_FILTER_REGISTRY, DEFAULT_SUBSCRIPTION } from "../constants";
 import { abi } from "../test/operatorRegistryABI";
 
-
-
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
-  const { deployer, upgradeAdmin, trustedForwarder } = await getNamedAccounts();
+  const {
+    deployer,
+    upgradeAdmin,
+    trustedForwarder,
+    filterOperatorSubscription,
+  } = await getNamedAccounts();
 
- await deploy("Asset", {
+  await deploy("Asset", {
     from: deployer,
     contract: "Asset",
     proxy: {
@@ -23,7 +26,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
           [1, 2, 3, 4, 5, 6],
           [2, 4, 6, 8, 10, 12],
           "ipfs://",
-          filterOperatorSubscription
+          filterOperatorSubscription,
         ],
       },
       upgradeIndex: 0,
@@ -32,13 +35,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 
   // get asset from deployment
-  const asset = await  deployments.get("Asset");
+  const asset = await deployments.get("Asset");
 
   // get asset from ethers
-  const asset2 = await hre.ethers.getContract("Asset")
-  
-  if(asset.address == asset2.address) console.log("asset address is same from both ethers and deployments")
+  const asset2 = await hre.ethers.getContract("Asset");
 
+  if (asset.address == asset2.address)
+    console.log("asset address is same from both ethers and deployments");
 
   const operatorFilterRegistry = await hre.ethers.getContractAt(
     abi,
@@ -49,8 +52,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     filterOperatorSubscription
   );
 
-  if(registered) console.log("Asset is registered. Checked in deployment")
-
+  if (registered) console.log("Asset is registered. Checked in deployment");
 };
 export default func;
 
