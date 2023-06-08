a301945559fad4a6cc5d5acd7e76ff5f9f0682bc
Ayush Tiwari <ayush@sandbox.game>
2023-06-08 15:45:04 +0530

    refactor: added defender address as admin in migation contract deployment

--- packages/core/deploy/02_land/14_deploy_land_tunnel_migration.ts
+++ packages/core/deploy/02_land/14_deploy_land_tunnel_migration.ts
@@ -10,11 +10,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const landTunnel = await deployments.get('LandTunnel');
   const land = await deployments.get('Land');
   const landTunnelV2 = await deployments.get('LandTunnelV2');
+  const defender = await deployments.get('DEFENDER');
 
   await deploy('LandTunnelMigration', {
     from: deployer,
     contract: 'LandTunnelMigration',
-    args: [land.address, landTunnelV2.address, landTunnel.address, deployer],
+    args: [
+      land.address,
+      landTunnelV2.address,
+      landTunnel.address,
+      defender.address,
+    ],
     log: true,
   });
 };
