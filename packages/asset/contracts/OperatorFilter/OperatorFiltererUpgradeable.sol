b6b0b3c4c3058ab5381c3a4e768f5cb4245d35f8
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-06-20 18:22:01 +0530

    fix: added named import

--- packages/asset/contracts/OperatorFilter/OperatorFiltererUpgradeable.sol
+++ packages/asset/contracts/OperatorFilter/OperatorFiltererUpgradeable.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {IOperatorFilterRegistry} from "./interfaces/IOperatorFilterRegistry.sol";
 
 ///@title OperatorFiltererUpgradeable
