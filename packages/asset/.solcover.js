0f52209d9cd9591e0a522f9b7b0d087cba8b3f59
atkinsonholly <atkinsonholly@gmail.com>
2023-06-21 13:40:54 +0100

    add: .solcover.js file in asset package

---
+++ packages/asset/.solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mocha: {
+    grep: '@skip-on-coverage', // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
+  skipFiles: [
+    '/mock',
+  ],
+};
