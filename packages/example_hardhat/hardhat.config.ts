80df7d7be995c533e675b1dc07d0f2b80b6ab84e
Andres Adjimann <andres@sandbox.game>
2023-06-30 09:48:56 -0300

    fix: add PR fixes, improve README and upgradable example

--- packages/example_hardhat/hardhat.config.ts
+++ packages/example_hardhat/hardhat.config.ts
@@ -2,6 +2,8 @@ import '@nomicfoundation/hardhat-toolbox';
 import {HardhatUserConfig} from 'hardhat/config';
 
 const config: HardhatUserConfig = {
+  // solidity compiler version may be updated for new packages as required
+  // to ensure packages use up-to-date dependencies
   solidity: {
     compilers: [
       {
