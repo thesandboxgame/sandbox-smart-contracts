52087dc36f4a2c7c246a57f6b486e98a54399ae9
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-07-14 14:05:33 +0530

    feat: added and updated deployment scripts

--- packages/deploy/deploy/400_asset/401_deploy_asset.ts
+++ packages/deploy/deploy/400_asset/401_deploy_asset.ts
@@ -1,12 +1,16 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 
+export const DEFAULT_BPS = 300;
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
-  const {deployer, assetAdmin, upgradeAdmin} = await getNamedAccounts();
+  const {deployer, assetAdmin, upgradeAdmin, commonRoyaltyReceiver} =
+    await getNamedAccounts();
 
   const TRUSTED_FORWARDER = await deployments.get('TRUSTED_FORWARDER_V2');
+  const RoyaltyManager = await deployments.get('RoyaltyManager');
 
   await deploy('Asset', {
     from: deployer,
@@ -22,6 +26,9 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
           [1, 2, 3, 4, 5, 6], // catalystTiers
           [2, 4, 6, 8, 10, 12], // catalystRecycleCopiesNeeded
           'ipfs://',
+          commonRoyaltyReceiver,
+          DEFAULT_BPS,
+          RoyaltyManager.address,
         ],
       },
       upgradeIndex: 0,
