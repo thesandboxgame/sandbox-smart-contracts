a5b7e8311fa04600350d453fd4df5391fcf108bb
wojciech-turek <contact@wojciechturek.com>
2023-07-18 11:47:50 +0200

    Add auth validator tests

---
+++ packages/asset/test/AuthValidator.test.ts
@@ -0,0 +1,111 @@
+import {ethers} from 'ethers';
+import {expect} from 'chai';
+import runSetup from './fixtures/authValidatorFixture';
+
+describe('AuthValidator, (/packages/asset/contracts/AuthValidator.sol)', function () {
+  describe('General', function () {
+    it('should assign DEFAULT_ADMIN_ROLE to the admin address from the constructor', async function () {
+      const {authValidatorAdmin, AuthValidatorContract} = await runSetup();
+      const DEFAULT_ADMIN_ROLE =
+        await AuthValidatorContract.DEFAULT_ADMIN_ROLE();
+      const hasRole = await AuthValidatorContract.hasRole(
+        DEFAULT_ADMIN_ROLE,
+        authValidatorAdmin.address
+      );
+      expect(hasRole).to.equal(true);
+    });
+    it('should allow admin to set signer for a given contract address', async function () {
+      const {MockContract, AuthValidatorContractAsAdmin, backendSigner} =
+        await runSetup();
+      await expect(
+        AuthValidatorContractAsAdmin.setSigner(
+          MockContract.address,
+          backendSigner.address
+        )
+      ).to.not.be.reverted;
+      const assignedSigner = await AuthValidatorContractAsAdmin.getSigner(
+        MockContract.address
+      );
+      expect(assignedSigner).to.equal(backendSigner.address);
+    });
+    it('should allow admin to remove signer for a given contract address', async function () {
+      const {MockContract, AuthValidatorContractAsAdmin, backendSigner} =
+        await runSetup();
+      await AuthValidatorContractAsAdmin.setSigner(
+        MockContract.address,
+        backendSigner.address
+      );
+      await AuthValidatorContractAsAdmin.setSigner(
+        MockContract.address,
+        ethers.constants.AddressZero
+      );
+      const assignedSigner = await AuthValidatorContractAsAdmin.getSigner(
+        MockContract.address
+      );
+      expect(assignedSigner).to.equal(ethers.constants.AddressZero);
+    });
+  });
+  describe('Signature verification', function () {
+    it('should correctly verify signature when a signer is set', async function () {
+      const {
+        AuthValidatorContractAsAdmin,
+        backendSigner,
+        authValidatorAdmin,
+        createMockSignature,
+      } = await runSetup();
+      await AuthValidatorContractAsAdmin.setSigner(
+        authValidatorAdmin.address,
+        backendSigner.address
+      );
+      const {signature, digest} = await createMockSignature(
+        authValidatorAdmin.address,
+        backendSigner
+      );
+      const isValid = await AuthValidatorContractAsAdmin.verify(
+        signature,
+        digest
+      );
+      expect(isValid).to.equal(true);
+    });
+    it('should revert when signature is not valid', async function () {
+      const {
+        AuthValidatorContractAsAdmin,
+        backendSigner,
+        authValidatorAdmin,
+        createMockSignature,
+        createMockDigest,
+      } = await runSetup();
+      await AuthValidatorContractAsAdmin.setSigner(
+        authValidatorAdmin.address,
+        backendSigner.address
+      );
+
+      // digest is using different address on purpose
+      const digest = createMockDigest(backendSigner.address);
+      const {signature} = await createMockSignature(
+        authValidatorAdmin.address,
+        backendSigner
+      );
+      const isValid = await AuthValidatorContractAsAdmin.verify(
+        signature,
+        digest
+      );
+      expect(isValid).to.equal(false);
+    });
+    it('should revert when there is no signer assigned for a given contract address', async function () {
+      const {
+        AuthValidatorContractAsAdmin,
+        createMockSignature,
+        authValidatorAdmin,
+        backendSigner,
+      } = await runSetup();
+      const {signature, digest} = await createMockSignature(
+        authValidatorAdmin.address,
+        backendSigner
+      );
+      await expect(
+        AuthValidatorContractAsAdmin.verify(signature, digest)
+      ).to.be.revertedWith('AuthValidator: signer not set');
+    });
+  });
+});
