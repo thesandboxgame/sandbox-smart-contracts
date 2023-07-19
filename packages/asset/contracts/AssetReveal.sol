f521f2a5226d7c70830c53b4f03aa2bc619ff2a8
wojciech-turek <contact@wojciechturek.com>
2023-07-19 10:19:40 +0200

    Fix formatting

--- packages/asset/contracts/AssetReveal.sol
+++ packages/asset/contracts/AssetReveal.sol
@@ -3,7 +3,10 @@ pragma solidity 0.8.18;
 
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {EIP712Upgradeable} from "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
-import {AccessControlUpgradeable, ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {
+    AccessControlUpgradeable,
+    ContextUpgradeable
+} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {TokenIdUtils} from "./libraries/TokenIdUtils.sol";
 import {AuthValidator} from "./AuthValidator.sol";
 import {ERC2771Handler} from "./ERC2771Handler.sol";
@@ -343,10 +346,10 @@ contract AssetReveal is IAssetReveal, Initializable, AccessControlUpgradeable, E
     /// @param metadataHashes The hashes of the metadata
     /// @param prevTokenId The previous token id from which the assets are revealed
     /// @return tokenIdArray The array of tokenIds to mint
-    function getRevealedTokenIds(
-        string[] calldata metadataHashes,
-        uint256 prevTokenId
-    ) internal returns (uint256[] memory) {
+    function getRevealedTokenIds(string[] calldata metadataHashes, uint256 prevTokenId)
+        internal
+        returns (uint256[] memory)
+    {
         IAsset.AssetData memory data = prevTokenId.getData();
         require(!data.revealed, "AssetReveal: already revealed");
         uint256[] memory tokenIdArray = new uint256[](metadataHashes.length);
