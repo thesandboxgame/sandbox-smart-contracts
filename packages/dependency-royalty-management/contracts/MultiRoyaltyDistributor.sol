94a1b57715c1f518957a2204f56f1998976259e0
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-07-26 18:17:06 +0530

    fix: updated contract and interface

--- packages/dependency-royalty-management/contracts/MultiRoyaltyDistributor.sol
+++ packages/dependency-royalty-management/contracts/MultiRoyaltyDistributor.sol
@@ -8,7 +8,7 @@ import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 import {
     IEIP2981MultiReceiverRoyaltyOverride
 } from "@manifoldxyz/royalty-registry-solidity/contracts/overrides/IMultiReceiverRoyaltyOverride.sol";
-import {IMultiRoyaltyDistributer} from "./interfaces/IMultiRoyaltyDistributer.sol";
+import {IMultiRoyaltyDistributor} from "./interfaces/IMultiRoyaltyDistributor.sol";
 import {
     IRoyaltySplitter,
     IERC165
@@ -20,7 +20,7 @@ import {IRoyaltyManager, Recipient} from "./interfaces/IRoyaltyManager.sol";
 /// @author The Sandbox
 /// @dev  The MultiRoyaltyDistributer contract implements the ERC-2981 and ERC-165 interfaces for a royalty payment system. This payment system can be used to pay royalties to multiple recipients through splitters.
 /// @dev  This contract calls to the Royalties manager contract to deploy RoyaltySplitter for a creator to slip its royalty between the creator and Sandbox and use it for every token minted by that creator.
-abstract contract MultiRoyaltyDistributor is IEIP2981, IMultiRoyaltyDistributer, ERC165Upgradeable {
+abstract contract MultiRoyaltyDistributor is IEIP2981, IMultiRoyaltyDistributor, ERC165Upgradeable {
     uint16 internal constant TOTAL_BASIS_POINTS = 10000;
     uint16 public _defaultRoyaltyBPS;
     address payable public _defaultRoyaltyReceiver;
@@ -52,6 +52,7 @@ abstract contract MultiRoyaltyDistributor is IEIP2981, IMultiRoyaltyDistributer,
         return
             interfaceId == type(IEIP2981).interfaceId ||
             interfaceId == type(IEIP2981MultiReceiverRoyaltyOverride).interfaceId ||
+            interfaceId == type(IMultiRoyaltyDistributor).interfaceId ||
             super.supportsInterface(interfaceId);
     }
 
