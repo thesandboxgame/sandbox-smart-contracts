fbeda86d9eec8117548c06ada8d6e92d8bc941d4
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-07-27 19:46:11 +0530

    feat: added and updated interfaces

--- packages/asset/contracts/interfaces/ITokenUtils.sol
+++ packages/asset/contracts/interfaces/ITokenUtils.sol
@@ -1,9 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-interface ITokenInfo {
-    function getCreatorAddress(uint256 tokenId) external pure returns (address creator);
+import "./IRoyaltyUGC.sol";
 
+interface ITokenUtils is IRoyaltyUGC {
     function getTier(uint256 tokenId) external pure returns (uint8 tier);
 
     function isRevealed(uint256 tokenId) external pure returns (bool);
