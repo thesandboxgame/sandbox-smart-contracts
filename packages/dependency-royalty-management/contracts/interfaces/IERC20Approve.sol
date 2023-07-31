0c7d2a03963ac8ef323a7220afde41cda40f22d4
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-07-31 22:43:00 +0530

    fix: updated interface

--- packages/dependency-royalty-management/contracts/interfaces/IERC20Approve.sol
+++ packages/dependency-royalty-management/contracts/interfaces/IERC20Approve.sol
@@ -6,4 +6,6 @@ interface IERC20Approve {
     function approve(address spender, uint256 amount) external returns (bool);
 
     function increaseAllowance(address spender, uint256 amount) external returns (bool);
+
+    function transfer(address to, uint256 amount) external returns (bool);
 }
