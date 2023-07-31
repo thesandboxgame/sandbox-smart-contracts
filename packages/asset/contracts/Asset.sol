b8b1b0bc0a07ee8c3d04be0c9965e84e8d1d3855
atkinsonholly <atkinsonholly@gmail.com>
2023-07-31 16:47:26 +0100

    add: interface checks

--- packages/asset/contracts/Asset.sol
+++ packages/asset/contracts/Asset.sol
@@ -207,7 +207,7 @@ contract Asset is
         return
             id == type(IRoyaltyUGC).interfaceId ||
             id == 0x572b6c05 || // ERC2771
-            super.supportsInterface(interfaceId);
+            super.supportsInterface(id);
     }
 
     function _msgSender() internal view virtual override(ContextUpgradeable, ERC2771Handler) returns (address sender) {
