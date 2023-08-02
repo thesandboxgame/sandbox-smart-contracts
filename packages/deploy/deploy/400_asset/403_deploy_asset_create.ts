5a97e47d595c1b34b6089541e3ec3e4675ed2efa
wojciech-turek <contact@wojciechturek.com>
2023-08-02 10:20:34 +0200

    Make sure both catalyst deploy and setup scripts run

--- packages/deploy/deploy/400_asset/403_deploy_asset_create.ts
+++ packages/deploy/deploy/400_asset/403_deploy_asset_create.ts
@@ -44,7 +44,7 @@ export default func;
 func.tags = ['Asset', 'AssetCreate', 'AssetCreate_deploy', 'L2'];
 func.dependencies = [
   'Asset_deploy',
-  'Catalyst_deploy',
+  'Catalyst',
   'AuthSuperValidator_deploy',
   'TRUSTED_FORWARDER_V2',
 ];
