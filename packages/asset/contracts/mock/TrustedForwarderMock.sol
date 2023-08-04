4f1e116e42020e1cdba15f3eafcf1c0f96d9a735
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-08-04 17:29:00 +0530

    fix: updated contracts

--- packages/asset/contracts/mock/TrustedForwarderMock.sol
+++
@@ -1,4 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import {TrustedForwarderMock} from "@sandbox-smart-contracts/dependency-metatx/contracts/test/TrustedForwarderMock.sol";
--- packages/dependency-metatx/contracts/test/TrustedForwarderMock.sol
+++ packages/dependency-metatx/contracts/test/TrustedForwarderMock.sol
@@ -2,7 +2,7 @@
 // solhint-disable-next-line compiler-version
 pragma solidity ^0.8.2;
 
-contract TrustedForwarderMock {
+contract MockTrustedForwarder {
     struct ForwardRequest {
         address from;
         address to;
