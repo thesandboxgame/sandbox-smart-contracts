f4dd7add6712a6d9c10211179f6f35ea7b1e58ce
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-08-04 17:15:00 +0530

    feat: added contracts

---
+++ packages/dependency-metatx/contracts/test/TrustedForwarderMock.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+// solhint-disable-next-line compiler-version
+pragma solidity ^0.8.2;
+
+contract TrustedForwarderMock {
+    struct ForwardRequest {
+        address from;
+        address to;
+        uint256 value;
+        uint256 gasLimit;
+        bytes data;
+    }
+
+    function execute(ForwardRequest calldata req) public payable returns (bool, bytes memory) {
+        (bool success, bytes memory returndata) = req.to.call{gas: req.gasLimit, value: req.value}(
+            abi.encodePacked(req.data, req.from)
+        );
+        assert(gasleft() > req.gasLimit / 63);
+        return (success, returndata);
+    }
+}
