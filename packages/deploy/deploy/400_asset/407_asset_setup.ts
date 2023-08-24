368c5e5a0d6aadce292f91bc6efe1433647e06cf
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-08-25 00:24:34 +0530

    feat : updated asset deployment setup

--- packages/deploy/deploy/400_asset/407_asset_setup.ts
+++ packages/deploy/deploy/400_asset/407_asset_setup.ts
@@ -5,7 +5,8 @@ export const DEFAULT_BPS = 300;
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {execute, log, read, catchUnknownSigner} = deployments;
-  const {assetAdmin, contractRoyaltySetter} = await getNamedAccounts();
+  const {assetAdmin, contractRoyaltySetter, royaltyManagerAdmin} =
+    await getNamedAccounts();
 
   const Asset = await deployments.get('Asset');
 
@@ -33,6 +34,21 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     )
   );
   log(`Asset set on RoyaltyManager with ${DEFAULT_BPS} BPS royalty`);
+  
+  const splitterDeployerRole = await read(
+    'RoyaltyManager',
+    'SPLITTER_DEPLOYER_ROLE'
+  );
+  await catchUnknownSigner(
+    execute(
+      'RoyaltyManager',
+      {from: royaltyManagerAdmin, log: true},
+      'grantRole',
+      splitterDeployerRole,
+      Asset.address
+    )
+  );
+  log(`Asset set on RoyaltyManager with Splitter Deployer Role`);
 };
 
 export default func;
