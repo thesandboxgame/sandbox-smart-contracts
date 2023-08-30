9221caf17ce3713295300595c0f124aa5a73a7a9
Rishabh Sharma <rishabh.sharma@sandbox.game>
2023-08-30 20:13:49 +0530

    fix: removed merge markers

--- packages/dependency-royalty-management/contracts/RoyaltySplitter.sol
+++ packages/dependency-royalty-management/contracts/RoyaltySplitter.sol
@@ -47,11 +47,7 @@ contract RoyaltySplitter is
 
     event ETHTransferred(address indexed account, uint256 amount);
     event ERC20Transferred(address indexed erc20Contract, address indexed account, uint256 amount);
-<<<<<<< HEAD
     event RecipientSet(address indexed recipientAddress);
-=======
-    event RecipientSet(address indexed newRecipient);
->>>>>>> 0a587f5d (feat : added events after sensitive changes)
 
     function supportsInterface(bytes4 interfaceId)
         public
