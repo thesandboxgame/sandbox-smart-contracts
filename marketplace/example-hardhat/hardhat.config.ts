bfb4e22e7f3751b7a1b4818b5d46ec2690237986
atkinsonholly <atkinsonholly@gmail.com>
2023-09-04 10:18:29 +0100

    feat: add second workspace

---
+++ marketplace/example-hardhat/hardhat.config.ts
@@ -0,0 +1,21 @@
+import '@nomicfoundation/hardhat-toolbox';
+import {HardhatUserConfig} from 'hardhat/config';
+
+const config: HardhatUserConfig = {
+  // solidity compiler version may be updated for new packages as required
+  // to ensure packages use up-to-date dependencies
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.18',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+    ],
+  },
+};
+export default config;
