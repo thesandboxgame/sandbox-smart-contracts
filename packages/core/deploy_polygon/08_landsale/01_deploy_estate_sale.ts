1b83633f2b98e8a8fce0f0ad02fdaf7e6bb6c2bc
León <leon.isla.22@gmail.com>
2023-09-05 11:17:59 -0300

    add: mumbai deployment

--- packages/core/deploy_polygon/08_landsale/01_deploy_estate_sale.ts
+++ packages/core/deploy_polygon/08_landsale/01_deploy_estate_sale.ts
@@ -27,7 +27,8 @@ const sales: SaleDeployment[] = [
   {name: 'LandPreSale_24', skip: async () => true},
   {name: 'LandPreSale_25', skip: async () => true},
   {name: 'LandPreSale_26', skip: async () => true},
-  {name: 'LandPreSale_27', skip: async () => false},
+  {name: 'LandPreSale_27', skip: async () => true},
+  {name: 'LandPreSale_28', skip: async () => false},
 ];
 
 const func: DeployFunction = async function (hre) {
