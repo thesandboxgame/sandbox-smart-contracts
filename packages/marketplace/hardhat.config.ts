ad4eeabb8c217fe8bdf788eb40641cc088e4bf0b
Ayush Tiwari <ayush@sandbox.game>
2023-09-11 13:54:23 +0530

    feat: hardhat marketplace workspace setup

---
+++ packages/marketplace/hardhat.config.ts
@@ -0,0 +1,23 @@
+import {HardhatUserConfig} from 'hardhat/config';
+
+const config: HardhatUserConfig = {
+  // solidity compiler version may be updated for new packages as required
+  // to ensure packages use up-to-date dependencies
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.19',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: 'src',
+  },
+};
+export default config;
