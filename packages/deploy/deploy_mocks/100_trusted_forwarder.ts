8bf904c77b5941305c8a67d7fddbc1ae07a40631
Andres Adjimann <andres@sandbox.game>
2023-09-26 09:32:41 -0300

    fix: use fqn for TrustedForwarderMock

--- packages/deploy/deploy_mocks/100_trusted_forwarder.ts
+++ packages/deploy/deploy_mocks/100_trusted_forwarder.ts
@@ -8,7 +8,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployer} = await getNamedAccounts();
   await deploy('TRUSTED_FORWARDER_V2', {
     from: deployer,
-    contract: 'TrustedForwarderMock',
+    contract: 'contracts/mocks/TrustedForwarderMock.sol:TrustedForwarderMock',
     log: true,
   });
 };
