d4f0d9ff4bb27a7e7c069d6379ddd3bd0c438d6e
Alin Mihai Barbatei <alin.barbatei@gmail.com>
2023-09-29 00:49:11 +0300

    major package cleanup; deployment related files moved; settings file added

---
+++ packages/avatar/hardhat.config.ts
@@ -0,0 +1,41 @@
+import '@nomicfoundation/hardhat-toolbox';
+import {HardhatUserConfig} from 'hardhat/config';
+import "@openzeppelin/hardhat-upgrades";
+
+const config: HardhatUserConfig = {
+  // solidity compiler version may be updated for new packages as required
+  // to ensure packages use up-to-date dependencies
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.18',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: '0.8.15',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: '0.8.2', // needed to compile fake PolygonSand to actually test with the sand implementation
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+
+    ],
+  },
+};
+export default config;
--- packages/avatar/src/mocks/proxy/MockUpgradeableBeacon.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
-
-import {UpgradeableBeacon} from "@openzeppelin/contracts-0.8.15/proxy/beacon/UpgradeableBeacon.sol";
-
-contract MockUpgradeableBeacon is UpgradeableBeacon {
-    constructor(address implementation_) UpgradeableBeacon(implementation_) {}
-}
