7931418494fc88bf7eaf17131d5e675d8ec1d6ec
Andres Adjimann <andres@sandbox.game>
2023-09-29 19:07:00 -0300

    fix: fix marketplace deployment

--- packages/deploy/deploy/marketplace/03_deploy_asset_matcher.ts
+++ packages/deploy/deploy/marketplace/03_deploy_asset_matcher.ts
@@ -6,25 +6,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deploy} = deployments;
   const {deployer} = await getNamedAccounts();
 
-  // TODO: to be fetched from env?
-  const deployMeta = process.env.DEPLOY_META;
-  const exchangeContract = deployMeta ? 'ExchangeMeta' : 'Exchange';
-
-  const assetMatcher = await deploy('AssetMatcher', {
+  await deploy('AssetMatcher', {
     from: deployer,
     contract:
       '@sandbox-smart-contracts/marketplace/contracts/exchange/AssetMatcher.sol:AssetMatcher',
     log: true,
     skipIfAlreadyDeployed: true,
   });
-
-  await deployments.execute(
-    exchangeContract,
-    {from: deployer},
-    'setAssetMatcherContract',
-    assetMatcher.address
-  );
 };
 export default func;
 func.tags = ['AssetMatcher', 'AssetMatcher_deploy'];
-func.dependencies = ['Exchange'];
