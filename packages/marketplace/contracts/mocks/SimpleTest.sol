57b4c2a27d499e2538a764c419e04d9c2d261357
Andres Adjimann <andres@sandbox.game>
2023-09-29 15:19:39 -0300

    feat: remove lazy-mint
    Just the assetClass, maybe some related functio or stil need to be removed

--- packages/marketplace/contracts/mocks/SimpleTest.sol
+++ packages/marketplace/contracts/mocks/SimpleTest.sol
@@ -2,21 +2,12 @@
 
 pragma solidity 0.8.21;
 
-import {LibERC721LazyMint} from "../lazy-mint/erc-721/LibERC721LazyMint.sol";
-import {LibERC1155LazyMint, LibPart} from "../lazy-mint/erc-1155/LibERC1155LazyMint.sol";
 import {TransferManager} from "../transfer-manager/TransferManager.sol";
 import {TransferExecutor, LibAsset} from "../transfer-manager/TransferExecutor.sol";
+import {LibPart} from "../lib-part/LibPart.sol";
 
 contract SimpleTest is TransferManager, TransferExecutor {
     function getRoyaltiesByAssetTest(LibAsset.AssetType memory matchNft) external returns (LibPart.Part[] memory) {
         return getRoyaltiesByAssetType(matchNft);
-    }
-
-    function encode721(LibERC721LazyMint.Mint721Data memory data) external view returns (bytes memory) {
-        return abi.encode(address(this), data);
-    }
-
-    function encode1155(LibERC1155LazyMint.Mint1155Data memory data) external view returns (bytes memory) {
-        return abi.encode(address(this), data);
     }
 }
