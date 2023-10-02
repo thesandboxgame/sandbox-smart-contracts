fa9e2553d669ea90ed2b891141fdd3306fee5eb4
Andres Adjimann <andres@sandbox.game>
2023-10-02 17:48:00 -0300

    feat: remove BUY and SELL orders

--- packages/marketplace/contracts/exchange/libraries/LibFill.sol
+++ packages/marketplace/contracts/exchange/libraries/LibFill.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {LibOrder, LibMath} from "../../lib-order/LibOrder.sol";
+import {LibOrder} from "../../lib-order/LibOrder.sol";
+import {LibMath} from "./LibMath.sol";
 
 /// @title This library provides `fillOrder` function.
 /// @notice It calculates fill of both orders (part of the Order that can be filled).
@@ -11,18 +12,11 @@ library LibFill {
         uint256 rightValue;
     }
 
-    struct IsMakeFill {
-        bool leftMake;
-        bool rightMake;
-    }
-
     /// @notice Should return filled values
     /// @param leftOrder left order
     /// @param rightOrder right order
     /// @param leftOrderFill current fill of the left order (0 if order is unfilled)
     /// @param rightOrderFill current fill of the right order (0 if order is unfilled)
-    /// @param leftIsMakeFill true if left orders fill is calculated from the make side, false if from the take side
-    /// @param rightIsMakeFill true if right orders fill is calculated from the make side, false if from the take side
     /// @dev We have 3 cases, 1st: left order should be fully filled
     /// @dev 2nd: right order should be fully filled or 3d: both should be fully filled if required values are the same
     /// @return the fill result of both orders
@@ -30,25 +24,28 @@ library LibFill {
         LibOrder.Order calldata leftOrder,
         LibOrder.Order calldata rightOrder,
         uint256 leftOrderFill,
-        uint256 rightOrderFill,
-        bool leftIsMakeFill,
-        bool rightIsMakeFill
+        uint256 rightOrderFill
     ) internal pure returns (FillResult memory) {
-        (uint256 leftMakeValue, uint256 leftTakeValue) = LibOrder.calculateRemaining(
-            leftOrder,
-            leftOrderFill,
-            leftIsMakeFill
-        );
-        (uint256 rightMakeValue, uint256 rightTakeValue) = LibOrder.calculateRemaining(
-            rightOrder,
-            rightOrderFill,
-            rightIsMakeFill
-        );
+        (uint256 leftMakeValue, uint256 leftTakeValue) = calculateRemaining(leftOrder, leftOrderFill);
+        (uint256 rightMakeValue, uint256 rightTakeValue) = calculateRemaining(rightOrder, rightOrderFill);
 
         if (rightTakeValue > leftMakeValue) {
             return fillLeft(leftMakeValue, leftTakeValue, rightOrder.makeAsset.value, rightOrder.takeAsset.value);
         }
         return fillRight(leftOrder.makeAsset.value, leftOrder.takeAsset.value, rightMakeValue, rightTakeValue);
+    }
+
+    /// @notice calculate the remaining fill from orders
+    /// @param order order that we will calculate the remaining fill
+    /// @param fill to be subtracted
+    /// @return makeValue remaining fill from make side
+    /// @return takeValue remaining fill from take side
+    function calculateRemaining(
+        LibOrder.Order calldata order,
+        uint256 fill
+    ) internal pure returns (uint256 makeValue, uint256 takeValue) {
+        takeValue = order.takeAsset.value - fill;
+        makeValue = LibMath.safeGetPartialAmountFloor(order.makeAsset.value, order.takeAsset.value, takeValue);
     }
 
     function fillRight(
--- packages/marketplace/test/utils/order.ts
+++ packages/marketplace/test/utils/order.ts
@@ -1,20 +1,21 @@
 // An order represents something offered (asset + who offers) plus what we want in exchange (asset + optionally for whom or everybody)
-// SEE: LibOrder.sol and LibOrderData.sol
+// SEE: LibOrder.sol
 import {Asset, hashAsset, hashAssetType} from './assets';
-import {bytes4Keccak} from './signature';
-import {AbiCoder, Numeric, keccak256, Signer, ZeroAddress} from 'ethers';
-import {BytesLike} from 'ethers/src.ts/utils/index';
+import {
+  AbiCoder,
+  Contract,
+  keccak256,
+  Numeric,
+  Signer,
+  ZeroAddress,
+} from 'ethers';
 
 export const ORDER_TYPEHASH = keccak256(
   Buffer.from(
-    'Order(address maker,Asset makeAsset,address taker,Asset takeAsset,uint256 salt,uint256 start,uint256 end,bytes4 dataType,bytes data)Asset(AssetType assetType,uint256 value)AssetType(uint256 assetClass,bytes data)'
+    'Order(address maker,Asset makeAsset,address taker,Asset takeAsset,uint256 salt,uint256 start,uint256 end)Asset(AssetType assetType,uint256 value)AssetType(uint256 assetClass,bytes data)'
   )
 );
 
-const ORDER_DATA_BUY = bytes4Keccak('BUY');
-const ORDER_DATA_SELL = bytes4Keccak('SELL');
-
-export const DEFAULT_ORDER_TYPE = '0xffffffff';
 export const UINT256_MAX_VALUE =
   115792089237316195423570985008687907853269984665640564039457584007913129639935n;
 
@@ -26,8 +27,6 @@ export type Order = {
   salt: Numeric;
   start: Numeric;
   end: Numeric;
-  dataType: string;
-  data: BytesLike;
 };
 
 export const OrderDefault = async (
@@ -46,93 +45,16 @@ export const OrderDefault = async (
   salt,
   start,
   end,
-  dataType: DEFAULT_ORDER_TYPE,
-  data: '0x',
-});
-
-export const OrderSell = async (
-  maker: Signer,
-  makeAsset: Asset,
-  taker: Signer | ZeroAddress,
-  takeAsset: Asset,
-  salt: Numeric,
-  start: Numeric,
-  end: Numeric,
-  payouts: string, // TODO: better type
-  originFeeFirst: string, // TODO: better type
-  originFeeSecond: string, // TODO: better type
-  maxFeesBasePoint: string, // TODO: better type
-  marketplaceMarker: string // TODO: better type
-): Promise<Order> => ({
-  maker: await maker.getAddress(),
-  makeAsset,
-  taker: taker === ZeroAddress ? ZeroAddress : await taker.getAddress(),
-  takeAsset,
-  salt,
-  start,
-  end,
-  dataType: ORDER_DATA_SELL,
-  data: AbiCoder.defaultAbiCoder().encode(
-    ['uint256', 'uint256', 'uint256', 'uint256', 'bytes32'],
-    [
-      payouts,
-      originFeeFirst,
-      originFeeSecond,
-      maxFeesBasePoint,
-      marketplaceMarker,
-    ]
-  ),
-});
-
-export const OrderBuy = async (
-  maker: Signer,
-  makeAsset: Asset,
-  taker: Signer | ZeroAddress,
-  takeAsset: Asset,
-  salt: Numeric,
-  start: Numeric,
-  end: Numeric,
-  payouts: string, // TODO: better type
-  originFeeFirst: string, // TODO: better type
-  originFeeSecond: string, // TODO: better type
-  marketplaceMarker: string // TODO: better type
-): Promise<Order> => ({
-  maker: await maker.getAddress(),
-  makeAsset,
-  taker: taker === ZeroAddress ? ZeroAddress : await taker.getAddress(),
-  takeAsset,
-  salt,
-  start,
-  end,
-  dataType: ORDER_DATA_BUY,
-  data: AbiCoder.defaultAbiCoder().encode(
-    ['uint256', 'uint256', 'uint256', 'bytes32'],
-    [payouts, originFeeFirst, originFeeSecond, marketplaceMarker]
-  ),
 });
 
 export function hashKey(order: Order): string {
-  if (order.dataType === DEFAULT_ORDER_TYPE) {
-    const encoded = AbiCoder.defaultAbiCoder().encode(
-      ['address', 'bytes32', 'bytes32', 'uint256'],
-      [
-        order.maker,
-        hashAssetType(order.makeAsset.assetType),
-        hashAssetType(order.takeAsset.assetType),
-        order.salt,
-      ]
-    );
-    return keccak256(encoded);
-  }
-  // TODO: Review this on solidity side, instead of passing order.data maybe is better keccak(data)s
   const encoded = AbiCoder.defaultAbiCoder().encode(
-    ['address', 'bytes32', 'bytes32', 'uint256', 'bytes'],
+    ['address', 'bytes32', 'bytes32', 'uint256'],
     [
       order.maker,
       hashAssetType(order.makeAsset.assetType),
       hashAssetType(order.takeAsset.assetType),
       order.salt,
-      order.data,
     ]
   );
   return keccak256(encoded);
@@ -171,8 +93,6 @@ export function hashOrder(order: Order): string {
       'uint256',
       'uint256',
       'uint256',
-      'bytes4',
-      'bytes32',
     ],
     [
       ORDER_TYPEHASH,
@@ -183,9 +103,43 @@ export function hashOrder(order: Order): string {
       order.salt,
       order.start,
       order.end,
-      order.dataType,
-      keccak256(order.data),
     ]
   );
   return keccak256(encoded);
+}
+
+export async function signOrder(
+  order: Order,
+  account: Signer,
+  verifyingContract: Contract
+) {
+  const network = await verifyingContract.runner?.provider?.getNetwork();
+  return account.signTypedData(
+    {
+      name: 'Exchange',
+      version: '1',
+      chainId: network.chainId,
+      verifyingContract: await verifyingContract.getAddress(),
+    },
+    {
+      AssetType: [
+        {name: 'assetClass', type: 'uint256'},
+        {name: 'data', type: 'bytes'},
+      ],
+      Asset: [
+        {name: 'assetType', type: 'AssetType'},
+        {name: 'value', type: 'uint256'},
+      ],
+      Order: [
+        {name: 'maker', type: 'address'},
+        {name: 'makeAsset', type: 'Asset'},
+        {name: 'taker', type: 'address'},
+        {name: 'takeAsset', type: 'Asset'},
+        {name: 'salt', type: 'uint256'},
+        {name: 'start', type: 'uint256'},
+        {name: 'end', type: 'uint256'},
+      ],
+    },
+    order
+  );
 }
