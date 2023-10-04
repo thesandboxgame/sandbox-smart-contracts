632cd70f36921433d3934a400c370b5fcadae523
atkinsonholly <atkinsonholly@gmail.com>
2023-10-04 14:58:00 +0100

    fix: update tests that are failing because of lint fixes

--- packages/asset/test/AuthSuperValidator.test.ts
+++ packages/asset/test/AuthSuperValidator.test.ts
@@ -23,7 +23,7 @@ describe('AuthSuperValidator, (/packages/asset/contracts/AuthSuperValidator.sol)
           DEFAULT_ADMIN_ROLE,
           authValidatorAdmin.address
         )
-      ).to.be.revertedWith('AuthSuperValidator: cant renounce admin role');
+      ).to.be.revertedWith('AuthSuperValidator: Admin needed');
     });
     it('should allow admin to set signer for a given contract address', async function () {
       const {MockContract, AuthValidatorContractAsAdmin, backendSigner} =
