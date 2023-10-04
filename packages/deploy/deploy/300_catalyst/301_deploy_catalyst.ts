1aaa221ab3bd9b779abf68069af3a3e1b0507fd7
wojciech-turek <contact@wojciechturek.com>
2023-10-04 12:11:12 +0200

    Update ipfs hashes

--- packages/deploy/deploy/300_catalyst/301_deploy_catalyst.ts
+++ packages/deploy/deploy/300_catalyst/301_deploy_catalyst.ts
@@ -3,15 +3,14 @@ import {HardhatRuntimeEnvironment} from 'hardhat/types';
 
 export const CATALYST_BASE_URI = 'ipfs://';
 
-// TODO: update for polygon-mainnet deployment
 export const CATALYST_IPFS_CID_PER_TIER = [
   'bafybeiecnz7snx763tcxwbsitbucltcxp7ma5siqbgda35bl3tsfeeti4m', // TSB Exclusive
-  'bafkreib5tky3dgsc7zy637dfunb4zwwnpzo3w3i5tepbfee42eq3srwnwq', // Common
-  'bafkreiegevvim5q3ati4htsncxwsejfc3lbkzb7wn2a2fzthc6tsof7v7m', // Uncommon
-  'bafkreifhtkou5a32xrtktdvfqrvgh4mp2ohvlyqdsih5xk4kgcfywtxefi', // Rare
-  'bafkreigqpb7qo3iqka4243oah3nka6agx3nmvwzauxze2jznotx3zwozqe', // Epic
-  'bafkreih3itsiwkn2urzfvg26mby3ssgfshvdr6zfabr6rxxrlzhedqil4e', // Legendary
-  'bafkreibmngauozzidz2eevyyb3umf2ew7zexing3ghup6l7io2ao522mvy', // Mythic
+  'bafkreieg56vikzi3kq3dqrrdjgetx6zf32btyh2f3gto3x6252yyqxzt4i', // Common
+  'bafkreidh2ar2he4gynkmelfwreu66v6il5ttxnzvfwpjblk63qg7nzelhy', // Uncommon
+  'bafkreifipzgz26f54wbmtcf6w2t2cy54nszys64vah7lnppf2v3am2spkm', // Rare
+  'bafkreihjwavnyuyzoiynzqul6b3ssfmydwfgjzokj6hmsv54gxsylfmpvq', // Epic
+  'bafkreiccvcwddfpyfmyam3fz2yokseitqsgyooudoh4vhkdx7ziahz7goa', // Legendary
+  'bafkreidzh2gvadeuvw4x4xdgrb6uhkyfgn4sk5d47hffro6lrrapjcajn4', // Mythic
 ];
 
 const func: DeployFunction = async function (
