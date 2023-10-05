4ba21bdda905e40fda266909810dad0e7b20885e
Ayush Tiwari <ayush@sandbox.game>
2023-10-05 20:06:44 +0530

    test: OrderValidator Contract

--- packages/marketplace/test/exchange/OrderValidator.test.ts
+++ packages/marketplace/test/exchange/OrderValidator.test.ts
@@ -159,6 +159,189 @@ describe('OrderValidator.sol', function () {
       .to.not.be.reverted;
   });
 
+  it('should validate when order maker is contract and sender', async function () {
+    const {
+      OrderValidatorAsUser,
+      ERC20Contract,
+      ERC721Contract,
+      ERC1271Contract,
+    } = await loadFixture(deployFixtures);
+    const makerAsset = await AssetERC721(ERC721Contract, 100);
+    const takerAsset = await AssetERC20(ERC20Contract, 100);
+    const order = await OrderDefault(
+      ERC1271Contract,
+      makerAsset,
+      ZeroAddress,
+      takerAsset,
+      1,
+      0,
+      0
+    );
+
+    await expect(
+      OrderValidatorAsUser.validate(
+        order,
+        '0x',
+        await ERC1271Contract.getAddress()
+      )
+    ).to.not.be.reverted;
+  });
+
+  it('should not validate when maker is contract but not sender and isValidSignature returns non-magic value', async function () {
+    const {
+      OrderValidatorAsUser,
+      ERC20Contract,
+      ERC721Contract,
+      ERC1271Contract,
+      user1,
+    } = await loadFixture(deployFixtures);
+    const makerAsset = await AssetERC721(ERC721Contract, 100);
+    const takerAsset = await AssetERC20(ERC20Contract, 100);
+    const order = await OrderDefault(
+      ERC1271Contract,
+      makerAsset,
+      ZeroAddress,
+      takerAsset,
+      1,
+      0,
+      0
+    );
+
+    await expect(
+      OrderValidatorAsUser.validate(order, '0x', user1.address)
+    ).to.be.revertedWith('contract order signature verification error');
+  });
+
+  it('should validate when maker is contract but not sender and isValidSignature returns magic value', async function () {
+    const {
+      OrderValidatorAsUser,
+      ERC20Contract,
+      ERC721Contract,
+      ERC1271Contract,
+      user1,
+    } = await loadFixture(deployFixtures);
+    const makerAsset = await AssetERC721(ERC721Contract, 100);
+    const takerAsset = await AssetERC20(ERC20Contract, 100);
+    const order = await OrderDefault(
+      ERC1271Contract,
+      makerAsset,
+      ZeroAddress,
+      takerAsset,
+      1,
+      0,
+      0
+    );
+    await ERC1271Contract.setReturnSuccessfulValidSignature(true);
+    await expect(OrderValidatorAsUser.validate(order, '0x', user1.address)).to
+      .not.be.reverted;
+  });
+
+  it('should validate when open is disabled, tsbOnly is enabled and makeTokenAddress have TSB_ROLE', async function () {
+    const {
+      OrderValidatorAsUser,
+      OrderValidatorAsAdmin,
+      ERC20Contract,
+      ERC721Contract,
+      user1,
+    } = await loadFixture(deployFixtures);
+
+    expect(await OrderValidatorAsAdmin.open()).to.be.equal(true);
+    expect(await OrderValidatorAsAdmin.tsbOnly()).to.be.equal(false);
+
+    await OrderValidatorAsAdmin.setPermissions(true, false, false, false);
+
+    expect(await OrderValidatorAsAdmin.open()).to.be.equal(false);
+    expect(await OrderValidatorAsAdmin.tsbOnly()).to.be.equal(true);
+
+    expect(
+      await OrderValidatorAsUser.hasRole(
+        TSBRole,
+        await ERC20Contract.getAddress()
+      )
+    ).to.be.equal(false);
+
+    await OrderValidatorAsAdmin.grantRole(
+      TSBRole,
+      await ERC20Contract.getAddress()
+    );
+
+    expect(
+      await OrderValidatorAsUser.hasRole(
+        TSBRole,
+        await ERC20Contract.getAddress()
+      )
+    ).to.be.equal(true);
+
+    const makerAsset = await AssetERC20(ERC20Contract, 100);
+    const takerAsset = await AssetERC721(ERC721Contract, 100);
+    const order = await OrderDefault(
+      user1,
+      makerAsset,
+      ZeroAddress,
+      takerAsset,
+      1,
+      0,
+      0
+    );
+    const signature = await signOrder(order, user1, OrderValidatorAsUser);
+
+    await expect(OrderValidatorAsUser.validate(order, signature, user1.address))
+      .to.not.be.reverted;
+  });
+
+  it('should validate when open is disabled, partners is enabled and makeTokenAddress have PARTNER_ROLE', async function () {
+    const {
+      OrderValidatorAsUser,
+      OrderValidatorAsAdmin,
+      ERC20Contract,
+      ERC721Contract,
+      user1,
+    } = await loadFixture(deployFixtures);
+
+    expect(await OrderValidatorAsAdmin.open()).to.be.equal(true);
+    expect(await OrderValidatorAsAdmin.partners()).to.be.equal(false);
+
+    await OrderValidatorAsAdmin.setPermissions(false, true, false, false);
+
+    expect(await OrderValidatorAsAdmin.open()).to.be.equal(false);
+    expect(await OrderValidatorAsAdmin.partners()).to.be.equal(true);
+
+    expect(
+      await OrderValidatorAsUser.hasRole(
+        PartnerRole,
+        await ERC20Contract.getAddress()
+      )
+    ).to.be.equal(false);
+
+    await OrderValidatorAsAdmin.grantRole(
+      PartnerRole,
+      await ERC20Contract.getAddress()
+    );
+
+    expect(
+      await OrderValidatorAsUser.hasRole(
+        PartnerRole,
+        await ERC20Contract.getAddress()
+      )
+    ).to.be.equal(true);
+
+    const makerAsset = await AssetERC20(ERC20Contract, 100);
+    const takerAsset = await AssetERC721(ERC721Contract, 100);
+    const order = await OrderDefault(
+      user1,
+      makerAsset,
+      ZeroAddress,
+      takerAsset,
+      1,
+      0,
+      0
+    );
+    const signature = await signOrder(order, user1, OrderValidatorAsUser);
+
+    await expect(OrderValidatorAsUser.validate(order, signature, user1.address))
+      .to.not.be.reverted;
+  });
+
   it('should not set permission for token if caller is not owner', async function () {
     const {OrderValidatorAsUser, user} = await loadFixture(deployFixtures);
     await expect(
