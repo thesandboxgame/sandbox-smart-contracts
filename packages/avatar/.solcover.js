0d38541f0416bfea73494a28ce5f9611772e9275
Alin Mihai Barbatei <alin.barbatei@gmail.com>
2023-10-06 16:58:25 +0300

    updated ./.solcover.js

--- packages/avatar/.solcover.js
+++ packages/avatar/.solcover.js
@@ -3,5 +3,9 @@ module.exports = {
     grep: '@skip-on-coverage', // Find everything with this tag
     invert: true, // Run the grep's inverse set.
   },
-  skipFiles: ['/contracts/mock'],
+  skipFiles: [
+    'contracts/mocks',
+    'contracts/raffleold/contracts/PeopleOfCrypto.sol',
+    'contracts/raffleold/contracts/Raffle.sol'
+  ],
 };
