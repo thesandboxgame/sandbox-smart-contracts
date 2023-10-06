dd0356c9e1eb6d831e7acb228946195c796777db
Alin Mihai Barbatei <alin.barbatei@gmail.com>
2023-10-06 14:35:51 +0300

    removed leftovers from package migration

--- packages/core/hardhat.config.ts
+++ packages/core/hardhat.config.ts
@@ -433,33 +433,4 @@ const config: HardhatUserConfig = {
   },
 };
 
-task(
-  'flat',
-  'Flattens and prints contracts and their dependencies (Resolves licenses)'
-)
-  .addOptionalVariadicPositionalParam(
-    'files',
-    'The files to flatten',
-    undefined,
-    types.inputFile
-  )
-  .setAction(async ({files}, hre) => {
-    let flattened = await hre.run('flatten:get-flattened-sources', {files});
-
-    // Remove every line started with "// SPDX-License-Identifier:"
-    flattened = flattened.replace(
-      /SPDX-License-Identifier:/gm,
-      'License-Identifier:'
-    );
-    flattened = `// SPDX-License-Identifier: MIXED\n\n${flattened}`;
-
-    // Remove every line started with "pragma experimental ABIEncoderV2;" except the first one
-    flattened = flattened.replace(
-      /pragma experimental ABIEncoderV2;\n/gm,
-      ((i) => (m: string) => (!i++ ? m : ''))(0)
-    );
-    console.log(flattened);
-    // more on this here https://github.com/NomicFoundation/hardhat/issues/1499
-  });
-
 export default config;
