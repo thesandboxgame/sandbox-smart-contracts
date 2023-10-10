b1ff84125c539f438b346d1319bbeda2d0422289
Lucas <luc4rd@gmail.com>
2023-10-10 13:32:49 +0200

    const in BpLibrary

--- packages/marketplace/contracts/lib-bp/BpLibrary.sol
+++ packages/marketplace/contracts/lib-bp/BpLibrary.sol
@@ -10,7 +10,7 @@ library BpLibrary {
     /// @notice basepoint calculation
     /// @param value value to be multiplied by basepoint
     /// @param bpValue basepoint value
-    /// @return value times basepoint divided by 10000
+    /// @return value times basepoint divided by base point (10000)
     function bp(uint256 value, uint256 bpValue) internal pure returns (uint256) {
         return (value * bpValue) / BASIS_POINTS;
     }
