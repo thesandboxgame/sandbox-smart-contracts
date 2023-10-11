729c947b42d874ba0ab7f29408e31dab564e1a35
Felipe Faria <felipeasf@gmail.com>
2023-10-10 23:47:32 -0300

    Missing docstrings

--- packages/marketplace/contracts/transfer-manager/interfaces/ITransferExecutor.sol
+++ packages/marketplace/contracts/transfer-manager/interfaces/ITransferExecutor.sol
@@ -5,5 +5,10 @@ pragma solidity 0.8.21;
 import {LibAsset} from "../../lib-asset/LibAsset.sol";
 
 abstract contract ITransferExecutor {
+    /// @notice function should be able to transfer any supported Asset
+    /// @param asset Asset to be transferred
+    /// @param from account holding the asset
+    /// @param to account that will receive the asset
+    /// @dev this is the main entry point, when used as a separated contract this method will be external
     function transfer(LibAsset.Asset memory asset, address from, address to) internal virtual;
 }
--- packages/marketplace/contracts/transfer-manager/interfaces/ITransferManager.sol
+++ packages/marketplace/contracts/transfer-manager/interfaces/ITransferManager.sol
@@ -12,6 +12,10 @@ abstract contract ITransferManager is ITransferExecutor {
         LibPart.Part[] payouts;
         address from;
     }
-
+    
+    /// @notice executes transfers for 2 matched orders
+    /// @param left DealSide from the left order (see LibDeal.sol)
+    /// @param right DealSide from the right order (see LibDeal.sol)
+    /// @dev this is the main entry point, when used as a separated contract this method will be external
     function doTransfers(DealSide memory left, DealSide memory right, LibAsset.FeeSide feeSide) internal virtual;
 }
