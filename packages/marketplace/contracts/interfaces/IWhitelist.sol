1d1b141c3d4938658db6650194d0dc0abf12ce7d
Lucas <luc4rd@gmail.com>
2023-10-13 18:02:46 +0200

    fixing natspec

--- packages/marketplace/contracts/interfaces/IWhitelist.sol
+++ packages/marketplace/contracts/interfaces/IWhitelist.sol
@@ -1,33 +1,32 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.19;
 
-/// @title interface for the WhiteList contract
-/// @notice contains the signature for the contract function
+/// @title Interface for the Whitelist contract
+/// @notice Contains the function signatures for the contract.
 interface IWhitelist {
     /// @notice Check if a specific role is enabled or disabled.
     /// @param role The role identifier.
-    /// @return true if the role is enabled, false if disabled.
+    /// @return True if the role is enabled, false if disabled.
     function isRoleEnabled(bytes32 role) external view returns (bool);
 
     /// @notice Check if whitelists are enabled.
     /// @return True if whitelists are enabled, false if disabled.
     function isWhitelistsEnabled() external view returns (bool);
 
-    /// @notice setting permissions for tokens
-    /// @param roles we want to enable or disable
-    /// @param permissions boolan
+    /// @notice Setting permissions for tokens.
+    /// @param roles Roles we want to enable or disable.
+    /// @param permissions Boolean.
     function setRolesEnabled(bytes32[] calldata roles, bool[] calldata permissions) external;
 
-    /// @notice enable role
-    /// @param role we want to enable
+    /// @notice Enable role.
+    /// @param role Role we want to enable.
     function enableRole(bytes32 role) external;
 
-    /// @notice disable role
-    /// @param role we want to disable
+    /// @notice Disable role.
+    /// @param role Role we want to disable.
     function disableRole(bytes32 role) external;
 
-    /// notice Enable whitelists, allowing orders with any token except for ERC20 tokens.
+    /// @notice Enable whitelists, allowing orders with any token except for ERC20 tokens.
     function enableWhitelists() external;
 
     /// @notice Disable whitelists, requiring orders to refer to the specified whitelists.
