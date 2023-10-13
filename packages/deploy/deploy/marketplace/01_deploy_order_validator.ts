e99c6fa872f68d916380429f3f215ed0c3b92f3e
Lucas <luc4rd@gmail.com>
2023-10-13 16:28:54 +0200

    keccak in deployment

--- packages/deploy/deploy/marketplace/01_deploy_order_validator.ts
+++ packages/deploy/deploy/marketplace/01_deploy_order_validator.ts
@@ -1,17 +1,15 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+import {utils} from 'ethers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
   const {deployer, sandAdmin, upgradeAdmin} = await getNamedAccounts();
 
-  const TSB_ROLE =
-    '0x6278160ef7ca8a5eb8e5b274bcc0427c2cc7e12eee2a53c5989a1afb360f6404';
-  const PARTNER_ROLE =
-    '0x2f049b28665abd79bc83d9aa564dba6b787ac439dba27b48e163a83befa9b260';
-  const ERC20_ROLE =
-    '0x839f6f26c78a3e8185d8004defa846bd7b66fef8def9b9f16459a6ebf2502162';
+  const TSB_ROLE = utils.keccak256(utils.toUtf8Bytes('TSB_ROLE'));
+  const PARTNER_ROLE = utils.keccak256(utils.toUtf8Bytes('PARTNER_ROLE'));
+  const ERC20_ROLE = utils.keccak256(utils.toUtf8Bytes('ERC20_ROLE'));
 
   await deploy('OrderValidator', {
     from: deployer,
