fc2dbb12f16a55c7bb03f60712c0df10310f894b
Lucas <luc4rd@gmail.com>
2023-10-23 11:50:10 +0200

    lint unused variables

--- packages/deploy/deploy/500_marketplace/503_royalties_registry_setup.ts
+++ packages/deploy/deploy/500_marketplace/503_royalties_registry_setup.ts
@@ -5,37 +5,12 @@ const func: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
 ): Promise<void> {
   const {deployments, getNamedAccounts} = hre;
-  const {deploy} = deployments;
   const {execute, read, catchUnknownSigner} = deployments;
-  const {deployer, sandAdmin, sandExecutionAdmin} = await getNamedAccounts();
+  const {sandAdmin} = await getNamedAccounts();
 
   const ERC20_ROLE = await read('OrderValidator', 'ERC20_ROLE');
 
-  /* const TRUSTED_FORWARDER_V2 = await deployments.get('TRUSTED_FORWARDER_V2');
-  let CHILD_CHAIN_MANAGER = await deployments.getOrNull('CHILD_CHAIN_MANAGER');
-  if (!CHILD_CHAIN_MANAGER) {
-    CHILD_CHAIN_MANAGER = await deploy('CHILD_CHAIN_MANAGER', {
-      from: deployer,
-      contract: 'FakeChildChainManager',
-      log: true,
-    });
-  } */
-  //let sandContract = await deployments.getOrNull('PolygonSand');
   let sandContract = await deployments.get('PolygonSand');
-
-  /* if (!sandContract) {
-    sandContract = await deploy('PolygonSand', {
-      from: deployer,
-      contract: 'PolygonSand',
-      args: [
-        CHILD_CHAIN_MANAGER.address,
-        TRUSTED_FORWARDER_V2?.address,
-        sandAdmin,
-        sandExecutionAdmin,
-      ],
-      log: true,
-    });
-  } */
 
   if (
     !(await read('OrderValidator', 'hasRole', ERC20_ROLE, sandContract.address))
