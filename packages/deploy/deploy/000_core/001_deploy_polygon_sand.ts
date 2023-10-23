bb25a944ca64fa49c1defc9e571c654b35a1c457
Lucas <luc4rd@gmail.com>
2023-10-23 11:38:31 +0200

    creating deployment sand

---
+++ packages/deploy/deploy/000_core/001_deploy_polygon_sand.ts
@@ -0,0 +1,29 @@
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+
+  const {deployer, sandAdmin, sandExecutionAdmin} = await getNamedAccounts();
+
+  const TRUSTED_FORWARDER_V2 = await deployments.getOrNull(
+    'TRUSTED_FORWARDER_V2'
+  );
+  const CHILD_CHAIN_MANAGER = await deployments.get('CHILD_CHAIN_MANAGER');
+  await deploy('PolygonSand', {
+    from: deployer,
+    args: [
+      CHILD_CHAIN_MANAGER.address,
+      TRUSTED_FORWARDER_V2?.address,
+      sandAdmin,
+      sandExecutionAdmin,
+    ],
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+};
+
+export default func;
+func.tags = ['PolygonSand', 'PolygonSand_deploy', 'L2'];
+func.dependencies = ['CHILD_CHAIN_MANAGER', 'TRUSTED_FORWARDER_V2'];
