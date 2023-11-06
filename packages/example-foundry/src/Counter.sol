630704fef9df467a3511140b25cf08c181cb96f0
wojciech-turek <contact@wojciechturek.com>
2023-11-06 10:35:04 +0100

    Add openzeppelin deps

--- packages/example-foundry/src/Counter.sol
+++ packages/example-foundry/src/Counter.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Counter is Ownable {
     uint256 public number;
+
+    constructor() Ownable(msg.sender) {}
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
