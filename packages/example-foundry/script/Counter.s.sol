0bac9b04bec96656bc1c3ee247bfa407f94ba090
wojciech-turek <contact@wojciechturek.com>
2023-11-07 12:32:28 +0100

    Update deploy script

--- packages/example-foundry/script/Counter.s.sol
+++ packages/example-foundry/script/Counter.s.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPK = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPK);
+        new Counter();
+        vm.startBroadcast();
     }
 }
