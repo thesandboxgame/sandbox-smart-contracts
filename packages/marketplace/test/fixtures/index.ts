0259317ee8398a5418c5572287302e20810e2c83
Ayush Tiwari <ayush@sandbox.game>
2023-11-09 12:11:32 +0530

    refactor: consolidating multiple variables into a single object

--- packages/marketplace/test/fixtures/index.ts
+++ packages/marketplace/test/fixtures/index.ts
@@ -4,42 +4,15 @@ import {ZeroAddress} from 'ethers';
 import {exchangeSetup} from './exchange';
 import {mockAssetsSetup} from './assets';
 
-async function deploy() {
+export async function deployFixtures() {
   const [deployer, admin, user, defaultFeeReceiver, user1, user2] =
     await ethers.getSigners();
 
-  const {
-    RoyaltiesRegistryAsUser,
-    Royalties2981ImplMock,
-    RoyaltiesRegistryAsDeployer,
-    OrderValidatorAsAdmin,
-    OrderValidatorAsDeployer,
-    OrderValidatorAsUser,
-    OrderValidatorUpgradeMock,
-    ExchangeUpgradeMock,
-    ExchangeContractAsUser,
-    ExchangeContractAsAdmin,
-    protocolFeePrimary,
-    protocolFeeSecondary,
-    ExchangeContractAsDeployer,
-    AssetMatcherAsDeployer,
-    AssetMatcherAsUser,
-    TrustedForwarder,
-  } = await exchangeSetup();
+  const exchange = await exchangeSetup();
 
-  const {
-    ERC20Contract,
-    ERC20Contract2,
-    ERC721Contract,
-    ERC1155Contract,
-    ERC721WithRoyaltyV2981,
-    ERC721WithRoyalty,
-    ERC1155WithRoyalty,
-    ERC721WithRoyaltyWithoutIROYALTYUGC,
-    RoyaltyInfo,
-    RoyaltiesProvider,
-    ERC1271Contract,
-  } = await mockAssetsSetup();
+  const mockAssets = await mockAssetsSetup();
+
+  const {ExchangeContractAsAdmin} = exchange;
 
   const EXCHANGE_ADMIN_ROLE =
     await ExchangeContractAsAdmin.EXCHANGE_ADMIN_ROLE();
@@ -49,37 +22,12 @@ async function deploy() {
   const PAUSER_ROLE = await ExchangeContractAsAdmin.PAUSER_ROLE();
 
   return {
-    protocolFeePrimary,
-    protocolFeeSecondary,
+    ...exchange,
+    ...mockAssets,
     EXCHANGE_ADMIN_ROLE,
     DEFAULT_ADMIN_ROLE,
     ERC1776_OPERATOR_ROLE,
     PAUSER_ROLE,
-    ExchangeContractAsDeployer,
-    ExchangeContractAsAdmin,
-    ExchangeContractAsUser,
-    ExchangeUpgradeMock,
-    AssetMatcherAsDeployer,
-    AssetMatcherAsUser,
-    TrustedForwarder,
-    ERC20Contract,
-    ERC20Contract2,
-    ERC721Contract,
-    ERC1155Contract,
-    OrderValidatorAsDeployer,
-    OrderValidatorAsAdmin,
-    OrderValidatorAsUser,
-    OrderValidatorUpgradeMock,
-    RoyaltiesRegistryAsDeployer,
-    RoyaltiesRegistryAsUser,
-    Royalties2981ImplMock,
-    ERC721WithRoyaltyV2981,
-    ERC721WithRoyalty,
-    ERC1155WithRoyalty,
-    ERC721WithRoyaltyWithoutIROYALTYUGC,
-    RoyaltyInfo,
-    RoyaltiesProvider,
-    ERC1271Contract,
     deployer,
     admin,
     user,
@@ -88,8 +36,4 @@ async function deploy() {
     defaultFeeReceiver,
     ZERO_ADDRESS: ZeroAddress,
   };
-}
-
-export async function deployFixtures() {
-  return deploy();
 }
