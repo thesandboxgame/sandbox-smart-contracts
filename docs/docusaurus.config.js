c69bf062e0d5a4e39436a43017856bcaa5e55efa
atkinsonholly <atkinsonholly@gmail.com>
2023-11-15 14:24:14 +0100

    add: outline TSB docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'The Sandbox Smart Contracts',
+  title: 'The Sandbox Smart Contracts Docs',
   tagline: '',
   favicon: 'img/favicon.ico',
 
