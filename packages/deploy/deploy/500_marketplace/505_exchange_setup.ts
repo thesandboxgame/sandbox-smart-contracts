e90232e6993cde90d81d555a21d3fb68199c428a
Ayush Tiwari <ayush@sandbox.game>
2023-11-24 15:54:45 +0530

    feat: added script to grant roles around exchange contract

---
+++ packages/deploy/deploy/500_marketplace/505_exchange_setup.ts
@@ -0,0 +1,54 @@
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+
+const func: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+): Promise<void> {
+  const {deployments, getNamedAccounts} = hre;
+  const {execute, read, catchUnknownSigner} = deployments;
+  const {sandAdmin} = await getNamedAccounts();
+
+  const ERC1776_OPERATOR_ROLE = await read('Exchange','ERC1776_OPERATOR_ROLE');
+  const EXCHANGE_ADMIN_ROLE = await read('Exchange','EXCHANGE_ADMIN_ROLE');
+  const sandContract = await deployments.get('PolygonSand');
+
+  const hasERC1776OperatorRole = await read(
+    'Exchange',
+    'hasRole',
+    ERC1776_OPERATOR_ROLE,
+    sandContract.address
+  );
+  if (!hasERC1776OperatorRole) {
+    await catchUnknownSigner(
+      execute(
+        'Exchange',
+        {from: sandAdmin, log: true},
+        'grantRole',
+        ERC1776_OPERATOR_ROLE,
+        sandContract.address
+      )
+    );
+  }
+
+  const hasExchangeAdminRole = await read(
+    'Exchange',
+    'hasRole',
+    EXCHANGE_ADMIN_ROLE,
+    sandAdmin
+  );
+  if (!hasExchangeAdminRole) {
+    await catchUnknownSigner(
+      execute(
+        'Exchange',
+        {from: sandAdmin, log: true},
+        'grantRole',
+        EXCHANGE_ADMIN_ROLE,
+        sandAdmin
+      )
+    );
+  }
+};
+
+export default func;
+func.tags = ['Exchange', 'ExchangeSetup_setup'];
+func.dependencies = ['Exchange_deploy'];
