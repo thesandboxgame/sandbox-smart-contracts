7f71f77492a43c508f8cff859c36d6bb88017b15
León <leon.isla.22@gmail.com>
2023-11-28 12:16:49 -0300

    add: exchange deployments polygon

--- packages/deploy/deploy/500_marketplace/503_order_validator_setup.ts
+++ packages/deploy/deploy/500_marketplace/503_order_validator_setup.ts
@@ -97,4 +97,4 @@ const func: DeployFunction = async function (
 
 export default func;
 func.tags = ['OrderValidator', 'OrderValidator_setup'];
-func.dependencies = ['OrderValidator_deploy', 'PolygonSand', 'Asset'];
+func.dependencies = ['OrderValidator_deploy', 'PolygonSand_deploy', 'Asset_deploy'];
