28c7459883a1921b8b7b275e11a3e8619241a2c9
Maxime Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2023-11-28 13:28:43 +0100

    whitelist setup

--- packages/deploy/deploy/500_marketplace/503_order_validator_setup.ts
+++ packages/deploy/deploy/500_marketplace/503_order_validator_setup.ts
@@ -7,27 +7,103 @@ const func: DeployFunction = async function (
   const {deployments, getNamedAccounts} = hre;
   const {execute, read, catchUnknownSigner} = deployments;
   const {sandAdmin} = await getNamedAccounts();
+
   const ERC20_ROLE = await read('OrderValidator', 'ERC20_ROLE');
-  const sandContract = await deployments.get('PolygonSand');
-  const hasRole = await read(
-    'OrderValidator',
-    'hasRole',
-    ERC20_ROLE,
-    sandContract.address
-  );
-  if (!hasRole) {
-    await catchUnknownSigner(
-      execute(
+  const TSB_ROLE = await read('OrderValidator', 'TSB_ROLE');
+  const PARTNER_ROLE = await read('OrderValidator', 'PARTNER_ROLE');
+
+  const SandContract = await deployments.get('PolygonSand');  
+  const AssetContract = await deployments.get('Asset');
+
+  let addressesToGrant = [];
+  addressesToGrant[ERC20_ROLE] = [SandContract.address];
+  addressesToGrant[TSB_ROLE] = [AssetContract.address];
+  addressesToGrant[PARTNER_ROLE] = [];
+
+  for (const role in addressesToGrant) {
+    for (const address of addressesToGrant[role]) {
+      const hasRole = await read(
         'OrderValidator',
-        {from: sandAdmin, log: true},
-        'grantRole',
-        ERC20_ROLE,
-        sandContract.address
-      )
+        'hasRole',
+        role,
+        address
+      );
+
+      if(!hasRole) {
+        await catchUnknownSigner(
+          execute(
+            'OrderValidator',
+            {from: sandAdmin, log: true},
+            'grantRole',
+            role,
+            address
+          )
+        );            
+      }
+    }       
+  }
+
+  const currentWhitelistsEnabled = await read(
+    'OrderValidator',
+    'isWhitelistsEnabled'
+  );
+  const whitelistsEnabled = true;
+
+  if (currentWhitelistsEnabled != whitelistsEnabled) {
+    if (whitelistsEnabled) {
+      await catchUnknownSigner(
+        execute(
+          'OrderValidator',
+          {from: sandAdmin, log: true},
+          'enableWhitelists'
+        )
+      );      
+    } else {
+      await catchUnknownSigner(
+        execute(
+          'OrderValidator',
+          {from: sandAdmin, log: true},
+          'disableWhitelists'
+        )
+      );
+    }
+  }
+
+  let rolesEnabled = [];
+  rolesEnabled[TSB_ROLE] = true;
+  rolesEnabled[PARTNER_ROLE] = false;
+
+  for (const role in rolesEnabled) {
+    const isRoleEnabled = await read(
+      'OrderValidator',
+      'isRoleEnabled',
+      role
     );
+    
+    if (isRoleEnabled != rolesEnabled[role]) {
+      if (rolesEnabled[role]) {
+        await catchUnknownSigner(
+          execute(
+            'OrderValidator',
+            {from: sandAdmin, log: true},
+            'enableRole',
+            role
+          )
+        );
+      } else {
+        await catchUnknownSigner(
+          execute(
+            'OrderValidator',
+            {from: sandAdmin, log: true},
+            'disableRole',
+            role
+          )
+        );        
+      }
+    }
   }
 };
 
 export default func;
 func.tags = ['OrderValidator', 'OrderValidator_setup'];
-func.dependencies = ['OrderValidator_deploy', 'PolygonSand'];
+func.dependencies = ['OrderValidator_deploy', 'PolygonSand', 'Asset'];
