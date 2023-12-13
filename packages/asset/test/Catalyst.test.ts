0ff07d35dbd37f52413a748f414b31ba0bddb8e5
atkinsonholly <atkinsonholly@gmail.com>
2023-12-13 16:27:32 +0000

    add: transfer checks and upgrade script

--- packages/asset/test/Catalyst.test.ts
+++ packages/asset/test/Catalyst.test.ts
@@ -654,7 +654,7 @@ describe('Catalyst (/packages/asset/contracts/Catalyst.sol)', function () {
     });
   });
 
-  describe('Token transfer and approval', function () {
+  describe.only('Token transfer and approval', function () {
     it('owner can approve operator', async function () {
       const {catalyst, user1, catalystAsMinter, user2} =
         await runCatalystSetup();
@@ -706,6 +706,42 @@ describe('Catalyst (/packages/asset/contracts/Catalyst.sol)', function () {
         );
       expect(await catalyst.balanceOf(user2.address, 1)).to.be.equal(10);
       expect(await catalyst.balanceOf(user2.address, 2)).to.be.equal(10);
+    });
+    it('cannot transfer if not approved operator', async function () {
+      const {catalyst, user1, catalystAsMinter, user2} =
+        await runCatalystSetup();
+      await catalystAsMinter.mint(user1.address, 1, 10);
+      expect(await catalyst.balanceOf(user1.address, 1)).to.be.equal(10);
+      expect(
+        await catalyst.isApprovedForAll(user1.address, user2.address)
+      ).to.be.equal(false);
+      await expect(catalyst
+        .connect(await ethers.provider.getSigner(user2.address)) // bad operator
+        .safeTransferFrom(user1.address, user2.address, 1, 10, zeroAddress)).to.be.revertedWith('Catalyst: Transfer error');
+      expect(await catalyst.balanceOf(user1.address, 1)).to.be.equal(10);
+    });
+    it('cannot batch transfer if not approved operator', async function () {
+      const {catalyst, user1, catalystAsMinter, user2} =
+        await runCatalystSetup();
+      await catalystAsMinter.mint(user1.address, 1, 10);
+      await catalystAsMinter.mint(user1.address, 2, 10);
+
+      expect(await catalyst.balanceOf(user1.address, 1)).to.be.equal(10);
+      expect(await catalyst.balanceOf(user1.address, 2)).to.be.equal(10);
+      expect(
+        await catalyst.isApprovedForAll(user1.address, user2.address)
+      ).to.be.equal(false);
+      await expect(catalyst
+        .connect(await ethers.provider.getSigner(user2.address)) // bad operator
+        .safeBatchTransferFrom(
+          user1.address,
+          user2.address,
+          [1, 2],
+          [10, 10],
+          zeroAddress
+        )).to.be.revertedWith('ERC1155: caller is not token owner or approved');
+      expect(await catalyst.balanceOf(user1.address, 1)).to.be.equal(10);
+      expect(await catalyst.balanceOf(user1.address, 2)).to.be.equal(10);
     });
     it('should fail on transfering non existing token', async function () {
       const {catalyst, user1, user2} = await runCatalystSetup();
---
+++ packages/deploy/deploy/300_catalyst/303_upgrade_catalyst.ts
@@ -0,0 +1,26 @@
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+
+const func: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+): Promise<void> {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+
+  const {deployer, upgradeAdmin} =
+    await getNamedAccounts();
+
+  await deploy('Catalyst', {
+    from: deployer,
+    log: true,
+    contract: '@sandbox-smart-contracts/asset/contracts/Catalyst.sol:Catalyst',
+    proxy: {
+      owner: upgradeAdmin,
+      proxyContract: 'OpenZeppelinTransparentProxy',
+      upgradeIndex: 1,
+    },
+  });
+};
+export default func;
+func.tags = ['Catalyst_upgrade', 'L2'];
+func.dependencies = ['Catalyst_deploy', 'Catalyst_setup'];
