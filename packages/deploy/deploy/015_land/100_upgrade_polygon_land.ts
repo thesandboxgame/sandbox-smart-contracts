281bbe5a6c740bcfbb63804895d9dd064d0b26d9
Andres Adjimann <andres@sandbox.game>
2024-01-30 08:11:25 -0300

    feat: new land package deploy
    We decided to filter just by tags and use a folder per contract
    
    - 000_core is now splitted in land and sand folters.
    - We don't use func.skip or conditionals inside the deploy scripts

---
+++ packages/deploy/deploy/015_land/100_upgrade_polygon_land.ts
@@ -0,0 +1,31 @@
+/*
+  The original Land contract was deployed from the `core` package.
+  We upgrade the Land contract from the new `land` package
+ */
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+
+const func: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+): Promise<void> {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy, catchUnknownSigner} = deployments;
+
+  const {deployer, upgradeAdmin} = await getNamedAccounts();
+  await catchUnknownSigner(
+    deploy('PolygonLand', {
+      from: deployer,
+      log: true,
+      contract:
+        '@sandbox-smart-contracts/land/contracts/PolygonLandV2.sol:PolygonLandV2',
+      proxy: {
+        owner: upgradeAdmin,
+        proxyContract: 'OpenZeppelinTransparentProxy',
+        upgradeIndex: 1,
+      },
+    })
+  );
+};
+export default func;
+func.tags = ['PolygonLand', 'PolygonLand_upgrade_1', 'L2'];
+func.dependencies = ['PolygonLand_deploy'];
