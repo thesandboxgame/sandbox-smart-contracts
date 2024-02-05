b6d46d4353b7ab1efe5155355e1b71f3452ff6f1
Lucas <luc4rd@gmail.com>
2024-02-05 12:05:51 +0100

    ramoving old polygon asset

--- packages/core/src/solc_0.8/test/FakeChildChainManager.sol
+++ packages/core/src/solc_0.8/test/FakeChildChainManager.sol
@@ -1,25 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.2;
 
-import "../polygon/child/asset/PolygonAssetERC1155.sol";
+//import "../polygon/child/asset/PolygonAssetERC1155.sol";
 import "../polygon/child/sand/PolygonSand.sol";
 
 /// @dev This is NOT a secure ChildChainManager contract implementation!
 /// DO NOT USE in production.
 
 contract FakeChildChainManager {
-    address public polygonAsset;
+    //address public polygonAsset;
 
     // solhint-disable-next-line no-empty-blocks
     constructor() {}
 
-    function setPolygonAsset(address _polygonAsset) external {
+    /* function setPolygonAsset(address _polygonAsset) external {
         polygonAsset = _polygonAsset;
-    }
+    } */
 
-    function callDeposit(address user, bytes calldata depositData) external {
+    /* function callDeposit(address user, bytes calldata depositData) external {
         PolygonAssetERC1155(polygonAsset).deposit(user, depositData);
-    }
+    } */
 
     function callSandDeposit(
         address polygonSand,
