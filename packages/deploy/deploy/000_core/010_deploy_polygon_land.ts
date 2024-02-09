600ed0ce557199a900d19b81e1b95cb71504e50c
Andres Adjimann <andres@sandbox.game>
2024-02-09 09:32:02 -0300

    feat: LR-50-land-deploy partial
    Only the deployments scripts for core land and sand.

---
+++ packages/deploy/deploy/000_core/010_deploy_polygon_land.ts
@@ -0,0 +1,28 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+  const {deployer, upgradeAdmin} = await getNamedAccounts();
+
+  const TRUSTED_FORWARDER = await deployments.get('TRUSTED_FORWARDER_V2');
+  await deploy('PolygonLand', {
+    from: deployer,
+    contract:
+      '@sandbox-smart-contracts/core/src/solc_0.8/polygon/child/land/PolygonLandV2.sol:PolygonLandV2',
+    proxy: {
+      owner: upgradeAdmin,
+      proxyContract: 'OpenZeppelinTransparentProxy',
+      execute: {
+        methodName: 'initialize',
+        args: [TRUSTED_FORWARDER.address],
+      },
+      upgradeIndex: 0,
+    },
+    log: true,
+  });
+};
+export default func;
+func.tags = ['PolygonLand', 'PolygonLand_deploy', 'L2'];
+func.dependencies = ['TRUSTED_FORWARDER_V2'];
