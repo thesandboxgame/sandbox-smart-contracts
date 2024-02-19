02c14f81e5a50f528516139e94d10e142ddf2a39
Ayush Tiwari <ayush@sandbox.game>
2024-02-19 17:54:52 +0530

    fix: updated Mock contract

--- packages/land/contracts/mock/LandMock.sol
+++ packages/land/contracts/mock/LandMock.sol
@@ -2,9 +2,184 @@
 
 pragma solidity 0.8.23;
 
-import {Land} from "../Land.sol";
+import {IOperatorFilterRegistry} from "../mainnet/IOperatorFilterRegistry.sol";
+import {OperatorFiltererUpgradeable} from "../mainnet/OperatorFiltererUpgradeable.sol";
+import {LandBaseToken} from "../mainnet/LandBaseToken.sol";
+import {IRoyaltyManager} from "@sandbox-smart-contracts/dependency-royalty-management/contracts/interfaces/IRoyaltyManager.sol";
 
-contract LandMock is Land {
+contract LandMock is LandBaseToken, OperatorFiltererUpgradeable {
+    uint16 internal constant TOTAL_BASIS_POINTS = 10000;
+
+    bool internal _initV4;
+    IRoyaltyManager private royaltyManager;
+    address private _owner;
+
+    event OperatorRegistrySet(address indexed registry);
+    event RoyaltyManagerSet(address indexed _royaltyManager);
+    event OwnerSet(address indexed owner);
+
+    modifier initializer() {
+        require(!_initV4, "already initialized");
+        _;
+    }
+
+    function initialize(
+        address metaTransactionContract,
+        address admin,
+        address _royaltyManager,
+        address owner
+    ) public initializer {
+        _admin = admin;
+        _setMetaTransactionProcessor(metaTransactionContract, true);
+        _setRoyaltyManager(_royaltyManager);
+        _initV4 = true;
+        _setOwner(owner);
+    }
+
+    /// @notice set royalty manager
+    /// @param _royaltyManager address of royalty manager to set
+    function _setRoyaltyManager(address _royaltyManager) internal {
+        royaltyManager = IRoyaltyManager(_royaltyManager);
+        emit RoyaltyManagerSet(_royaltyManager);
+    }
+
+    function _setOwner(address newOwner) internal {
+        _owner = newOwner;
+        emit OwnerSet(newOwner);
+    }
+
+    /// @notice This function is used to register Land contract on the Operator Filterer Registry of Opensea.can only be called by admin.
+    /// @dev used to register contract and subscribe to the subscriptionOrRegistrantToCopy's black list.
+    /// @param subscriptionOrRegistrantToCopy registration address of the list to subscribe.
+    /// @param subscribe bool to signify subscription "true"" or to copy the list "false".
+    function register(address subscriptionOrRegistrantToCopy, bool subscribe) external onlyAdmin {
+        require(subscriptionOrRegistrantToCopy != address(0), "subscription can't be zero");
+        _register(subscriptionOrRegistrantToCopy, subscribe);
+    }
+
+    /// @notice sets filter registry address deployed in test
+    /// @param registry the address of the registry
+    function setOperatorRegistry(address registry) external onlyAdmin {
+        operatorFilterRegistry = IOperatorFilterRegistry(registry);
+        emit OperatorRegistrySet(registry);
+    }
+
+    /// @notice Returns how much royalty is owed and to whom based on ERC2981
+    /// @dev tokenId is one of the EIP2981 args for this function can't be removed
+    /// @param _salePrice the price of token on which the royalty is calculated
+    /// @return receiver the receiver of royalty
+    /// @return royaltyAmount the amount of royalty
+    function royaltyInfo(
+        uint256 /*_tokenId */,
+        uint256 _salePrice
+    ) external view returns (address receiver, uint256 royaltyAmount) {
+        uint16 royaltyBps;
+        (receiver, royaltyBps) = royaltyManager.getRoyaltyInfo();
+        royaltyAmount = (_salePrice * royaltyBps) / TOTAL_BASIS_POINTS;
+        return (receiver, royaltyAmount);
+    }
+
+    /// @notice returns the royalty manager
+    /// @return royaltyManagerAddress address of royalty manager contract.
+    function getRoyaltyManager() external view returns (IRoyaltyManager royaltyManagerAddress) {
+        return royaltyManager;
+    }
+
+    /// @notice returns the owner address
+    /// @return owner address of contract owner
+    function getOwner() external view returns (address owner) {
+        return _owner;
+    }
+
+    /// @notice Set new owner to contract
+    /// @param newOwner address of new owner
+    function setOwner(address newOwner) external onlyAdmin {
+        require(newOwner != address(0), "owner cannot be zero address");
+        _setOwner(newOwner);
+    }
+
+    /**
+     * @notice Approve an operator to spend tokens on the sender behalf
+     * @param sender The address giving the approval
+     * @param operator The address receiving the approval
+     * @param id The id of the token
+     */
+    function approveFor(
+        address sender,
+        address operator,
+        uint256 id
+    ) public override onlyAllowedOperatorApproval(operator) {
+        super.approveFor(sender, operator, id);
+    }
+
+    /**
+     * @notice Set the approval for an operator to manage all the tokens of the sender
+     * @param operator The address receiving the approval
+     * @param approved The determination of the approval
+     */
+    function setApprovalForAll(address operator, bool approved) public override onlyAllowedOperatorApproval(operator) {
+        super.setApprovalForAll(operator, approved);
+    }
+
+    /**
+     * @notice Set the approval for an operator to manage all the tokens of the sender
+     * @param sender The address giving the approval
+     * @param operator The address receiving the approval
+     * @param approved The determination of the approval
+     */
+    function setApprovalForAllFor(
+        address sender,
+        address operator,
+        bool approved
+    ) public override onlyAllowedOperatorApproval(operator) {
+        super.setApprovalForAllFor(sender, operator, approved);
+    }
+
+    /**
+     * @notice Approve an operator to spend tokens on the sender behalf
+     * @param operator The address receiving the approval
+     * @param id The id of the token
+     */
+    function approve(address operator, uint256 id) public override onlyAllowedOperatorApproval(operator) {
+        super.approve(operator, id);
+    }
+
+    /**
+     * @notice Transfer a token between 2 addresses
+     * @param from The sender of the token
+     * @param to The recipient of the token
+     * @param id The id of the token
+     */
+    function transferFrom(address from, address to, uint256 id) public override onlyAllowedOperator(from) {
+        super.transferFrom(from, to, id);
+    }
+
+    /**
+     * @notice Transfer a token between 2 addresses letting the receiver knows of the transfer
+     * @param from The sender of the token
+     * @param to The recipient of the token
+     * @param id The id of the token
+     * @param data Additional data
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        bytes memory data
+    ) public override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, id, data);
+    }
+
+    /**
+     * @notice Transfer a token between 2 addresses letting the receiver knows of the transfer
+     * @param from The send of the token
+     * @param to The recipient of the token
+     * @param id The id of the token
+     */
+    function safeTransferFrom(address from, address to, uint256 id) external override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, id, "");
+    }
+
     /// @notice sets Approvals with operator filterer check in case to test the transfer.
     /// @param operator address of the operator to be approved
     /// @param approved bool value denoting approved (true) or not Approved(false)
