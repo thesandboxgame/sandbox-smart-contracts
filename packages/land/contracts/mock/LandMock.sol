fdc54757564efb617de6a91efe2f78a9987df06f
Ayush Tiwari <ayush@sandbox.game>
2024-03-01 20:32:54 +0530

    fix: updated mock contracts

--- packages/land/contracts/mock/LandMock.sol
+++ packages/land/contracts/mock/LandMock.sol
@@ -10,16 +10,16 @@ import {IRoyaltyManager} from "@sandbox-smart-contracts/dependency-royalty-manag
 contract LandMock is LandBaseToken, OperatorFiltererUpgradeable {
     uint16 internal constant TOTAL_BASIS_POINTS = 10000;
 
-    bool internal _initV4;
     IRoyaltyManager private royaltyManager;
     address private _owner;
+    bool private _initializing;
 
     event OperatorRegistrySet(address indexed registry);
     event RoyaltyManagerSet(address indexed _royaltyManager);
-    event OwnerSet(address indexed owner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     modifier initializer() {
-        require(!_initV4, "already initialized");
+        require(!_initializing, "already initialized");
         _;
     }
 
@@ -27,13 +27,14 @@ contract LandMock is LandBaseToken, OperatorFiltererUpgradeable {
         address metaTransactionContract,
         address admin,
         address _royaltyManager,
-        address owner
+        address _newOwner
     ) public initializer {
         _admin = admin;
         _setMetaTransactionProcessor(metaTransactionContract, true);
         _setRoyaltyManager(_royaltyManager);
-        _initV4 = true;
-        _setOwner(owner);
+        _transferOwnership(_newOwner);
+        _initializing = true;
+        emit AdminChanged(address(0), _admin);
     }
 
     /// @notice set royalty manager
@@ -43,9 +44,9 @@ contract LandMock is LandBaseToken, OperatorFiltererUpgradeable {
         emit RoyaltyManagerSet(_royaltyManager);
     }
 
-    function _setOwner(address newOwner) internal {
-        _owner = newOwner;
-        emit OwnerSet(newOwner);
+    function _transferOwnership(address _newOwner) internal {
+        emit OwnershipTransferred(_owner, _newOwner);
+        _owner = _newOwner;
     }
 
     /// @notice This function is used to register Land contract on the Operator Filterer Registry of Opensea.can only be called by admin.
@@ -85,17 +86,16 @@ contract LandMock is LandBaseToken, OperatorFiltererUpgradeable {
         return royaltyManager;
     }
 
-    /// @notice returns the owner address
-    /// @return owner address of contract owner
-    function getOwner() external view returns (address owner) {
+    /// @notice Get the address of the owner
+    /// @return ownerAddress The address of the owner.
+    function owner() external view returns (address ownerAddress) {
         return _owner;
     }
 
-    /// @notice Set new owner to contract
-    /// @param newOwner address of new owner
-    function setOwner(address newOwner) external onlyAdmin {
-        require(newOwner != address(0), "owner cannot be zero address");
-        _setOwner(newOwner);
+    /// @notice Set the address of the new owner of the contract
+    /// @param _newOwner address of new owner
+    function transferOwnership(address _newOwner) external onlyAdmin {
+        _transferOwnership(_newOwner);
     }
 
     /**
--- packages/land/contracts/mock/PolygonLandMock.sol
+++ packages/land/contracts/mock/PolygonLandMock.sol
@@ -21,13 +21,18 @@ contract PolygonLandMock is PolygonLandBaseToken, ERC2771Handler, OperatorFilter
 
     event OperatorRegistrySet(address indexed registry);
     event RoyaltyManagerSet(address indexed _royaltyManager);
-    event OwnerSet(address indexed owner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    function initialize(address trustedForwarder, address _royaltyManager, address owner) external initializer {
-        _admin = _msgSender();
+    function initialize(
+        address trustedForwarder,
+        address admin,
+        address _royaltyManager,
+        address _newOwner
+    ) external initializer {
+        _admin = admin;
         __ERC2771Handler_initialize(trustedForwarder);
         _setRoyaltyManager(_royaltyManager);
-        _setOwner(owner);
+        _transferOwnership(_newOwner);
         emit AdminChanged(address(0), _admin);
     }
 
@@ -38,9 +43,9 @@ contract PolygonLandMock is PolygonLandBaseToken, ERC2771Handler, OperatorFilter
         emit RoyaltyManagerSet(_royaltyManager);
     }
 
-    function _setOwner(address newOwner) internal {
-        _owner = newOwner;
-        emit OwnerSet(newOwner);
+    function _transferOwnership(address _newOwner) internal {
+        emit OwnershipTransferred(_owner, _newOwner);
+        _owner = _newOwner;
     }
 
     /// @dev Change the address of the trusted forwarder for meta-TX
@@ -71,17 +76,16 @@ contract PolygonLandMock is PolygonLandBaseToken, ERC2771Handler, OperatorFilter
         return royaltyManager;
     }
 
-    /// @notice returns the owner address
-    /// @return owner address of contract owner
-    function getOwner() external view returns (address owner) {
+    /// @notice Get the address of the owner
+    /// @return ownerAddress The address of the owner.
+    function owner() external view returns (address ownerAddress) {
         return _owner;
     }
 
-    /// @notice Set new owner to contract
-    /// @param newOwner address of new owner
-    function setOwner(address newOwner) external onlyAdmin {
-        require(newOwner != address(0), "owner cannot be zero address");
-        _setOwner(newOwner);
+    /// @notice Set the address of the new owner of the contract
+    /// @param _newOwner address of new owner
+    function transferOwnership(address _newOwner) external onlyAdmin {
+        _transferOwnership(_newOwner);
     }
 
     function _msgSender() internal view override(ContextUpgradeable, ERC2771Handler) returns (address) {
