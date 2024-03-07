b10d281818658987941af90842d1f9f509321936
Andres Adjimann <andres@sandbox.game>
2024-03-07 14:06:07 -0300

    test: RoyaltyManager is RoyaltyManagerMock add RoyaltySplitterMock

---
+++ packages/land/contracts/mock/RoyaltySplitterMock.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+// solhint-disable no-unused-import
+// solhint-disable no-empty-blocks
+pragma solidity 0.8.23;
+import {RoyaltySplitter} from "@sandbox-smart-contracts/dependency-royalty-management/contracts/RoyaltySplitter.sol";
+
+contract RoyaltySplitterMock is RoyaltySplitter {}
