142504c7ca22d645b14c4cd59eac213c9cb36d55
lucas <luc4rd@gmail.com>
2024-03-21 15:29:27 +0100

    ranamings

--- packages/core/deploy/07_giveaway/17_deploy_multi_giveaway_1.ts
+++ packages/core/deploy/07_giveaway/17_deploy_multi_giveaway_1.ts
@@ -6,7 +6,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deploy} = deployments;
   const {deployer, multiGiveawayAdmin} = await getNamedAccounts();
 
-  await deploy('Multi_Giveaway_0', {
+  await deploy('Multi_Giveaway_1', {
     contract: 'MultiGiveawayV0',
     from: deployer,
     log: true,
@@ -15,6 +15,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 };
 export default func;
-func.tags = ['Multi_Giveaway_0', 'Multi_Giveaway_0_deploy'];
+func.tags = ['Multi_Giveaway_1', 'Multi_Giveaway_1_deploy'];
 func.dependencies = [];
 func.skip = async (hre) => hre.network.name === 'mainnet';
