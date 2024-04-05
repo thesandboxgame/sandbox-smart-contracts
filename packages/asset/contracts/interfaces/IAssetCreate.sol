1e4de97d7168e76b69b7123e7a347f9f8365b6c6
wojciech-turek <contact@wojciechturek.com>
2024-04-05 11:33:42 +0200

    Add lazy minting functionality to AssetCreate
    Add fee receiver
    
    Fix formatting
    
    Update documentation
    
    Add success tests
    
    Add exchange contract interaction when lazy minting
    
    Add caller param to lazy mint and add tests
    
    Fix formatting
    
    Add more potential test cases
    
    Move variables to the top, below existing ones
    
    Use the mintData.caller instead of msgSender in batch lazy mint
    
    Update lazy minting params and add more tests
    
    Update contract
    
    Changed so that the first param for lazy minting is the from address to satisfy approveAndCall requirements and fix empty array in batch lazy mint
    
    Add tests
    
    Batch lazy minting tests
    
    Formatting update
    
    Add new requires, update tests and documentation
    
    Fix linter warnings
    
    Further small lint fixes
    
    Change docs formatting
    
    Shorten revert string
    
    Bump npm version
    
    Update lockfile

--- packages/asset/contracts/interfaces/IAssetCreate.sol
+++ packages/asset/contracts/interfaces/IAssetCreate.sol
@@ -4,6 +4,28 @@ pragma solidity 0.8.18;
 /// @title AssetCreate interface
 /// @author The Sandbox
 interface IAssetCreate {
+    struct LazyMintData {
+        address caller;
+        uint8 tier;
+        uint256 amount;
+        uint256 unitPrice;
+        address paymentToken;
+        string metadataHash;
+        uint256 maxSupply;
+        address creator;
+    }
+
+    struct LazyMintBatchData {
+        address caller;
+        uint8[] tiers;
+        uint256[] amounts;
+        uint256[] unitPrices;
+        address[] paymentTokens;
+        string[] metadataHashes;
+        uint256[] maxSupplies;
+        address[] creators;
+    }
+
     event TrustedForwarderChanged(address indexed newTrustedForwarderAddress);
     event AssetMinted(
         address indexed creator,
@@ -36,5 +58,21 @@ interface IAssetCreate {
         uint256[] amounts,
         string[] metadataHashes,
         bool[] revealed
+    );
+    event AssetLazyMinted(
+        address indexed recipient,
+        address indexed creator,
+        uint256 tokenId,
+        uint16 tier,
+        uint256 amount,
+        string metadataHash
+    );
+    event AssetBatchLazyMinted(
+        address indexed recipient,
+        address[] indexed creators,
+        uint256[] tokenIds,
+        uint8[] tiers,
+        uint256[] amounts,
+        string[] metadataHashes
     );
 }
---
+++ packages/asset/contracts/libraries/LibMath.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+/// @author The Sandbox
+/// @title LibMath: A library for handling specific math operations.
+library LibMath {
+    uint256 internal constant ACCEPTABLE_RELATIVE_ERROR = 1000;
+
+    /// @dev Calculates partial value given a numerator and denominator rounded down.
+    ///      Reverts if rounding error is >= 0.1%
+    /// @param numerator Numerator.
+    /// @param denominator Denominator.
+    /// @param target Value to calculate partial of.
+    /// @return partialAmount value of target rounded down.
+    function safeGetPartialAmountFloor(
+        uint256 numerator,
+        uint256 denominator,
+        uint256 target
+    ) internal pure returns (uint256 partialAmount) {
+        if (isRoundingErrorFloor(numerator, denominator, target)) {
+            revert("rounding error");
+        }
+        partialAmount = (numerator * target) / (denominator);
+    }
+
+    /// @dev Checks if rounding error >= 0.1% when rounding down.
+    /// @param numerator Numerator.
+    /// @param denominator Denominator.
+    /// @param target Value to multiply with numerator/denominator.
+    /// @return isError Rounding error is present.
+    function isRoundingErrorFloor(
+        uint256 numerator,
+        uint256 denominator,
+        uint256 target
+    ) internal pure returns (bool isError) {
+        if (denominator == 0) {
+            revert("division by zero");
+        }
+
+        // The absolute rounding error is the difference between the rounded
+        // value and the ideal value. The relative rounding error is the
+        // absolute rounding error divided by the absolute value of the
+        // ideal value. This is undefined when the ideal value is zero.
+        //
+        // The ideal value is `numerator * target / denominator`.
+        // Let's call `numerator * target % denominator` the remainder.
+        // The absolute error is `remainder / denominator`.
+        //
+        // When the ideal value is zero, we require the absolute error to
+        // be zero. Fortunately, this is always the case. The ideal value is
+        // zero iff `numerator == 0` and/or `target == 0`. In this case the
+        // remainder and absolute error are also zero.
+        if (target == 0 || numerator == 0) {
+            return false;
+        }
+
+        // Otherwise, we want the relative rounding error to be strictly
+        // less than 0.1%.
+        // The relative error is `remainder / (numerator * target)`.
+        // We want the relative error less than 1 / 1000:
+        //        remainder / (numerator * target)  <  1 / 1000
+        // or equivalently:
+        //        1000 * remainder  <  numerator * target
+        // so we have a rounding error iff:
+        //        1000 * remainder  >=  numerator * target
+        uint256 remainder = mulmod(target, numerator, denominator);
+        isError = remainder * ACCEPTABLE_RELATIVE_ERROR >= numerator * target;
+    }
+}
