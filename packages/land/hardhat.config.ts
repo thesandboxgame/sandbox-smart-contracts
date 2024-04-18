d49d1e64d75b5648ae107478e60b1cd56a6f149b
Andres Adjimann <andres@sandbox.game>
2024-04-18 10:08:56 -0300

    fix: rename _checkOwner* remove some TODOs
    - rename _checkOwner => _checkQuadIsNotMinted
    - rename _checkAndClearOwner => _checkQuadIsNotMintedAndClearOwner
    - remove TODOs that doesn't make sense anymore

--- packages/land/hardhat.config.ts
+++ packages/land/hardhat.config.ts
@@ -23,7 +23,6 @@ const config: HardhatUserConfig = {
   mocha: {
     ...(!process.env.CI ? {} : {invert: true, grep: '@skip-on-ci'}),
   },
-  // TODO: Remove or at least check that the only contracts over the max size are mocks.
   networks: {
     hardhat: {allowUnlimitedContractSize: true},
   },
