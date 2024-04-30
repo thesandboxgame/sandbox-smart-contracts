28387da4a7fb687955954ec0cf6732618aadc8cf
mike-lush <mike-lush@proton.me>
2024-04-30 08:53:28 +0200

    Rm unused import

--- packages/asset/contracts/AssetCreate.sol
+++ packages/asset/contracts/AssetCreate.sol
@@ -20,7 +20,6 @@ import {IAssetCreate} from "./interfaces/IAssetCreate.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IExchange, ExchangeMatch} from "./interfaces/IExchange.sol";
-import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 /// @title AssetCreate
 /// @author The Sandbox
