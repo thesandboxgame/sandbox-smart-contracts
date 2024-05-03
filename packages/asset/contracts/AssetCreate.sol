fb6d8d767fcba59bf233b6b06abda8bf51f9f787
wojciech-turek <contact@wojciechturek.com>
2024-05-03 22:06:46 +0200

    Add security contact

--- packages/asset/contracts/AssetCreate.sol
+++ packages/asset/contracts/AssetCreate.sol
@@ -24,6 +24,7 @@ import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 /// @title AssetCreate
 /// @author The Sandbox
+/// @custom:security-contact contact-blockchain@sandbox.game
 /// @notice User-facing contract for creating new assets
 contract AssetCreate is
     IAssetCreate,
