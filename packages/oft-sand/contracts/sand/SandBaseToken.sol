58561d6e659e22cf0accb25a37e77378de987b3f
Ayush Tiwari <ayush@sandbox.game>
2024-05-22 01:59:36 +0530

    feat/oft-sand package

---
+++ packages/oft-sand/contracts/sand/SandBaseToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.23;
+
+import "./ERC20BasicApproveExtension.sol";
+import "./ERC20BaseToken.sol";
+
+contract SandBaseToken is ERC20BaseToken, ERC20BasicApproveExtension {
+    constructor(
+        address sandAdmin,
+        address executionAdmin,
+        address beneficiary,
+        uint256 amount
+    ) ERC20BaseToken("SAND", "SAND", sandAdmin, executionAdmin) {
+        _admin = sandAdmin;
+        if (beneficiary != address(0)) {
+            uint256 initialSupply = amount * (1 ether);
+            _mint(beneficiary, initialSupply);
+        }
+    }
+}
