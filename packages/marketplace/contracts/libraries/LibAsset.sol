0a8cb0f1141878065d5407f5b5584db8aad53ca0
atkinsonholly <atkinsonholly@gmail.com>
2024-05-29 09:39:24 +0100

    add: quad struct added to bundle

--- packages/marketplace/contracts/libraries/LibAsset.sol
+++ packages/marketplace/contracts/libraries/LibAsset.sol
@@ -12,7 +12,6 @@ library LibAsset {
         ERC20, // Represents an ERC20 token.
         ERC721, // Represents a single ERC721 token.
         ERC1155, // Represents an ERC1155 token.
-        QUADS, // Represents a batch of LAND ERC721 token (enables LAND quad batch transfer).
         BUNDLE // Represents a group of tokens of various types.
     }
 
@@ -56,18 +55,20 @@ library LibAsset {
         uint256[] supplies;
     }
 
-    /// @dev Represents a group (i.e. bundle) of assets on the Ethereum blockchain with its types and values.
-    struct Bundle {
-        BundledERC20[] bundledERC20;
-        BundledERC721[] bundledERC721;
-        BundledERC1155[] bundledERC1155;
-    }
-
+    /// @dev Represents a group of LAND ERC721 token (enables LAND quad batch transfer functionality).
     struct Quads {
         uint256[] sizes;
         uint256[] xs;
         uint256[] ys;
         bytes data;
+    }
+
+    /// @dev Represents a group (i.e. bundle) of assets on the Ethereum blockchain with its types and values.
+    struct Bundle {
+        BundledERC20[] bundledERC20;
+        BundledERC721[] bundledERC721;
+        BundledERC1155[] bundledERC1155;
+        Quads quads;
     }
 
     bytes32 internal constant ASSET_TYPE_TYPEHASH = keccak256("AssetType(uint256 assetClass,bytes data)");
@@ -138,13 +139,6 @@ library LibAsset {
     /// @return The address of the token.
     function decodeAddress(AssetType memory assetType) internal pure returns (address) {
         return abi.decode(assetType.data, (address));
-    }
-
-    /// @notice Decode the token details (address and tokenId) from a group of AssetTypes.
-    /// @param assetType The asset type to decode.
-    /// @return Array of LAND quad information.
-    function decodeQuads(AssetType memory assetType) internal pure returns (Quads memory) {
-        return abi.decode(assetType.data, (Quads));
     }
 
     /// @notice Decode the token details (address and tokenId) from a group of AssetTypes.
