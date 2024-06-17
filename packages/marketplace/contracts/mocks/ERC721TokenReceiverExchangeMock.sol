a22376def938fd1c8de6277c7724f1c67fe2c909
atkinsonholly <atkinsonholly@gmail.com>
2024-06-17 14:13:41 +0100

    add: mock land contracts for testing Bundles with Quads

---
+++ packages/marketplace/contracts/mocks/ERC721TokenReceiverExchangeMock.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.23;
+
+import {ERC721TokenReceiverMock} from "@sandbox-smart-contracts/land/contracts/mock/ERC721TokenReceiverMock.sol";
+
+contract ERC721TokenReceiverExchangeMock is ERC721TokenReceiverMock {}
---
+++ packages/marketplace/contracts/mocks/MarketPlaceExchangeMock.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.23;
+
+import {MarketPlaceMock} from "@sandbox-smart-contracts/land/contracts/mock/MarketPlaceMock.sol";
+
+contract MarketPlaceExchangeMock is MarketPlaceMock {}
