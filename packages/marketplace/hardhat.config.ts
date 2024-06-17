416e90a06c51cb2ce6ac1e7b8267b7575a23665e
atkinsonholly <atkinsonholly@gmail.com>
2024-06-17 12:57:12 +0100

    add: hardhat config updated for land package compiler version

--- packages/marketplace/hardhat.config.ts
+++ packages/marketplace/hardhat.config.ts
@@ -10,6 +10,15 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: '0.8.23',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+      {
         version: '0.8.19',
         settings: {
           optimizer: {
