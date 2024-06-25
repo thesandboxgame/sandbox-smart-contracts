2cf842db71fd83dbe41e1706e214fae1ccde1fce
Ayush Tiwari <ayush@sandbox.game>
2024-06-25 18:19:48 +0530

    feat: updated OFTAdapterForSand constructor arg

--- packages/deploy/deploy/800_oft_sand/800_deploy_oft_adapter.ts
+++ packages/deploy/deploy/800_oft_sand/800_deploy_oft_adapter.ts
@@ -8,15 +8,21 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const Sand = await deployments.get('Sand');
   const EndpointV2 = await deployments.get('EndpointV2');
+  const TRUSTED_FORWARDER_V2 = await deployments.get('TRUSTED_FORWARDER_V2');
 
   await deploy('OFTAdapterForSand', {
     from: deployer,
     contract: 'OFTAdapterForSand',
-    args: [Sand.address, EndpointV2.address, deployer],
+    args: [
+      Sand.address,
+      EndpointV2.address,
+      deployer,
+      TRUSTED_FORWARDER_V2.address,
+    ],
     log: true,
     skipIfAlreadyDeployed: true,
   });
 };
 export default func;
 func.tags = ['OFTAdapterForSand', 'OFTAdapterForSand_deploy'];
-func.dependencies = ['Sand', 'EndpointV2'];
+func.dependencies = ['Sand', 'TRUSTED_FORWARDER_V2', 'EndpointV2'];
