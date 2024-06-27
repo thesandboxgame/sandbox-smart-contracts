5a42022b0c5867333733260b62854524a1833305
Ayush Tiwari <ayush@sandbox.game>
2024-06-27 13:59:18 +0530

    feat: added OApp and OAppSender contracts

--- packages/oft-sand/contracts/oft/OFTAdapter.sol
+++ packages/oft-sand/contracts/oft/OFTAdapter.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.20;
+pragma solidity 0.8.23;
 
 import {IERC20Metadata, IERC20} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
