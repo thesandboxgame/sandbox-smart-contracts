adc8af601c5f34af0fbad9a5242c8a478b0f9d3a
wojciech-turek <contact@wojciechturek.com>
2024-07-02 11:54:47 +0200

    Marketplace fee whitelists

--- packages/marketplace/contracts/Exchange.sol
+++ packages/marketplace/contracts/Exchange.sol
@@ -37,6 +37,14 @@ contract Exchange is
     /// @return Hash for PAUSER_ROLE.
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
+    /// @notice Role for TSB owned addresses that list TSB owned assets for sale, forces primary sales conditions.
+    /// @return Hash for TSB_SELLER_ROLE.
+    bytes32 public constant TSB_SELLER_ROLE = keccak256("TSB_SELLER_ROLE");
+
+    /// @notice Role for addresses that should be whitelisted from any marketplace fees including royalties.
+    /// @return Hash for FEE_WHITELIST_ROLE.
+    bytes32 public constant FEE_WHITELIST_ROLE = keccak256("FEE_WHITELIST_ROLE");
+
     /// @dev This protects the implementation contract from being initialized.
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -156,11 +164,18 @@ contract Exchange is
         _unpause();
     }
 
-    /// @dev Check if fees & royalties should be skipped for users with the EXCHANGE_ADMIN_ROLE.
+    /// @dev Check if fees & royalties should be skipped for users with the FEE_WHITELIST_ROLE.
     /// @param from Address to check.
     /// @return True if fees should be skipped, false otherwise.
     function _mustSkipFees(address from) internal view override returns (bool) {
-        return hasRole(EXCHANGE_ADMIN_ROLE, from);
+        return hasRole(FEE_WHITELIST_ROLE, from);
+    }
+
+    /// @dev Check if the address is a TSB seller, which forces primary sales conditions regardless if the seller is the creator of the token.
+    /// @param from Address to check.
+    /// @return True if the address is a TSB seller, false otherwise.
+    function _isTSBSeller(address from) internal view override returns (bool) {
+        return hasRole(TSB_SELLER_ROLE, from);
     }
 
     function _msgSender()
