4058fc2f8614ca3992c13424f2c0b3a092402c32
Andres Adjimann <adjisb@sandbox.game>
2024-07-08 11:05:59 -0300

    fix: N-02 Lack of a Security Contact

--- packages/land/contracts/interfaces/IERC173.sol
+++ packages/land/contracts/interfaces/IERC173.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 /// @title ERC-173 Contract Ownership Standard
+/// @custom:security-contact contact-blockchain@sandbox.game
 /// @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-173.md
 ///  Note: the ERC-165 identifier for this interface is 0x7f5828d0
 interface IERC173 {
