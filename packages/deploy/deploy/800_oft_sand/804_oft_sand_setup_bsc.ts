a8668084482656882981fd6269b39ba963bdf9ee
Ayush Tiwari <ayush@sandbox.game>
2024-07-17 00:04:02 +0530

    fix: added check in oft-sand setup

--- packages/deploy/deploy/800_oft_sand/804_oft_sand_setup_bsc.ts
+++ packages/deploy/deploy/800_oft_sand/804_oft_sand_setup_bsc.ts
@@ -6,7 +6,7 @@ const func: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
 ): Promise<void> {
   const {deployments, getNamedAccounts} = hre;
-  const {execute} = deployments;
+  const {execute, read} = deployments;
   const {deployer} = await getNamedAccounts();
 
   let eidEthereum, eidBase;
@@ -30,26 +30,42 @@ const func: DeployFunction = async function (
 
   const hreBase = hre.companionNetworks.base;
   const deploymentsBase = hreBase.deployments;
-  const OFTSand = await deploymentsBase.getOrNull('OFTSand');
+  const OFTSandBase = await deploymentsBase.getOrNull('OFTSand');
 
-  if (OFTAdapterForSand && OFTSand) {
-    // setting OFTAdapterForSand as peer to  OFTSand(bsc) using eidEthereum
-    await execute(
-      'OFTSand',
-      {from: deployer, log: true},
-      'setPeer',
+  if (OFTAdapterForSand && OFTSandBase) {
+    const isPeerForEthereum = await read(
+      'OFTAdapterForSand',
+      'isPeer',
       eidEthereum,
       ethers.zeroPadValue(OFTAdapterForSand.address, 32)
     );
+    if (!isPeerForEthereum) {
+      // setting OFTAdapterForSand as peer to  OFTSand(bsc) using eidEthereum
+      await execute(
+        'OFTSand',
+        {from: deployer, log: true},
+        'setPeer',
+        eidEthereum,
+        ethers.zeroPadValue(OFTAdapterForSand.address, 32)
+      );
+    }
 
-    // setting OFTSand(base) as peer to  OFTSand(bsc) using eidBase
-    await execute(
-      'OFTSand',
-      {from: deployer, log: true},
-      'setPeer',
+    const isPeerForBase = await read(
+      'OFTAdapterForSand',
+      'isPeer',
       eidBase,
-      ethers.zeroPadValue(OFTSand.address, 32)
+      ethers.zeroPadValue(OFTSandBase.address, 32)
     );
+    if (!isPeerForBase) {
+      // setting OFTSand(base) as peer to  OFTSand(bsc) using eidBase
+      await execute(
+        'OFTSand',
+        {from: deployer, log: true},
+        'setPeer',
+        eidBase,
+        ethers.zeroPadValue(OFTSandBase.address, 32)
+      );
+    }
   }
 };
 
