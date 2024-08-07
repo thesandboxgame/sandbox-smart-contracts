f371d354e758a4d9a49d74307192e599c41be32c
Andres Adjimann <adjisb@sandbox.game>
2024-08-07 09:43:58 -0300

    feat: upgrade solc version, use only one OZ version

--- packages/avatar/hardhat.config.ts
+++ packages/avatar/hardhat.config.ts
@@ -11,6 +11,15 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: '0.8.26',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: '0.8.18',
         settings: {
           optimizer: {
