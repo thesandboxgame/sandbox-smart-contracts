af019b439fb87f7fc87a0ec35273b238bfd3ff7f
León Isla <leon.isla.22@gmail.com>
2024-10-01 11:58:13 -0300

    feat: deploy batch mint contracts (#1579)
    * feat: deploy batch mint contracts
    
    * fix: ci
    
    * add: mainnet deployments
    
    * fix: format

---
+++ packages/core/deploy/999_helpers/00_deploy_batch_mint.ts
@@ -0,0 +1,30 @@
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {skipUnlessL1} from '../../utils/network';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy, execute, read} = deployments;
+  const {deployer, sandboxAccount} = await getNamedAccounts();
+  const batch = await deploy(`Batch-${sandboxAccount}`, {
+    contract: `Batch`,
+    from: deployer,
+    args: [sandboxAccount],
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+  const isMinter = await read('Land', 'isMinter', batch.address);
+  if (!isMinter) {
+    const admin = await read('Land', 'getAdmin');
+    await execute(
+      'Land',
+      {from: admin, log: true},
+      'setMinter',
+      batch.address,
+      true
+    );
+  }
+};
+export default func;
+func.skip = skipUnlessL1;
+func.tags = ['DeployerBatchMint', 'DeployerBatchMint_deploy'];
---
+++ packages/core/deploy_polygon/999_helpers/01_deploy_batch_mint.ts
@@ -0,0 +1,30 @@
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {skipUnlessL2} from '../../utils/network';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy, execute, read} = deployments;
+  const {deployer, sandboxAccount} = await getNamedAccounts();
+  const batch = await deploy(`Batch-${sandboxAccount}`, {
+    contract: `Batch`,
+    from: deployer,
+    args: [sandboxAccount],
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+  const isMinter = await read('PolygonLand', 'isMinter', batch.address);
+  if (!isMinter) {
+    const admin = await read('PolygonLand', 'getAdmin');
+    await execute(
+      'PolygonLand',
+      {from: admin, log: true},
+      'setMinter',
+      batch.address,
+      true
+    );
+  }
+};
+export default func;
+func.skip = skipUnlessL2;
+func.tags = ['DeployerBatchMintPolygon', 'DeployerBatchMintPolygon_deploy'];
