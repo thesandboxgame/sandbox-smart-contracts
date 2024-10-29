d2b2ffcaa8149d780d1487ed1f80cb738863b24d
Ayush Tiwari <ayush@sandbox.game>
2024-10-29 11:17:48 +0530

    feat: land-sale package

---
+++ packages/land-sale/hardhat.config.ts
@@ -0,0 +1,30 @@
+import '@nomicfoundation/hardhat-toolbox';
+import {HardhatUserConfig} from 'hardhat/config';
+import 'hardhat-storage-layout';
+import 'hardhat-contract-sizer';
+import '@openzeppelin/hardhat-upgrades';
+
+const config: HardhatUserConfig = {
+  // solidity compiler version may be updated for new packages as required
+  // to ensure packages use up-to-date dependencies
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.5',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+    ],
+  },
+  mocha: {
+    ...(!process.env.CI ? {} : {invert: true, grep: '@skip-on-ci'}),
+  },
+  networks: {
+    hardhat: {allowUnlimitedContractSize: true},
+  },
+};
+export default config;
