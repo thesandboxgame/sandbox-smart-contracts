6248dbe6d17ac6bbab548bf7d71c84c675c332e7
Andres Adjimann <adjisb@sandbox.game>
2024-11-21 10:16:32 -0300

    feat: add a way to check ids

---
+++ packages/deploy/scripts/claimRevoke/check.ts
@@ -0,0 +1,60 @@
+/**
+ * How to use:
+ *  - yarn ts-node scripts/claimRevoke/check.ts
+ * This script always use: ./enabled_claims_denied_users.csv
+ * This script always use: ./event_data.csv
+ */
+import csv from "csv-parser";
+import * as fs from "fs";
+import * as path from "path";
+import { ethers, Result } from "ethers";
+
+const CHUNK_SIZE = 1000;
+type IDS = { [k: string]: { user_id: string; claim_id: string } };
+
+async function main(claimsFilename: string, eventsFilename: string) {
+  const ids: IDS = await new Promise((resolve) => {
+    const ret: IDS = {};
+    fs.createReadStream(claimsFilename)
+      .pipe(csv())
+      .on("data", (data) => {
+        ret[BigInt(data.claim_id).toString(16)] = data;
+      })
+      .on("end", () => {
+        resolve(ret);
+      });
+  });
+  const eventData: string[] = await new Promise((resolve) => {
+    const ret: string[] = [];
+    fs.createReadStream(eventsFilename)
+      .pipe(csv())
+      .on("data", (data) => {
+        ret.push(data.data);
+      })
+      .on("end", () => {
+        resolve(ret);
+      });
+  });
+
+  const coder = ethers.AbiCoder.defaultAbiCoder();
+  for (const d of eventData) {
+    const result: Result = coder.decode(["uint256[]"], d);
+    for (const r of result[0]) {
+      if (ids[r.toString(16)]) {
+        delete ids[r];
+      } else {
+        console.log(`this one was revoked somewhere else which is ok ${r}`);
+      }
+    }
+  }
+  console.log(`"user_id","claim_id"`);
+  for (const e in ids) {
+    const v = ids[e];
+    console.log(`"${v.user_id}","${v.claim_id}"`);
+  }
+}
+
+main(
+  path.join(__dirname, "enabled_claims_denied_users.csv"),
+  path.join(__dirname, "event_data.csv")
+).catch((err) => console.error(err));
