51a100b7203099dca8937697e7ecfea4c7a2a567
Andres Adjimann <adjisb@sandbox.game>
2025-02-20 12:52:44 -0300

    feat: add deploy steps to set the royalty for the land contract

---
+++ packages/deploy/deploy/700_land/land/742_setup_land_v4_royalty.ts
@@ -0,0 +1,44 @@
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DEPLOY_TAGS} from '../../../hardhat.config';
+
+const ROYALTY_PERCENTAGE = 500;
+const func: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+): Promise<void> {
+  const {deployments, getNamedAccounts} = hre;
+  const {execute, read, catchUnknownSigner} = deployments;
+  const {royaltyManagerAdmin, commonRoyaltyReceiver} = await getNamedAccounts();
+  const Land = await deployments.get('Land');
+  const info = await read('Land', 'royaltyInfo', 1, 10000);
+  if (info.receiver != commonRoyaltyReceiver) {
+    console.warn(
+      `The hardhat.config commonRoyaltyReceiver (${commonRoyaltyReceiver}) is different from the one configured in the royaltyManager (${info.receiver})`
+    );
+  }
+  if (!info.royaltyAmount.eq(ROYALTY_PERCENTAGE)) {
+    console.log(
+      `Changing land royalty from ${info.royaltyAmount} to ${ROYALTY_PERCENTAGE}`
+    );
+    await catchUnknownSigner(
+      execute(
+        'RoyaltyManager',
+        {from: royaltyManagerAdmin, log: true},
+        'setContractRoyalty',
+        Land.address,
+        ROYALTY_PERCENTAGE
+      )
+    );
+  }
+};
+export default func;
+func.tags = [
+  'Land',
+  'LandV4_setup',
+  'LandRoyaltyManager',
+  'LandRoyaltyManager_setup_royalty',
+  DEPLOY_TAGS.L1,
+  DEPLOY_TAGS.L1_PROD,
+  DEPLOY_TAGS.L1_TEST,
+];
+func.dependencies = ['LandRoyaltyManager_setup'];
