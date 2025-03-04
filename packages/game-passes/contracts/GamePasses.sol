4d76694761b89ea4489b0a6cf5bdd48a0002bdbc
wojciech-turek <contact@wojciechturek.com>
2025-03-04 10:32:24 +0100

    lint fix

--- packages/game-passes/contracts/GamePasses.sol
+++ packages/game-passes/contracts/GamePasses.sol
@@ -19,7 +19,7 @@ import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "hardhat/console.sol";
+
 
 contract SandboxPasses1155Upgradeable is
     Initializable,
