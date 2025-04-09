95bc3a5f3a7c1edecb93270e1d8f2a2b2f0c19cc
Andres Adjimann <adjisb@sandbox.game>
2025-04-09 08:26:18 -0300

    feat: declare royalty percentages in a global file

--- packages/deploy/deploy/700_land/polygonLand/732_setup_polygonland_v3_royalty.ts
+++ packages/deploy/deploy/700_land/polygonLand/732_setup_polygonland_v3_royalty.ts
@@ -1,8 +1,11 @@
 import {DeployFunction} from 'hardhat-deploy/types';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DEPLOY_TAGS} from '../../../hardhat.config';
+import {
+  ROYALTY_MANAGER_POLYGON_LAND_ROYALTY_BPS,
+  ROYALTY_MANAGER_TOTAL_BASIS_POINTS,
+} from '../../../royaltiesConfig';
 
-const ROYALTY_PERCENTAGE = 500;
 const func: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
 ): Promise<void> {
@@ -11,15 +14,20 @@ const func: DeployFunction = async function (
   const {royaltyManagerAdmin, commonRoyaltyReceiver} = await getNamedAccounts();
   const PolygonLand = await deployments.get('PolygonLand');
   // Here we assume a fixed royalty percentage that does not depend on the tokenId
-  const info = await read('PolygonLand', 'royaltyInfo', 1, 10000);
+  const info = await read(
+    'PolygonLand',
+    'royaltyInfo',
+    1,
+    ROYALTY_MANAGER_TOTAL_BASIS_POINTS
+  );
   if (info.receiver != commonRoyaltyReceiver) {
     console.warn(
       `The hardhat.config commonRoyaltyReceiver (${commonRoyaltyReceiver}) is different from the one configured in the royaltyManager (${info.receiver})`
     );
   }
-  if (!info.royaltyAmount.eq(ROYALTY_PERCENTAGE)) {
+  if (!info.royaltyAmount.eq(ROYALTY_MANAGER_POLYGON_LAND_ROYALTY_BPS)) {
     console.log(
-      `Changing land royalty from ${info.royaltyAmount} to ${ROYALTY_PERCENTAGE}`
+      `Changing land royalty from ${info.royaltyAmount} to ${ROYALTY_MANAGER_POLYGON_LAND_ROYALTY_BPS}`
     );
     await catchUnknownSigner(
       execute(
@@ -27,7 +35,7 @@ const func: DeployFunction = async function (
         {from: royaltyManagerAdmin, log: true},
         'setContractRoyalty',
         PolygonLand.address,
-        ROYALTY_PERCENTAGE
+        ROYALTY_MANAGER_POLYGON_LAND_ROYALTY_BPS
       )
     );
   }
