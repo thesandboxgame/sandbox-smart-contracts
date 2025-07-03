124921072bbe77000df1dbdf45f10d3b6d01f8db
Andres Adjimann <adjisb@sandbox.game>
2025-07-03 12:05:25 -0300

    chore: prices used for landsale 72 second attempt

--- packages/deploy/land-sale-artifacts/prices.ts
+++ packages/deploy/land-sale-artifacts/prices.ts
@@ -1,17 +1,28 @@
 import {BigNumber} from '@ethersproject/bignumber';
+import {formatEther} from 'ethers';
 
-function sandWei(v: number) {
-  return BigNumber.from(v).mul('1000000000000000000').toString();
+function sandWeiScale(v: number) {
+  return BigNumber.from(v)
+    .mul('1000000000000000000')
+    .div('2022')
+    .mul('10')
+    .toString();
 }
 
 // These are Mainnet prices based on 0.036367$ per SAND
 const prices: {[priceId: string]: string} = {
-  '1x1': sandWei(1011),
-  '3x3': sandWei(8648),
-  '6x6': sandWei(32772),
-  '12x12': sandWei(123806),
-  '24x24': sandWei(466102),
-  premium_1x1: sandWei(4683),
+  '1x1': sandWeiScale(1011),
+  '3x3': sandWeiScale(8648),
+  '6x6': sandWeiScale(32772),
+  '12x12': sandWeiScale(123806),
+  '24x24': sandWeiScale(466102),
+  premium_1x1: sandWeiScale(4683),
 };
-
+console.log(
+  '--------> Land prices',
+  Object.keys(prices).reduce(
+    (acc, val) => ({...acc, [val]: formatEther(prices[val])}),
+    {}
+  )
+);
 export default prices;
